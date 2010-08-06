bcb18204f68dae9bc3177956f22f79519a5fc41b
aph <none@none>
2010-08-06 14:30:53 +0100

    New file.

---
+++ compiler-java/src/com/redhat/ceylon/compiler/codegen/CeylonFileObject.java
@@ -0,0 +1,91 @@
+package com.redhat.ceylon.compiler.codegen;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.URI;
+
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.NestingKind;
+import javax.tools.FileObject;
+import javax.tools.ForwardingFileObject;
+import javax.tools.JavaFileObject;
+import javax.tools.JavaFileObject.Kind;
+
+class CeylonFileObject
+extends ForwardingFileObject<JavaFileObject>
+implements JavaFileObject
+{
+    private final JavaFileObject f;
+
+    public CeylonFileObject(JavaFileObject f)
+    {
+        super(f);
+        this.f = f;
+    }
+
+    /**
+     * Gets the kind of this file object.
+     *
+     * @return the kind
+     */
+     public Kind getKind() {
+         return f.getKind();
+     }
+
+     /**
+      * Checks if this file object is compatible with the specified
+      * simple name and kind.  A simple name is a single identifier
+      * (not qualified) as defined in the <a
+      * href="http://java.sun.com/docs/books/jls/">Java Language
+      * Specification</a> 3rd ed., section 6.2 "Names and Identifiers".
+      *
+      * @param simpleName a simple name of a class
+      * @param kind a kind
+      * @return {@code true} if this file object is compatible; false
+      * otherwise
+      */
+     public boolean isNameCompatible(String simpleName, Kind kind)
+     {     
+         if (kind == Kind.SOURCE)
+         {
+             String name = f.getName();
+             String n = simpleName + ".ceylon";
+             if (name.endsWith(n)) {
+                 return true;
+             }
+         }               
+
+         return f.isNameCompatible(simpleName, kind);
+     }
+
+     /**
+      * Provides a hint about the nesting level of the class
+      * represented by this file object.  This method may return
+      * {@link NestingKind#MEMBER} to mean
+      * {@link NestingKind#LOCAL} or {@link NestingKind#ANONYMOUS}.
+      * If the nesting level is not known or this file object does not
+      * represent a class file this method returns {@code null}.
+      *
+      * @return the nesting kind, or {@code null} if the nesting kind
+      * is not known
+      */
+     public NestingKind getNestingKind() {
+         return f.getNestingKind();
+     }
+
+     /**
+      * Provides a hint about the access level of the class represented
+      * by this file object.  If the access level is not known or if
+      * this file object does not represent a class file this method
+      * returns {@code null}.
+      *
+      * @return the access level
+      */
+     public Modifier getAccessLevel() {
+         return f.getAccessLevel();
+     }
+
+}
