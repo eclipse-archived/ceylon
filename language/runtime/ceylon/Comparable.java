f961d3be9ee9db62944850a041cc1cf14d8f55c2
Gary Benson <gbenson@redhat.com>
2010-08-24 10:41:57 +0100

    Add Comparable interface and Comparison class

---
+++ language/runtime/ceylon/Comparable.java
@@ -0,0 +1,7 @@
+package ceylon;
+
+public interface Comparable<T> {
+    /** The binary compare operator |<=>|.  Compares this
+        object with the given object. */
+    public Comparison compare(T other);
+}
