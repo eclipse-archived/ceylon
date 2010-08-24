cd8062bae749a0dfe13c9e80313c19791beb98c1
Gary Benson <gbenson@redhat.com>
2010-08-24 13:21:56 +0100

    Ah

--- language/runtime/ceylon/Numeric.java
+++ language/runtime/ceylon/Numeric.java
@@ -3,17 +3,17 @@ package ceylon;
 public interface Numeric<N> extends Number, Comparable<N> {
 
     /** The binary |+| operator. */
-    public Numeric plus(Numeric number); // XXX not as spec
+    public N plus(N number);
 
     /** The binary |-| operator. */
-    public Numeric minus(Numeric number); // XXX not as spec
+    public N minus(N number);
 
     /** The binary |*| operator. */
-    public Numeric times(Numeric number); // XXX not as spec
+    public N times(N number);
 
     /** The binary |/| operator. */
-    public Numeric divided(Numeric number); // XXX not as spec
+    public N divided(N number);
 
     /** The binary |**| operator. */
-    public Numeric power(Numeric number); // XXX not as spec
+    public N power(N number);
 }
