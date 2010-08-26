d444b65326f6d89dcb046b2678e3398aa476c49c
aph <none@none>
2010-08-26 16:58:46 +0100

    Move langtools.

--- compiler-java/src/langtools/test/tools/javac/jvm/6397652/com/test/Test$Test$Test.java
+++
@@ -1,8 +0,0 @@
-package com.test;
-
-public class Test$Test$Test {
-
-    public Test$Test$Test() {
-    }
-
-}
--- compiler-java/src/langtools/test/tools/javac/jvm/6397652/com/test/Test$Test.java
+++
@@ -1,8 +0,0 @@
-package com.test;
-
-public class Test$Test {
-
-    public Test$Test(double value) {
-    }
-
-}
