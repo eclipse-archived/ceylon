50253eeb2a4f4696b1170d6e5e5bb80b823cb06d
aph <none@none>
2010-10-15 14:44:02 +0100

    Call top-level methods.

--- compiler-java/langtools/src/share/classes/com/sun/tools/javac/comp/Resolve.java
+++ compiler-java/langtools/src/share/classes/com/sun/tools/javac/comp/Resolve.java
@@ -1197,6 +1197,14 @@ public class Resolve {
             if (sym.kind >= WRONG_MTHS)
                 sym = findFun(env, name, argtypes, typeargtypes, true, env.info.varArgs=true);
         }
+        // Look for a top-level method
+        if (Context.isCeylon() && sym.kind >= WRONG_MTHS) {
+        	Symbol site = findIdent(env, name, TYP | PCK);
+        	if (site.exists()) {
+        		sym = resolveQualifiedMethod(pos, env, site.type, names.fromString("run"),
+        				argtypes, typeargtypes);
+        	}
+        }
         if (sym.kind >= AMBIGUOUS) {
             sym = access(
                 sym, pos, env.enclClass.sym.type, name, false, argtypes, typeargtypes);
