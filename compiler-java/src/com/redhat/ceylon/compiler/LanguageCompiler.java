1c7bf0e58aeff042eacdb09160bd16b3be33f2df
aph <none@none>
2010-11-11 16:43:01 +0000

    Delete unnecessary file.

--- compiler-java/src/com/redhat/ceylon/compiler/LanguageCompiler.java
+++ compiler-java/src/com/redhat/ceylon/compiler/LanguageCompiler.java
@@ -1,18 +1,17 @@
 package com.redhat.ceylon.compiler;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
-
-import java.util.*;
+import java.util.Queue;
 
 import javax.tools.JavaFileObject;
 
-import org.antlr.runtime.*;
-import org.antlr.runtime.tree.*;
+import org.antlr.runtime.ANTLRInputStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.tree.CommonTree;
 
-import com.redhat.ceylon.compiler.parser.CeylonLexer;
 import com.redhat.ceylon.compiler.codegen.CeylonFileObject;
 import com.redhat.ceylon.compiler.codegen.Gen;
+import com.redhat.ceylon.compiler.parser.CeylonLexer;
 import com.redhat.ceylon.compiler.parser.CeylonParser;
 import com.redhat.ceylon.compiler.tree.CeylonTree;
 import com.redhat.ceylon.compiler.tree.Grok;
@@ -20,12 +19,13 @@ import com.sun.tools.javac.comp.AttrContext;
 import com.sun.tools.javac.comp.Env;
 import com.sun.tools.javac.main.JavaCompiler;
 import com.sun.tools.javac.tree.JCTree;
-import com.sun.tools.javac.tree.JCTree.JCCompilationUnit;
 import com.sun.tools.javac.tree.JCTree.JCClassDecl;
-
+import com.sun.tools.javac.tree.JCTree.JCCompilationUnit;
+import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.Pair;
+import com.sun.tools.javac.util.Position;
 import com.sun.tools.javac.util.Context.SourceLanguage.Language;
 import com.sun.tools.javac.util.Position.LineMap;
-import com.sun.tools.javac.util.*;
 
 public class LanguageCompiler extends JavaCompiler {
 
@@ -98,18 +98,16 @@ public class LanguageCompiler extends JavaCompiler {
 				cu.file = filename;
 		    	cu.accept(new Grok());
 
-				char[] chars = filename.getCharContent(true).toString().toCharArray();
+				char[] chars = readSource.toString().toCharArray();
 				LineMap map = Position.makeLineMap(chars, chars.length, false);
 				gen.setMap(map);
 
 				return gen.convert(cu);
-			}
-			
+			}			
 		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
-		
-		
+
 		return null;
 	}
 	
