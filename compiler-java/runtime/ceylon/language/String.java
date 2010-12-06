cbcb893c94ac6edc1652fb4c4713782c9130ac97
Gary Benson <gbenson@redhat.com>
2010-12-06 10:46:30 +0000

    New test

--- compiler-java/runtime/ceylon/language/String.java
+++ compiler-java/runtime/ceylon/language/String.java
@@ -15,4 +15,12 @@ public final class String extends Object
     public static ceylon.language.String instance(java.lang.String s) {
         return new ceylon.language.String(s);
     }
+
+    public ceylon.language.String uppercase() {
+        return instance(value.toUpperCase());
+    }
+
+    public ceylon.language.String lowercase() {
+        return instance(value.toLowerCase());
+    }
 }
