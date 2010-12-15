dc5097a6209793739d9bca5ceb9630c0e843b275
aph <none@none>
2010-12-15 18:58:31 +0000

    New method.

--- compiler-java/runtime/ceylon/language/Process.java
+++ compiler-java/runtime/ceylon/language/Process.java
@@ -5,4 +5,7 @@ public class Process
     public void writeLine(ceylon.language.String s) {
         java.lang.System.out.println(s.toJavaString());
     }
+    public void write(ceylon.language.String s) {
+        java.lang.System.out.print(s.toJavaString());
+    }
 }
