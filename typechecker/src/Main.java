bb1cf56ec906f69e0f5ab0bfa3c03aed7036c978
Gavin King <gavin@hibernate.org>
2011-01-24 23:46:55 -0600

    quick test harness for running grammar against corpus

---
+++ typechecker/src/Main.java
@@ -0,0 +1,65 @@
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.antlr.runtime.ANTLRInputStream;
+import org.antlr.runtime.CommonTokenStream;
+
+import com.redhat.ceylon.compiler.parser.CeylonLexer;
+import com.redhat.ceylon.compiler.parser.CeylonParser;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		fileOrDir( new File(args[0]) );
+	}
+	
+	private static void file(File file) throws Exception {
+		if ( file.getName().endsWith(".ceylon") ) {
+		
+		System.out.println("Parsing " + file.getName());
+        InputStream is = new FileInputStream( file );
+        ANTLRInputStream input = new ANTLRInputStream(is);
+        CeylonLexer lexer = new CeylonLexer(input);
+
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        CeylonParser parser = new CeylonParser(tokens);
+        CeylonParser.compilationUnit_return r = parser.compilationUnit();
+
+        //CommonTree t = (CommonTree)r.getTree();
+
+        if (lexer.getNumberOfSyntaxErrors() != 0) {
+            System.out.println("Lexer failed");
+        }
+        else if (parser.getNumberOfSyntaxErrors() != 0) {
+        	System.out.println("Parser failed");
+        }
+        else {
+        	System.out.println("Parser succeeded");
+        }
+        
+		}
+        
+
+	}
+
+	
+	
+	private static void fileOrDir(File file) throws Exception {
+		if (file.isDirectory()) 
+			dir(file);
+		else
+			file(file);
+	}
+
+
+
+	private static void dir(File dir) throws Exception {
+		for (File file: dir.listFiles()) 
+			fileOrDir(file);
+	}
+
+}
