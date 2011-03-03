081ee8922175a1c8224748198c55a71ab64d5df3
Emmanuel Bernard <emmanuel@hibernate.org>
2011-03-03 18:30:18 +0100

    Add test or what looks like test

---
+++ typechecker/src/MainForTest.java
@@ -0,0 +1,27 @@
+import com.redhat.ceylon.compiler.typechecker.TypeChecker;
+import com.redhat.ceylon.compiler.typechecker.TypeCheckerBuilder;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree;
+
+import java.io.File;
+
+/**
+ * Some hack before a proper unit test harness is put in place
+ *
+ * @author Emmanuel Bernard <emmanuel@hibernate.org>
+ */
+public class MainForTest {
+    /**
+     * Files that are not under a proper module structure are placed under a <nomodule> module.
+     */
+    public static void main(String[] args) throws Exception {
+
+        final TypeChecker typeChecker = new TypeCheckerBuilder()
+                .verbose(false)
+                .addSrcDirectory( new File("test") )
+                .getTypeChecker();
+        final Tree.CompilationUnit compilationUnit = typeChecker.getCompilationUnitFromRelativePath("ceylon/language/BaseObject.ceylon");
+        if ( compilationUnit == null ) {
+            throw new RuntimeException("Failed to pass getCompilationUnitFromRelativePath");
+        }
+    }
+}
