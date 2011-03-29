9d9ff71cabc342abe99d61e72e2a68211e85ee1e
Emmanuel Bernard <emmanuel@hibernate.org>
2011-03-29 11:55:21 +0200

    Make tests and build use the right language module version

--- typechecker/src/MainForTest.java
+++ typechecker/src/MainForTest.java
@@ -37,7 +37,7 @@ public class MainForTest {
         }
         typeChecker = new TypeCheckerBuilder()
                 .verbose(false)
-                .addSrcDirectory( new File("languagesrc") )
+                .addSrcDirectory( new File("languagesrc/final") )
                 .getTypeChecker();
     }
 }
