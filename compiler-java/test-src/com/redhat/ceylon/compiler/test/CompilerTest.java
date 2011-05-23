352f8c6973e393404ba99dd7fc855955d0003fb4
Stephane Epardaud <stef@epardaud.fr>
2011-05-23 23:46:59 +0200

    CompilerTest: made abstract and moved the tests out of it

--- compiler-java/test-src/com/redhat/ceylon/compiler/test/CompilerTest.java
+++ compiler-java/test-src/com/redhat/ceylon/compiler/test/CompilerTest.java
@@ -11,87 +11,91 @@ import java.util.Collections;
 import javax.tools.JavaCompiler;
 import javax.tools.JavaFileManager;
 import javax.tools.JavaFileObject;
-import javax.tools.StandardJavaFileManager;
 
 import junit.framework.Assert;
 
 import org.junit.Before;
-import org.junit.Test;
 
 import com.redhat.ceylon.compiler.tools.CeyloncFileManager;
 import com.redhat.ceylon.compiler.tools.CeyloncTool;
 import com.redhat.ceylon.compiler.tools.LanguageCompiler;
 import com.sun.tools.javac.tree.JCTree.JCCompilationUnit;
 import com.sun.tools.javac.util.Context;
-import com.sun.tools.javac.util.JavacFileManager;
 import com.sun.tools.javac.util.List;
 
-public class CompilerTest {
-	
+public abstract class CompilerTest {
+
 	private final static String dir = "test-src";
-	
-	private LanguageCompiler comp;
-	private JavacFileManager fileManager;
+
 	private String path;
+
+	// for comparing
+	private LanguageCompiler compareCompiler;
+	private CeyloncFileManager compareFileManager;
+
+	// for running
+	private JavaCompiler runCompiler;
+	private CeyloncFileManager runFileManager;
 
 	@Before
 	public void setup(){
-        Context context = new Context();
-        CeyloncFileManager.preRegister(context);
-        comp = (LanguageCompiler) LanguageCompiler.instance(context);
-        fileManager = (JavacFileManager) context.get(JavaFileManager.class);
-        String pkg = getClass().getPackage().getName().replaceAll("\\.", File.separator);
-        path = dir + File.separator + pkg + File.separator;
+		// for comparing with java source
+		Context context = new Context();
+		CeyloncFileManager.preRegister(context);
+		compareCompiler = (LanguageCompiler) LanguageCompiler.instance(context);
+		compareFileManager = (CeyloncFileManager) context.get(JavaFileManager.class);
+		compareFileManager.setSourcePath(dir);
+		String pkg = getClass().getPackage().getName().replaceAll("\\.", File.separator);
+		path = dir + File.separator + pkg + File.separator;
+		// for running
+		runCompiler = new CeyloncTool();
+		runFileManager = (CeyloncFileManager)runCompiler.getStandardFileManager(null, null, null);
+		runFileManager.setSourcePath(dir);
 	}
 
-	private void compareWithJavaSource(String name) throws IOException{
+	protected void compareWithJavaSource(String name) {
 		compareWithJavaSource(name+".ceylon", name+".src");
 	}
 
-	private void compareWithJavaSource(String ceylon, String java) throws IOException{
+	protected void compareWithJavaSource(String ceylon, String java) {
 		List<JavaFileObject> files = List.nil();
-        File file = new File(path+ceylon);
-        for (JavaFileObject fo : fileManager.getJavaFileObjectsFromFiles(Collections.singletonList(file)))
-            files = files.prepend(fo);
-        Assert.assertEquals(1, files.size());
-        JCCompilationUnit compilationUnit = comp.parse(files.get(0));
-        String src = readFile(new File(path+java));
-        Assert.assertEquals(src.trim(), compilationUnit.toString().trim());
+		File file = new File(path+ceylon);
+		for (JavaFileObject fo : compareFileManager.getJavaFileObjectsFromFiles(Collections.singletonList(file)))
+			files = files.prepend(fo);
+		Assert.assertEquals(1, files.size());
+		JCCompilationUnit compilationUnit = compareCompiler.parse(files.get(0));
+		String src = readFile(new File(path+java));
+		Assert.assertEquals(src.trim(), compilationUnit.toString().trim());
 	}
 
-	private String readFile(File file) throws IOException {
-		Reader reader = new FileReader(file);
-		StringBuilder strbuf = new StringBuilder();
+	private String readFile(File file) {
 		try{
-			char[] buf = new char[1024];
-			int read;
-			while((read = reader.read(buf)) > -1)
-				strbuf.append(buf, 0, read);
-		}finally{
-			reader.close();
+			Reader reader = new FileReader(file);
+			StringBuilder strbuf = new StringBuilder();
+			try{
+				char[] buf = new char[1024];
+				int read;
+				while((read = reader.read(buf)) > -1)
+					strbuf.append(buf, 0, read);
+			}finally{
+				reader.close();
+			}
+			return strbuf.toString();
+		}catch(IOException x){
+			throw new RuntimeException(x);
 		}
-		return strbuf.toString();
 	}
 
-	@Test
-	public void test() throws IOException{
-        compareWithJavaSource("helloworld/helloworld");
-	}
-
-	@Test
-	public void test2() throws Exception{
-		compileAndRun("helloworld/helloworld.ceylon", "helloworld");
-	}
-	
-	public void compileAndRun(String ceylon, String main) throws Exception{
-		JavaCompiler compiler = new CeyloncTool();
-		StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
-
+	protected void compileAndRun(String ceylon, String main) {
 		Iterable<? extends JavaFileObject> compilationUnits1 =
-			fileManager.getJavaFileObjectsFromFiles(Arrays.asList(new File(path+ceylon)));
-		compiler.getTask(null, fileManager, null, Arrays.asList("-d", "build/classes"), null, compilationUnits1).call();
-		Class<?> klass = Class.forName(main);
-		Method m = klass.getMethod("run", ceylon.language.Process.class);
-		m.invoke(null, new ceylon.language.Process());
+			compareFileManager.getJavaFileObjectsFromFiles(Arrays.asList(new File(path+ceylon)));
+		runCompiler.getTask(null, runFileManager, null, Arrays.asList("-d", "build/classes"), null, compilationUnits1).call();
+		try{
+			Class<?> klass = Class.forName(main);
+			Method m = klass.getMethod("run", ceylon.language.Process.class);
+			m.invoke(null, new ceylon.language.Process());
+		}catch(Exception x){
+			throw new RuntimeException(x);
+		}
 	}
 }
