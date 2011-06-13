988b53b0a09ef678fdc7462a6d6086fdfd429331
Stephane Epardaud <stef@epardaud.fr>
2011-06-13 21:49:39 +0200

    CeylonEnter: run the full typechecker now, using CeylonModelLoader

--- compiler-java/src/com/redhat/ceylon/compiler/codegen/CeylonEnter.java
+++ compiler-java/src/com/redhat/ceylon/compiler/codegen/CeylonEnter.java
@@ -1,13 +1,20 @@
 package com.redhat.ceylon.compiler.codegen;
 
 import com.redhat.ceylon.compiler.tools.LanguageCompiler;
+import com.redhat.ceylon.compiler.typechecker.analyzer.AnalysisError;
 import com.redhat.ceylon.compiler.typechecker.analyzer.ModuleValidator;
 import com.redhat.ceylon.compiler.typechecker.context.PhasedUnit;
 import com.redhat.ceylon.compiler.typechecker.context.PhasedUnits;
+import com.redhat.ceylon.compiler.typechecker.model.Declaration;
+import com.redhat.ceylon.compiler.typechecker.model.Module;
+import com.redhat.ceylon.compiler.typechecker.model.Package;
+import com.redhat.ceylon.compiler.typechecker.tree.Node;
+import com.redhat.ceylon.compiler.typechecker.util.AssertionVisitor;
 import com.sun.tools.javac.comp.Enter;
 import com.sun.tools.javac.tree.JCTree.JCCompilationUnit;
 import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.List;
+import com.sun.tools.javac.util.Log;
 
 public class CeylonEnter extends Enter {
 
@@ -22,7 +29,9 @@ public class CeylonEnter extends Enter {
     private boolean hasRun = false;
     private PhasedUnits phasedUnits;
     private com.redhat.ceylon.compiler.typechecker.context.Context ceylonContext;
-
+    private Log log;
+    private CeylonModelLoader modelLoader;
+    
     protected CeylonEnter(Context context) {
         super(context);
         System.err.println("CeylonEnter created");
@@ -34,21 +43,44 @@ public class CeylonEnter extends Enter {
         }
         phasedUnits = LanguageCompiler.getPhasedUnitsInstance(context);
         ceylonContext = LanguageCompiler.getCeylonContextInstance(context);
+        log = Log.instance(context);
+        modelLoader = new CeylonModelLoader(context);
     }
 
     @Override
     public void main(List<JCCompilationUnit> trees) {
         System.err.println("CeylonEnter main");
+        // complete the javac AST with a completed ceylon model
         completeCeylonTrees(trees);
         super.main(trees);
+        System.err.println("CeylonEnter done");
+    }
+
+    private void printModules() {
+        for(Module module : ceylonContext.getModules().getListOfModules()){
+            System.err.println("Found module: "+module.getNameAsString());
+            for(Package pkg : module.getPackages()){
+                System.err.println(" Found package: "+pkg.getNameAsString());
+                for(Declaration decl : pkg.getMembers()){
+                    System.err.println("  Found Decl: "+decl);
+                }
+            }
+        }
     }
 
     public void completeCeylonTrees(List<JCCompilationUnit> trees) {
         if (hasRun)
             throw new RuntimeException("Waaaaa, running twice!!!");
         hasRun = true;
-        // let's see how far we can get the type checker now
+        // load modules required by the typechecker
+        modelLoader.loadRequiredModules();
+        // run the type checker
         typeCheck();
+        // some debugging
+        printModules();
+        /*
+         * Here we convert the ceylon tree to its javac AST, after the typechecker has run
+         */
         for (JCCompilationUnit tree : trees) {
             if (tree instanceof CeylonCompilationUnit) {
                 CeylonCompilationUnit ceylonTree = (CeylonCompilationUnit) tree;
@@ -77,10 +109,38 @@ public class CeylonEnter extends Enter {
             pu.validateControlFlow();
             pu.validateSpecification();
         }
-        // FIXME: this fails at this phase
-        /*for (PhasedUnit pu : listOfUnits) { pu.scanTypeDeclarations(); } for
-         * (PhasedUnit pu: listOfUnits) { pu.validateRefinement(); } for
-         * (PhasedUnit pu : listOfUnits) { pu.analyseTypes(); } for (PhasedUnit
-         * pu : listOfUnits) { pu.display(); pu.runAssertions(); } */
+        for (PhasedUnit pu : listOfUnits) { 
+            pu.scanTypeDeclarations(); 
+        } 
+        for (PhasedUnit pu: listOfUnits) { 
+            pu.validateRefinement();
+        }
+        for (PhasedUnit pu : listOfUnits) { 
+            pu.analyseTypes(); 
+        }
+        for (PhasedUnit pu : listOfUnits) { 
+            pu.display(); 
+            pu.getCompilationUnit().visit(new AssertionVisitor(){
+                // FIXME: use regular javac log.error. We must define a localisation key for that, and 
+                // be able to access the javac AST we generated for a given CU because it holds the 
+                // source file object for javac 
+                @Override
+                protected void out(AnalysisError err) {
+                    log.rawError(0, err.getMessage() + "\n at " + 
+                            err.getTreeNode().getAntlrTreeNode().getLine() + ":" +
+                            err.getTreeNode().getAntlrTreeNode().getCharPositionInLine() + " of " +
+                            err.getTreeNode().getUnit().getFilename());
+                }
+                @Override
+                protected void out(Node that, String message) {
+                    log.rawError(0, message + "\n at " + 
+                            that.getAntlrTreeNode().getLine() + ":" +
+                            that.getAntlrTreeNode().getCharPositionInLine() + " of " +
+                            that.getUnit().getFilename());
+                }
+            });
+        }
+        /*
+        */
     }
 }
