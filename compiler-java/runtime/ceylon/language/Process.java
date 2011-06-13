f7151fef8a147107bc45a4280a5ef814ce4ebf7a
Stephane Epardaud <stef@epardaud.fr>
2011-06-13 21:49:18 +0200

    made ceylon.language.Process extend Object

--- compiler-java/runtime/ceylon/language/Process.java
+++ compiler-java/runtime/ceylon/language/Process.java
@@ -1,7 +1,6 @@
 package ceylon.language;
 
-public class Process
-{
+public class Process extends Object {
     public void writeLine(ceylon.language.String s) {
         java.lang.System.out.println(s.toJavaString());
     }
