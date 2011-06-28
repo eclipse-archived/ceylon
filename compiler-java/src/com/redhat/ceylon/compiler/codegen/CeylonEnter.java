55b6e5c41d97e97dfd4a97357166a2952a1e9980
Stephane Epardaud <stephane@epardaud.fr>
2011-06-28 10:19:43 +0200

    CeylonEnter: synced typechecker process with upstream

--- compiler-java/src/com/redhat/ceylon/compiler/codegen/CeylonEnter.java
+++ compiler-java/src/com/redhat/ceylon/compiler/codegen/CeylonEnter.java
@@ -103,8 +103,6 @@ public class CeylonEnter extends Enter {
         for (PhasedUnit pu : listOfUnits) {
             pu.validateTree();
             pu.scanDeclarations();
-            pu.validateControlFlow();
-            pu.validateSpecification();
         }
         for (PhasedUnit pu : listOfUnits) { 
             pu.scanTypeDeclarations(); 
@@ -114,6 +112,11 @@ public class CeylonEnter extends Enter {
         }
         for (PhasedUnit pu : listOfUnits) { 
             pu.analyseTypes(); 
+        }
+        for (PhasedUnit pu: listOfUnits) {
+            pu.validateControlFlow();
+            pu.validateSelfReferences();
+            pu.validateSpecification();
         }
         for (PhasedUnit pu : listOfUnits) { 
             pu.display(); 
