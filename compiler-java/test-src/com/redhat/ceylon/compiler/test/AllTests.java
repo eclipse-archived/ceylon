4dfaf9ff1a46426f25e182d875e141f0cb4c3115
Stephane Epardaud <stephane@epardaud.fr>
2011-07-04 23:38:20 +0200

    Added TypeParserTest and ModelLoaderTest to AllTests

--- compiler-java/test-src/com/redhat/ceylon/compiler/test/AllTests.java
+++ compiler-java/test-src/com/redhat/ceylon/compiler/test/AllTests.java
@@ -6,6 +6,8 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import com.redhat.ceylon.compiler.test.expression.ExpressionTest;
 import com.redhat.ceylon.compiler.test.misc.MiscTest;
+import com.redhat.ceylon.compiler.test.model.ModelLoaderTest;
+import com.redhat.ceylon.compiler.test.model.TypeParserTest;
 import com.redhat.ceylon.compiler.test.statement.StatementTest;
 import com.redhat.ceylon.compiler.test.structure.StructureTest;
 
@@ -14,7 +16,9 @@ import com.redhat.ceylon.compiler.test.structure.StructureTest;
 	MiscTest.class,
 	StructureTest.class,
 	StatementTest.class,
-	ExpressionTest.class
+	ExpressionTest.class,
+	TypeParserTest.class,
+	ModelLoaderTest.class
 })
 public class AllTests {
 }
