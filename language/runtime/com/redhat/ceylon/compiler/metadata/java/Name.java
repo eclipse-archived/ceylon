8fa549ff95a6fe60a7d70a3afb9fab67daeff706
Stephane Epardaud <stef@epardaud.fr>
2011-07-08 23:46:44 +0200

    @Name, @TypeInfo: remove location restrictions

--- language/runtime/com/redhat/ceylon/compiler/metadata/java/Name.java
+++ language/runtime/com/redhat/ceylon/compiler/metadata/java/Name.java
@@ -1,7 +1,5 @@
 package com.redhat.ceylon.compiler.metadata.java;
 
-// stef: disabled because the compiler doesn't let us use it otherwise
-//@Target(ElementType.PARAMETER)
 public @interface Name {
     String value() default "";
 }
