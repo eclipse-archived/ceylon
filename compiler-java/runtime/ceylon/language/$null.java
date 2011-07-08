131627aa7ca5a2fda72f51ebdf8ac497394f4370
Stephane Epardaud <stef@epardaud.fr>
2011-07-08 23:46:00 +0200

    $null: fixed implementation of nothing toplevel

--- compiler-java/runtime/ceylon/language/$null.java
+++ compiler-java/runtime/ceylon/language/$null.java
@@ -4,8 +4,17 @@ import com.redhat.ceylon.compiler.metadata.java.Attribute;
 import com.redhat.ceylon.compiler.metadata.java.Ceylon;
 
 @Ceylon @Attribute
+class nothing {
+    private final static Nothing value = new Nothing(){};
+    
+    static Nothing getNothing(){
+        return value;
+    }
+}
+
+@Ceylon @Attribute
 public class $null {
-    private final static Nothing value = Nothing.NULL;
+    private final static Nothing value = nothing.getNothing();
     
     public static Nothing getNull(){
         return value;
