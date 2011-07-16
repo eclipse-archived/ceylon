40f9b791afe4fc9ce615ec109b563c2bae3c314d
Sergej Koščejev <sergey.p.k@gmail.com>
2011-07-16 19:11:50 +0200

    Refactored argument processing of batch files into args.bat

--- compiler-java/bin/ceylon.bat
+++ compiler-java/bin/ceylon.bat
@@ -1,42 +1,15 @@
 @echo off
 setlocal
 
-rem Find CEYLON_HOME
-if "%CEYLON_HOME%" == "" set CEYLON_HOME=%~dp0..
+call %~dp0\args.bat %*
 
-rem Find Java
-if "%JAVA_HOME%" == "" (
-	set JAVA=java
-) else (
-	set JAVA="%JAVA_HOME%\bin\java.exe"
-)
-
-rem Process -cp and other args
-set USER_CP=
-set ARGS=
-
-:loop_args
-if "%~1" == "" goto :done_args
-
-if "%~1" == "-cp" (
-	set USER_CP=%~2
-	shift
-) else if "%~1" == "-classpath" (
-	set USER_CP=%~2
-	shift
-) else (
-	set ARGS=%ARGS% %1
-)
-
-shift
-goto loop_args
-
-:done_args
+rem Classes required by the launcher and by the code being launched
+set JAVA_CP=%CEYLON_HOME%\build\lib\runtime.jar
+set JAVA_CP=%JAVA_CP%;%USER_CP%
 
 %JAVA% ^
 	-enableassertions ^
-	-classpath ^
-	"%CEYLON_HOME%\build\lib\runtime.jar;%CEYLON_HOME%\build\classes;%USER_CP%" ^
+	-classpath "%JAVA_CP%" ^
 	ceylon.language.Launcher ^
 	%ARGS%
 
