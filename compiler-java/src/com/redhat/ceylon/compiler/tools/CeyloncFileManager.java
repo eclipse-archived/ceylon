a801f5088bf85abab114a11d186b0dda5f89d4ad
Stephane Epardaud <stephane@epardaud.fr>
2011-07-26 00:03:47 +0200

    CeyloncFileManager: preliminary support for generating jars

--- compiler-java/src/com/redhat/ceylon/compiler/tools/CeyloncFileManager.java
+++ compiler-java/src/com/redhat/ceylon/compiler/tools/CeyloncFileManager.java
@@ -26,16 +26,29 @@
 package com.redhat.ceylon.compiler.tools;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilterOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.URI;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.jar.JarFile;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
 
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.NestingKind;
 import javax.tools.FileObject;
 import javax.tools.JavaFileManager;
 import javax.tools.JavaFileObject;
 import javax.tools.StandardJavaFileManager;
+import javax.tools.StandardLocation;
 
 import com.sun.tools.javac.util.Context;
 import com.sun.tools.javac.util.JavacFileManager;
@@ -44,6 +57,8 @@ import com.redhat.ceylon.compiler.codegen.CeylonFileObject;
 
 public class CeyloncFileManager extends JavacFileManager implements StandardJavaFileManager {
     protected String[] sourcePath = new String[0];
+    private JarOutputStream jarFile;
+    private String currentPackage;
 
     public void setSourcePath(String sourcePath) {
         StringTokenizer st = new StringTokenizer(sourcePath, File.pathSeparator);
@@ -119,10 +134,125 @@ public class CeyloncFileManager extends JavacFileManager implements StandardJava
         return super.inferBinaryName(location, file);
     }
 
-    protected JavaFileObject getFileForOutput(Location location, String fileName, FileObject sibling) throws IOException {
+    protected JavaFileObject getFileForOutput(Location location, final String fileName, FileObject sibling) throws IOException {
         if (sibling instanceof CeylonFileObject) {
             sibling = ((CeylonFileObject) sibling).getFile();
         }
-        return super.getFileForOutput(location, fileName, sibling);
+        if(location == StandardLocation.CLASS_OUTPUT){
+            openJar();
+            return new JavaFileObject(){
+
+                @Override
+                public URI toUri() {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public String getName() {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public InputStream openInputStream() throws IOException {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public OutputStream openOutputStream() throws IOException {
+                    jarFile.putNextEntry(new ZipEntry(fileName));
+                    return new FilterOutputStream(jarFile){
+                        @Override
+                        public void close() throws IOException {
+                            jarFile.closeEntry();
+                        }
+                    };
+                }
+
+                @Override
+                public Reader openReader(boolean ignoreEncodingErrors)
+                        throws IOException {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public CharSequence getCharContent(boolean ignoreEncodingErrors)
+                        throws IOException {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public Writer openWriter() throws IOException {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public long getLastModified() {
+                    // TODO Auto-generated method stub
+                    return 0;
+                }
+
+                @Override
+                public boolean delete() {
+                    // TODO Auto-generated method stub
+                    return false;
+                }
+
+                @Override
+                public Kind getKind() {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public boolean isNameCompatible(String simpleName, Kind kind) {
+                    // TODO Auto-generated method stub
+                    return false;
+                }
+
+                @Override
+                public NestingKind getNestingKind() {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public Modifier getAccessLevel() {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+            };
+        }else
+            return super.getFileForOutput(location, fileName, sibling);
+    }
+
+    @Override
+    public void flush() {
+        super.flush();
+        if(jarFile != null){
+            try {
+                jarFile.flush();
+                jarFile.close();
+                jarFile = null;
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+    
+    private void openJar() throws IOException {
+        if(jarFile == null){
+            jarFile = new JarOutputStream(new FileOutputStream(currentPackage+".jar"));
+        }
+    }
+
+    public void setPackage(String string) {
+        currentPackage = string;
     }
 }
