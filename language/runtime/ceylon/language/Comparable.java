e9d9de35ad7d185972654f7f83d5e184bf930df4
Tako Schotanus <tako@codejive.org>
2011-08-02 18:42:34 +0200

    Added TypeParam annotations to some of the runtime classes.

--- language/runtime/ceylon/language/Comparable.java
+++ language/runtime/ceylon/language/Comparable.java
@@ -1,5 +1,14 @@
 package ceylon.language;
 
+import com.redhat.ceylon.compiler.metadata.java.Ceylon;
+import com.redhat.ceylon.compiler.metadata.java.TypeParameter;
+import com.redhat.ceylon.compiler.metadata.java.TypeParameters;
+import com.redhat.ceylon.compiler.metadata.java.Variance;
+
+@Ceylon
+@TypeParameters({
+    @TypeParameter(value = "T", variance = Variance.IN)
+ })
 public interface Comparable<T> {
     /** The binary compare operator |<=>|.  Compares this
         object with the given object. */
--- language/runtime/ceylon/language/Iterable.java
+++ language/runtime/ceylon/language/Iterable.java
@@ -1,5 +1,14 @@
 package ceylon.language;
 
-public interface Iterable<Element> {
+import com.redhat.ceylon.compiler.metadata.java.Ceylon;
+import com.redhat.ceylon.compiler.metadata.java.TypeParameter;
+import com.redhat.ceylon.compiler.metadata.java.TypeParameters;
+import com.redhat.ceylon.compiler.metadata.java.Variance;
+
+@Ceylon
+@TypeParameters({
+    @TypeParameter(value = "Element", variance = Variance.OUT)
+ })
+ public interface Iterable<Element> {
     Iterator<Element> iterator();
 }
