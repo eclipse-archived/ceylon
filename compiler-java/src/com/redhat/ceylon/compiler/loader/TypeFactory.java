e74fcb9a7608dd58a79ae54877b1f38f689c8f81
Tom Bentley <tom.bentley@cantab.net>
2011-08-06 20:05:50 +0100

    Some javadoc for TypeFactory

--- compiler-java/src/com/redhat/ceylon/compiler/loader/TypeFactory.java
+++ compiler-java/src/com/redhat/ceylon/compiler/loader/TypeFactory.java
@@ -61,38 +61,74 @@ public class TypeFactory {
         return null;
     }
     
+    /**
+     * Gets the declaration of {@code Nothing}
+     * @return The declaration
+     */
     public Class getNothingDeclaration() {
         return (Class) ((TypeDeclaration) getLanguageModuleDeclaration("Nothing"));
     }
 
+    /**
+     * Gets the declaration of {@code Empty}
+     * @return The declaration
+     */
     public Interface getEmptyDeclaration() {
         return (Interface) ((TypeDeclaration) getLanguageModuleDeclaration("Empty"));
     }
 
+    /**
+     * Gets the declaration of {@code Sequence}
+     * @return The declaration
+     */
     public Interface getSequenceDeclaration() {
         return (Interface) ((TypeDeclaration) getLanguageModuleDeclaration("Sequence"));
     }
 
+    /**
+     * Gets the declaration of {@code ArraySequence}
+     * @return The declaration
+     */
     public Class getDefaultSequenceDeclaration() {
         return (Class) ((TypeDeclaration) getLanguageModuleDeclaration("ArraySequence"));
     }
 
+    /**
+     * Gets the declaration of {@code Iterable}
+     * @return The declaration
+     */
     public Interface getIterableDeclaration() {
         return (Interface) ((TypeDeclaration) getLanguageModuleDeclaration("Iterable"));
     }
     
+    /**
+     * Gets the declaration of {@code Iterator}
+     * @return The declaration
+     */
     public Interface getIteratorDeclaration() {
         return (Interface) ((TypeDeclaration) getLanguageModuleDeclaration("Iterator"));
     }
     
+    /**
+     * Gets the declaration of {@code Castable}
+     * @return The declaration
+     */
     public Interface getCastableDeclaration() {
         return (Interface) ((TypeDeclaration) getLanguageModuleDeclaration("Castable"));
     }
     
+    /**
+     * Gets the declaration of {@code Entry}
+     * @return The declaration
+     */
     public Class getEntryDeclaration() {
         return (Class) ((TypeDeclaration) getLanguageModuleDeclaration("Entry"));
     }
     
+    /**
+     * Gets the declaration of {@code Range}
+     * @return The declaration
+     */
     public Class getRangeDeclaration() {
         return (Class) ((TypeDeclaration) getLanguageModuleDeclaration("Range"));
     }
@@ -122,6 +158,11 @@ public class TypeFactory {
         return typeArguments;
     }
     
+    /**
+     * Returns a ProducedType corresponding to the union of the given types
+     * @param types The types to union together
+     * @return The type
+     */
     public ProducedType unionType(ProducedType... types) {
         List<ProducedType> list = new ArrayList<ProducedType>();
         for (ProducedType t : types) {
@@ -132,11 +173,21 @@ public class TypeFactory {
         return ut.getType();
     }
     
+    /**
+     * Determines whether the given ProducedType is a union
+     * @param pt 
+     * @return whether the type is a union type
+     */
     public boolean isUnion(ProducedType pt) {
         TypeDeclaration tdecl = pt.getDeclaration();
         return (tdecl instanceof UnionType && tdecl.getCaseTypes().size() > 1);
     }
     
+    /**
+     * Determines whether the given ProducedType is {@code Empty|T}
+     * @param pt The ProducedType corresponding to {@code T}
+     * @return whether the type is a union type with empty
+     */
     public boolean isUnionWithEmpty(ProducedType pt) {
         TypeDeclaration tdecl = pt.getDeclaration();
         if (tdecl instanceof UnionType && tdecl.getCaseTypes().size() == 2) {
@@ -145,6 +196,11 @@ public class TypeFactory {
         return false;
     }
     
+    /**
+     * Returns a ProducedType corresponding to {@code Empty|T}
+     * @param pt The ProducedType corresponding to {@code T}
+     * @return The ProducedType corresponding to {@code Empty|T}
+     */
     public ProducedType makeEmptyType(ProducedType pt) {
         if (pt==null) {
             return null;
@@ -154,6 +210,11 @@ public class TypeFactory {
         }
     }
     
+    /**
+     * Returns a ProducedType corresponding to {@code Nothing|T}
+     * @param pt The ProducedType corresponding to {@code T}
+     * @return The ProducedType corresponding to {@code Nothing|T}
+     */
     public ProducedType makeOptionalType(ProducedType pt) {
         if (pt==null) {
             return null;
@@ -163,14 +224,29 @@ public class TypeFactory {
         }
     }
     
+    /**
+     * Returns a ProducedType corresponding to {@code Sequence<T>}
+     * @param et The ProducedType corresponding to {@code T}
+     * @return The ProducedType corresponding to {@code Sequence<T>}
+     */
     public ProducedType makeSequenceType(ProducedType et) {
         return producedType(getSequenceDeclaration(), et);
     }
     
+    /**
+     * Returns a ProducedType corresponding to {@code ArraySequence<T>}
+     * @param et The ProducedType corresponding to {@code T}
+     * @return The ProducedType corresponding to {@code ArraySequence<T>}
+     */
     public ProducedType makeDefaultSequenceType(ProducedType et) {
         return producedType(getDefaultSequenceDeclaration(), et);
     }
     
+    /**
+     * Returns a ProducedType corresponding to {@code Iterable<T>}
+     * @param et The ProducedType corresponding to {@code T}
+     * @return The ProducedType corresponding to {@code Iterable<T>}
+     */
     public ProducedType makeIterableType(ProducedType et) {
         return producedType(getIterableDeclaration(), et);
     }
@@ -184,26 +260,57 @@ public class TypeFactory {
         }
     }
 
+    /**
+     * Returns a ProducedType corresponding to {@code Iterator<T>}
+     * @param et The ProducedType corresponding to {@code T}
+     * @return The ProducedType corresponding to {@code Iterator<T>}
+     */
     public ProducedType makeIteratorType(ProducedType et) {
         return producedType(getIteratorDeclaration(), et);
     }
 
+    /**
+     * Returns a ProducedType corresponding to {@code Castable<T>}
+     * @param et The ProducedType corresponding to {@code T}
+     * @return The ProducedType corresponding to {@code Castable<T>}
+     */
     public ProducedType makeCastableType(ProducedType et) {
         return producedType(getCastableDeclaration(), et);
     }
 
+    /**
+     * Returns a ProducedType corresponding to {@code Entry<K,V>}
+     * @param kt The ProducedType corresponding to {@code K}
+     * @param vt The ProducedType corresponding to {@code V}
+     * @return The ProducedType corresponding to {@code Entry<K,V>}
+     */
     public ProducedType makeEntryType(ProducedType kt, ProducedType vt) {
         return producedType(getEntryDeclaration(), kt, vt);
     }
 
+    /**
+     * Returns a ProducedType corresponding to {@code Range<T>}
+     * @param rt The ProducedType corresponding to {@code T}
+     * @return The ProducedType corresponding to {@code Range<T>}
+     */
     public ProducedType makeRangeType(ProducedType rt) {
         return producedType(getRangeDeclaration(), rt);
     }
 
+    /**
+     * Removes Nothing from a type such as Nothing|T
+     * @param pt The ProducedType corresponding to {@code Nothing|T}
+     * @return The ProducedType corresponding to {@code T}
+     */
     public ProducedType getDefiniteType(ProducedType pt) {
         return pt.minus(getNothingDeclaration());
     }
 
+    /**
+     * Removes Nothing and Empty from a type such as Nothing|Empty|T
+     * @param pt The ProducedType corresponding to {@code Nothing|Empty|T}
+     * @return The ProducedType corresponding to {@code T}
+     */
     public ProducedType getNonemptyType(ProducedType pt) {
         return pt.minus(getNothingDeclaration()).minus(getEmptyDeclaration());
     }
