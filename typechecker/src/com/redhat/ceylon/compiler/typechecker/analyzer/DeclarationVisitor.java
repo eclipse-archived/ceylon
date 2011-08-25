1f9cd865ee375246d3546da512567e1e15635ac9
Gavin King <gavin@hibernate.org>
2011-08-25 00:58:57 -0500

    dont need *three* errors in this case!

--- typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/DeclarationVisitor.java
+++ typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/DeclarationVisitor.java
@@ -326,9 +326,9 @@ public class DeclarationVisitor extends Visitor {
 
     @Override
     public void visit(Tree.ObjectDefinition that) {
-        if (that.getClassBody()==null) {
+        /*if (that.getClassBody()==null) {
             that.addError("missing object body");
-        }
+        }*/
         Class c = new Class();
         visitDeclaration(that, c);
         Value v = new Value();
@@ -343,9 +343,9 @@ public class DeclarationVisitor extends Visitor {
 
     @Override
     public void visit(Tree.ObjectArgument that) {
-        if (that.getClassBody()==null) {
+        /*if (that.getClassBody()==null) {
             that.addError("missing named argument body");
-        }
+        }*/
         Class c = new Class();
         visitArgument(that, c);
         Value v = new Value();
