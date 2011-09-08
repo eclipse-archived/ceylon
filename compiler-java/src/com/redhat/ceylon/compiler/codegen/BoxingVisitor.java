3b89bbe34e3be35e8be49b8c929dd8596793c1bf
Stephane Epardaud <stef@epardaud.fr>
2011-09-08 23:00:05 +0200

    BoxingVisitor: refactored for cleanliness

--- compiler-java/src/com/redhat/ceylon/compiler/codegen/BoxingVisitor.java
+++ compiler-java/src/com/redhat/ceylon/compiler/codegen/BoxingVisitor.java
@@ -1,14 +1,18 @@
 package com.redhat.ceylon.compiler.codegen;
 
+import com.redhat.ceylon.compiler.typechecker.model.Declaration;
+import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.AnyAttribute;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.CharLiteral;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.Expression;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.FloatLiteral;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.InvocationExpression;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.NegativeOp;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.NotOp;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.PositiveOp;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.QualifiedMemberExpression;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.StringLiteral;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.Term;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.BaseMemberExpression;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.NaturalLiteral;
@@ -26,72 +30,81 @@ public class BoxingVisitor extends Visitor {
     public void visit(BaseMemberExpression that) {
         super.visit(that);
         if(Util.isUnBoxed(that.getDeclaration()) || transformer.isCeylonBoolean(that.getTypeModel()))
-            that.setAttribute(BoxingDeclarationVisitor.IS_UNBOXED, true);
+            markUnboxed(that);
     }
 
     @Override
     public void visit(QualifiedMemberExpression that) {
         super.visit(that);
-        if(Util.isUnBoxed(that.getDeclaration()))
-            that.setAttribute(BoxingDeclarationVisitor.IS_UNBOXED, true);
+        propagateFromDeclaration(that, that.getDeclaration());
     }
 
     @Override
     public void visit(AnyAttribute that) {
         super.visit(that);
-        if(Util.isUnBoxed(that.getDeclarationModel()))
-            that.setAttribute(BoxingDeclarationVisitor.IS_UNBOXED, true);
+        propagateFromDeclaration(that, that.getDeclarationModel());
     }
 
     @Override
     public void visit(Expression that) {
         super.visit(that);
-        if(Util.isUnBoxed(that.getTerm()))
-            that.setAttribute(BoxingDeclarationVisitor.IS_UNBOXED, true);
+        propagateFromTerm(that, that.getTerm());
     }
 
     @Override
     public void visit(InvocationExpression that) {
         super.visit(that);
-        if(Util.isUnBoxed(that.getPrimary()))
-            that.setAttribute(BoxingDeclarationVisitor.IS_UNBOXED, true);
+        propagateFromTerm(that, that.getPrimary());
     }
 
     @Override
     public void visit(NaturalLiteral that) {
         super.visit(that);
-        that.setAttribute(BoxingDeclarationVisitor.IS_UNBOXED, true);
+        markUnboxed(that);
     }
 
     @Override
     public void visit(FloatLiteral that) {
         super.visit(that);
-        that.setAttribute(BoxingDeclarationVisitor.IS_UNBOXED, true);
+        markUnboxed(that);
     }
 
     @Override
     public void visit(StringLiteral that) {
         super.visit(that);
-        that.setAttribute(BoxingDeclarationVisitor.IS_UNBOXED, true);
+        markUnboxed(that);
     }
 
     @Override
     public void visit(CharLiteral that) {
         super.visit(that);
-        that.setAttribute(BoxingDeclarationVisitor.IS_UNBOXED, true);
+        markUnboxed(that);
     }
 
     @Override
     public void visit(PositiveOp that) {
         super.visit(that);
-        if(Util.isUnBoxed(that.getTerm()))
-            that.setAttribute(BoxingDeclarationVisitor.IS_UNBOXED, true);
+        propagateFromTerm(that, that.getTerm());
     }
 
     @Override
     public void visit(NegativeOp that) {
         super.visit(that);
-        if(Util.isUnBoxed(that.getTerm()))
-            that.setAttribute(BoxingDeclarationVisitor.IS_UNBOXED, true);
+        propagateFromTerm(that, that.getTerm());
     }
+
+    private void propagateFromDeclaration(Node that, Declaration term) {
+        if(Util.isUnBoxed(term))
+            markUnboxed(that);
+    }
+
+    private void propagateFromTerm(Node that, Term term) {
+        if(Util.isUnBoxed(term))
+            markUnboxed(that);
+    }
+
+    private void markUnboxed(Node that) {
+        that.setAttribute(BoxingDeclarationVisitor.IS_UNBOXED, true);
+    }
+
 }
