7b8dea0dd3fa6c6060ff727e651d6867bb22346f
Stephane Epardaud <stef@epardaud.fr>
2011-09-18 22:14:55 +0200

    JarOutputRepositoryManager: make new jars by adding to old jars and not removing non-overwritten class files
    Just like a file system

--- compiler-java/src/com/redhat/ceylon/compiler/tools/JarOutputRepositoryManager.java
+++ compiler-java/src/com/redhat/ceylon/compiler/tools/JarOutputRepositoryManager.java
@@ -3,8 +3,14 @@ package com.redhat.ceylon.compiler.tools;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.jar.JarOutputStream;
 
 import javax.tools.JavaFileObject;
@@ -13,34 +19,33 @@ import com.redhat.ceylon.compiler.typechecker.model.Module;
 
 public class JarOutputRepositoryManager {
     
-    private Map<Module,JarOutputStream> openJars = new HashMap<Module, JarOutputStream>();
+    private Map<Module,ProgressiveJar> openJars = new HashMap<Module, ProgressiveJar>();
     
     public JavaFileObject getFileObject(File outputDir, Module module, String fileName) throws IOException{
-        JarOutputStream jarFile = getJarOutputStream(outputDir, module);
-        return new JarEntryFileObject(jarFile, fileName);
+        ProgressiveJar progressiveJar = getProgressiveJar(outputDir, module);
+        return progressiveJar.getJavaFileObject(fileName);
     }
     
-    private JarOutputStream getJarOutputStream(File outputDir, Module module) throws IOException {
-        JarOutputStream jarFile = openJars.get(module);
+    private ProgressiveJar getProgressiveJar(File outputDir, Module module) throws IOException {
+        ProgressiveJar jarFile = openJars.get(module);
         if(jarFile == null){
             String jarName = getJarName(module);
             System.out.println("Jar name: "+jarName);
-            jarFile = new JarOutputStream(new FileOutputStream(new File(outputDir, jarName)));
+            jarFile = new ProgressiveJar(new File(outputDir, jarName));
             openJars.put(module, jarFile);
         }
         return jarFile;
     }
 
     public void flush(){
-        for(JarOutputStream jarFile : openJars.values()){
+        for(ProgressiveJar jarFile : openJars.values()){
             flush(jarFile);
         }
         openJars.clear();
     }
     
-    private void flush(JarOutputStream jarFile) {
+    private void flush(ProgressiveJar jarFile) {
         try {
-            jarFile.flush();
             jarFile.close();
         } catch (IOException e) {
             // TODO Auto-generated catch block
@@ -59,4 +64,60 @@ public class JarOutputRepositoryManager {
         return moduleName+"-"+version+".jar";
     }
 
+    static class ProgressiveJar {
+        final private File originalJarFile;
+        final private File outputJarFile;
+        final private JarOutputStream jarOutputStream;
+        final private Set<String> writtenClasses = new HashSet<String>();
+        
+        public ProgressiveJar(File targetJarFile) throws IOException{
+            if(targetJarFile.exists()){
+                outputJarFile = File.createTempFile(targetJarFile.getName(), ".tmp", targetJarFile.getParentFile());
+                originalJarFile = targetJarFile;
+            }else{
+                outputJarFile = targetJarFile; 
+                originalJarFile = null;
+            }
+            jarOutputStream = new JarOutputStream(new FileOutputStream(outputJarFile));
+        }
+
+        public void close() throws IOException {
+            // now copy all previous jar entries
+            if(originalJarFile != null){
+                JarFile jarFile = new JarFile(originalJarFile);
+                Enumeration<JarEntry> entries = jarFile.entries();
+                while(entries.hasMoreElements()){
+                    JarEntry entry = entries.nextElement();
+                    // skip the old entry if we overwrote it
+                    if(writtenClasses.contains(entry.getName()))
+                        continue;
+                    jarOutputStream.putNextEntry(entry);
+                    InputStream inputStream = jarFile.getInputStream(entry);
+                    copy(inputStream, jarOutputStream);
+                    inputStream.close();
+                    jarOutputStream.closeEntry();
+                }
+            }
+            jarOutputStream.flush();
+            jarOutputStream.close();
+            if(originalJarFile != null){
+                // now rename to the original name
+                outputJarFile.renameTo(originalJarFile);
+            }
+        }
+
+        private void copy(InputStream inputStream, JarOutputStream outputStream) throws IOException {
+            byte[] buffer = new byte[4096];
+            int read;
+            while((read = inputStream.read(buffer)) != -1){
+                outputStream.write(buffer, 0, read);
+            }
+        }
+
+        public JavaFileObject getJavaFileObject(String fileName) {
+            // record the class file we produce so that we don't save it from the original jar
+            writtenClasses.add(fileName);
+            return new JarEntryFileObject(jarOutputStream, fileName);
+        }
+    }
 }
