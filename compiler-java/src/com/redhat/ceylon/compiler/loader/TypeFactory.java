e233e7db1a58034349c1419f9cf27ffb96a648e0
Stephane Epardaud <stef@epardaud.fr>
2011-09-27 23:31:48 +0200

    Made TypeFactory a subclass of Unit (to inherit all getLanguageXXXDecl methods)
    Removed all duplicates and non-used methods and renamed makeXX into getXX to be like
    the superclass

--- compiler-java/src/com/redhat/ceylon/compiler/loader/TypeFactory.java
+++ compiler-java/src/com/redhat/ceylon/compiler/loader/TypeFactory.java
@@ -1,26 +1,15 @@
 package com.redhat.ceylon.compiler.loader;
 
-import static com.redhat.ceylon.compiler.typechecker.model.Util.addToUnion;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 import com.redhat.ceylon.compiler.tools.LanguageCompiler;
 import com.redhat.ceylon.compiler.typechecker.context.Context;
-import com.redhat.ceylon.compiler.typechecker.model.BottomType;
-import com.redhat.ceylon.compiler.typechecker.model.Class;
-import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.model.Interface;
-import com.redhat.ceylon.compiler.typechecker.model.Module;
-import com.redhat.ceylon.compiler.typechecker.model.Package;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
 import com.redhat.ceylon.compiler.typechecker.model.TypeDeclaration;
 import com.redhat.ceylon.compiler.typechecker.model.UnionType;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree;
+import com.redhat.ceylon.compiler.typechecker.model.Unit;
+import com.redhat.ceylon.compiler.typechecker.model.Util;
 
-public class TypeFactory {
+public class TypeFactory extends Unit {
     private Context context;
 
     public static TypeFactory instance(com.sun.tools.javac.util.Context context) {
@@ -41,74 +30,13 @@ public class TypeFactory {
     }
     
     /**
-     * Search for a declaration in the language module. 
-     */
-    private Declaration getLanguageModuleDeclaration(String name) {
-        //all elements in ceylon.language are auto-imported
-        //traverse all default module packages provided they have not been traversed yet
-        Module languageModule = getContext().getModules().getLanguageModule();
-        if ( languageModule != null && languageModule.isAvailable() ) {
-            if ("Bottom".equals(name)) {
-                return new BottomType();
-            }
-            for (Package languageScope : languageModule.getPackages() ) {
-                Declaration d = languageScope.getMember(name);
-                if (d != null) {
-                    return d;
-                }
-            }
-        }
-        return null;
-    }
-    
-    /**
-     * Gets the declaration of {@code Void}
-     * @return The declaration
-     */
-    public Class getVoidDeclaration() {
-        return (Class) ((TypeDeclaration) getLanguageModuleDeclaration("Void"));
-    }
-    
-    /**
-     * Gets the declaration of {@code Nothing}
-     * @return The declaration
-     */
-    public Class getNothingDeclaration() {
-        return (Class) ((TypeDeclaration) getLanguageModuleDeclaration("Nothing"));
-    }
-
-    /**
-     * Gets the declaration of {@code Empty}
-     * @return The declaration
-     */
-    public Interface getEmptyDeclaration() {
-        return (Interface) ((TypeDeclaration) getLanguageModuleDeclaration("Empty"));
-    }
-
-    /**
-     * Gets the declaration of {@code Sequence}
-     * @return The declaration
-     */
-    public Interface getSequenceDeclaration() {
-        return (Interface) ((TypeDeclaration) getLanguageModuleDeclaration("Sequence"));
-    }
-
-    /**
      * Gets the declaration of {@code ArraySequence}
      * @return The declaration
      */
-    public Class getDefaultSequenceDeclaration() {
-        return (Class) ((TypeDeclaration) getLanguageModuleDeclaration("ArraySequence"));
+    public TypeDeclaration getDefaultSequenceDeclaration() {
+        return ((TypeDeclaration) getLanguageModuleDeclaration("ArraySequence"));
     }
 
-    /**
-     * Gets the declaration of {@code Iterable}
-     * @return The declaration
-     */
-    public Interface getIterableDeclaration() {
-        return (Interface) ((TypeDeclaration) getLanguageModuleDeclaration("Iterable"));
-    }
-    
     /**
      * Gets the declaration of {@code Iterator}
      * @return The declaration
@@ -116,71 +44,7 @@ public class TypeFactory {
     public Interface getIteratorDeclaration() {
         return (Interface) ((TypeDeclaration) getLanguageModuleDeclaration("Iterator"));
     }
-    
-    /**
-     * Gets the declaration of {@code Castable}
-     * @return The declaration
-     */
-    public Interface getCastableDeclaration() {
-        return (Interface) ((TypeDeclaration) getLanguageModuleDeclaration("Castable"));
-    }
-    
-    /**
-     * Gets the declaration of {@code Entry}
-     * @return The declaration
-     */
-    public Class getEntryDeclaration() {
-        return (Class) ((TypeDeclaration) getLanguageModuleDeclaration("Entry"));
-    }
-    
-    /**
-     * Gets the declaration of {@code Range}
-     * @return The declaration
-     */
-    public Class getRangeDeclaration() {
-        return (Class) ((TypeDeclaration) getLanguageModuleDeclaration("Range"));
-    }
-    
-    public static ProducedType producedType(TypeDeclaration declaration, ProducedType typeArgument) {
-        return declaration.getProducedType(null, Collections.singletonList(typeArgument));
-    }
 
-    public static ProducedType producedType(TypeDeclaration declaration, ProducedType... typeArguments) {
-        return declaration.getProducedType(null, Arrays.asList(typeArguments));
-    }
-
-    public static List<ProducedType> getTypeArguments(Tree.TypeArguments tal) {
-        List<ProducedType> typeArguments = new ArrayList<ProducedType>();
-        if (tal instanceof Tree.TypeArgumentList) {
-            for (Tree.Type ta: ( (Tree.TypeArgumentList) tal ).getTypes()) {
-                ProducedType t = ta.getTypeModel();
-                if (t==null) {
-                    ta.addError("could not resolve type argument");
-                    typeArguments.add(null);
-                }
-                else {
-                    typeArguments.add(t);
-                }
-            }
-        }
-        return typeArguments;
-    }
-    
-    /**
-     * Returns a ProducedType corresponding to the union of the given types
-     * @param types The types to union together
-     * @return The type
-     */
-    public ProducedType unionType(ProducedType... types) {
-        List<ProducedType> list = new ArrayList<ProducedType>();
-        for (ProducedType t : types) {
-            addToUnion(list, t);
-        }
-        UnionType ut = new UnionType();
-        ut.setCaseTypes(list);
-        return ut.getType();
-    }
-    
     /**
      * Determines whether the given ProducedType is a union
      * @param pt 
@@ -190,92 +54,14 @@ public class TypeFactory {
         TypeDeclaration tdecl = pt.getDeclaration();
         return (tdecl instanceof UnionType && tdecl.getCaseTypes().size() > 1);
     }
-    
-    /**
-     * Determines whether the given ProducedType is {@code Empty|T}
-     * @param pt The ProducedType to check
-     * @return whether the type is a union type with empty
-     */
-    public boolean isUnionWithEmpty(ProducedType pt) {
-        TypeDeclaration tdecl = pt.getDeclaration();
-        if (tdecl instanceof UnionType && tdecl.getCaseTypes().size() == 2) {
-            return !(pt.minus(getEmptyDeclaration()).getDeclaration() instanceof UnionType);
-        }
-        return false;
-    }
-    
-    /**
-     * Determines whether the given ProducedType is optional ({@code Nothing|T})
-     * @param pt The ProducedType to check
-     * @return whether the type is optional
-     */
-    public boolean isOptional(ProducedType pt) {
-        TypeDeclaration tdecl = pt.getDeclaration();
-        return (tdecl instanceof UnionType && tdecl.getCaseTypes().size() > 1 && getNothingDeclaration().getType().isSubtypeOf(pt));
-    }
-    
-    /**
-     * Returns a ProducedType corresponding to {@code Empty|T}
-     * @param pt The ProducedType corresponding to {@code T}
-     * @return The ProducedType corresponding to {@code Empty|T}
-     */
-    public ProducedType makeEmptyType(ProducedType pt) {
-        if (pt==null) {
-            return null;
-        }
-        else {
-            return unionType(pt, getEmptyDeclaration().getType());
-        }
-    }
-    
-    /**
-     * Returns a ProducedType corresponding to {@code Nothing|T}
-     * @param pt The ProducedType corresponding to {@code T}
-     * @return The ProducedType corresponding to {@code Nothing|T}
-     */
-    public ProducedType makeOptionalType(ProducedType pt) {
-        if (pt==null) {
-            return null;
-        }
-        else {
-            return unionType(pt, getNothingDeclaration().getType());
-        }
-    }
-    
-    /**
-     * Returns a ProducedType corresponding to {@code Sequence<T>}
-     * @param et The ProducedType corresponding to {@code T}
-     * @return The ProducedType corresponding to {@code Sequence<T>}
-     */
-    public ProducedType makeSequenceType(ProducedType et) {
-        return producedType(getSequenceDeclaration(), et);
-    }
-    
+
     /**
      * Returns a ProducedType corresponding to {@code ArraySequence<T>}
      * @param et The ProducedType corresponding to {@code T}
      * @return The ProducedType corresponding to {@code ArraySequence<T>}
      */
-    public ProducedType makeDefaultSequenceType(ProducedType et) {
-        return producedType(getDefaultSequenceDeclaration(), et);
-    }
-    
-    /**
-     * Returns a ProducedType corresponding to {@code Iterable<T>}
-     * @param et The ProducedType corresponding to {@code T}
-     * @return The ProducedType corresponding to {@code Iterable<T>}
-     */
-    public ProducedType makeIterableType(ProducedType et) {
-        return producedType(getIterableDeclaration(), et);
-    }
-    
-    public ProducedType getIteratedType(ProducedType type) {
-        ProducedType st = type.getSupertype(getIterableDeclaration());
-        if (st!=null && st.getTypeArguments().size()==1) {
-                return st.getTypeArgumentList().get(0);
-        } else {
-                return null;
-        }
+    public ProducedType getDefaultSequenceType(ProducedType et) {
+        return Util.producedType(getDefaultSequenceDeclaration(), et);
     }
 
     /**
@@ -283,27 +69,8 @@ public class TypeFactory {
      * @param et The ProducedType corresponding to {@code T}
      * @return The ProducedType corresponding to {@code Iterator<T>}
      */
-    public ProducedType makeIteratorType(ProducedType et) {
-        return producedType(getIteratorDeclaration(), et);
-    }
-
-    /**
-     * Returns a ProducedType corresponding to {@code Castable<T>}
-     * @param et The ProducedType corresponding to {@code T}
-     * @return The ProducedType corresponding to {@code Castable<T>}
-     */
-    public ProducedType makeCastableType(ProducedType et) {
-        return producedType(getCastableDeclaration(), et);
-    }
-
-    /**
-     * Returns a ProducedType corresponding to {@code Entry<K,V>}
-     * @param kt The ProducedType corresponding to {@code K}
-     * @param vt The ProducedType corresponding to {@code V}
-     * @return The ProducedType corresponding to {@code Entry<K,V>}
-     */
-    public ProducedType makeEntryType(ProducedType kt, ProducedType vt) {
-        return producedType(getEntryDeclaration(), kt, vt);
+    public ProducedType getIteratorType(ProducedType et) {
+        return Util.producedType(getIteratorDeclaration(), et);
     }
 
     /**
@@ -311,30 +78,8 @@ public class TypeFactory {
      * @param rt The ProducedType corresponding to {@code T}
      * @return The ProducedType corresponding to {@code Range<T>}
      */
-    public ProducedType makeRangeType(ProducedType rt) {
-        return producedType(getRangeDeclaration(), rt);
-    }
-
-    /**
-     * Removes Nothing from a type such as Nothing|T
-     * @param pt The ProducedType corresponding to {@code Nothing|T}
-     * @return The ProducedType corresponding to {@code T}
-     */
-    public ProducedType getDefiniteType(ProducedType pt) {
-        return pt.minus(getNothingDeclaration());
-    }
-
-    /**
-     * Removes Nothing and Empty from a type such as Nothing|Empty|T
-     * @param pt The ProducedType corresponding to {@code Nothing|Empty|T}
-     * @return The ProducedType corresponding to {@code T}
-     */
-    public ProducedType getNonemptyType(ProducedType pt) {
-        return pt.minus(getNothingDeclaration()).minus(getEmptyDeclaration());
-    }
-
-    public ProducedType getNonemptySequenceType(ProducedType pt) {
-        return pt.minus(getEmptyDeclaration()).getSupertype(getSequenceDeclaration());
+    public ProducedType getRangeType(ProducedType rt) {
+        return Util.producedType(getRangeDeclaration(), rt);
     }
 
     public ProducedType getNonemptyIterableType(ProducedType pt) {
