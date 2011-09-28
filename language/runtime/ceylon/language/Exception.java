e954312ce1021bf53d31ddf6bf578a9680d52192
Stephane Epardaud <stef@epardaud.fr>
2011-09-28 23:42:05 +0200

    Exception: made it inherit from RuntimeException

--- language/runtime/ceylon/language/Exception.java
+++ language/runtime/ceylon/language/Exception.java
@@ -3,7 +3,7 @@ package ceylon.language;
 import com.redhat.ceylon.compiler.metadata.java.Name;
 import com.redhat.ceylon.compiler.metadata.java.TypeInfo;
 
-public class Exception extends IdentifiableObject {
+public class Exception extends RuntimeException {
 
     public Exception(
             @TypeInfo("ceylon.language.String|ceylon.language.Nothing")
