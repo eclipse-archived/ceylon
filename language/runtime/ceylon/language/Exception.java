cd1c83861f24b92241630f4f27b736c71436a8bf
Stephane Epardaud <stef@epardaud.fr>
2011-09-28 23:42:19 +0200

    Exception: fixed parameter order according to spec

--- language/runtime/ceylon/language/Exception.java
+++ language/runtime/ceylon/language/Exception.java
@@ -6,12 +6,13 @@ import com.redhat.ceylon.compiler.metadata.java.TypeInfo;
 public class Exception extends RuntimeException {
 
     public Exception(
-            @TypeInfo("ceylon.language.String|ceylon.language.Nothing")
-            @Name("message")
-            ceylon.language.String message,
             @TypeInfo("ceylon.language.Exception|ceylon.language.Nothing")
             @Name("cause")
-            java.lang.Throwable cause) {
+            java.lang.Throwable cause,
+            @TypeInfo("ceylon.language.String|ceylon.language.Nothing")
+            @Name("message")
+            ceylon.language.String message
+            ) {
         // This isn't actually used at runtime, but is required by the type checker
     }
     
