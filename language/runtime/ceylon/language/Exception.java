263e66bea44967245838e61764cd62d94c49f4f1
Tom Bentley <tom.bentley@cantab.net>
2011-09-30 11:57:47 +0100

    Futher fixes to Exception

--- language/runtime/ceylon/language/Exception.java
+++ language/runtime/ceylon/language/Exception.java
@@ -5,22 +5,35 @@ import com.redhat.ceylon.compiler.metadata.java.TypeInfo;
 
 public class Exception extends RuntimeException {
 
+    private ceylon.language.String description;
     public Exception(
+            @TypeInfo("ceylon.language.String|ceylon.language.Nothing")
+            @Name("description")
+            ceylon.language.String description,
             @TypeInfo("ceylon.language.Exception|ceylon.language.Nothing")
             @Name("cause")
-            java.lang.Throwable cause,
-            @TypeInfo("ceylon.language.String|ceylon.language.Nothing")
-            @Name("message")
-            ceylon.language.String message
+            java.lang.Throwable cause
+            
             ) {
-        // This isn't actually used at runtime, but is required by the type checker
+        super(description.value, cause);
+        this.description = description;
     }
     
-    public Exception getCause() {
-        return null;
+    public java.lang.Exception getCause() {
+        return (java.lang.Exception)super.getCause();
     }
     
     public java.lang.String getMessage() {
-        return null;
+        if (description != null) {
+            return description.value;
+        } else if (getCause() != null 
+                && getCause().getMessage() != null) {
+            return getCause().getMessage();
+        }
+        return "";
+    }
+    
+    public java.lang.String getString() {
+        return /*super.toString() + */ " \"" + getMessage() +"\""; 
     }
 }
