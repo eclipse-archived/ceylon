4e7c9e5015340069621d51f7fc7b7b667f29bef3
Gavin King <gavin@hibernate.org>
2011-10-06 12:23:07 -0500

    ws

--- language/runtime/ceylon/language/Number.java
+++ language/runtime/ceylon/language/Number.java
@@ -3,13 +3,16 @@ package ceylon.language;
 import com.redhat.ceylon.compiler.metadata.java.TypeInfo;
 
 public interface Number extends Format {
+    
 	@TypeInfo(value="ceylon.language.Natural")
     public long getNatural();
 	@TypeInfo(value="ceylon.language.Integer")
     public long getInteger();
 	@TypeInfo(value="ceylon.language.Float")
     public double getFloat();
+	
     public boolean getIntegral();
     public boolean getZero();
     public boolean getUnit();
+    
 }
