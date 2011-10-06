d1cc3bafd5336d6f8e99e4282422e835bd7f7dfb
Gavin King <gavin@hibernate.org>
2011-10-06 16:09:02 -0500

    add implementation of print()

---
+++ language/runtime/ceylon/language/print.java
@@ -0,0 +1,16 @@
+package ceylon.language;
+
+import com.redhat.ceylon.compiler.metadata.java.Ceylon;
+import com.redhat.ceylon.compiler.metadata.java.Method;
+import com.redhat.ceylon.compiler.metadata.java.Name;
+import com.redhat.ceylon.compiler.metadata.java.TypeInfo;
+
+@Ceylon
+@Method
+public final class print {
+	static final process p = process.getProcess();
+    public static void print(@Name("line") @TypeInfo("ceylon.language.String") java.lang.String line){
+    	p.writeLine(line);
+    }
+    private print(){}
+}
