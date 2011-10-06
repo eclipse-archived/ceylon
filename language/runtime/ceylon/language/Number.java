51b0e125b6025942b9127bb1f5f9f9c8478c1ded
Tom Bentley <tom.bentley@cantab.net>
2011-10-06 13:40:21 +0100

    It should be Number.float, but float's a Java keyword, so use an annotation

--- language/runtime/ceylon/language/Number.java
+++ language/runtime/ceylon/language/Number.java
@@ -1,7 +1,10 @@
 package ceylon.language;
 
+import com.redhat.ceylon.compiler.metadata.java.Name;
+
 public interface Number extends Format {
     public ceylon.language.Natural natural();
     public ceylon.language.Integer integer();
+    @Name("float")
     public ceylon.language.Float toFloat();
 }
