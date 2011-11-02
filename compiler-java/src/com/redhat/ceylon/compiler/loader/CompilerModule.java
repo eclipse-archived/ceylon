d384adcd84187117e0d76288edfe73c209a3ad2d
Stephane Epardaud <stef@epardaud.fr>
2011-11-02 14:21:55 +0100

    CompilerModule: added a constructor with a model loader
    This allows us to create one from the model loader itself

--- compiler-java/src/com/redhat/ceylon/compiler/loader/CompilerModule.java
+++ compiler-java/src/com/redhat/ceylon/compiler/loader/CompilerModule.java
@@ -7,18 +7,30 @@ import com.sun.tools.javac.util.Context;
 public class CompilerModule extends Module {
 
     private Context context;
+    private CeylonModelLoader modelLoader;
 
     public CompilerModule(com.sun.tools.javac.util.Context context) {
         this.context = context;
     }
-    
+
+    public CompilerModule(CeylonModelLoader modelLoader) {
+        this.modelLoader = modelLoader;
+    }
+
     @Override
     public Package getPackage(String name) {
         System.err.println("Looking up package "+name);
         Package pkg = super.getPackage(name);
         if(pkg == null)
-            pkg = CeylonModelLoader.instance(context).findOrCreatePackage(this, name);
+            pkg = getModelLoader().findOrCreatePackage(this, name);
         return pkg;
+    }
+
+    private CeylonModelLoader getModelLoader() {
+        if(modelLoader == null){
+            modelLoader = CeylonModelLoader.instance(context);
+        }
+        return modelLoader;
     }
 
 }
