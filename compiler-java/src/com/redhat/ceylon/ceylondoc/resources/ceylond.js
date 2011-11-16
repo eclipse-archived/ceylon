fd1369e795c7ef219652a5a97f38865c78fba399
Stephane Epardaud <stef@epardaud.fr>
2011-11-16 10:52:45 +0100

    ceylondoc: added tooltip to collapsible elements

--- compiler-java/src/com/redhat/ceylon/ceylondoc/resources/ceylond.js
+++ compiler-java/src/com/redhat/ceylon/ceylondoc/resources/ceylond.js
@@ -9,6 +9,8 @@ jQuery(function(){
   var long = jQuery(".long", this);
   if(short.size() > 0 && long.size() > 0 && short.html() != long.html()){
    tr.addClass("more");
+   short.attr("title", "Click to expand");
+   long.attr("title", "Click to hide");
    tr.click(function(){toggleLongDoc(this)});
   }
  });
