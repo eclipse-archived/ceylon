61b914be4ae4d382f7ee3042a294622e27fee1ba
Tako Schotanus <tako@codejive.org>
2011-11-17 13:45:36 +0100

    Fixed #195 Improve scripts

--- compiler-java/bin/args.sh
+++ compiler-java/bin/args.sh
@@ -1,7 +1,19 @@
 # This file is intended to be sourced, not run directly
 
+# resolve links - $0 may be a softlink
+PRG="$0"
+while [ -h "$PRG" ]; do
+  ls=`ls -ld "$PRG"`
+  link=`expr "$ls" : '.*-> \(.*\)$'`
+  if expr "$link" : '/.*' > /dev/null; then
+    PRG="$link"
+  else
+    PRG=`dirname "$PRG"`/"$link"
+  fi
+done
+
 # Set CEYLON_HOME
-CEYLON_HOME=$(dirname $0)/..
+CEYLON_HOME=$(dirname "$PRG)")/..
 
 unset USER_CP
 unset ARGS
