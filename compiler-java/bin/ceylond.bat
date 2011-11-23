895892d97ee87e8e91f75ecdd0b3fa79ef87f040
Stephane Epardaud <stef@epardaud.fr>
2011-11-23 10:56:21 +0100

    One more fix for #206: remove invalid Windows variables

--- compiler-java/bin/ceylond.bat
+++ compiler-java/bin/ceylond.bat
@@ -5,7 +5,7 @@ call %~dp0\args.bat %*
 
 rem JAVA_CP are classes required by the tool to run
 set JAVA_CP=%CEYLON_HOME%\build\lib\compiler.jar
-set JAVA_CP=%JAVA_CP%;%HOMEDRIVE%%HOMEPATH%\.ceylon\repo\com\redhat\ceylon\typechecker\0.1\com.redhat.ceylon.typechecker-0.1.jar
+set JAVA_CP=%JAVA_CP%;%HOME%\.ceylon\repo\com\redhat\ceylon\typechecker\0.1\com.redhat.ceylon.typechecker-0.1.jar
 set JAVA_CP=%JAVA_CP%;%CEYLON_HOME%\lib\antlrworks-1.3.1.jar
 set JAVA_CP=%JAVA_CP%;%CEYLON_HOME%\lib\markdownj-core-faf3bf83000.jar
 
