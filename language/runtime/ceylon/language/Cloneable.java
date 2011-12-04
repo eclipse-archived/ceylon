a94615933f8ba9d867c3aa34cd4a63218fe6ad7e
Gavin King <gavin@hibernate.org>
2011-12-04 12:20:56 -0600

    correct variance for type param of Cloneable

--- language/runtime/ceylon/language/Cloneable.java
+++ language/runtime/ceylon/language/Cloneable.java
@@ -1,5 +1,11 @@
 package ceylon.language;
 
+import com.redhat.ceylon.compiler.metadata.java.TypeParameter;
+import com.redhat.ceylon.compiler.metadata.java.TypeParameters;
+import com.redhat.ceylon.compiler.metadata.java.Variance;
+
+@TypeParameters({@TypeParameter(value="Clone", variance=Variance.OUT,
+        satisfies="ceylon.language.Cloneable<Clone>")})
 public interface Cloneable<Clone extends Cloneable<Clone>> {
     public Clone getClone();
 }
