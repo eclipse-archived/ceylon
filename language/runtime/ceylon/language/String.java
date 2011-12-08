679524c371999c7269ae58abd841a3be7cdf2912
Gavin King <gavin@hibernate.org>
2011-12-08 09:12:44 -0600

    ws

--- language/runtime/ceylon/language/String.java
+++ language/runtime/ceylon/language/String.java
@@ -119,7 +119,7 @@ public final class String
 
     @TypeInfo("ceylon.language.Nothing|ceylon.language.Natural")
     public Natural getLastIndex() {
-    	int length = value.length();
+        int length = value.length();
         return length==0 ? null : Natural.instance(length-1);
     }
 
@@ -181,7 +181,7 @@ public final class String
         return Correspondence$impl.items(this, keys);
     }
 
-	class StringIterator implements Iterator<Character> {
+    class StringIterator implements Iterator<Character> {
 
         private final int offset;
         private final int codePoint;
@@ -198,7 +198,7 @@ public final class String
 
         @Override
         public Iterator<Character> getTail() {
-        	int nextOffset = offset + java.lang.Character.charCount(codePoint);
+            int nextOffset = offset + java.lang.Character.charCount(codePoint);
             return nextOffset == value.length() ? 
                     null : new StringIterator(nextOffset);
         }
@@ -231,65 +231,65 @@ public final class String
     
     @TypeInfo("ceylon.language.Nothing|ceylon.language.Natural")
     public Natural firstOccurrence(@Name("substring") java.lang.String substring) {
-    	int index = value.indexOf(substring);
-		return index>=0 ? Natural.instance(index) : null;
+        int index = value.indexOf(substring);
+        return index>=0 ? Natural.instance(index) : null;
     }
     
     @TypeInfo("ceylon.language.Nothing|ceylon.language.Natural")
     public Natural lastOccurrence(@Name("substring") java.lang.String substring) {
-    	int index = value.lastIndexOf(substring);
-		return index>=0 ? Natural.instance(index) : null;
+        int index = value.lastIndexOf(substring);
+        return index>=0 ? Natural.instance(index) : null;
     }
     
     @TypeInfo("ceylon.language.Nothing|ceylon.language.Natural")
     public Natural firstCharacterOccurrence(@Name("character") 
     @TypeInfo("ceylon.language.Character") int character) {
-    	int index = value.indexOf(character);
-		return index>=0 ? Natural.instance(index) : null;
+        int index = value.indexOf(character);
+        return index>=0 ? Natural.instance(index) : null;
     }
     
     @TypeInfo("ceylon.language.Nothing|ceylon.language.Natural")
     public Natural lastCharacterOccurrence(@Name("character") 
     @TypeInfo("ceylon.language.Character") int character) {
-    	int index = value.lastIndexOf(character);
-		return index>=0 ? Natural.instance(index) : null;
+        int index = value.lastIndexOf(character);
+        return index>=0 ? Natural.instance(index) : null;
     }
     
     @Override
     public boolean contains(@Name("element") 
     @TypeInfo("ceylon.language.Equality") 
     java.lang.Object element) {
-    	if (element instanceof String) {
-    	    return value.indexOf(((String)element).value)>=0;
-    	}
-    	else if (element instanceof Character) {
-    	    return value.indexOf(((Character)element).intValue())>=0;
-    	}
-    	else {
-    		return false;
-    	}
+        if (element instanceof String) {
+            return value.indexOf(((String)element).value)>=0;
+        }
+        else if (element instanceof Character) {
+            return value.indexOf(((Character)element).intValue())>=0;
+        }
+        else {
+            return false;
+        }
     }
     
     public boolean startsWith(@Name("substring") java.lang.String substring) {
-    	return value.startsWith(substring);
+        return value.startsWith(substring);
     }
     
     public boolean endsWith(@Name("substring") java.lang.String substring) {
-    	return value.endsWith(substring);
+        return value.endsWith(substring);
     }
     
     @Override
     public boolean containsAny(@Sequenced @Name("elements") 
     @TypeInfo("ceylon.language.Empty|ceylon.language.Sequence<ceylon.language.Equality>")
     Iterable<?> elements) {
-    	return Category$impl.containsAny(this, elements);
+        return Category$impl.containsAny(this, elements);
     }
 
     @Override
     public boolean containsEvery(@Sequenced @Name("elements") 
     @TypeInfo("ceylon.language.Empty|ceylon.language.Sequence<ceylon.language.Equality>")
     Iterable<?> elements) {
-    	return Category$impl.containsEvery(this, elements);
+        return Category$impl.containsEvery(this, elements);
     }
     
     public boolean longerThan(@TypeInfo("ceylon.language.Natural") 
@@ -299,211 +299,211 @@ public final class String
 
     public boolean shorterThan(@TypeInfo("ceylon.language.Natural") 
     @Name("length") long length) {
-    	return getSize()<length; //TODO: really inefficient!
+        return getSize()<length; //TODO: really inefficient!
     }
     
     public java.lang.String getTrimmed() {
-    	return value.trim();
+        return value.trim();
     }
     
     public java.lang.String getNormalized() {
-    	java.lang.StringBuilder result = new java.lang.StringBuilder();
-    	boolean previousWasWhitespace=false;
-    	for (int i=0;i<value.length();) {
-    		int c = java.lang.Character.codePointAt(value, i);
-    		boolean isWhitespace = java.lang.Character.isWhitespace(c);
-    		if (!isWhitespace) {
-    			result.appendCodePoint(c);
-    		}
-    		else if (!previousWasWhitespace) {
-    			result.append(" ");
-    		}
-    		previousWasWhitespace = isWhitespace;
-    		i+=java.lang.Character.charCount(c);
-    	}
-    	return result.toString();
+        java.lang.StringBuilder result = new java.lang.StringBuilder();
+        boolean previousWasWhitespace=false;
+        for (int i=0;i<value.length();) {
+            int c = java.lang.Character.codePointAt(value, i);
+            boolean isWhitespace = java.lang.Character.isWhitespace(c);
+            if (!isWhitespace) {
+                result.appendCodePoint(c);
+            }
+            else if (!previousWasWhitespace) {
+                result.append(" ");
+            }
+            previousWasWhitespace = isWhitespace;
+            i+=java.lang.Character.charCount(c);
+        }
+        return result.toString();
     }
     
     @TypeInfo("ceylon.language.String")
     public java.lang.String initial(@TypeInfo("ceylon.language.Natural") 
     @Name("length") long length) {
         return length>value.length() ? value : 
-        	value.substring(0, (int)length);
+            value.substring(0, (int)length);
     }
 
     @TypeInfo("ceylon.language.String")
     public java.lang.String terminal(@TypeInfo("ceylon.language.Natural") 
     @Name("length") long length) {
         return length>value.length() ? value : 
-        	value.substring(value.length()-(int) length, value.length());
+            value.substring(value.length()-(int) length, value.length());
     }
     
     public java.lang.String join(@Name("strings") @Sequenced
     @TypeInfo("ceylon.language.Empty|ceylon.language.Sequence<ceylon.language.String>")
     Iterable<? extends String> strings) {
-		Iterator<? extends String> it = strings.getIterator();
-    	if (it==null) {
-    		return "";
-    	}
-    	else {
-    		java.lang.StringBuilder result = new java.lang.StringBuilder();
-    		result.append(it.getHead());
-    		for (it=it.getTail();it!=null;it=it.getTail()) {
-    			result.append(value).append(it.getHead());
-    		}
-    		return result.toString();
-    	}
+        Iterator<? extends String> it = strings.getIterator();
+        if (it==null) {
+            return "";
+        }
+        else {
+            java.lang.StringBuilder result = new java.lang.StringBuilder();
+            result.append(it.getHead());
+            for (it=it.getTail();it!=null;it=it.getTail()) {
+                result.append(value).append(it.getHead());
+            }
+            return result.toString();
+        }
     }
     
     @Override
     @TypeInfo("ceylon.language.String")
     public String segment(@Name("from") final Natural from, 
-    		@Name("length") final Natural length) {
-    	long fromIndex = from.longValue();
-    	long resultLength = length.longValue();
-    	int len = value.length();
-		if (fromIndex>=len || resultLength==0) return instance("");
-    	if (fromIndex+resultLength>len) resultLength = len-fromIndex;
-    	return instance(value.substring((int) fromIndex, 
-    			(int) (fromIndex+resultLength)));
+            @Name("length") final Natural length) {
+        long fromIndex = from.longValue();
+        long resultLength = length.longValue();
+        int len = value.length();
+        if (fromIndex>=len || resultLength==0) return instance("");
+        if (fromIndex+resultLength>len) resultLength = len-fromIndex;
+        return instance(value.substring((int) fromIndex, 
+                (int) (fromIndex+resultLength)));
     }
     
     @Override
     @TypeInfo("ceylon.language.String")
     public String span(@Name("from") final Natural from, 
-    		@Name("to") @TypeInfo("ceylon.language.Nothing|ceylon.language.Natural") 
+            @Name("to") @TypeInfo("ceylon.language.Nothing|ceylon.language.Natural") 
             final Natural to) {
-    	int len = value.length();
-    	if (len==0) return instance("");
-    	long fromIndex = from.longValue();
-    	long toIndex = to==null ? len-1 : to.longValue();
-    	if (fromIndex>=len||toIndex<fromIndex) return instance("");
-    	if (toIndex>=len) toIndex = len-1;
-    	return instance(value.substring((int) fromIndex, (int) toIndex+1));
+        int len = value.length();
+        if (len==0) return instance("");
+        long fromIndex = from.longValue();
+        long toIndex = to==null ? len-1 : to.longValue();
+        if (fromIndex>=len||toIndex<fromIndex) return instance("");
+        if (toIndex>=len) toIndex = len-1;
+        return instance(value.substring((int) fromIndex, (int) toIndex+1));
     }
     
     public java.lang.String getReversed() {
-    	int len = value.length();
-    	if (len<2) return value;
-    	java.lang.StringBuilder builder = new java.lang.StringBuilder();
-		for (int i=len-1; i>=0; i--) {
-    		builder.append(value.charAt(i));
-    	}
-    	return builder.toString();
+        int len = value.length();
+        if (len<2) return value;
+        java.lang.StringBuilder builder = new java.lang.StringBuilder();
+        for (int i=len-1; i>=0; i--) {
+            builder.append(value.charAt(i));
+        }
+        return builder.toString();
     }
     
     public java.lang.String repeat(
-    		@TypeInfo("ceylon.language.Natural") 
-    		@Name("times") long times) {
-    	int len = value.length();
-    	if (times==0 || len==0) return "";
-    	if (times==1) return value;
-    	java.lang.StringBuilder builder = new java.lang.StringBuilder(len*(int)times);
-    	for (int i=0; i<times; i++) {
-    		builder.append(value);
-    	}
-    	return builder.toString();
+            @TypeInfo("ceylon.language.Natural") 
+            @Name("times") long times) {
+        int len = value.length();
+        if (times==0 || len==0) return "";
+        if (times==1) return value;
+        java.lang.StringBuilder builder = new java.lang.StringBuilder(len*(int)times);
+        for (int i=0; i<times; i++) {
+            builder.append(value);
+        }
+        return builder.toString();
     }
     
     public java.lang.String replace( 
-    		@Name("substring") java.lang.String substring,
-    		@Name("replacement") java.lang.String replacement) {
-    	return value.replace(substring, replacement);
+            @Name("substring") java.lang.String substring,
+            @Name("replacement") java.lang.String replacement) {
+        return value.replace(substring, replacement);
     }
     
-	@TypeInfo("ceylon.language.Ordered<ceylon.language.String>")
+    @TypeInfo("ceylon.language.Ordered<ceylon.language.String>")
     public Ordered<? extends String> split(
-    		@TypeInfo("ceylon.language.Nothing|ceylon.language.Iterable<ceylon.language.Character>")
-    		@Name("separator") Iterable<? extends Character> separators,
-    		@Name("discardSeparators") boolean discardSeparators) {
-    	java.lang.String delims;
-    	if (separators==null) {
-    		delims = " \t\n\r\f";
-    	}
-    	else if (separators instanceof String) {
-    		delims = separators.toString();
-    	}
-    	else {
-	    	java.lang.StringBuilder builder = new java.lang.StringBuilder();
-	    	for (Iterator<? extends Character> it=separators.getIterator(); it!=null; it=it.getTail()) {
-	    		builder.append(it.getHead());
-	    	}
-	    	delims = builder.toString();
-    	}
-	    return new Tokens(new StringTokenizer(value, delims, !discardSeparators));
+            @TypeInfo("ceylon.language.Nothing|ceylon.language.Iterable<ceylon.language.Character>")
+            @Name("separator") Iterable<? extends Character> separators,
+            @Name("discardSeparators") boolean discardSeparators) {
+        java.lang.String delims;
+        if (separators==null) {
+            delims = " \t\n\r\f";
+        }
+        else if (separators instanceof String) {
+            delims = separators.toString();
+        }
+        else {
+            java.lang.StringBuilder builder = new java.lang.StringBuilder();
+            for (Iterator<? extends Character> it=separators.getIterator(); it!=null; it=it.getTail()) {
+                builder.append(it.getHead());
+            }
+            delims = builder.toString();
+        }
+        return new Tokens(new StringTokenizer(value, delims, !discardSeparators));
     }
 
-	private static final class Tokens implements Ordered<String> {
-		private final StringTokenizer tokens;
-		private final boolean empty;
-		private final String first;
-		private TokenIterator it;
-		private final boolean more;
+    private static final class Tokens implements Ordered<String> {
+        private final StringTokenizer tokens;
+        private final boolean empty;
+        private final String first;
+        private TokenIterator it;
+        private final boolean more;
 
-		private Tokens(StringTokenizer tokens) {
-			this.tokens = tokens;
-			empty = !tokens.hasMoreTokens();
-			first = empty ? null : String.instance(tokens.nextToken());
-			more = tokens.hasMoreTokens();
-		}
+        private Tokens(StringTokenizer tokens) {
+            this.tokens = tokens;
+            empty = !tokens.hasMoreTokens();
+            first = empty ? null : String.instance(tokens.nextToken());
+            more = tokens.hasMoreTokens();
+        }
 
-		@Override
-		public Iterator<? extends String> getIterator() {
-			if (!more) {
-				return null;
-			}
-			else if (it==null) {
-				it = new TokenIterator(tokens, first);
-				return it;
-			}
-			else {
-			    return it;
-			}
-		}
+        @Override
+        public Iterator<? extends String> getIterator() {
+            if (!more) {
+                return null;
+            }
+            else if (it==null) {
+                it = new TokenIterator(tokens, first);
+                return it;
+            }
+            else {
+                return it;
+            }
+        }
 
-		@Override
-		public boolean getEmpty() {
-			return empty;
-		}
+        @Override
+        public boolean getEmpty() {
+            return empty;
+        }
 
-		@Override
-		//@TypeInfo("ceylon.language.Nothing|ceylon.language.String")
-		public String getFirst() {
-			return first;
-		}
-	}
+        @Override
+        //@TypeInfo("ceylon.language.Nothing|ceylon.language.String")
+        public String getFirst() {
+            return first;
+        }
+    }
 
     private static final class TokenIterator implements Iterator<String> {
-		private final StringTokenizer tokens;
-		private final String head;
-		private TokenIterator it;
-		private final boolean more;
+        private final StringTokenizer tokens;
+        private final String head;
+        private TokenIterator it;
+        private final boolean more;
 
-		private TokenIterator(StringTokenizer tokens, String head) {
-			this.tokens = tokens;
-			this.head = head;
-			more = tokens.hasMoreTokens();
-		}
+        private TokenIterator(StringTokenizer tokens, String head) {
+            this.tokens = tokens;
+            this.head = head;
+            more = tokens.hasMoreTokens();
+        }
 
-		@Override
-		public String getHead() {
-			return head;
-		}
+        @Override
+        public String getHead() {
+            return head;
+        }
 
-		@Override
-		public Iterator<? extends String> getTail() {
-			if (!more) {
-				return null;
-			}
-			else if (it==null) {
-				it = new TokenIterator(tokens, 
-						String.instance(tokens.nextToken()));
-				return it;
-			}
-			else {
-			    return it;
-			}
-		}
-	}
+        @Override
+        public Iterator<? extends String> getTail() {
+            if (!more) {
+                return null;
+            }
+            else if (it==null) {
+                it = new TokenIterator(tokens, 
+                        String.instance(tokens.nextToken()));
+                return it;
+            }
+            else {
+                return it;
+            }
+        }
+    }
 
 }
