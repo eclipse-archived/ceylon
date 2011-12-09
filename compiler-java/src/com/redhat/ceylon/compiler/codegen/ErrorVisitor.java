8b608f35e863c15b313c1ab3a373455ddeae620f
Stephane Epardaud <stef@epardaud.fr>
2011-12-09 16:41:51 +0100

    ErrorVisitor: don't bail on warnings

--- compiler-java/src/com/redhat/ceylon/compiler/codegen/ErrorVisitor.java
+++ compiler-java/src/com/redhat/ceylon/compiler/codegen/ErrorVisitor.java
@@ -1,5 +1,7 @@
 package com.redhat.ceylon.compiler.codegen;
 
+import com.redhat.ceylon.compiler.typechecker.analyzer.AnalysisWarning;
+import com.redhat.ceylon.compiler.typechecker.tree.Message;
 import com.redhat.ceylon.compiler.typechecker.tree.NaturalVisitor;
 import com.redhat.ceylon.compiler.typechecker.tree.Node;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
@@ -28,9 +30,17 @@ public class ErrorVisitor extends Visitor implements NaturalVisitor {
     @Override
     public void visitAny(Node that) {
         // fast termination
-        if(!that.getErrors().isEmpty())
+        if(hasAnyError(that))
             throw new HasErrorException();
         super.visitAny(that);
+    }
+
+    private boolean hasAnyError(Node that) {
+        // skip warnings
+        for(Message message : that.getErrors())
+            if(!(message instanceof AnalysisWarning))
+                return true;
+        return false;
     }
 
 }
