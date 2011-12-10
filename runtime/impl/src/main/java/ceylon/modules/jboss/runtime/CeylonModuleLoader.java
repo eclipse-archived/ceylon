9f18a4476e138e32e38df12df07d3cd7fd1939df
Ales Justin <ales.justin@gmail.com>
2011-12-10 23:05:16 +0100

    We don't export anything atm.

--- runtime/impl/src/main/java/ceylon/modules/jboss/runtime/CeylonModuleLoader.java
+++ runtime/impl/src/main/java/ceylon/modules/jboss/runtime/CeylonModuleLoader.java
@@ -211,7 +211,7 @@ public class CeylonModuleLoader extends ModuleLoader {
 
                     ModuleIdentifier mi = createModuleIdentifier(i);
                     Graph.Vertex<ModuleIdentifier, Boolean> dv = graph.createVertex(mi, mi);
-                    Graph.Edge.create(true, vertex, dv);
+                    Graph.Edge.create(false, vertex, dv);
                 }
                 if (root.isEmpty() == false) {
                     LocalLoader onDemandLoader = new OnDemandLocalLoader(moduleIdentifier, this, root);
