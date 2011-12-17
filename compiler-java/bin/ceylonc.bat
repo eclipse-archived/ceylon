fe969aa4e4be8796fe57b9ae787b72b5ea119771
Stephane Epardaud <stef@epardaud.fr>
2011-12-17 17:53:42 +0100

    Fixed windows batch files and added java.bat to detect where java is installed on windows

--- compiler-java/bin/ceylonc.bat
+++ compiler-java/bin/ceylonc.bat
@@ -3,19 +3,23 @@ setlocal
 
 call %~dp0\args.bat %*
 
+if "%exit%" == "true" (
+    exit /b 1
+)
+
 rem JAVA_CP are classes required by the compiler to run
-set JAVA_CP=%HOME%\.ceylon\repo\com\redhat\ceylon\compiler\%CEYLON_VERSION%\com.redhat.ceylon.compiler-%CEYLON_VERSION%.jar
-set JAVA_CP=%JAVA_CP%;%HOME%\.ceylon\repo\com\redhat\ceylon\typechecker\%CEYLON_VERSION%\com.redhat.ceylon.typechecker-%CEYLON_VERSION%.jar
-set JAVA_CP=%JAVA_CP%;%CEYLON_HOME%\lib\antlr-3.4-complete.jar
+set "JAVA_CP=%CEYLON_REPO%\com\redhat\ceylon\compiler\%CEYLON_VERSION%\com.redhat.ceylon.compiler-%CEYLON_VERSION%.jar"
+set "JAVA_CP=%JAVA_CP%;%CEYLON_REPO%\com\redhat\ceylon\typechecker\%CEYLON_VERSION%\com.redhat.ceylon.typechecker-%CEYLON_VERSION%.jar"
+set "JAVA_CP=%JAVA_CP%;%CEYLON_HOME%\lib\antlr-3.4-complete.jar"
 
 rem COMPILE_CP are classes required by the code being compiled
-set COMPILE_CP=%HOME%\.ceylon\repo\ceylon\language\%CEYLON_VERSION%\ceylon.language-%CEYLON_VERSION%.car;%USER_CP%
+set "COMPILE_CP=%CEYLON_REPO%\ceylon\language\%CEYLON_VERSION%\ceylon.language-%CEYLON_VERSION%.car;%USER_CP%"
 
-%JAVA% ^
-	-enableassertions ^
-	-classpath "%JAVA_CP%" ^
-	com.redhat.ceylon.compiler.Main ^
-	-classpath "%COMPILE_CP%" ^
-	%ARGS%
+"%JAVA%" ^
+    -enableassertions ^
+    -classpath "%JAVA_CP%;%COMPILE_CP%" ^
+    "-Dceylon.home=%CEYLON_HOME%" ^
+    com.redhat.ceylon.compiler.Main ^
+    %ARGS%
 
 endlocal
--- compiler-java/bin/ceylond.bat
+++ compiler-java/bin/ceylond.bat
@@ -3,17 +3,25 @@ setlocal
 
 call %~dp0\args.bat %*
 
-rem JAVA_CP are classes required by the tool to run
-set JAVA_CP=%HOME%\.ceylon\repo\com\redhat\ceylon\compiler\%CEYLON_VERSION%\com.redhat.ceylon.compiler-%CEYLON_VERSION%.jar
-set JAVA_CP=%JAVA_CP%;%HOME%\.ceylon\repo\com\redhat\ceylon\typechecker\%CEYLON_VERSION%\com.redhat.ceylon.typechecker-%CEYLON_VERSION%.jar
-set JAVA_CP=%JAVA_CP%;%CEYLON_HOME%\lib\antlr-3.4-complete.jar
-set JAVA_CP=%JAVA_CP%;%CEYLON_HOME%\lib\markdownj-core-faf3bf83000.jar
+if "%exit%" == "true" (
+    exit /b 1
+)
 
-%JAVA% ^
-	-enableassertions ^
-	-classpath "%JAVA_CP%" ^
-	com.redhat.ceylon.ceylondoc.Main ^
-	-classpath "%USER_CP%" ^
-	%ARGS%
+rem JAVA_CP are classes required by the tool to run
+set "JAVA_CP=%CEYLON_REPO%\com\redhat\ceylon\compiler\%CEYLON_VERSION%\com.redhat.ceylon.compiler-%CEYLON_VERSION%.jar"
+set "JAVA_CP=%JAVA_CP%;%CEYLON_REPO%\com\redhat\ceylon\typechecker\%CEYLON_VERSION%\com.redhat.ceylon.typechecker-%CEYLON_VERSION%.jar"
+set "JAVA_CP=%JAVA_CP%;%CEYLON_HOME%\lib\antlr-3.4-complete.jar"
+set "JAVA_CP=%JAVA_CP%;%CEYLON_HOME%\lib\markdownj-core-faf3bf83000.jar"
+
+if "%USER_CP%" NEQ "" (
+    set "JAVA_CP=%JAVA_CP%;%USER_CP%"
+)
+
+"%JAVA%" ^
+    -enableassertions ^
+    -classpath "%JAVA_CP%" ^
+    "-Dceylon.home=%CEYLON_HOME%" ^
+    com.redhat.ceylon.ceylondoc.Main ^
+    %ARGS%
 
 endlocal
