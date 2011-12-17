ea58c55ffe5b43e7747ad0b9d43662b5c76a9807
Stephane Epardaud <stef@epardaud.fr>
2011-12-17 18:01:12 +0100

    windows java detection: better error message if java is not found

--- compiler-java/bin/java.bat
+++ compiler-java/bin/java.bat
@@ -20,7 +20,8 @@ if "%ValueValue%" NEQ "" (
 )
 
 if "%JAVA_CURRENT%" == "" (
-    @echo "Java not found"
+    @echo "Java not found, you must install Java in order to compiler and run Ceylon programs"
+    @echo "Go to http://www.java.com/getjava/ to download the latest version of Java
     exit /b 0
 )
 
