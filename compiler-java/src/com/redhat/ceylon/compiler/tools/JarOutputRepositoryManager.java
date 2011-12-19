a01d35bc5d8a00e2e498422bba16339ecba7813b
David Festal <david.festal@serli.com>
2011-12-19 18:34:45 +0100

    Obsolete classes cleaning through class-source mapping management

--- compiler-java/src/com/redhat/ceylon/compiler/tools/JarOutputRepositoryManager.java
+++ compiler-java/src/com/redhat/ceylon/compiler/tools/JarOutputRepositoryManager.java
@@ -29,6 +29,7 @@ import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
@@ -59,8 +60,7 @@ public class JarOutputRepositoryManager {
     
     public JavaFileObject getFileObject(File outputDir, Module module, String fileName, File sourceFile) throws IOException{
         ProgressiveJar progressiveJar = getProgressiveJar(outputDir, module);
-        progressiveJar.addSource(sourceFile);
-        return progressiveJar.getJavaFileObject(fileName);
+        return progressiveJar.getJavaFileObject(fileName, sourceFile);
     }
     
     private ProgressiveJar getProgressiveJar(File outputDir, Module module) throws IOException {
@@ -89,14 +89,15 @@ public class JarOutputRepositoryManager {
     }
 
     static class ProgressiveJar {
+        private static final String MAPPING_FILE = "META-INF/mapping.txt";
         private File originalJarFile;
         private File outputJarFile;
         private JarOutputStream jarOutputStream;
         private File originalSrcFile;
         private File outputSrcFile;
         private JarOutputStream srcOutputStream;
-        final private Set<String> writtenClasses = new HashSet<String>();
-        final private Set<String> sourceFiles = new HashSet<String>();
+        final private Set<String> modifiedSourceFiles = new HashSet<String>();
+        final private Properties writtenClassesMapping = new Properties(); 
         private Log log;
         private Options options;
         private CeyloncFileManager ceyloncFileManager;
@@ -146,35 +147,114 @@ public class JarOutputRepositoryManager {
             srcOutputStream = new JarOutputStream(new FileOutputStream(outputSrcFile));
         }
 
-        public void addSource(File sourceFile) {
-            sourceFiles.add(sourceFile.getPath());
-        }
-
-        public void close() throws IOException {
-            finishUpdatingJar(originalJarFile, outputJarFile, jarOutputStream, writtenClasses);
-            Set<String> copiedSourceFiles = copySourceFiles();
-            finishUpdatingJar(originalSrcFile, outputSrcFile, srcOutputStream, copiedSourceFiles);
+        private Properties getPreviousMapping() throws IOException {
+            if (originalJarFile != null) {
+                JarFile jarFile = null;
+                jarFile = new JarFile(originalJarFile);
+                try {
+                    Enumeration<JarEntry> entries = jarFile.entries();
+                    JarEntry entry = jarFile.getJarEntry(MAPPING_FILE);
+                    InputStream inputStream = jarFile.getInputStream(entry);
+                    try {
+                        Properties previousMapping = new Properties();
+                        previousMapping.load(inputStream);
+                        return previousMapping;
+                    } finally {
+                        inputStream.close();
+                    }
+                } finally {
+                    jarFile.close();
+                }
+            }
+            return null;
         }
         
+        private static interface EntryFilter {
+            boolean avoid(String entryFullName);
+        }
+        
+        public void close() throws IOException {
+            final Set<String> copiedSourceFiles = copySourceFiles();
+            finishUpdatingJar(originalSrcFile, outputSrcFile, srcOutputStream, new EntryFilter() {
+                @Override
+                public boolean avoid(String entryFullName) {
+                    return copiedSourceFiles.contains(entryFullName);
+                }
+            });
+
+            final Properties previousMapping = getPreviousMapping();
+            EntryFilter filterForCars = new EntryFilter() {
+                @Override
+                public boolean avoid(String entryFullName) {
+                    boolean classWasUpdated = writtenClassesMapping.containsKey(entryFullName);
+                    if (previousMapping != null) {
+                        String sourceFileForClass = previousMapping.getProperty(entryFullName);
+                        classWasUpdated = classWasUpdated || copiedSourceFiles.contains(sourceFileForClass);
+                    }
+                    return classWasUpdated || entryFullName.equals(MAPPING_FILE);
+                }
+            };
+            writeMappingJarEntry(previousMapping, filterForCars);
+            finishUpdatingJar(originalJarFile, outputJarFile, jarOutputStream, filterForCars);
+        }
+
+        private void writeMappingJarEntry(Properties previousMapping, EntryFilter filter) {
+            Properties newMapping = new Properties();
+            newMapping.putAll(writtenClassesMapping);
+            if (previousMapping != null) {
+                // Add the previous mapping entries that are not related to an updated source file 
+                for (String classFullName : previousMapping.stringPropertyNames() ) {
+                    if (! filter.avoid(classFullName)) {
+                        newMapping.setProperty(classFullName, previousMapping.getProperty(classFullName));
+                    }
+                }
+            }
+            // Write the mapping file to the Jar
+            try {
+                jarOutputStream.putNextEntry(new ZipEntry(MAPPING_FILE));
+                newMapping.store(jarOutputStream, "");
+            }
+            catch(IOException e) {
+                // TODO : log to the right place
+            }
+            finally {
+                try {
+                    jarOutputStream.closeEntry();
+                } catch (IOException e) {
+                }
+            }
+        }
+
         private Set<String> copySourceFiles() throws IOException {
             Set<String> copiedFiles = new HashSet<String>();
-            for(String prefixedSourceFile : sourceFiles){
+            for(String prefixedSourceFile : modifiedSourceFiles){
                 // must remove the prefix first
-                String sourceFile = Util.getSourceFilePath(ceyloncFileManager, prefixedSourceFile);
-                // zips are UNIX-friendly
-                sourceFile = sourceFile.replace(File.separatorChar, '/');
+                String sourceFile = toPlatformIndependentPath(prefixedSourceFile);
                 srcOutputStream.putNextEntry(new ZipEntry(sourceFile));
-                InputStream inputStream = new FileInputStream(prefixedSourceFile);
-                copy(inputStream, srcOutputStream);
-                inputStream.close();
-                srcOutputStream.closeEntry();
+                try {
+                    InputStream inputStream = new FileInputStream(prefixedSourceFile);
+                    try {
+                        copy(inputStream, srcOutputStream);
+                    } finally {
+                        inputStream.close();
+                    }
+                } finally {
+                    srcOutputStream.closeEntry();
+                }
                 copiedFiles.add(sourceFile);
             }
             return copiedFiles;
         }
 
+        private String toPlatformIndependentPath(String prefixedSourceFile) {
+            String sourceFile = Util.getSourceFilePath(ceyloncFileManager, prefixedSourceFile);
+            // zips are UNIX-friendly
+            sourceFile = sourceFile.replace(File.separatorChar, '/');
+            return sourceFile;
+        }
+
         public void finishUpdatingJar(File originalFile, File outputFile, 
-                JarOutputStream jarOutputStream, Set<String> skipEntries) throws IOException {
+                JarOutputStream jarOutputStream, EntryFilter filter) throws IOException {
             // now copy all previous jar entries
             if(originalFile != null){
                 JarFile jarFile = new JarFile(originalFile);
@@ -182,9 +262,9 @@ public class JarOutputRepositoryManager {
                 while(entries.hasMoreElements()){
                     JarEntry entry = entries.nextElement();
                     // skip the old entry if we overwrote it
-                    if(skipEntries.contains(entry.getName()))
+                    if(filter.avoid(entry.getName()))
                         continue;
-                    jarOutputStream.putNextEntry(entry);
+                    jarOutputStream.putNextEntry(new ZipEntry(entry.getName()));
                     InputStream inputStream = jarFile.getInputStream(entry);
                     copy(inputStream, jarOutputStream);
                     inputStream.close();
@@ -218,11 +298,17 @@ public class JarOutputRepositoryManager {
             }
         }
 
-        public JavaFileObject getJavaFileObject(String fileName) {
+        public JavaFileObject getJavaFileObject(String fileName, File sourceFile) {
+            modifiedSourceFiles.add(sourceFile.getPath());
             // record the class file we produce so that we don't save it from the original jar
         	fileName = fileName.replace(File.separatorChar, '/');
-            writtenClasses.add(fileName);
+        	addMappingEntry(fileName, toPlatformIndependentPath(sourceFile.getPath()));
             return new JarEntryFileObject(outputJarFile.getPath(), jarOutputStream, fileName);
+        }
+
+        private void addMappingEntry(String className,
+                String sourcePath) {
+            writtenClassesMapping.put(className, sourcePath);
         }
     }
 }
