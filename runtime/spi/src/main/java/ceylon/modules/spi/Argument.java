21656b66358d1ad96db43582d37195c8d78523ba
Ales Justin <ales.justin@gmail.com>
2011-12-19 18:15:40 +0100

    Fix ASL2.0 header.

--- runtime/spi/src/main/java/ceylon/modules/spi/Argument.java
+++ runtime/spi/src/main/java/ceylon/modules/spi/Argument.java
@@ -1,10 +1,21 @@
+/*
+ * Copyright 2011 Red Hat inc. and third party contributors as noted 
+ * by the author tags.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ceylon.modules.spi;
 
-/**
- * Represents a command-line argument
- * 
- * @author Stephane Epardaud stef@epardaud.fr
- */
 public enum Argument {
 
     EXECUTABLE("executable", ArgumentType.IMPL, 1),
@@ -12,8 +23,8 @@ public enum Argument {
     IMPLEMENTATION("impl", ArgumentType.IMPL, 2),
     RUN("run", ArgumentType.CEYLON, 1),
     REPOSITORY("rep", ArgumentType.CEYLON, 1),
-    SOURCE("src", ArgumentType.CEYLON, 1), 
-    HELP("help", ArgumentType.CEYLON, 0, "h", "-help"), 
+    SOURCE("src", ArgumentType.CEYLON, 1),
+    HELP("help", ArgumentType.CEYLON, 0, "h", "-help"),
     VERSION("version", ArgumentType.CEYLON, 0, "v", "-version");
 
     private String value;
@@ -28,18 +39,18 @@ public enum Argument {
         this.aliases = aliases;
     }
 
-    public int getRequiredValues(){
+    public int getRequiredValues() {
         return requiredValues;
     }
 
-    public static Argument forArgumentName(String name, ArgumentType type){
-        for(Argument argument : Argument.values()){
-            if(argument.argumentType != type)
+    public static Argument forArgumentName(String name, ArgumentType type) {
+        for (Argument argument : Argument.values()) {
+            if (argument.argumentType != type)
                 continue;
-            if(argument.value.equals(name))
+            if (argument.value.equals(name))
                 return argument;
-            for(String alias : argument.aliases)
-                if(alias.equals(name))
+            for (String alias : argument.aliases)
+                if (alias.equals(name))
                     return argument;
         }
         return null;
--- runtime/spi/src/main/java/ceylon/modules/spi/ArgumentType.java
+++ runtime/spi/src/main/java/ceylon/modules/spi/ArgumentType.java
@@ -1,8 +1,21 @@
+/*
+ * Copyright 2011 Red Hat inc. and third party contributors as noted 
+ * by the author tags.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ceylon.modules.spi;
 
-/**
- * Represents a type of command-line argument.
- */
 public enum ArgumentType {
     /**
      * Implementation-dependent argument
@@ -11,5 +24,5 @@ public enum ArgumentType {
     /**
      * Ceylon argument
      */
-    CEYLON;
+    CEYLON
 }
