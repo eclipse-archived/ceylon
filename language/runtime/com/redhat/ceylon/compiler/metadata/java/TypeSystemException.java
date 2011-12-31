fc06f921320a627545c946898670cf1a9cf87997
Tom Bentley <tom.bentley@cantab.net>
2011-12-31 21:39:44 +0000

    Add exception type to represent subversion of Ceylon's type system, required by switch/case

---
+++ language/runtime/com/redhat/ceylon/compiler/metadata/java/TypeSystemException.java
@@ -0,0 +1,15 @@
+package com.redhat.ceylon.compiler.metadata.java;
+
+/**
+ * Indicates the occurance of an 'impossible' situation which breaks Ceylon's
+ * type system. For instance, a binary incompatibility arising from a 
+ * subclass being added to an enumerated type.
+ * @author tom
+ */
+public class TypeSystemException extends RuntimeException {
+
+    public TypeSystemException(String message) {
+        super(message);
+    }
+
+}
