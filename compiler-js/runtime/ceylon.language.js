fbd0a9ab5c1f61fbea7756ffd1876df45b6e9193
Gavin King <gavin@hibernate.org>
2011-12-30 21:59:46 -0600

    better

--- compiler-js/runtime/ceylon.language.js
+++ compiler-js/runtime/ceylon.language.js
@@ -1,25 +1,31 @@
 //the Ceylon language module
-this.print=function print(line) { console.log(line.toString()) }
+function print(line) { console.log(line.toString()) }
 
-this.Integer=function Integer(value) {
+function Integer(value) {
     this.value = value;
     this.toString = function() { return value.toString() }
     this.plus = function(other) { return new Integer(value+other.value) }
 }
 
-this.Float=function Float(value) {
+function Float(value) {
     this.value = value;
     this.toString = function() { return value.toString() }
     this.plus = function(other) { return new Float(value+other.value) }
 }
 
-this.String=function String(value) {
+function String(value) {
     this.value = value;
     this.toString = function() { return value }
     this.plus = function(other) { return new String(value+other.value) }
 }
 
-this.ArraySequence=function ArraySequence(value) {
+function ArraySequence(value) {
     this.value = value;
     this.toString = function() { return value.toString() }
 }
+
+this.print=print;
+this.Integer=Integer;
+this.Float=Float;
+this.String=String;
+this.ArraySequence=ArraySequence;
