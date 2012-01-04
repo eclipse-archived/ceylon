e47233c0e99c7bf51e024e54611cc2c8d07c5e46
Stephane Epardaud <stef@epardaud.fr>
2012-01-04 13:42:23 +0100

    ceylond: added ceylon.language to classpath since it's not loaded dynamically yet

--- compiler-java/bin/ceylond.bat
+++ compiler-java/bin/ceylond.bat
@@ -10,6 +10,7 @@ if "%exit%" == "true" (
 rem JAVA_CP are classes required by the tool to run
 set "JAVA_CP=%CEYLON_REPO%\com\redhat\ceylon\compiler\java\%CEYLON_VERSION%\com.redhat.ceylon.compiler.java-%CEYLON_VERSION%.jar"
 set "JAVA_CP=%JAVA_CP%;%CEYLON_REPO%\com\redhat\ceylon\typechecker\%CEYLON_VERSION%\com.redhat.ceylon.typechecker-%CEYLON_VERSION%.jar"
+set "JAVA_CP=%JAVA_CP%;%CEYLON_REPO%\ceylon\language\%CEYLON_VERSION%\ceylon.language-%CEYLON_VERSION%.car"
 set "JAVA_CP=%JAVA_CP%;%CEYLON_HOME%\lib\antlr-3.4-complete.jar"
 set "JAVA_CP=%JAVA_CP%;%CEYLON_HOME%\lib\markdownj-core-faf3bf83000.jar"
 
