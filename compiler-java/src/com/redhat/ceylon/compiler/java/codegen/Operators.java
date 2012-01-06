29907f8fc163c0a7e83eac2a227674a582e11207
Stephane Epardaud <stef@epardaud.fr>
2012-01-06 19:29:08 +0100

    Added copyright and comments for new files for #300

--- compiler-java/src/com/redhat/ceylon/compiler/java/codegen/Operators.java
+++ compiler-java/src/com/redhat/ceylon/compiler/java/codegen/Operators.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the authors tag. All rights reserved.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU General Public License version 2.
+ * 
+ * This particular file is subject to the "Classpath" exception as provided in the 
+ * LICENSE file that accompanied this code.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License,
+ * along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
 package com.redhat.ceylon.compiler.java.codegen;
 
 import java.util.HashMap;
@@ -10,6 +30,11 @@ import com.redhat.ceylon.compiler.typechecker.tree.Tree.AssignmentOp;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.BinaryOperatorExpression;
 import com.sun.tools.javac.tree.JCTree;
 
+/**
+ * Aggregation of mappings from ceylon operator to java operator and optimisation strategy.
+ *
+ * @author Stéphane Épardaud <stef@epardaud.fr>
+ */
 public class Operators {
 
     private enum PrimitiveType {
