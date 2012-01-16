b0eb7affbc75532a8515317bab60e8db50a4eb82
Tako Schotanus <tako@ceylon-lang.org>
2012-01-16 14:37:16 +0100

    Fixed ceylon scripts because of the change in the way the module-resolver is built

--- compiler-java/bin/ceylonc.bat
+++ compiler-java/bin/ceylonc.bat
@@ -10,11 +10,7 @@ if "%exit%" == "true" (
 rem JAVA_CP are classes required by the compiler to run
 set "JAVA_CP=%CEYLON_REPO%\com\redhat\ceylon\compiler\java\%CEYLON_VERSION%\com.redhat.ceylon.compiler.java-%CEYLON_VERSION%.jar"
 set "JAVA_CP=%JAVA_CP%;%CEYLON_REPO%\com\redhat\ceylon\typechecker\%CEYLON_VERSION%\com.redhat.ceylon.typechecker-%CEYLON_VERSION%.jar"
-rem FIXME: https://github.com/ceylon/ceylon-module-resolver/issues/7
-set "JAVA_CP=%JAVA_CP%;%HOME%\.m2\repository\com\redhat\ceylon\cmr\cmr-api\1.0.0-SNAPSHOT\cmr-api-1.0.0-SNAPSHOT.jar"
-set "JAVA_CP=%JAVA_CP%;%HOME%\.m2\repository\com\redhat\ceylon\cmr\cmr-spi\1.0.0-SNAPSHOT\cmr-spi-1.0.0-SNAPSHOT.jar"
-set "JAVA_CP=%JAVA_CP%;%HOME%\.m2\repository\com\redhat\ceylon\cmr\cmr-impl\1.0.0-SNAPSHOT\cmr-impl-1.0.0-SNAPSHOT.jar"
-set "JAVA_CP=%JAVA_CP%;%HOME%\.m2\repository\com\redhat\ceylon\cmr\cmr-webdav\1.0.0-SNAPSHOT\cmr-webdav-1.0.0-SNAPSHOT.jar"
+set "JAVA_CP=%JAVA_CP%;%CEYLON_REPO%\com\redhat\ceylon\module-resolver\%CEYLON_VERSION%\com.redhat.ceylon.module-resolver-%CEYLON_VERSION%.jar"
 set "JAVA_CP=%JAVA_CP%;%CEYLON_HOME%\lib\antlr-3.4-complete.jar"
 
 rem COMPILE_CP are classes required by the code being compiled
