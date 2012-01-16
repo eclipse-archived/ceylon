a218e69f5ea2f24caa556640347279aa8849e12e
Stephane Epardaud <stef@epardaud.fr>
2012-01-16 10:55:07 +0100

    CeyloncFileManager: remove unused imports

--- compiler-java/src/com/redhat/ceylon/compiler/java/tools/CeyloncFileManager.java
+++ compiler-java/src/com/redhat/ceylon/compiler/java/tools/CeyloncFileManager.java
@@ -39,8 +39,6 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.tools.FileObject;
 import javax.tools.JavaFileManager;
