be3d6a8ff6b8cd5042c582acbcfb968b68a6f8a6
Stephane Epardaud <stef@epardaud.fr>
2012-01-19 14:30:56 +0100

    Fix for #305: Added Ceylond logger

---
+++ compiler-java/src/com/redhat/ceylon/ceylondoc/CeylondLogger.java
@@ -0,0 +1,34 @@
+package com.redhat.ceylon.ceylondoc;
+
+import com.redhat.ceylon.cmr.api.Logger;
+
+public class CeylondLogger implements Logger {
+
+    private int errors;
+    
+    public int getErrors(){
+        return errors;
+    }
+
+    @Override
+    public void error(String str) {
+        errors++;
+        System.err.println("Error: "+str);
+    }
+
+    @Override
+    public void warning(String str) {
+        System.err.println("Warning: "+str);
+    }
+
+    @Override
+    public void info(String str) {
+        System.err.println("Note: "+str);
+    }
+
+    @Override
+    public void debug(String str) {
+        System.err.println("Debug: "+str);
+    }
+
+}
