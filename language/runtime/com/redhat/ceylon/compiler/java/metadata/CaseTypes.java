66ebe50b9fbdcc140c801c354dc6882d37f5ccea
Tako Schotanus <tako@ceylon-lang.org>
2012-01-20 22:02:39 +0100

    Added @CaseTypes for #328

---
+++ language/runtime/com/redhat/ceylon/compiler/java/metadata/CaseTypes.java
@@ -0,0 +1,9 @@
+package com.redhat.ceylon.compiler.java.metadata;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CaseTypes {
+    String[] value() default {};
+}
