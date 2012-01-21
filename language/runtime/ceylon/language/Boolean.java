3d8db0565f4597b582de404004cded0d97c9a83f
Gavin King <gavin@hibernate.org>
2012-01-20 18:10:43 -0600

    add @CaseTypes to Boolean

--- language/runtime/ceylon/language/Boolean.java
+++ language/runtime/ceylon/language/Boolean.java
@@ -1,11 +1,13 @@
 package ceylon.language;
 
+import com.redhat.ceylon.compiler.java.metadata.CaseTypes;
 import com.redhat.ceylon.compiler.java.metadata.Ceylon;
 import com.redhat.ceylon.compiler.java.metadata.Class;
 import com.redhat.ceylon.compiler.java.metadata.Name;
 
 @Ceylon
 @Class(extendsType = "ceylon.language.Case")
+@CaseTypes({"ceylon.language.true", "ceylon.language.false"})
 public abstract class Boolean extends Case {
 
     public Boolean(@Name("name") java.lang.String name) {
