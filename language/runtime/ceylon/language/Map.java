85afa54a5ce193bbf12a10168bdba8abe3d1d927
Gavin King <gavin@hibernate.org>
2012-02-05 01:00:53 +0100

    Map count()

--- language/runtime/ceylon/language/Map.java
+++ language/runtime/ceylon/language/Map.java
@@ -13,14 +13,29 @@ import com.redhat.ceylon.compiler.java.metadata.Variance;
                  @TypeParameter(value = "Element", variance = Variance.OUT, 
                                satisfies = "ceylon.language.Equality")})
 @SatisfiedTypes({"ceylon.language.Collection<ceylon.language.Entry<Key,Item>>",
-                 "ceylon.language.Correspondence<Key,Item>",
+                 "ceylon.language.Correspondence<ceylon.language.Object,Item>",
                  "ceylon.language.Cloneable<ceylon.language.Map<Key,Item>>"})
 public interface Map<Key,Item> 
-        extends Correspondence<Key,Item>, 
+        extends Correspondence<java.lang.Object,Item>, 
                 Collection<Entry<Key,Item>> {
 	
     @Ignore
     public static final class Map$impl {
+        public static <Key,Item> long count(final Map<Key,Item> $this, java.lang.Object element) {
+            if (element instanceof Entry) {
+                Entry e = (Entry) element;
+                java.lang.Object item = $this.item(e.getKey());
+                if (item!=null) {
+                    return item.equals(e.getItem()) ? 1 : 0;
+                }
+                else {
+                    return 0;
+                }
+            }
+            else {
+                return 0;
+            }
+        }
         public static <Key,Item> boolean equals(final Map<Key,Item> $this, java.lang.Object that) {
         	if (that instanceof Map) {
         		Map other = (Map) that;
