8131ebec9451640383d4cf76fbbef16f559caaae
Enrique Zamudio <alterbass@gmail.com>
2012-02-14 15:10:26 -0600

    JsCallableList does not need to extend ArraySequence.

--- compiler-js/runtime/ceylon/language/0.1/callable.js
+++ compiler-js/runtime/ceylon/language/0.1/callable.js
@@ -4,6 +4,7 @@ function Callable(wat) {
 initType(Callable, 'ceylon.language.Callable');
 exports.Callable=Callable;
 
+//This is used for plain method references
 function JsCallable(o,f) {
     var that = new JsCallable.$$;
     that.o=o;
@@ -23,5 +24,29 @@ $JsCallable.prototype.call = function() {
     return this.f.apply(this.o,al);
 }
 
+//This is used for spread method references
+function JsCallableList(value) {
+    var that = new JsCallableList.$$;
+    that.value=value;
+    return that;
+}
+initType(JsCallableList, 'ceylon.language.JsCallableList', Callable);
+inheritProto(JsCallableList, Object$, '$Object$');
+var $JsCallableList = JsCallableList.$$;
+$JsCallableList.prototype.call = function() {
+    var al=[];
+    if (arguments !== undefined && arguments.length > 0) {
+        for (var i=0; i<arguments.length;i++) {
+            al.push(arguments[i]);
+        }
+    }
+    var rval = [];
+    for (var i = 0; i < this.value.length; i++) {
+        var c = this.value[i];
+        rval.push(c.f.apply(c.o, al));
+    }
+    return ArraySequence(rval);
+}
 
+exports.JsCallableList=JsCallableList;
 exports.JsCallable=JsCallable;
