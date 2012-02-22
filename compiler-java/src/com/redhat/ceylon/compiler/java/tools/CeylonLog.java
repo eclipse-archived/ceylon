06fc6f82b2dc6b453def2fddeef0dd75d0d19fa2
Tom Bentley <tom.bentley@cantab.net>
2012-02-22 08:51:30 +0000

    Issue #392: Add copyright headers to java and ceylon source files which were lacking them.

--- compiler-java/src/com/redhat/ceylon/compiler/java/tools/CeylonLog.java
+++ compiler-java/src/com/redhat/ceylon/compiler/java/tools/CeylonLog.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the authors tag. All rights reserved.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU General Public License version 2.
+ * 
+ * This particular file is subject to the "Classpath" exception as provided in the 
+ * LICENSE file that accompanied this code.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License,
+ * along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
 package com.redhat.ceylon.compiler.java.tools;
 
 import javax.tools.JavaFileObject;
@@ -8,7 +27,6 @@ import com.sun.tools.javac.util.JCDiagnostic;
 import com.sun.tools.javac.util.JCDiagnostic.DiagnosticSource;
 import com.sun.tools.javac.util.JCDiagnostic.DiagnosticType;
 import com.sun.tools.javac.util.Log;
-import com.sun.tools.javac.util.JCDiagnostic.DiagnosticPosition;
 
 public class CeylonLog extends Log {
 
