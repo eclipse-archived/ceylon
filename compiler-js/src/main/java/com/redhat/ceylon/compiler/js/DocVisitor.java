ac606b80a3619d88a56f8e3a170322d44a40ba0f
Enrique Zamudio <alterbass@gmail.com>
2012-02-23 15:13:05 -0600

    This visitor gathers all references to available ceylondoc descriptions. This is why the "doc" annotation kicks javadoc's comments' ass any day.

---
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/DocVisitor.java
@@ -0,0 +1,49 @@
+package com.redhat.ceylon.compiler.js;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.redhat.ceylon.compiler.typechecker.model.Annotation;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.MemberOrTypeExpression;
+import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
+
+/** A Visitor that gathers ceylondoc info for each node's declaration.
+ * 
+ * @author Enrique Zamudio
+ */
+public class DocVisitor extends Visitor {
+
+    /** Here we store all the docs, as they are encountered */
+    private List<String> docs = new ArrayList<String>();
+    /** Here we store all the refs to the docs (index in the 'docs' list), keyed by location. */
+    private Map<String, Integer> locs = new HashMap<String, Integer>();
+
+    @Override
+    public void visit(MemberOrTypeExpression that) {
+        for (Annotation ann : that.getDeclaration().getAnnotations()) {
+            if ("doc".equals(ann.getName()) && !ann.getPositionalArguments().isEmpty()) {
+                String doc = ann.getPositionalArguments().get(0);
+                if (doc.charAt(0) == '"' && doc.charAt(doc.length()-1) == '"') {
+                    doc = doc.substring(1, doc.length()-1);
+                }
+                int idx = docs.indexOf(doc);
+                if (idx < 0) {
+                    idx = docs.size();
+                    docs.add(doc);
+                }
+                locs.put(that.getLocation(), idx);
+            }
+        }
+        super.visit(that);
+    }
+
+    public List<String> getDocs() {
+        return docs;
+    }
+    public Map<String, Integer> getLocations() {
+        return locs;
+    }
+
+}
