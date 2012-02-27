00a607bd8ae03f572319f02778ff7070b4f57c83
Enrique Zamudio <alterbass@gmail.com>
2012-02-27 14:36:18 -0600

    Fix doc retrieval for several nodes

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/DocVisitor.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/DocVisitor.java
@@ -8,8 +8,9 @@ import java.util.Map;
 import com.redhat.ceylon.compiler.typechecker.model.Annotation;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.AnyAttribute;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.AnyMethod;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.BaseType;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.MemberOrTypeExpression;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.Parameter;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 
 /** A Visitor that gathers ceylondoc info for each node's declaration.
@@ -23,7 +24,8 @@ public class DocVisitor extends Visitor {
     /** Here we store all the refs to the docs (index in the 'docs' list), keyed by location. */
     private Map<String, Integer> locs = new HashMap<String, Integer>();
 
-    private void retrieveDocs(List<Annotation> annotations, String location) {
+    private boolean retrieveDocs(List<Annotation> annotations, String location) {
+        boolean rval = false;
         for (Annotation ann : annotations) {
             if ("doc".equals(ann.getName()) && !ann.getPositionalArguments().isEmpty()) {
                 String doc = ann.getPositionalArguments().get(0);
@@ -36,8 +38,10 @@ public class DocVisitor extends Visitor {
                     docs.add(doc);
                 }
                 locs.put(location, idx);
+                rval = true;
             }
         }
+        return rval;
     }
 
     //This catches function calls, method/attribute calls, object refs, constructors.
@@ -49,19 +53,31 @@ public class DocVisitor extends Visitor {
                 retrieveDocs(pt.getDeclaration().getAnnotations(), that.getLocation());
             }
         }
-        retrieveDocs(that.getDeclaration().getAnnotations(), that.getLocation());
-        super.visit(that);
-    }
-    @Override
-    public void visit(Parameter that) {
-        //function/method def parameters
-        retrieveDocs(that.getDeclarationModel().getTypeDeclaration().getAnnotations(), that.getType().getLocation());
+        if (that.getDeclaration() != null && that.getLocation() != null) {
+            retrieveDocs(that.getDeclaration().getAnnotations(), that.getLocation());
+        }
         super.visit(that);
     }
     @Override
     public void visit(AnyAttribute that) {
         //local vars
-        retrieveDocs(that.getDeclarationModel().getTypeDeclaration().getAnnotations(), that.getType().getLocation());
+        retrieveDocs(that.getType().getTypeModel().getDeclaration().getAnnotations(), that.getType().getLocation());
+        super.visit(that);
+    }
+
+    @Override
+    public void visit(AnyMethod that) {
+        retrieveDocs(that.getDeclarationModel().getType().getDeclaration().getAnnotations(), that.getType().getLocation());
+        super.visit(that);
+    }
+    @Override
+    public void visit(BaseType that) {
+        retrieveDocs(that.getDeclarationModel().getType().getDeclaration().getAnnotations(), that.getLocation());
+        super.visit(that);
+    }
+
+    @Override
+    public void visit(com.redhat.ceylon.compiler.typechecker.tree.Tree.Annotation that) {
         super.visit(that);
     }
 
