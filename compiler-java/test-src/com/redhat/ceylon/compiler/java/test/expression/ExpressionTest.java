068a9a7448012305c591827af254f73c2323395c
Tom Bentley <tom.bentley@cantab.net>
2012-03-09 12:10:55 +0000

    Higher Order Functions: Add all the Callable tests to the ExpressionTest

--- compiler-java/test-src/com/redhat/ceylon/compiler/java/test/expression/ExpressionTest.java
+++ compiler-java/test-src/com/redhat/ceylon/compiler/java/test/expression/ExpressionTest.java
@@ -282,6 +282,187 @@ public class ExpressionTest extends CompilerTest {
     public void testInvOptionalTypeParamArgument(){
         compareWithJavaSource("invoke/OptionalTypeParamArgument");
     }
+    
+    @Test
+    public void testCallableArgumentNullary(){
+        compareWithJavaSource("invoke/CallableArgumentNullary");
+    }
+    
+    @Test
+    public void testCallableArgumentUnary(){
+        compareWithJavaSource("invoke/CallableArgumentUnary");
+    }
+    
+    @Test
+    public void testCallableArgumentBinary(){
+        compareWithJavaSource("invoke/CallableArgumentBinary");
+    }
+    
+    @Test
+    public void testCallableArgumentTernary(){
+        compareWithJavaSource("invoke/CallableArgumentTernary");
+    }
+    
+    @Test
+    public void testCallableArgumentNary(){
+        compareWithJavaSource("invoke/CallableArgumentNary");
+    }
+    
+    @Test
+    public void testCallableArgumentSequenced(){
+        compareWithJavaSource("invoke/CallableArgumentSequenced");
+    }
+    @Test
+    public void testCallableArgumentVarargs(){
+        compareWithJavaSource("invoke/CallableArgumentVarargs");
+    }
+    @Test
+    public void testCallableArgumentVarargs2(){
+        compareWithJavaSource("invoke/CallableArgumentVarargs2");
+    }
+    
+    @Test
+    public void testCallableArgumentParameterClass(){
+        compareWithJavaSource("invoke/CallableArgumentParameterClass");
+    }
+    
+    @Test
+    public void testCallableArgumentParameterQualified(){
+        compareWithJavaSource("invoke/CallableArgumentParameterQualified");
+    }
+    
+    @Test
+    public void testCallableArgumentParameterTypeParam(){
+        compareWithJavaSource("invoke/CallableArgumentParameterTypeParam");
+    }
+    
+    @Test
+    public void testCallableArgumentParameterTypeParamMixed(){
+        compareWithJavaSource("invoke/CallableArgumentParameterTypeParamMixed");
+    }
+    
+    @Test
+    @Ignore("Awaiting support for parameter bounds")
+    public void testCallableArgumentParameterCtor(){
+        compareWithJavaSource("invoke/CallableArgumentParameterCtor");
+    }
+    
+    @Test
+    public void testCallableArgumentPassed(){
+        compareWithJavaSource("invoke/CallableArgumentPassed");
+    }
+    
+    @Test
+    public void testCallableArgumentReturningInteger(){
+        compareWithJavaSource("invoke/CallableArgumentReturningInteger");
+    }
+    
+    @Test
+    public void testCallableArgumentReturningClass(){
+        compareWithJavaSource("invoke/CallableArgumentReturningClass");
+    }
+    
+    @Test
+    public void testCallableArgumentReturningQualifiedClass(){
+        compareWithJavaSource("invoke/CallableArgumentReturningQualifiedClass");
+    }
+    
+    @Test
+    public void testCallableArgumentReturningTypeParam(){
+        compareWithJavaSource("invoke/CallableArgumentReturningTypeParam");
+    }
+    
+    @Test
+    public void testCallableReturnNullary(){
+        compareWithJavaSource("invoke/CallableReturnNullary");
+    }
+    
+    @Test
+    public void testCallableReturnUnary(){
+        compareWithJavaSource("invoke/CallableReturnUnary");
+    }
+    
+    @Test
+    public void testCallableReturnBinary(){
+        compareWithJavaSource("invoke/CallableReturnBinary");
+    }
+    
+    @Test
+    public void testCallableReturnTernary(){
+        compareWithJavaSource("invoke/CallableReturnTernary");
+    }
+    
+    @Test
+    public void testCallableReturnNary(){
+        compareWithJavaSource("invoke/CallableReturnNary");
+    }
+    
+    @Test
+    public void testCallableReturnCallable(){
+        compareWithJavaSource("invoke/CallableReturnCallable");
+    }
+    
+    @Test
+    public void testCallableReturnMethod(){
+        compareWithJavaSource("invoke/CallableReturnMethod");
+    }
+    
+    @Test
+    public void testCallableReturnReturningInteger(){
+        compareWithJavaSource("invoke/CallableReturnReturningInteger");
+    }
+    
+    @Test
+    public void testCallableReturnReturningClass(){
+        compareWithJavaSource("invoke/CallableReturnReturningClass");
+    }
+    
+    @Test
+    public void testCallablePositionalInvocationNullary(){
+        compareWithJavaSource("invoke/CallablePositionalInvocationNullary");
+    }
+    
+    @Test
+    public void testCallablePositionalInvocationUnary(){
+        compareWithJavaSource("invoke/CallablePositionalInvocationUnary");
+    }
+    
+    @Test
+    public void testCallablePositionalInvocationSequenced(){
+        compareWithJavaSource("invoke/CallablePositionalInvocationSequenced");
+    }
+    
+    @Test
+    @Ignore("Awaiting fix for ceylon-spec#218")
+    public void testCallablePositionalInvocationQualified(){
+        compareWithJavaSource("invoke/CallablePositionalInvocationQualified");
+    }
+    
+    @Test
+    public void testCallableNamedInvocationNullary(){
+        compareWithJavaSource("invoke/CallableNamedInvocationNullary");
+    }
+    
+    @Test
+    public void testCallableNamedInvocationUnary(){
+        compareWithJavaSource("invoke/CallableNamedInvocationUnary");
+    }
+    
+    @Test
+    public void testCallableNamedInvocationBinary(){
+        compareWithJavaSource("invoke/CallableNamedInvocationBinary");
+    }
+    
+    @Test
+    public void testCallableNamedInvocationNary(){
+        compareWithJavaSource("invoke/CallableNamedInvocationNary");
+    }
+    
+    @Test
+    public void testCallableNamedInvocationSequenced(){
+        compareWithJavaSource("invoke/CallableNamedInvocationSequenced");
+    }
+
 
     // Self references
     
