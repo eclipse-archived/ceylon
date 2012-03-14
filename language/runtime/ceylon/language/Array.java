edfe08c191aef0d529ab9e4ae5534f859c57ef2a
Tako Schotanus <tako@ceylon-lang.org>
2012-03-14 18:36:14 +0100

    Improved implementation of getLength()

--- language/runtime/ceylon/language/Array.java
+++ language/runtime/ceylon/language/Array.java
@@ -251,27 +251,7 @@ public abstract class Array<Element> implements List<Element>, FixedSized<Elemen
     @Override
     @TypeInfo("ceylon.language.Integer")
     public long getSize() {
-        if (array instanceof char[]) {
-            return ((char[])array).length;
-        } else if (array instanceof byte[]) {
-            return ((byte[])array).length;
-        } else if (array instanceof short[]) {
-            return ((short[])array).length;
-        } else if (array instanceof int[]) {
-            return ((int[])array).length;
-        } else if (array instanceof long[]) {
-            return ((long[])array).length;
-        } else if (array instanceof float[]) {
-            return ((float[])array).length;
-        } else if (array instanceof double[]) {
-            return ((double[])array).length;
-        } else if (array instanceof boolean[]) {
-            return ((boolean[])array).length;
-        } else if (array instanceof java.lang.String[]) {
-            return ((java.lang.String[])array).length;
-        } else {
-            return ((Element[])array).length;
-        }
+        return java.lang.reflect.Array.getLength(array);
     }
 
     @Override
