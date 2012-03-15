881171600c7d7a63f01a841b43c0a7a129ec8437
Stephane Epardaud <stef@epardaud.fr>
2012-03-15 17:34:18 +0100

    Update for 0.2

--- language/runtime/ceylon/language/module.java
+++ language/runtime/ceylon/language/module.java
@@ -6,14 +6,14 @@ import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
 
 @Ceylon
 @Attribute
-@com.redhat.ceylon.compiler.java.metadata.Module(name = "ceylon.language", version = "0.1", dependencies = {})
+@com.redhat.ceylon.compiler.java.metadata.Module(name = "ceylon.language", version = "0.2", dependencies = {})
 final class module {
     
     @TypeInfo("ceylon.language.descriptor.Module")
     static ceylon.language.descriptor.Module getModule() {
         return new ceylon.language.descriptor.Module(
         		ceylon.language.Quoted.instance("ceylon.language"), 
-        		ceylon.language.Quoted.instance("0.1"), 
+        		ceylon.language.Quoted.instance("0.2"), 
         		"The Ceylon language module containing the core types referred to in the language specification.", 
         		new ceylon.language.ArraySequence<ceylon.language.String>(ceylon.language.String.instance("Gavin King")), 
         		ceylon.language.Quoted.instance("http://www.apache.org/licenses/LICENSE-2.0.html"), 
--- language/src/ceylon/language/module.ceylon
+++ language/src/ceylon/language/module.ceylon
@@ -1,6 +1,6 @@
 Module module {
     name = 'ceylon.language';
-    version = '0.1';
+    version = '0.2';
     doc = "The Ceylon language module containing
            the core types referred to in the 
            language specification.";
