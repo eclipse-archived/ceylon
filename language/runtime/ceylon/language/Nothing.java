006ae022c1feaa26b6b6ca5ef265f846290d489a
Stephane Epardaud <stef@epardaud.fr>
2012-03-15 11:00:32 +0100

    Nothing: added @CaseTypes

--- language/runtime/ceylon/language/Nothing.java
+++ language/runtime/ceylon/language/Nothing.java
@@ -1,8 +1,10 @@
 package ceylon.language;
 
+import com.redhat.ceylon.compiler.java.metadata.CaseTypes;
 import com.redhat.ceylon.compiler.java.metadata.Ceylon;
 import com.redhat.ceylon.compiler.java.metadata.Class;
 
 @Ceylon
 @Class(extendsType="ceylon.language.Void")
+@CaseTypes({"ceylon.language.null"})
 public abstract class Nothing extends Void {}
