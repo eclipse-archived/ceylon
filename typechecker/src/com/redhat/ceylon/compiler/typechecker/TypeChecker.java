a314cb8a820103e6237a9348c7836fe43c3ef003
Stephane Epardaud <stef@epardaud.fr>
2012-03-15 17:34:35 +0100

    Update for 0.2

--- typechecker/src/com/redhat/ceylon/compiler/typechecker/TypeChecker.java
+++ typechecker/src/com/redhat/ceylon/compiler/typechecker/TypeChecker.java
@@ -23,7 +23,7 @@ import com.redhat.ceylon.compiler.typechecker.util.StatisticsVisitor;
 //TODO make an interface?
 public class TypeChecker {
 
-    public static final String LANGUAGE_MODULE_VERSION = "0.1";
+    public static final String LANGUAGE_MODULE_VERSION = "0.2";
 
     private final boolean verbose;
     private final List<VirtualFile> srcDirectories;
