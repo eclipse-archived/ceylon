40b2a10aefab63cca3d156be5ce68e682cf70557
Tom Bentley <tom.bentley@cantab.net>
2012-04-30 14:41:56 +0100

    Fix 'case is' following change is ceylon.language

--- compiler-java/src/com/redhat/ceylon/compiler/java/codegen/AbstractTransformer.java
+++ compiler-java/src/com/redhat/ceylon/compiler/java/codegen/AbstractTransformer.java
@@ -1658,7 +1658,7 @@ public abstract class AbstractTransformer implements Transformation {
                 return makeUtilInvocation("isEquality", List.of(varExpr));
             } else if (type.isExactly(typeFact().getIdentifiableObjectDeclaration().getType())){
                 // it's erased
-                return makeUtilInvocation("isIdentifiableObject", List.of(varExpr));
+                return makeUtilInvocation("isIdentifiable", List.of(varExpr));
             } else if (type.getDeclaration() instanceof BottomType){
                 // nothing is Bottom
                 return makeIgnoredEvalAndReturn(varExpr, makeBoolean(false));
