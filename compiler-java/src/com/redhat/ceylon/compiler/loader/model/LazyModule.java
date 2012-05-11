cc30def31e921213acca287728ae6c1279cf1198
Stephane Epardaud <stef@epardaud.fr>
2012-05-11 11:43:49 +0200

    Fix for #530: in some cases we had duplicate packages

--- compiler-java/src/com/redhat/ceylon/compiler/loader/model/LazyModule.java
+++ compiler-java/src/com/redhat/ceylon/compiler/loader/model/LazyModule.java
@@ -88,8 +88,15 @@ public abstract class LazyModule extends Module {
     }
 
     private Package findPackageInModule(LazyModule module, String name) {
-        if(module.containsPackage(name))
+        if(module.containsPackage(name)){
+            // first try the already loaded packages from that module
+            for(Package pkg : module.getPackages()){
+                if(pkg.getNameAsString().equals(name))
+                    return pkg;
+            }
+            // create/load a new one
             return getModelLoader().findOrCreatePackage(module, name);
+        }
         return null;
     }
 
