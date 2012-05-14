0a12f5768fb7010a9c19f952f5c8c962c91b83d3
Enrique Zamudio <alterbass@gmail.com>
2012-05-14 15:07:04 -0500

    Define map/fold/find/filter methods

--- language/runtime/ceylon/language/Iterable$impl.java
+++ language/runtime/ceylon/language/Iterable$impl.java
@@ -14,4 +14,30 @@ public final class Iterable$impl<Element> {
     static <Element> boolean _getEmpty(Iterable<Element> $this){
         return $this.getIterator().next() instanceof Finished;
     }
+
+    public Iterable<? extends Element> getSequence() {
+        return Iterable$impl._getSequence($this);
+    }
+    static <Element> Iterable<? extends Element> _getSequence(Iterable<Element> $this) {
+        final SequenceBuilder<Element> sb = new SequenceBuilder();
+        sb.appendAll($this);
+        return sb.getSequence();
+    }
+
+    public <Result> Iterable<Result> map(Callable<Result> collecting) {
+        return null;
+    }
+
+    public Iterable<? extends Element> filter(Callable<Boolean> selecting) {
+        return null;
+    }
+
+    public <Result> Result fold(Callable<Result> accumulating) {
+        return null;
+    }
+
+    public Element find(Callable<Boolean> selecting) {
+        return null;
+    }
+
 }
--- language/runtime/ceylon/language/Iterable.java
+++ language/runtime/ceylon/language/Iterable.java
@@ -1,5 +1,6 @@
 package ceylon.language;
 
+import com.redhat.ceylon.compiler.java.metadata.Name;
 import com.redhat.ceylon.compiler.java.metadata.Ceylon;
 import com.redhat.ceylon.compiler.java.metadata.SatisfiedTypes;
 import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
@@ -16,5 +17,28 @@ public interface Iterable<Element> extends Container {
     
     @TypeInfo("ceylon.language.Iterator<Element>")
     public Iterator<? extends Element> getIterator();
-    
+
+    @TypeInfo("ceylon.language.Empty|ceylon.language.Sequence<Element>")
+    public Iterable<? extends Element> getSequence();
+
+    @TypeInfo("ceylon.language.Iterator<Result>")
+    public <Result> Iterable<Result> map(@Name("collecting")
+        @TypeInfo("ceylon.language.Callable<Result,Element>")
+        Callable<Result> collecting);
+
+    @TypeInfo("ceylon.language.Iterator<Element>")
+    public Iterable<? extends Element> filter(@Name("selecting")
+        @TypeInfo("ceylon.language.Callable<ceylon.language.Boolean,Element>")
+        Callable<Boolean> selecting);
+
+    @TypeInfo("Result")
+    public <Result> Result fold(@Name("accumulating")
+        @TypeInfo("ceylon.language.Callable<Result,Result,Element>")
+        Callable<Result> accumulating);
+
+    @TypeInfo("Element|ceylon.language.Nothing")
+    public Element find(@Name("selecting")
+        @TypeInfo("ceylon.language.Callable<ceylon.language.Boolean,Element>")
+        Callable<Boolean> selecting);
+
 }
