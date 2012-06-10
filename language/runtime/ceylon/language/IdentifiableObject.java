5fbb9e96df9201079a2d0fa81dffc33a9160387f
Gavin King <gavin@hibernate.org>
2012-06-10 10:41:17 -0500

    can now be @Ignored

--- language/runtime/ceylon/language/IdentifiableObject.java
+++ language/runtime/ceylon/language/IdentifiableObject.java
@@ -2,6 +2,7 @@ package ceylon.language;
 
 import com.redhat.ceylon.compiler.java.metadata.Ceylon;
 import com.redhat.ceylon.compiler.java.metadata.Class;
+import com.redhat.ceylon.compiler.java.metadata.Ignore;
 import com.redhat.ceylon.compiler.java.metadata.Name;
 import com.redhat.ceylon.compiler.java.metadata.SatisfiedTypes;
 import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
@@ -11,13 +12,13 @@ import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
 @SatisfiedTypes("ceylon.language.Identifiable")
 public abstract class IdentifiableObject implements Identifiable {
 
-    @Override //@Ignore
+    @Override @Ignore
     public boolean equals(@Name("that") @TypeInfo("ceylon.language.Object") 
     java.lang.Object that) {
         return Identifiable$impl._equals(this, that);
     }
     
-    @Override //@Ignore
+    @Override @Ignore
     public int hashCode() {
         return super.hashCode();
     }
