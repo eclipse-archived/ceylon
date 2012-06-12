38aa12191133a2774c69f4edb4921ada0bbab114
Gavin King <gavin@hibernate.org>
2012-06-12 10:24:43 -0500

    unused import

--- typechecker/src/com/redhat/ceylon/compiler/typechecker/model/ExternalUnit.java
+++ typechecker/src/com/redhat/ceylon/compiler/typechecker/model/ExternalUnit.java
@@ -1,9 +1,4 @@
 package com.redhat.ceylon.compiler.typechecker.model;
 
-import java.util.HashSet;
-import java.util.Set;
 
-import com.redhat.ceylon.compiler.typechecker.context.PhasedUnit;
-
-public class ExternalUnit extends Unit {
-}
+public class ExternalUnit extends Unit {}
