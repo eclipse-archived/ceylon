00bd1081322cb919c948a0261a120f6d63f10939
Stephane Epardaud <stef@epardaud.fr>
2012-06-13 12:02:07 +0200

    Fixed test for #542

--- compiler-java/test-src/com/redhat/ceylon/compiler/java/test/issues/PackageIssuesTest.java
+++ compiler-java/test-src/com/redhat/ceylon/compiler/java/test/issues/PackageIssuesTest.java
@@ -58,7 +58,7 @@ public class PackageIssuesTest extends CompilerTest {
     }
     
     @Test
-    public void testBug542_fail(){
+    public void testBug542(){
         compareWithJavaSource("Bug542");
     }
     
