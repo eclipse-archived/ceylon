316bb92e45e887f7d99d6f92cd257909647bb877
Stephane Epardaud <stef@epardaud.fr>
2012-06-14 13:20:17 +0200

    Fix for #559: Add version to generated bytecode
    Added @Ceylon(major, minor)

--- language/runtime/com/redhat/ceylon/compiler/java/metadata/Ceylon.java
+++ language/runtime/com/redhat/ceylon/compiler/java/metadata/Ceylon.java
@@ -3,7 +3,17 @@ package com.redhat.ceylon.compiler.java.metadata;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * Marks types as being generated Ceylon-based
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Ceylon {
-    // Marks types as being generated Ceylon-based
+    /**
+     * Ceylon binary interface major number.
+     */
+    int major() default 0;
+    /**
+     * Ceylon binary interface minor number.
+     */
+    int minor() default 0;
 }
