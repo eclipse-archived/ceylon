01053603cb4f97258d853cab0426b022a2d9aedf
Tom Bentley <tom.bentley@cantab.net>
2012-06-18 10:48:12 +0100

    Make the default Class.extendsType() reflect the default superclass of classes; Make Void's superclass empty since it has no superclass. (See #628)

--- language/runtime/ceylon/language/Void.java
+++ language/runtime/ceylon/language/Void.java
@@ -4,6 +4,6 @@ import com.redhat.ceylon.compiler.java.metadata.Ceylon;
 import com.redhat.ceylon.compiler.java.metadata.Class;
 
 @Ceylon(major = 1)
-@Class
+@Class(extendsType="")
 public abstract class Void {
 }
--- language/runtime/com/redhat/ceylon/compiler/java/metadata/Class.java
+++ language/runtime/com/redhat/ceylon/compiler/java/metadata/Class.java
@@ -8,5 +8,5 @@ import java.lang.annotation.RetentionPolicy;
  */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Class {
-    String extendsType() default "";
+    String extendsType() default "ceylon.language.IdentifiableObject";
 }
