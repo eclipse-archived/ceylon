f8be3ab3d8e0496e406488c11ad3ce40023ff000
Enrique Zamudio <alterbass@gmail.com>
2012-06-19 17:19:49 -0500

    Avoid adding module filters twice

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/Main.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/Main.java
@@ -159,6 +159,7 @@ public class Main {
                         stop=true;
                     } else {
                         modfilters.add(filedir);
+                        f = null;
                     }
                 }
                 if (f != null) {
