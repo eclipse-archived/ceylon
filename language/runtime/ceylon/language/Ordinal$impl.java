fadb3e48346cda5bf8eced8c4a15a1ed06f6d1d2
Tom Bentley <tom.bentley@cantab.net>
2012-06-20 11:22:43 +0100

    Fix Ordinal$impl, see #634

--- language/runtime/ceylon/language/Ordinal$impl.java
+++ language/runtime/ceylon/language/Ordinal$impl.java
@@ -1,5 +1,5 @@
 package ceylon.language;
 
 public class Ordinal$impl<Other extends Ordinal<? extends Other>> {
-    public Ordinal$impl(Ordinal<Other> $this) {}
+    public Ordinal$impl(Ordinal<? extends Other> $this) {}
 }
