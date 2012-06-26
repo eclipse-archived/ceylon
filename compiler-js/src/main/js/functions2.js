ab9ebb6628fc6fbaf5865beb10b655d3e3973718
Enrique Zamudio <alterbass@gmail.com>
2012-06-26 01:39:53 -0500

    Implement string() for #2523

--- compiler-js/src/main/js/functions2.js
+++ compiler-js/src/main/js/functions2.js
@@ -3,6 +3,9 @@ var exports=null;//IGNORE
 var $finished=null;//IGNORE
 var $true=true;//IGNORE
 function Integer(x){}//IGNORE
+function String$(a,b){}//IGNORE
+function StringBuilder(){}//IGNORE
+
 function byIncreasing/*<Element,Value>*/(/*Callable<Value?,Element>*/comp) {
     return function(x, y) {
         var a = comp(x);
@@ -38,6 +41,18 @@ function count(/*Iterable<Boolean>*/truths) {
     }
     return Integer(c);
 }
+
+function string(/*Iterable<Character>*/chars) {
+    if (chars === undefined) return String$('',0);
+    var s = StringBuilder();
+    var iter = chars.getIterator();
+    var c; while ((c = iter.next()) !== $finished) {
+        s.appendCharacter(c);
+    }
+    return s.getString();
+}
+
 exports.byIncreasing=byIncreasing;
 exports.byDecreasing=byDecreasing;
 exports.count=count;
+exports.string=string;
