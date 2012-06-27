7063618d56061fadecc5802e59c35b5550820adb
Tako Schotanus <tako@ceylon-lang.org>
2012-06-27 16:10:07 +0200

    Revert "Some cleanup for array types", I was just being stupid
    This reverts commit 31df8183484f0409890355a8ec3b1d52193a51d2.

--- language/runtime/ceylon/language/Array.java
+++ language/runtime/ceylon/language/Array.java
@@ -599,3 +599,215 @@ public abstract class Array<Element> implements List<Element>, FixedSized<Elemen
         return $empty.getEmpty();
     }
 }
+
+@Ignore
+@Ceylon(major = 1)
+class ArrayOfNone<Element> extends Array<Element> implements None<Element> {
+
+    @Ignore
+    protected ArrayOfNone(char... array) {
+        super(array);
+    }
+    
+    @Ignore
+    protected ArrayOfNone(byte... array) {
+        super(array);
+    }
+    
+    @Ignore
+    protected ArrayOfNone(short... array) {
+        super(array);
+    }
+    
+    @Ignore
+    protected ArrayOfNone(int... array) {
+        super(array);
+    }
+    
+    @Ignore
+    protected ArrayOfNone(long... array) {
+        super(array);
+    }
+    
+    @Ignore
+    protected ArrayOfNone(float... array) {
+        super(array);
+    }
+    
+    @Ignore
+    protected ArrayOfNone(double... array) {
+        super(array);
+    }
+    
+    @Ignore
+    protected ArrayOfNone(boolean... array) {
+        super(array);
+    }
+
+    @Ignore
+    protected ArrayOfNone(java.lang.String... array) {
+        super(array);
+    }
+
+    @Ignore
+    protected ArrayOfNone(Element... array) {
+        super(array);
+    }
+    
+    @Ignore
+    public static <T> Array<T> instance(java.lang.Class typeClass) {
+        return instance(typeClass, null);
+    }
+    
+    @Override
+    public long getSize() {
+        return 0;
+    }
+    
+    @Override
+    public boolean getEmpty() {
+        return true;
+    }
+    
+    @Override
+    public Element getFirst() {
+        return null;
+    }
+
+    @Override @Ignore public Iterable<? extends Element> getSequence() { return (Iterable)$empty.getEmpty(); }
+    @Override @Ignore public Element find(Callable<? extends Boolean> f) { return null; }
+    @Override @Ignore public Iterable<? extends Element> sorted(Callable<? extends Comparison> f) { return this; }
+    @Override @Ignore public <Result> Iterable<Result> map(Callable<? extends Result> f) { return (Iterable)$empty.getEmpty(); }
+    @Override @Ignore public Iterable<? extends Element> filter(Callable<? extends Boolean> f) { return this; }
+    @Override @Ignore public <Result> Result fold(Result ini, Callable<? extends Result> f) { return ini; }
+    @Override @Ignore public boolean any(Callable<? extends Boolean> f) { return false; }
+    @Override @Ignore public boolean every(Callable<? extends Boolean> f) { return false; }
+}
+
+@Ignore
+@Ceylon(major = 1)
+class ArrayOfSome<Element> extends Array<Element> implements Some<Element> {
+
+    public ArrayOfSome(char... array) {
+        super(array);
+    }
+    
+    public ArrayOfSome(byte... array) {
+        super(array);
+    }
+    
+    public ArrayOfSome(short... array) {
+        super(array);
+    }
+    
+    public ArrayOfSome(int... array) {
+        super(array);
+    }
+    
+    public ArrayOfSome(long... array) {
+        super(array);
+    }
+    
+    public ArrayOfSome(float... array) {
+        super(array);
+    }
+    
+    public ArrayOfSome(double... array) {
+        super(array);
+    }
+    
+    public ArrayOfSome(boolean... array) {
+        super(array);
+    }
+
+    public ArrayOfSome(java.lang.String... array) {
+        super(array);
+    }
+
+    public ArrayOfSome(Element... array) {
+        super(array);
+    }
+    
+    @Override
+    public FixedSized<? extends Element> getRest() {
+        if (getSize() == 1) {
+            return $arrayOfNone.<Element>arrayOfNone();
+        } else {
+            if (array instanceof char[]) {
+                return new ArrayOfSome<Element>(Arrays.copyOfRange((char[])array, 1, (int)getSize()));
+            } else if (array instanceof byte[]) {
+                return new ArrayOfSome<Element>(Arrays.copyOfRange((byte[])array, 1, (int)getSize()));
+            } else if (array instanceof short[]) {
+                return new ArrayOfSome<Element>(Arrays.copyOfRange((short[])array, 1, (int)getSize()));
+            } else if (array instanceof int[]) {
+                return new ArrayOfSome<Element>(Arrays.copyOfRange((int[])array, 1, (int)getSize()));
+            } else if (array instanceof long[]) {
+                return new ArrayOfSome<Element>(Arrays.copyOfRange((long[])array, 1, (int)getSize()));
+            } else if (array instanceof float[]) {
+                return new ArrayOfSome<Element>(Arrays.copyOfRange((float[])array, 1, (int)getSize()));
+            } else if (array instanceof double[]) {
+                return new ArrayOfSome<Element>(Arrays.copyOfRange((double[])array, 1, (int)getSize()));
+            } else if (array instanceof boolean[]) {
+                return new ArrayOfSome<Element>(Arrays.copyOfRange((boolean[])array, 1, (int)getSize()));
+            } else if (array instanceof java.lang.String[]) {
+                return new ArrayOfSome<Element>(Arrays.copyOfRange((java.lang.String[])array, 1, (int)getSize()));
+            } else {
+                return new ArrayOfSome<Element>(Arrays.copyOfRange((Element[])array, 1, (int)getSize()));
+            }
+        }
+    }
+
+    @Override
+    public boolean getEmpty() {
+        return false;
+    }
+
+    /*@Override
+    public FixedSized<? extends Element> getRest() {
+        return span(Integer.instance(1), null);
+    }*/
+    
+    @Override
+    public Element getFirst() {
+        return unsafeItem(0); //FixedSized$impl._getFirst(this);
+    }
+    
+    @Override 
+    @Ignore 
+    public Iterable<? extends Element> getSequence() { 
+        return Iterable$impl._getSequence(this); 
+    }
+    @Override 
+    @Ignore 
+    public Element find(Callable<? extends Boolean> f) { 
+        return Iterable$impl._find(this, f); 
+    }
+    @Override 
+    @Ignore
+    public Iterable<? extends Element> sorted(Callable<? extends Comparison> f) { 
+        return Iterable$impl._sorted(this, f); 
+    }
+    @Override 
+    @Ignore 
+    public <Result> Iterable<Result> map(Callable<? extends Result> f) { 
+        return new MapIterable<Element, Result>(this, f); 
+    }
+    @Override 
+    @Ignore 
+    public Iterable<? extends Element> filter(Callable<? extends Boolean> f) { 
+        return new FilterIterable<Element>(this, f); 
+    }
+    @Override 
+    @Ignore 
+    public <Result> Result fold(Result ini, Callable<? extends Result> f) { 
+        return Iterable$impl._fold(this, ini, f); 
+    }
+    @Override @Ignore
+    public boolean any(Callable<? extends Boolean> f) {
+        return Iterable$impl._any(this, f);
+    }
+    @Override @Ignore
+    public boolean every(Callable<? extends Boolean> f) {
+        return Iterable$impl._every(this, f);
+    }
+}
