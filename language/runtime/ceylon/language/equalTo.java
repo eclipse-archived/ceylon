e5a878e9eb5489c4eca721eee6f24989a02806ac
Enrique Zamudio <alterbass@gmail.com>
2012-06-29 11:07:24 -0500

    Implement new functions equalTo, greaterThan, lessThan for #4980

---
+++ language/runtime/ceylon/language/equalTo.java
@@ -0,0 +1,27 @@
+package ceylon.language;
+
+import com.redhat.ceylon.compiler.java.language.AbstractCallable;
+import com.redhat.ceylon.compiler.java.metadata.Ceylon;
+import com.redhat.ceylon.compiler.java.metadata.Method;
+import com.redhat.ceylon.compiler.java.metadata.Name;
+import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
+import com.redhat.ceylon.compiler.java.metadata.TypeParameter;
+import com.redhat.ceylon.compiler.java.metadata.TypeParameters;
+
+@Ceylon(major=1)
+@Method
+public class equalTo {
+    private equalTo(){}
+
+    @TypeParameters(@TypeParameter(value="Element", satisfies="ceylon.language.Object"))
+    @TypeInfo("ceylon.language.Callable<ceylon.language.Boolean,Element>")
+    public static <Element extends Comparable<? super Element>> Callable<? extends Boolean> equalTo(
+            @Name("val") @TypeInfo("Element")
+            final Element val) {
+        return new AbstractCallable<Boolean>("equalTo"){
+            public Boolean $call(java.lang.Object element) {
+                return Boolean.instance(val.equals(element));
+            }
+        };
+    }
+}
