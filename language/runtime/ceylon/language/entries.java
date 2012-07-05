c4e83ed60366cbc3336783a8dbe0f9b8c0046aae
Enrique Zamudio <alterbass@gmail.com>
2012-07-05 12:40:29 -0500

    Fix return type

--- language/runtime/ceylon/language/entries.java
+++ language/runtime/ceylon/language/entries.java
@@ -17,13 +17,13 @@ public final class entries {
     
     @TypeParameters(@TypeParameter("Element"))
     @TypeInfo("ceylon.language.Iterable<ceylon.language.Entry<ceylon.language.Integer,Element&ceylon.language.Object>>")
-    public static <Element> Iterable<? extends Entry<? extends Integer,? extends Element>> entries(@Name("elements")
+    public static <Element> Iterable<? extends Entry<? extends Integer, ? extends Element>> entries(@Name("elements")
     @Sequenced @TypeInfo("ceylon.language.Iterable<Element>")
     final ceylon.language.Iterable<? extends Element> elements) {
         return elements.getIndexed();
     }
     @Ignore
-    public static <Element> Iterable<? extends Entry<? extends Integer,? extends Element>> entries() {
+    public static <Element> Iterable<? extends Entry<? extends Integer, ? extends Element>> entries() {
         return (Iterable) $empty.getEmpty();
     }
 }
