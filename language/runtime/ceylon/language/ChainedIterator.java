3c5eecf15ee4ca188415564f255f95bef4138ee2
Enrique Zamudio <alterbass@gmail.com>
2012-07-09 14:41:52 -0500

    Runtime impl of ChainedIterator

---
+++ language/runtime/ceylon/language/ChainedIterator.java
@@ -0,0 +1,36 @@
+package ceylon.language;
+
+import com.redhat.ceylon.compiler.java.metadata.Annotation;
+import com.redhat.ceylon.compiler.java.metadata.Annotations;
+import com.redhat.ceylon.compiler.java.metadata.Ceylon;
+import com.redhat.ceylon.compiler.java.metadata.Class;
+import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
+import com.redhat.ceylon.compiler.java.metadata.TypeParameter;
+import com.redhat.ceylon.compiler.java.metadata.TypeParameters;
+
+@Ceylon(major = 2)
+@Class(extendsType="ceylon.language.IdentifiableObject")
+@TypeParameters({@TypeParameter("Element"), @TypeParameter("Other")})
+public class ChainedIterator<Element,Other> implements Iterator {
+
+    private Iterator iter;
+    private final Iterable<? extends Other> two;
+    private boolean more = true;
+
+    public ChainedIterator(final Iterable<? extends Element> i1, final Iterable<? extends Other> i2) {
+        iter = i1.getIterator();
+        two = i2;
+    }
+
+    @Override
+    @TypeInfo("Element|Other|ceylon.language.Finished")
+    public java.lang.Object next() {
+        java.lang.Object e = iter.next();
+        if (e == exhausted.getExhausted() && more) {
+            more = false;
+            iter = two.getIterator();
+            e = iter.next();
+        }
+        return e;
+    }
+}
