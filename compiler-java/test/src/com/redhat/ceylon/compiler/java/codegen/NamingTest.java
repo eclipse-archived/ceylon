094cc9f27abbab9e841fb53aba412be74a4149d2
Tom Bentley <tom.bentley@cantab.net>
2012-07-17 21:37:26 +0100

    Naming: Issue #602: Fixup tests following refactoring, now that makeDeclName() is an instance method.

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/codegen/NamingTest.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/codegen/NamingTest.java
@@ -1,10 +1,10 @@
 package com.redhat.ceylon.compiler.java.codegen;
 
-import static com.redhat.ceylon.compiler.java.codegen.Naming.declName;
 import static com.redhat.ceylon.compiler.java.codegen.Naming.DeclNameFlag.COMPANION;
 import static com.redhat.ceylon.compiler.java.codegen.Naming.DeclNameFlag.QUALIFIED;
 
 import java.io.File;
+import java.nio.charset.Charset;
 import java.util.List;
 
 import org.junit.Assert;
@@ -13,7 +13,7 @@ import org.junit.Test;
 import com.redhat.ceylon.cmr.api.Logger;
 import com.redhat.ceylon.cmr.api.RepositoryManager;
 import com.redhat.ceylon.cmr.api.RepositoryManagerBuilder;
-import com.redhat.ceylon.compiler.java.codegen.LocalId;
+import com.redhat.ceylon.compiler.java.tools.CeyloncFileManager;
 import com.redhat.ceylon.compiler.typechecker.context.Context;
 import com.redhat.ceylon.compiler.typechecker.context.PhasedUnit;
 import com.redhat.ceylon.compiler.typechecker.context.PhasedUnits;
@@ -21,6 +21,7 @@ import com.redhat.ceylon.compiler.typechecker.io.VFS;
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.model.Scope;
 import com.redhat.ceylon.compiler.typechecker.model.TypeDeclaration;
+import com.sun.tools.javac.tree.TreeMaker;
 
 public class NamingTest {
     
@@ -99,182 +100,188 @@ public class NamingTest {
         return (TypeDeclaration)found;
     }
 
-    private static final LocalId lid = new LocalId() {
-        @Override
-        public String getLocalId(Scope d) {
-            return "0";
-        }
-    };
+    private final Naming naming;
+    public NamingTest () {
+        super();
+        com.sun.tools.javac.util.Context context = new com.sun.tools.javac.util.Context();
+        new CeyloncFileManager(context, true, Charset.forName("UTF-8"));
+        naming = new Naming(context) {
+            @Override
+            public String getLocalId(Scope d) {
+                return "0";
+            }
+        };
+    }
     
     @Test
     public void testC() throws Exception {
         final TypeDeclaration decl = findDecl("C.ceylon", "C");
-        Assert.assertEquals("C", declName(lid, decl));
-        Assert.assertEquals("C$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "C", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals(QUAL + "C$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("C", naming.declName(decl));
+        Assert.assertEquals("C$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "C", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals(QUAL + "C$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testCC() throws Exception {
         final TypeDeclaration decl = findDecl("CC.ceylon", "CC.C");
-        Assert.assertEquals("C", declName(lid, decl));
-        Assert.assertEquals("C$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "CC.C", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals(QUAL + "CC.C$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("C", naming.declName(decl));
+        Assert.assertEquals("C$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "CC.C", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals(QUAL + "CC.C$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testCI() throws Exception {
         final TypeDeclaration decl = findDecl("CI.ceylon", "CI.I");
-        Assert.assertEquals("CI$I", declName(lid, decl));
-        Assert.assertEquals("I$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "CI$I", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals(QUAL + "CI.I$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("CI$I", naming.declName(decl));
+        Assert.assertEquals("I$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "CI$I", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals(QUAL + "CI.I$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testCo() throws Exception {
         final TypeDeclaration decl = findDecl("Co.ceylon", "Co.o");
-        Assert.assertEquals("o", declName(lid, decl));
-        Assert.assertEquals("o$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "Co.o", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals(QUAL + "Co.o$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("o_", naming.declName(decl));
+        Assert.assertEquals("o$impl_", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "Co.o_", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals(QUAL + "Co.o$impl_", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testI() throws Exception {
         final TypeDeclaration decl = findDecl("I.ceylon", "I");
-        Assert.assertEquals("I", declName(lid, decl));
-        Assert.assertEquals("I$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "I", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals(QUAL + "I$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("I", naming.declName(decl));
+        Assert.assertEquals("I$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "I", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals(QUAL + "I$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testIC() throws Exception {
         final TypeDeclaration decl = findDecl("IC.ceylon", "IC.C");
-        Assert.assertEquals("C", declName(lid, decl));
-        Assert.assertEquals("C$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "IC$impl.C", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals(QUAL + "IC$impl.C$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("C", naming.declName(decl));
+        Assert.assertEquals("C$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "IC$impl.C", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals(QUAL + "IC$impl.C$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testII() throws Exception {
         final TypeDeclaration decl = findDecl("II.ceylon", "II.I");
-        Assert.assertEquals("II$I", declName(lid, decl));
-        Assert.assertEquals("I$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "II$I", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals(QUAL + "II$impl.I$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("II$I", naming.declName(decl));
+        Assert.assertEquals("I$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "II$I", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals(QUAL + "II$impl.I$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testIo() throws Exception {
         final TypeDeclaration decl = findDecl("Io.ceylon", "Io.o");
-        Assert.assertEquals("o", declName(lid, decl));
-        Assert.assertEquals("o$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "Io$impl.o", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals(QUAL + "Io$impl.o$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("o_", naming.declName(decl));
+        Assert.assertEquals("o$impl_", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "Io$impl.o_", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals(QUAL + "Io$impl.o$impl_", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testo() throws Exception {
         final TypeDeclaration decl = findDecl("o.ceylon", "o");
-        Assert.assertEquals("o", declName(lid, decl));
-        Assert.assertEquals("o$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "o", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals(QUAL + "o$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("o_", naming.declName(decl));
+        Assert.assertEquals("o$impl_", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "o_", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals(QUAL + "o$impl_", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testoC() throws Exception {
         final TypeDeclaration decl = findDecl("oC.ceylon", "oC.C");
-        Assert.assertEquals("C", declName(lid, decl));
-        Assert.assertEquals("C$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "oC.C", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals(QUAL + "oC.C$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("C", naming.declName(decl));
+        Assert.assertEquals("C$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "oC_.C", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals(QUAL + "oC_.C$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testoI() throws Exception {
         final TypeDeclaration decl = findDecl("oI.ceylon", "oI.I");
-        Assert.assertEquals("oI$I", declName(lid, decl));
-        Assert.assertEquals("I$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "oI$I", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals(QUAL + "oI.I$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("oI$I_", naming.declName(decl));
+        Assert.assertEquals("I$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "oI$I_", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals(QUAL + "oI_.I$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testoo() throws Exception {
         final TypeDeclaration decl = findDecl("oo.ceylon", "oo.o");
-        Assert.assertEquals("o", declName(lid, decl));
-        Assert.assertEquals("o$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "oo.o", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals(QUAL + "oo.o$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("o_", naming.declName(decl));
+        Assert.assertEquals("o$impl_", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "oo_.o_", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals(QUAL + "oo_.o$impl_", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testfC() throws Exception {
         final TypeDeclaration decl = findDecl("fC.ceylon", "fC.C");
-        Assert.assertEquals("C", declName(lid, decl));
-        Assert.assertEquals("C$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals("C", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals("C$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("C", naming.declName(decl));
+        Assert.assertEquals("C$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals("C", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals("C$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testfCC() throws Exception {
         final TypeDeclaration decl = findDecl("fCC.ceylon", "fCC.CC.C");
-        Assert.assertEquals("C", declName(lid, decl));
-        Assert.assertEquals("C$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals("CC.C", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals("CC.C$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("C", naming.declName(decl));
+        Assert.assertEquals("C$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals("CC.C", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals("CC.C$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testfCI() throws Exception {
         final TypeDeclaration decl = findDecl("fCI.ceylon", "fCI.CI.I");
-        Assert.assertEquals("fCI$0$CI$I", declName(lid, decl));
-        Assert.assertEquals("I$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "fCI$0$CI$I", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals("CI.I$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("fCI$0$CI$I_", naming.declName(decl));
+        Assert.assertEquals("I$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "fCI$0$CI$I_", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals("CI.I$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testfI() throws Exception {
         final TypeDeclaration decl = findDecl("fI.ceylon", "fI.I");
-        Assert.assertEquals("fI$0$I", declName(lid, decl));
-        Assert.assertEquals("I$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "fI$0$I", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals("I$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("fI$0$I_", naming.declName(decl));
+        Assert.assertEquals("I$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "fI$0$I_", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals("I$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testfIC() throws Exception {
         final TypeDeclaration decl = findDecl("fIC.ceylon", "fIC.IC.C");
-        Assert.assertEquals("C", declName(lid, decl));
-        Assert.assertEquals("C$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals("IC$impl.C", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals("IC$impl.C$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("C", naming.declName(decl));
+        Assert.assertEquals("C$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals("IC$impl.C", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals("IC$impl.C$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testfII() throws Exception {
         final TypeDeclaration decl = findDecl("fII.ceylon", "fII.II.I");
-        Assert.assertEquals("fII$0$II$I", declName(lid, decl));
-        Assert.assertEquals("I$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals(QUAL + "fII$0$II$I", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals("II$impl.I$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("fII$0$II$I_", naming.declName(decl));
+        Assert.assertEquals("I$impl", naming.declName(decl, COMPANION));
+        Assert.assertEquals(QUAL + "fII$0$II$I_", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals("II$impl.I$impl", naming.declName(decl, COMPANION, QUALIFIED));
     }
     
     @Test
     public void testfo() throws Exception {
         final TypeDeclaration decl = findDecl("fo.ceylon", "fo.o");
-        Assert.assertEquals("o", declName(lid, decl));
-        Assert.assertEquals("o$impl", declName(lid, decl, COMPANION));
-        Assert.assertEquals("o", declName(lid, decl, QUALIFIED));
-        Assert.assertEquals("o$impl", declName(lid, decl, COMPANION, QUALIFIED));
+        Assert.assertEquals("o_", naming.declName(decl));
+        Assert.assertEquals("o$impl_", naming.declName(decl, COMPANION));
+        Assert.assertEquals("o_", naming.declName(decl, QUALIFIED));
+        Assert.assertEquals("o$impl_", naming.declName(decl, COMPANION, QUALIFIED));
     }
 
 }
