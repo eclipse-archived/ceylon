cd606ca9e9d0b2e4ee2acb179e9ffde7d29abeab
Tako Schotanus <tako@ceylon-lang.org>
2012-08-04 20:06:35 +0200

    Added test suite (#4602)

---
+++ common/test/src/com/redhat/ceylon/common/test/AllTests.java
@@ -0,0 +1,14 @@
+package com.redhat.ceylon.common.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class) 
+@SuiteClasses({
+    CeylonConfigTest.class,
+    ConfigWriterTest.class,
+    RepositoriesTest.class
+})
+public class AllTests {
+}
