f2359198aff94aa8fe0099e041d60ea70203e69d
Stephane Epardaud <stef@epardaud.fr>
2012-08-06 13:41:49 +0200

    Changed release name to black ops temporary secret code name of Ratatouille

--- runtime/spi/src/main/java/ceylon/modules/spi/Constants.java
+++ runtime/spi/src/main/java/ceylon/modules/spi/Constants.java
@@ -35,7 +35,7 @@ public enum Constants {
     CEYLON_RUNTIME_MODULE("ceylon.runtime"),
     MERGE_STRATEGY("com.redhat.ceylon.cmr.spi.MergeStrategy"),
     CONTENT_TRANSFORMER("com.redhat.ceylon.cmr.spi.ContentTransformer"),
-    CEYLON_VERSION("0.4 'V2000'");
+    CEYLON_VERSION("0.4 'Ratatouille'");
 
     private String value;
 
