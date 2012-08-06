23e1d91f5027b866ce4671ecc36fdccab478c04a
Tako Schotanus <tako@ceylon-lang.org>
2012-08-06 13:02:06 +0200

    More tests for the ConfigWriter (#4602)

--- common/test/src/com/redhat/ceylon/common/test/ConfigWriterTest.java
+++ common/test/src/com/redhat/ceylon/common/test/ConfigWriterTest.java
@@ -22,8 +22,8 @@ public class ConfigWriterTest {
     
     @Before
     public void setup() throws IOException {
-        testFile = new File("test/src/com/redhat/ceylon/common/test/test.config");
-        testConfig = ConfigParser.loadConfigFromFile(testFile);
+        testFile = new File("test/src/com/redhat/ceylon/common/test/writer.config");
+        testConfig = ConfigParser.loadOriginalConfigFromFile(testFile);
     }
     
     @Test
@@ -33,21 +33,56 @@ public class ConfigWriterTest {
         ConfigWriter.write(testConfig, testFile, out);
         Assert.assertEquals(contents, out.toString("UTF-8"));
     }
+    
+    @Test
+    public void testSimpleWipe() throws IOException {
+        String contents = readFile(new File("test/src/com/redhat/ceylon/common/test/writer-wiped.config"));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ConfigWriter.write(new CeylonConfig(), testFile, out);
+        Assert.assertEquals(contents, out.toString("UTF-8"));
+    }
+    
+    @Test
+    public void testCRUD() throws IOException {
+        CeylonConfig testConfigCopy = testConfig.copy();
+        testConfigCopy.setOption("test.two", "twee");
+        testConfigCopy.setOption("test.string-hello", "hola");
+        testConfigCopy.setOption("test.string-world", "mundo");
+        testConfigCopy.setOption("test.string-spaces", " con espacios   ");
+        testConfigCopy.setOption("test.string-multiline", "wim\nzus\njet");
+        testConfigCopy.setOption("test.string-multiline-with-spaces", "wim\nzus\njet");
+        testConfigCopy.setOption("test.string-quoted-multiline", "wim\nzus\njet ");
+        testConfigCopy.setOption("test.three", "tres");
+        testConfigCopy.setOptionValues("test.multiple.strings", new String[] {"wim", "zus"});
+        testConfigCopy.setOptionValues("test.section.Aap.foo", new String[] {"1", "2", "3"});
+        testConfigCopy.removeOption("test.section.Noot.foo");
+        testConfigCopy.removeOption("test.section.Mies.foo");
+        testConfigCopy.removeOption("test.section.Mies.fooz");
+        testConfigCopy.setOptionValues("test.section.Mies.bar", new String[] {"test1", "test2"});
+        testConfigCopy.setOptionValues("test.newsection.baz", new String[] {"qed", "qam"});
+        testConfigCopy.setOption("anothersection.buzz", "lightyear");
+        
+        String contents = readFile(new File("test/src/com/redhat/ceylon/common/test/writer-crud.config"));
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        ConfigWriter.write(testConfigCopy, testFile, out);
+        Assert.assertEquals(contents, out.toString("UTF-8"));
+    }
 
     private String readFile(File file) {
-        try{
+        try {
             Reader reader = new FileReader(file);
             StringBuilder strbuf = new StringBuilder();
-            try{
+            try {
                 char[] buf = new char[1024];
                 int read;
-                while((read = reader.read(buf)) > -1)
+                while ((read = reader.read(buf)) > -1) {
                     strbuf.append(buf, 0, read);
-            }finally{
+                }
+            } finally {
                 reader.close();
             }
             return strbuf.toString();
-        }catch(IOException x){
+        } catch (IOException x) {
             throw new RuntimeException(x);
         }
     }
