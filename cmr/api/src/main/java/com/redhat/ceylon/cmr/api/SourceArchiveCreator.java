c2033796bac6881c86f5c0c4ec211662d47e166e
Enrique Zamudio <alterbass@gmail.com>
2012-08-07 12:46:03 -0500

    SourceArchiveCreator, refactored from code in ceylon-compiler
    This component creates the .src archive which contains the source files
    from a compilation process.

---
+++ cmr/api/src/main/java/com/redhat/ceylon/cmr/api/SourceArchiveCreator.java
@@ -0,0 +1,19 @@
+package com.redhat.ceylon.cmr.api;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+
+/** Contract for a component that can create a source archive as a compilation artifact.
+ * 
+ * @author Enrique Zamudio
+ */
+public interface SourceArchiveCreator {
+
+    /** Copy the specified source files into the .src archive, avoiding duplicate entries. */
+    public abstract Set<String> copySourceFiles(Set<String> sources) throws IOException;
+
+    /** Return the root directories that can contain source files. */
+    public abstract Iterable<? extends File> getSourcePaths();
+
+}
---
+++ cmr/impl/src/main/java/com/redhat/ceylon/cmr/impl/SourceArchiveCreatorImpl.java
@@ -0,0 +1,82 @@
+package com.redhat.ceylon.cmr.impl;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
+
+import com.redhat.ceylon.cmr.api.ArtifactContext;
+import com.redhat.ceylon.cmr.api.Logger;
+import com.redhat.ceylon.cmr.api.RepositoryManager;
+import com.redhat.ceylon.cmr.api.SourceArchiveCreator;
+import com.redhat.ceylon.cmr.util.JarUtils;
+
+/** Creates a .src archive in the specified location, containing the
+ * specified source files, avoiding duplicates.
+ * 
+ * @author Enrique Zamudio
+ */
+public class SourceArchiveCreatorImpl implements SourceArchiveCreator {
+
+    private final ArtifactContext srcContext;
+    private final RepositoryManager repoManager;
+    private File originalSrcFile;
+    private File outputSrcFile;
+    private JarOutputStream srcOutputStream;
+    private boolean verbose;
+    private Logger log;
+    private final Iterable<? extends File> sourcePaths;
+
+    public SourceArchiveCreatorImpl(RepositoryManager repoManager, Iterable<? extends File> sourcePaths, String moduleName, String moduleVersion, boolean verbose, Logger log) throws IOException {
+        this.repoManager = repoManager;
+        this.verbose = verbose;
+        this.log = log;
+        this.sourcePaths = sourcePaths;
+        this.srcContext = new ArtifactContext(moduleName, moduleVersion, ArtifactContext.SRC);
+        setupSrcOutput();
+    }
+
+    private void setupSrcOutput() throws IOException {
+        File targetSrcFile = repoManager.getArtifact(srcContext);
+        outputSrcFile = File.createTempFile("src", ".tmp");
+        originalSrcFile = targetSrcFile;
+        srcOutputStream = new JarOutputStream(new FileOutputStream(outputSrcFile));
+    }
+
+    public Set<String> copySourceFiles(Set<String> sources) throws IOException {
+        final Set<String> copiedFiles = new HashSet<String>();
+        for (String prefixedSourceFile : sources) {
+            // must remove the prefix first
+            String sourceFile = JarUtils.toPlatformIndependentPath(sourcePaths, prefixedSourceFile);
+            srcOutputStream.putNextEntry(new ZipEntry(sourceFile));
+            try {
+                InputStream inputStream = new FileInputStream(prefixedSourceFile);
+                try {
+                    JarUtils.copy(inputStream, srcOutputStream);
+                } finally {
+                    inputStream.close();
+                }
+            } finally {
+                srcOutputStream.closeEntry();
+            }
+            copiedFiles.add(sourceFile);
+        }
+        JarUtils.finishUpdatingJar(originalSrcFile, outputSrcFile, srcContext, srcOutputStream, new JarUtils.JarEntryFilter() {
+            @Override
+            public boolean avoid(String entryFullName) {
+                return copiedFiles.contains(entryFullName);
+            }
+        }, repoManager, verbose, log);
+        return copiedFiles;
+    }
+
+    public Iterable<? extends File> getSourcePaths() {
+        return sourcePaths;
+    }
+
+}
