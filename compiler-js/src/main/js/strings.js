0535789909cd2a7f21efdb2e99e0d2768382b555
Enrique Zamudio <alterbass@gmail.com>
2012-08-13 13:50:55 -0500

    separator in String.split is now Iterable|Callable

--- compiler-js/src/main/js/strings.js
+++ compiler-js/src/main/js/strings.js
@@ -293,69 +293,96 @@ function isWhitespace(c) { return c.value in $WS; }
 String$proto.$split = function(sep, discard, group) {
     // shortcut for empty input
     if (this.length === 0) {return Singleton(this);}
-    
+
     if (sep === undefined) {sep = isWhitespace}
     if (discard === undefined) {discard = true}
     if (group === undefined) {group = true}
-    
+
     //TODO: return an iterable which determines the next token on demand
     var tokens = [];
     var tokenBegin = 0;
     var tokenBeginCount = 0;
-    var separator = true;
+    var count = 0;
     var value = this;
+
     function pushToken(tokenEnd) {
         tokens.push(String$(value.substring(tokenBegin, tokenEnd), count-tokenBeginCount));
     }
-    for (var i=0, count=0; i<this.length; ++count) {
-        var j = i;
-        var cp = this.charCodeAt(i++);
-        if ((cp&0xfc00)===0xd800 && i<this.length) {
-            cp = (cp<<10) + this.charCodeAt(i++) - 0x35fdc00;
-        }
-        
-        if (sep(Character(cp))) {
-            if (!group) {
-                // ungrouped separator: store preceding token
-                pushToken(j);
-                if (!discard) {
-                    // store separator as token
-                    tokens.push(String$(this.substring(j, i), 1));
+    if (isOfType(sep, 'ceylon.language.Iterable')) {
+        var sepChars = {}
+        var it = sep.getIterator();
+        var c; while ((c=it.next()) !== $finished) {sepChars[c.value] = true}
+        for (var i=0; i<this.length;) {
+            var j = i;
+            var cp = this.charCodeAt(i++);
+            if ((cp&0xfc00)===0xd800 && i<this.length) {
+                cp = (cp<<10) + this.charCodeAt(i++) - 0x35fdc00;
+            }
+            ++count;
+            if (cp in sepChars) {
+                if (tokenBegin != j) {
+                    pushToken(j);
                 }
-                // next token begins after this character
+                if (!discard) {tokens.push(String$(value.substring(j, i), 1))}
                 tokenBegin = i;
-                tokenBeginCount = count + 1;
-            } else if (!separator || (j == 0)) {
-                // begin of grouped separator: store preceding token
-                pushToken(j);
-                // separator token begins at this character
-                tokenBegin = j;
                 tokenBeginCount = count;
             }
-            separator = true;
-            
-        } else if (separator) {
-            // first non-separator after separators or at beginning
-            if (!discard && (tokenBegin != j)) {
-                // store preceding grouped separator (if group=false then tokenBegin=j)
-                pushToken(j);
+        }
+        if (tokenBegin != i) {
+            pushToken(i);
+        }
+    } else {
+        var separator = true;
+        for (var i=0; i<this.length; ++count) {
+            var j = i;
+            var cp = this.charCodeAt(i++);
+            if ((cp&0xfc00)===0xd800 && i<this.length) {
+                cp = (cp<<10) + this.charCodeAt(i++) - 0x35fdc00;
             }
-            // non-separator token begins at this character
-            tokenBegin = j;
-            tokenBeginCount = count;
-            separator = false;
+
+            if (sep(Character(cp))) {
+                if (!group) {
+                    // ungrouped separator: store preceding token
+                    pushToken(j);
+                    if (!discard) {
+                        // store separator as token
+                        tokens.push(String$(this.substring(j, i), 1));
+                    }
+                    // next token begins after this character
+                    tokenBegin = i;
+                    tokenBeginCount = count + 1;
+                } else if (!separator || (j == 0)) {
+                    // begin of grouped separator: store preceding token
+                    pushToken(j);
+                    // separator token begins at this character
+                    tokenBegin = j;
+                    tokenBeginCount = count;
+                }
+                separator = true;
+
+            } else if (separator) {
+                // first non-separator after separators or at beginning
+                if (!discard && (tokenBegin != j)) {
+                    // store preceding grouped separator (if group=false then tokenBegin=j)
+                    pushToken(j);
+                }
+                // non-separator token begins at this character
+                tokenBegin = j;
+                tokenBeginCount = count;
+                separator = false;
+            }
+        }
+
+        if ((tokenBegin != i) && !(separator && discard)) {
+            // store preceding token (may be a grouped separator)
+            pushToken(i);
+        }
+        if (separator) {
+            // if last character was a separator then there's another empty token
+            tokens.push(String$("", 0));
         }
     }
-    
-    if ((tokenBegin != i) && !(separator && discard)) {
-        // store preceding token (may be a grouped separator)
-        pushToken(i);
-    }
-    if (separator) {
-        // if last character was a separator then there's another empty token
-        tokens.push(String$("", 0));
-    }
-    
+
     this.codePoints = count;
     return ArraySequence(tokens);
 }
