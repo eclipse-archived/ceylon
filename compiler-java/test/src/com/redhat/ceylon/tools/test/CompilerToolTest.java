8403cfd693f4648bbdf2d5c72974a8df63c22784
Tom Bentley <tom.bentley@cantab.net>
2012-08-20 09:39:44 +0100

    Issue #501: Tools: Some simple tests for the CompileTool and DocTool

--- compiler-java/test/src/com/redhat/ceylon/tools/test/CompilerToolTest.java
+++ compiler-java/test/src/com/redhat/ceylon/tools/test/CompilerToolTest.java
@@ -1,11 +1,14 @@
 package com.redhat.ceylon.tools.test;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.redhat.ceylon.ceylondoc.DocTool;
 import com.redhat.ceylon.common.tool.ArgumentParserFactory;
+import com.redhat.ceylon.common.tool.OptionArgumentException;
 import com.redhat.ceylon.common.tool.PluginFactory;
 import com.redhat.ceylon.common.tool.PluginLoader;
 import com.redhat.ceylon.common.tool.PluginModel;
@@ -18,11 +21,34 @@ public class CompilerToolTest {
     protected final PluginLoader pluginLoader = new PluginLoader(apf);
 
     @Test
-    public void testCompileJhelp()  throws Exception {
+    public void testNoModules()  throws Exception {
         PluginModel<CompileTool> model = pluginLoader.loadToolModel("compile");
         Assert.assertNotNull(model);
-        CompileTool tool = pluginFactory.bindArguments(model, Arrays.asList("-jhelp"));
+        try {
+            pluginFactory.bindArguments(model, Collections.<String>emptyList());
+            Assert.fail();
+        } catch (OptionArgumentException e) {
+            Assert.assertEquals("Argument modules should appear at least 1 time(s)", e.getMessage());
+        }
+    }
+    
+    @Test
+    public void testCompile()  throws Exception {
+        PluginModel<CompileTool> model = pluginLoader.loadToolModel("compile");
+        Assert.assertNotNull(model);
+        CompileTool tool = pluginFactory.bindArguments(model, 
+                Arrays.asList("--src=test/src", "com.redhat.ceylon.tools.test.ceylon"));
         tool.run();
     }
+    
+    @Test
+    public void testCompileVerbose()  throws Exception {
+        PluginModel<CompileTool> model = pluginLoader.loadToolModel("compile");
+        Assert.assertNotNull(model);
+        CompileTool tool = pluginFactory.bindArguments(model, 
+                Arrays.asList("--verbose", "--src=test/src", "com.redhat.ceylon.tools.test.ceylon"));
+        tool.run();
+    }
+    
     
 }
