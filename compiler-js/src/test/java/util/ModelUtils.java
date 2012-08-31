061e96d2c152c1e283ef0e9c9612e21d074a9254
Enrique Zamudio <alterbass@gmail.com>
2012-08-30 23:08:18 -0500

    Methods to check for types with their parameters

--- compiler-js/src/test/java/util/ModelUtils.java
+++ compiler-js/src/test/java/util/ModelUtils.java
@@ -37,30 +37,84 @@ public class ModelUtils {
                     "1", parm.get("seq"));
             Assert.assertEquals("Sequenced parameter should be last", params.size()-1, pos);
             Assert.assertEquals("ceylon.language.Iterable", String.format("%s.%s", tmap.get("pkg"), tmap.get("name")));
-            List<Map<String, Object>> pts = (List<Map<String, Object>>)tmap.get("paramtypes");
-            Assert.assertNotNull("Sequenced param of " + method.get("name") + " should have parameter type", tmap);
-            Assert.assertFalse(tmap.isEmpty());
-            tmap = pts.get(0);
-            Assert.assertEquals("typeparam", tmap.get("mt"));
-            Assert.assertEquals("Wrong param type for sequenced param of " + method.get("name"),
-                    type, String.format("%s.%s", tmap.get("pkg"), tmap.get("name")));
+            List<Map<String, Object>> pts = (List<Map<String, Object>>)tmap.get("tparams");
+            checkTypeParameters(pts, type);
         } else {
             Assert.assertNull("Param " + name + " of method " + method.get("name") + " should not be sequenced",
                     parm.get("seq"));
-            Assert.assertEquals("Wrong type for param " + name + " of method " + method.get("name"),
-                    type, String.format("%s.%s", tmap.get("pkg"), tmap.get("name")));
+            checkType(parm, type);
         }
         return tmap;
     }
 
-    /** Check that the map either contains the specified type or is the specified type. */
+    /** Check that the map either contains the specified type or is the specified type.
+     * The type name can be parameterized, i.e. Sequence&lt;String&gt; (but with fully qualified names);
+     * when it's parameterized, the type parameters are checked as well. */
     @SuppressWarnings("unchecked")
-    public static void checkType(Map<String, Object> map, String name, Object parameterTypes) {
+    public static void checkType(Map<String, Object> map, String name) {
         Map<String, Object> tmap = (Map<String, Object>)map.get("type");
         if (tmap == null) {
             tmap = map;
         }
+        int sep = name.indexOf('<');
+        String typeParams = null;
+        if (sep > 0) {
+            typeParams = name.substring(sep+1, name.length()-1);
+            name = name.substring(0, sep);
+        }
         Assert.assertEquals(name, String.format("%s.%s", tmap.get("pkg"), tmap.get("name")));
+        if (typeParams != null) {
+            List<Map<String, Object>> tparms = (List<Map<String, Object>>)tmap.get("tparams");
+            Assert.assertFalse("Type parameters shouldn't be empty", tparms.isEmpty());
+            checkTypeParameters(tparms, typeParams);
+        }
     }
 
+    @SuppressWarnings("unchecked")
+    public static void checkTypeParameters(List<Map<String, Object>> map, String name) {
+        int comma = name.indexOf(',');
+        if (comma > 0) {
+            while (comma > 0 && !pointyBracketsEven(name.substring(0, comma))) {
+                comma = name.indexOf(',', comma+1);
+            }
+            if (comma > 0) {
+                String left = name.substring(0, comma);
+                checkTypeParameters(map, left);
+                left = name.substring(comma+1);
+                checkTypeParameters(map, left);
+                return;
+            }
+            
+        }
+        int lt = name.indexOf('<');
+        if (lt > 0) {
+            String plain = name.substring(0, lt);
+            String parms = name.substring(lt+1, name.length()-1);
+            //Type with parameters
+            for (Map<String, Object> tp : map) {
+                if (plain.equals(String.format("%s.%s", tp.get("pkg"), tp.get("name")))) {
+                    checkTypeParameters((List<Map<String, Object>>)tp.get("tparams"), parms);
+                    return;
+                }
+            }
+            Assert.assertTrue("Missing parameter type " + name, false);
+        } else {
+            //Simple type
+            for (Map<String, Object> tp : map) {
+                if (name.equals(String.format("%s.%s", tp.get("pkg"), tp.get("name")))) {
+                    return;
+                }
+            }
+            Assert.assertTrue("Missing parameter type " + name, false);
+        }
+    }
+
+    private static boolean pointyBracketsEven(String s) {
+        int open = 0, close = 0;
+        for (char c : s.toCharArray()) {
+            if (c=='<') open++;
+            else if (c=='>') close++;
+        }
+        return open == close;
+    }
 }
