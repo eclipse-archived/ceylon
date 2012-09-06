6039f7cc77fcf3767b3f5780103dee49a4cdecfe
Tom Bentley <tom.bentley@cantab.net>
2012-09-06 12:08:26 +0100

    Issue #501: tools: Move the cey scripts from ceylon-common fixing them for the new name of top level tool (CeylonTool, not Tool)

---
+++ compiler-java/bin/cey.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal
+
+call %~dp0\cey-cp.bat %*
+
+if "%exit%" == "true" (
+    exit /b 1
+)
+
+set "LIB=%CEYLON_HOME%\lib"
+set "RUNTIME_REPO=%CEYLON_HOME%\runtime-repo"
+
+JAVA_CP="%LIB%\jboss-modules.jar"
+JAVA_CP="%JAVA_CP%;%LIB%\ceylon-runtime-bootstrap.jar"
+JAVA_CP="%JAVA_CP%;%MINIMAL_CP%"
+
+"%JAVA%" ^
+    -cp $JAVA_CP ^
+    "-Dceylon.home=%CEYLON_HOME%" ^
+    "-Dcelon.runtime.repo=%CEYLON_REPO%" ^
+    com.redhat.ceylon.tools.CeylonTool ^
+    %*
+
+endlocal
