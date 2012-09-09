197c90db69a0f98dcef75ac92e4fa4f90a1acb24
Tako Schotanus <tako@ceylon-lang.org>
2012-09-09 03:13:59 +0200

    Fixed typo in the unix shell file. Renamed "ceylon.runtime.repo" to "ceylon.system.repo".

---
+++ common/bin/cey.bat
@@ -0,0 +1,23 @@
+@echo off
+setlocal
+
+call %~dp0\cey-cp.bat %*
+
+if "%exit%" == "true" (
+    exit /b 1
+)
+
+set "LIB=%CEYLON_HOME%\lib"
+
+JAVA_CP="%LIB%\jboss-modules.jar"
+JAVA_CP="%JAVA_CP%;%LIB%\ceylon-runtime-bootstrap.jar"
+JAVA_CP="%JAVA_CP%;%MINIMAL_CP%"
+
+"%JAVA%" ^
+    -cp $JAVA_CP ^
+    "-Dceylon.home=%CEYLON_HOME%" ^
+    "-Dceylon.system.repo=%CEYLON_REPO%" ^
+    com.redhat.ceylon.common.tool.Tool ^
+    %*
+
+endlocal
