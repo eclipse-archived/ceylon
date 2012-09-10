cbbd18ae1628e17bd9685a77bd43c6f47d638c25
Stephane Epardaud <stef@epardaud.fr>
2012-09-10 18:36:02 +0200

    Test for #752: importing incompatible module

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/bc/BcTests.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/bc/BcTests.java
@@ -20,6 +20,22 @@
 package com.redhat.ceylon.compiler.java.test.bc;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.jar.JarOutputStream;
+import java.util.zip.ZipEntry;
+
+import javax.tools.JavaCompiler;
+import javax.tools.JavaCompiler.CompilationTask;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
 
 import junit.framework.Assert;
 
@@ -27,6 +43,14 @@ import org.junit.Test;
 
 import com.redhat.ceylon.compiler.java.test.CompilerError;
 import com.redhat.ceylon.compiler.java.test.CompilerTest;
+import com.redhat.ceylon.compiler.java.tools.CeyloncFileManager;
+import com.redhat.ceylon.compiler.java.tools.CeyloncTaskImpl;
+import com.redhat.ceylon.compiler.java.tools.CeyloncTool;
+import com.redhat.ceylon.compiler.java.util.Util;
+import com.sun.source.util.JavacTask;
+import com.sun.tools.javac.api.JavacTool;
+import com.sun.tools.javac.file.JavacFileManager;
+import com.sun.tools.javac.file.ZipFileIndexCache;
 
 public class BcTests extends CompilerTest {
     
@@ -116,5 +140,51 @@ public class BcTests extends CompilerTest {
         compile("JavaOldVersion.java");
         assertErrors("CeylonNewVersion", 
                 new CompilerError(-1, "You are using a Ceylon class compiled for an incompatible version of the Ceylon compiler (0.0).\n  This compiler supports 3.0.\n  Please try to recompile your module using a compatible compiler.\n  Binary compatibility will only be supported after Ceylon 1.0.\n  Offending class: com.redhat.ceylon.compiler.java.test.bc.JavaOldVersion"));
+    }
+    
+    @Test
+    public void testBinaryVersionIncompatibleModule() throws IOException {
+        CompilationTask compiler = compileJava("binaryVersionOld/module_.java");
+        Assert.assertTrue(compiler.call());
+        
+        // so now make a jar containing the java module
+        File jarFolder = new File(destDir, "com/redhat/ceylon/compiler/java/test/bc/binaryVersionOld/1/");
+        jarFolder.mkdirs();
+        File jarFile = new File(jarFolder, "com.redhat.ceylon.compiler.java.test.bc.binaryVersionOld-1.jar");
+        // now jar it up
+        JarOutputStream outputStream = new JarOutputStream(new FileOutputStream(jarFile));
+        ZipEntry entry = new ZipEntry("com/redhat/ceylon/compiler/java/test/bc/binaryVersionOld/module_.class");
+        outputStream.putNextEntry(entry);
+        
+        File javaClass = new File(destDir, "com/redhat/ceylon/compiler/java/test/bc/binaryVersionOld/module_.class");
+        FileInputStream inputStream = new FileInputStream(javaClass);
+        Util.copy(inputStream, outputStream);
+        inputStream.close();
+        outputStream.close();
+        
+        assertErrors("binaryVersion/module", new CompilerError(21, "This module was compiled for an incompatible version of the Ceylon compiler (0.0).\n"
+                +"  This compiler supports 3.0.\n"
+                +"  Please try to recompile your module using a compatible compiler.\n"
+                +"  Binary compatibility will only be supported after Ceylon 1.0."));
+    }
+
+    private CompilationTask compileJava(String... sourcePaths) {
+        java.util.List<File> sourceFiles = new ArrayList<File>(sourcePaths.length);
+        for(String file : sourcePaths){
+            sourceFiles.add(new File(getPackagePath(), file));
+        }
+
+        JavaCompiler runCompiler = ToolProvider.getSystemJavaCompiler();
+        StandardJavaFileManager runFileManager = runCompiler.getStandardFileManager(null, null, null);
+
+        // make sure the destination repo exists
+        new File(destDir).mkdirs();
+
+        List<String> options = new LinkedList<String>();
+        options.addAll(Arrays.asList("-sourcepath", getSourcePath(), "-d", destDir));
+        Iterable<? extends JavaFileObject> compilationUnits1 =
+                runFileManager.getJavaFileObjectsFromFiles(sourceFiles);
+        return runCompiler.getTask(null, runFileManager, null, 
+                options, null, compilationUnits1);
     }
 }
---
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/bc/binaryVersionOld/module_.java
@@ -0,0 +1,9 @@
+package com.redhat.ceylon.compiler.java.test.bc.binaryVersionOld;
+
+@com.redhat.ceylon.compiler.java.metadata.Ceylon
+@com.redhat.ceylon.compiler.java.metadata.Module(name = "com.redhat.ceylon.compiler.java.test.bc.binaryVersionOld", version = "1")
+final class module_ {
+    
+    private module_() {
+    }
+}
