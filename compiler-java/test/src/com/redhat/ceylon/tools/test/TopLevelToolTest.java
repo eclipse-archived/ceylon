dcd9e498a3894a31fbdcb2af7e5ae355eb0c8c98
Tom Bentley <tom.bentley@cantab.net>
2012-09-14 14:19:47 +0100

    tools: Add the old TopLevelToolTest, which got lost when CeylonTool was moved from ceylon-common

--- compiler-java/test/src/com/redhat/ceylon/tools/test/TopLevelToolTest.java
+++ compiler-java/test/src/com/redhat/ceylon/tools/test/TopLevelToolTest.java
@@ -1,5 +1,7 @@
 package com.redhat.ceylon.tools.test;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.redhat.ceylon.common.tool.ToolFactory;
@@ -16,29 +18,89 @@ public class TopLevelToolTest {
         return args;
     }
     
+    private CeylonTool tool;
+    
+    @Before
+    public void setup() {
+        tool = new CeylonTool();
+    }
+    
+    
     @Test
     public void testNoArgs()  throws Exception {
-        new CeylonTool().bootstrap(args());
+        Assert.assertEquals(CeylonTool.SC_NO_SUCH_TOOL, tool.bootstrap(args()));
+        Assert.assertEquals("help", tool.getToolName());
     }
     
     @Test
-    public void testVersion()  throws Exception {
-        new CeylonTool().bootstrap(args("--version"));
+    public void testVersionOption()  throws Exception {
+        Assert.assertEquals(CeylonTool.SC_OK, tool.bootstrap(args("--version")));
+        Assert.assertEquals(null, tool.getToolName());
+    }
+    
+    @Test
+    public void testVersionOptionHelp()  throws Exception {
+        // --version beats everything
+        Assert.assertEquals(CeylonTool.SC_OK, tool.bootstrap(args("--version", "help")));
+        Assert.assertEquals(null, tool.getToolName());
+    }
+    
+    @Test
+    public void testHelpVersionOption()  throws Exception {
+        // We expect NO_SUCH_TOOL in this case because the HelpTool doesn't 
+        // exist in ceylon-common
+        Assert.assertEquals(CeylonTool.SC_NO_SUCH_TOOL, tool.bootstrap(args("help", "--version")));
+        Assert.assertEquals("help", tool.getToolName());
     }
     
     @Test
     public void testEmptyArg()  throws Exception {
-        new CeylonTool().bootstrap(args(""));
+        Assert.assertEquals(CeylonTool.SC_NO_SUCH_TOOL, tool.bootstrap(args("")));
+        Assert.assertEquals("help", tool.getToolName());
     }
     
     @Test
     public void testHelp()  throws Exception {
-        new CeylonTool().bootstrap(args("help"));
+        Assert.assertEquals(CeylonTool.SC_NO_SUCH_TOOL, tool.bootstrap(args("help")));
+        Assert.assertEquals("help", tool.getToolName());
     }
     
     @Test
     public void testHelpOption()  throws Exception {
-        new CeylonTool().bootstrap(args("--help"));
+        Assert.assertEquals(CeylonTool.SC_NO_SUCH_TOOL, tool.bootstrap(args("--help")));
+        Assert.assertEquals("help", tool.getToolName());
     }
+    
+    @Test
+    public void testExample()  throws Exception {
+        Assert.assertEquals(CeylonTool.SC_OK, tool.bootstrap(args("example")));
+        Assert.assertEquals("example", tool.getToolName());
+    }
+    
+    @Test
+    public void testStacktracesOptionExample()  throws Exception {
+        Assert.assertEquals(CeylonTool.SC_OK, tool.bootstrap(args("--stacktraces", "example")));
+        Assert.assertEquals("example", tool.getToolName());
+        Assert.assertTrue(tool.getStacktraces());
+    }
+    
+    @Test
+    public void testFileOptionExample()  throws Exception {
+        // the top level tool doesn't take a --file option
+        Assert.assertEquals(CeylonTool.SC_ARGS, tool.bootstrap(args("--file=.", "example")));
+    }
+    
+    @Test
+    public void testExampleFileOption()  throws Exception {
+        // the top level tool doesn't take a --file option
+        Assert.assertEquals(CeylonTool.SC_OK, tool.bootstrap(args("example", "--file=.")));
+    }
+    
+    @Test
+    public void testBashCompletion()  throws Exception {
+        Assert.assertEquals(CeylonTool.SC_OK, tool.bootstrap(
+                args("bash-completion", "--cword=1", "--", "./cey")));
+    }
+    
     
 }
