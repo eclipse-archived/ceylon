1968d8e57a8bac0943bbb3ae462ff5942baf4681
Tom Bentley <tom.bentley@cantab.net>
2012-10-01 12:30:02 +0100

    Fix CompileJsToolTest due to changes in the tool API

--- compiler-js/src/test/java/com/redhat/ceylon/compiler/js/CompileJsToolTest.java
+++ compiler-js/src/test/java/com/redhat/ceylon/compiler/js/CompileJsToolTest.java
@@ -3,22 +3,23 @@ package com.redhat.ceylon.compiler.js;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-import com.redhat.ceylon.common.tool.ArgumentParserFactory;
+import com.redhat.ceylon.common.tool.ServiceToolLoader;
+import com.redhat.ceylon.common.tool.Tool;
 import com.redhat.ceylon.common.tool.ToolFactory;
 import com.redhat.ceylon.common.tool.ToolLoader;
 import com.redhat.ceylon.common.tool.ToolModel;
 
 public class CompileJsToolTest {
 
-    protected final ArgumentParserFactory apf = new ArgumentParserFactory();
-    protected final ToolFactory pluginFactory = new ToolFactory(apf);
-    protected final ToolLoader pluginLoader = new ToolLoader(apf){
-
+    protected final ToolFactory pluginFactory = new ToolFactory();
+    protected final ToolLoader pluginLoader = new ServiceToolLoader(Tool.class) {
+        
         @Override
-        protected String getToolName(String className) {
-            return camelCaseToDashes(className.replaceAll("^(.*\\.)?Ceylon(.*)Tool$", "$2"));    
+        public String getToolName(String className) {
+            return camelCaseToDashes(className.replaceAll("^(.*\\.)?Ceylon(.*)Tool$", "$2"));
         }
     };
 
@@ -29,6 +30,7 @@ public class CompileJsToolTest {
     @Test
     public void testLoad() {
         ToolModel<CeylonCompileJsTool> tool = pluginLoader.loadToolModel("compile-js");
+        Assert.assertNotNull(tool);
         pluginFactory.bindArguments(tool, args());
     }
     
