f291a21f9a9ec465ef37ca61f5ed6bd6c823a034
Tako Schotanus <tako@ceylon-lang.org>
2012-10-03 21:55:00 +0200

    Improved className() a bit

--- language/runtime/ceylon/language/className_.java
+++ language/runtime/ceylon/language/className_.java
@@ -15,16 +15,27 @@ public final class className_ {
     @TypeInfo("ceylon.language.Object")
     final java.lang.Object object) {
     	//TODO: type args?
-        java.lang.String name = object.getClass().getName();
-        int i = name.indexOf("$");
-        if (i>0) {
-            name = name.substring(0, i) + 
-                    name.substring(i+1);
+        Name annot = object.getClass().getAnnotation(Name.class);
+        if (annot != null) {
+            if (object.getClass().getPackage() != null) {
+                return object.getClass().getPackage().getName() + "." + annot.value();
+            } else {
+                return annot.value();
+            }
+        } else {
+            java.lang.String name = object.getClass().getName();
+            // FIXME The next part really shouldn't be necessary as long as we add @Name
+            // annotations to all classes with mangled names
+            int i = name.indexOf("$");
+            if (i>0) {
+                name = name.substring(0, i) + 
+                        name.substring(i+1);
+            }
+            if (name.endsWith("_") 
+                    && object.getClass().getAnnotation(Ceylon.class) != null) {
+                name = name.substring(0, name.length()-1);
+            }
+            return name;
         }
-        if (name.endsWith("_") 
-                && object.getClass().getAnnotation(Ceylon.class) != null) {
-            name = name.substring(0, name.length()-1);
-        }
-        return name;
     }
 }
