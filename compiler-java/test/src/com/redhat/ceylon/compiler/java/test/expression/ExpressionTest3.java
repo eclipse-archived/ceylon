41cc735145615e89e6290fa1a402d629b526e389
Tom Bentley <tom.bentley@cantab.net>
2012-10-11 15:42:22 +0100

    Conditions: Copy the 'if(...)' tests, modifying them to create equivalent 'for (...) if (...)' tests

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/expression/ExpressionTest3.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/expression/ExpressionTest3.java
@@ -6,6 +6,11 @@ import com.redhat.ceylon.compiler.java.test.CompilerTest;
 
 public class ExpressionTest3 extends CompilerTest {
 
+    @Override
+    protected String transformDestDir(String name) {
+        return name + "-3";
+    }
+    
     @Test
     public void testComprehensions1() {
         compareWithJavaSource("comprehensions/comp1");
@@ -55,6 +60,114 @@ public class ExpressionTest3 extends CompilerTest {
     public void testComprehensionNonempty() {
         compareWithJavaSource("comprehensions/nonempty_cond");
     }
+    
+    @Test
+    public void testComprehensionForIfExists(){
+        compareWithJavaSource("comprehensions/ForIfExists");
+    }
+    
+    @Test
+    public void testComprehensionForIfExistsSequence(){
+        compareWithJavaSource("comprehensions/ForIfExistsSequence");
+    }
+    
+    @Test
+    public void testComprehensionForIfExistsWithMethod(){
+        compareWithJavaSource("comprehensions/ForIfExistsWithMethod");
+    }
+    
+    @Test
+    public void testComprehensionForIfIsFoo(){
+        compile("comprehensions/FooBar.ceylon");
+        compareWithJavaSource("comprehensions/ForIfIsFoo");
+    }
+    
+    @Test
+    public void testComprehensionForIfIsNotNothing(){
+        compareWithJavaSource("comprehensions/ForIfIsNotNothing");
+    }
+    
+    @Test
+    public void testComprehensionForIfIsNotObject(){
+        compareWithJavaSource("comprehensions/ForIfIsNotObject");
+    }
+    
+    @Test
+    public void testComprehensionForIfIsNullUnion(){
+        compareWithJavaSource("comprehensions/ForIfIsNullUnion");
+    }
+    
+    @Test
+    public void testComprehensionForIfIsWithIntersection(){
+        compile("comprehensions/FooBar.ceylon");
+        compareWithJavaSource("comprehensions/ForIfIsWithIntersection");
+    }
+    
+    @Test
+    public void testComprehensionForIfIsWithMethod(){
+        compile("comprehensions/FooBar.ceylon");
+        compareWithJavaSource("comprehensions/ForIfIsWithMethod");
+    }
+
+    @Test
+    public void testComprehensionForIfIsWithUnion(){
+        compile("comprehensions/FooBar.ceylon");
+        compareWithJavaSource("comprehensions/ForIfIsWithUnion");
+    }
+    
+    @Test
+    public void testComprehensionForIfIsNothing2(){
+        compareWithJavaSource("comprehensions/ForIfIsNothing2");
+    }
+    
+    @Test
+    public void testComprehensionForIfIsNothing(){
+        compareWithJavaSource("comprehensions/ForIfIsNothing");
+    }
+
+    @Test
+    public void testComprehensionForIfNonEmptySequence(){
+        compareWithJavaSource("comprehensions/ForIfNonEmptySequence");
+    }
+    
+    @Test
+    public void testComprehensionForIfNonEmptyString(){
+        compareWithJavaSource("comprehensions/ForIfNonEmptyString");
+    }
+    
+    @Test
+    public void testComprehensionForIfConditionListBoolBool(){
+        compareWithJavaSource("comprehensions/ForIfConditionListBoolBool");
+    }
+    @Test
+    public void testComprehensionForIfConditionListIsIs(){
+        compareWithJavaSource("comprehensions/ForIfConditionListIsIs");
+    }
+    @Test
+    public void testComprehensionForIfConditionListBoolBoolIs(){
+        compareWithJavaSource("comprehensions/ForIfConditionListBoolBoolIs");
+    }
+    @Test
+    public void testComprehensionForIfConditionListBoolIsBool(){
+        compareWithJavaSource("comprehensions/ForIfConditionListBoolIsBool");
+    }
+    @Test
+    public void testComprehensionForIfConditionListExistsIsBool(){
+        compareWithJavaSource("comprehensions/ForIfConditionListExistsIsBool");
+    }
+    @Test
+    public void testComprehensionForIfConditionListIsBool(){
+        compareWithJavaSource("comprehensions/ForIfConditionListIsBool");
+    }
+    @Test
+    public void testComprehensionForIfConditionListIsBoolBool(){
+        compareWithJavaSource("comprehensions/ForIfConditionListIsBoolBool");
+    }
+    @Test
+    public void testComprehensionForIfConditionListNonemptyIsBool(){
+        compareWithJavaSource("comprehensions/ForIfConditionListNonemptyIsBool");
+    }
+
 
     @Test
     public void testComprehensionExists() {
---
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/expression/comprehensions/ForIfIsNotWithUnionOfTwo.ceylon
@@ -0,0 +1,35 @@
+/*
+ * Copyright Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the authors tag. All rights reserved.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU General Public License version 2.
+ * 
+ * This particular file is subject to the "Classpath" exception as provided in the 
+ * LICENSE file that accompanied this code.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License,
+ * along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+@nomodel
+class ForIfIsNotWithUnionOfTwo() {
+    shared void m(Sequence<String|Integer> xs) {
+        print({ for (x in xs) if (!is Integer x) x.reversed });
+        print({ for (x in xs) if (!is String x) x-2 });
+    }
+    shared void mvar(Sequence<String|Integer> xs) {
+        print({ for (x in xs) if (!is Integer s=x) x.reversed });
+        print({ for (x in xs) if (!is String s=x) s-2 });
+    }
+    
+    shared void m2(Sequence<String|Integer> xs) {
+        print({ for (x in xs) if (!is Integer x, x.size==1) aint1.uppercased });
+        print({ for (x in xs) if (!is String x, x>0) aint2/2 });
+    }
+}
