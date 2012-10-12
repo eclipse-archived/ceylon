c1161efcbf535cc7059b08544f0c1a4068558853
Tom Bentley <tom.bentley@cantab.net>
2012-10-12 13:57:27 +0100

    Issue #804: Fix a call site

--- compiler-java/src/com/redhat/ceylon/compiler/java/codegen/ClassTransformer.java
+++ compiler-java/src/com/redhat/ceylon/compiler/java/codegen/ClassTransformer.java
@@ -644,7 +644,7 @@ public class ClassTransformer extends AbstractTransformer {
     }
 
     private Boolean hasImpl(Interface iface) {
-        if (gen().willEraseToObjectOrList(iface.getType())) {
+        if (gen().willEraseToObject(iface.getType())) {
             return false;
         }
         if (iface instanceof LazyInterface) {
