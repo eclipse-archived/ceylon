a8d7cd438111af597e1623d627270cba79bdefee
Ivo Kasiuk <i.kasiuk@gmail.com>
2012-10-13 14:16:23 +0200

    fix #2563 by relying on "captured" flag instead of analyzing scopes

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/EnclosingFunctionVisitor.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/EnclosingFunctionVisitor.java
@@ -1,9 +1,8 @@
 package com.redhat.ceylon.compiler.js;
 
-import com.redhat.ceylon.compiler.typechecker.model.Scope;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.BaseMemberExpression;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.Block;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.MethodDefinition;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.Declaration;
+import com.redhat.ceylon.compiler.typechecker.tree.Tree.Statement;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 
 /** This visitor is used to check if a block of code contains method definitions which use expressions
@@ -13,37 +12,17 @@ import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
  */
 public class EnclosingFunctionVisitor extends Visitor {
 
-    private Scope mainScope;
-    private boolean check;
-    private boolean enclose;
-
     /** Determines whether the specified block should be enclosed in a function. */
     public boolean encloseBlock(Block block) {
-        enclose = false;
-        check = false;
-        if (block.getStatements().isEmpty()) {
-            return false;
-        } else {
-            mainScope = block.getStatements().get(0).getScope();
+        // just check if the block contains a captured declaration
+        for (Statement statement : block.getStatements()) {
+            if (statement instanceof Declaration) {
+                if (((Declaration) statement).getDeclarationModel().isCaptured()) {
+                    return true;
+                }
+            }
         }
-        block.visit(this);
-        mainScope = null;
-        return enclose;
-    }
-
-    /** Enables checking of base member expressions. */
-    public void visit(MethodDefinition that) {
-        check = true;
-        super.visit(that);
-    }
-
-    /** Checks if the expression is used in a scope different from the main one, and if the declaration
-     * of this expression is in the main scope. */
-    public void visit(BaseMemberExpression that) {
-        if (check && !enclose) {
-            enclose = that.getScope()!=mainScope && that.getDeclaration().getContainer()==mainScope;
-        }
-        super.visit(that);
+        return false;
     }
 
 }
