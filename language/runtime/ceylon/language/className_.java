a422ae07e5972ca2098ce1ab9a24cc32f816900a
Enrique Zamudio <alterbass@gmail.com>
2012-10-17 11:15:26 -0500

    Work on #5030 - works now except for Callables which return "::"

--- language/runtime/ceylon/language/className_.java
+++ language/runtime/ceylon/language/className_.java
@@ -18,24 +18,30 @@ public final class className_ {
         Name annot = object.getClass().getAnnotation(Name.class);
         if (annot != null) {
             if (object.getClass().getPackage() != null) {
-                return object.getClass().getPackage().getName() + "." + annot.value();
+                return object.getClass().getPackage().getName() + "::" + annot.value();
             } else {
                 return annot.value();
             }
         } else {
-            java.lang.String name = object.getClass().getName();
+            Ceylon ann = object.getClass().getAnnotation(Ceylon.class);
+            java.lang.String pkg = object.getClass().getPackage() == null ? ""
+                    : mangle(object.getClass().getPackage().getName(), ann);
+            java.lang.String name = mangle(object.getClass().getSimpleName(), ann);
             // FIXME The next part really shouldn't be necessary as long as we add @Name
             // annotations to all classes with mangled names
-            int i = name.indexOf("$");
-            if (i>0) {
-                name = name.substring(0, i) + 
-                        name.substring(i+1);
-            }
-            if (name.endsWith("_") 
-                    && object.getClass().getAnnotation(Ceylon.class) != null) {
-                name = name.substring(0, name.length()-1);
-            }
-            return name;
+            return pkg + "::" + name;
         }
     }
+
+    private final static java.lang.String mangle(java.lang.String s, Ceylon ann) {
+        int i = s.indexOf("$");
+        if (i>0) {
+            s = s.substring(0, i) + s.substring(i+1);
+        }
+        if (s.endsWith("_") && ann != null) {
+            s = s.substring(0, s.length()-1);
+        }
+        return s;
+    }
+
 }
