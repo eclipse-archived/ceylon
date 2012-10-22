3bb0963cf423d376dca957db237bb8b5baaad520
Enrique Zamudio <alterbass@gmail.com>
2012-10-22 14:07:57 -0500

    Use Java7Checker from ceylon-common

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/Main5.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/Main5.java
@@ -1,5 +1,7 @@
 package com.redhat.ceylon.compiler.js;
 
+import com.redhat.ceylon.common.tool.Java7Checker;
+
 public class Main5 {
 	public static void main(String[] args) throws Exception{
 		Java7Checker.check();
