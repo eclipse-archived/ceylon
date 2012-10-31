b1c18916433b3b140c237d157f7e9166fd6dc5a4
Tako Schotanus <tako@ceylon-lang.org>
2012-10-31 23:50:51 +0100

    Updated versions to 0.5

--- common/src/com/redhat/ceylon/common/Versions.java
+++ common/src/com/redhat/ceylon/common/Versions.java
@@ -2,11 +2,14 @@ package com.redhat.ceylon.common;
 
 public class Versions {
     
+    // The current version is Ceylon 0.5 "Zaphod Beeblebrox"
+    // This comment is here so this file will show up in searches for the current version number
+    
     public static final int CEYLON_VERSION_MAJOR = 0;
-    public static final int CEYLON_VERSION_MINOR = 4;
+    public static final int CEYLON_VERSION_MINOR = 5;
     public static final int CEYLON_VERSION_RELEASE = 0;
     public static final String CEYLON_VERSION_NUMBER = CEYLON_VERSION_MAJOR + "." + CEYLON_VERSION_MINOR /* + "." + CEYLON_VERSION_RELEASE */;
-    public static final String CEYLON_VERSION_NAME = "Analytical Engine";
+    public static final String CEYLON_VERSION_NAME = "Zaphod Beeblebrox";
     public static final String CEYLON_VERSION = CEYLON_VERSION_NUMBER + " (" + CEYLON_VERSION_NAME + ")";
 
     /**
