f1acfe6c55f36f6ec2430e90a48b49ccf44f8cdb
Enrique Zamudio <alterbass@gmail.com>
2012-10-31 18:56:12 -0600

    Update version number to 0.5

--- compiler-js/src/main/js/process.js
+++ compiler-js/src/main/js/process.js
@@ -14,7 +14,7 @@ function languageClass() {
 initTypeProto(languageClass, "ceylon.language::language", IdentifiableObject);
 var lang$proto=languageClass.$$.prototype;
 lang$proto.getVersion=function() {
-    return String$("0.4",3);
+    return String$("0.5",3);
 }
 lang$proto.getMajorVersion=function() { return 0; }
 lang$proto.getMinorVersion=function() { return 4; }
