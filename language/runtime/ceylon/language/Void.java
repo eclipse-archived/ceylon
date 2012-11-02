d2363d5687b84a07390511c6a0f3b2d54b07485d
Gavin King <gavin@hibernate.org>
2012-11-02 14:44:31 +0100

    add case types to Void

--- language/runtime/ceylon/language/Void.java
+++ language/runtime/ceylon/language/Void.java
@@ -1,9 +1,11 @@
 package ceylon.language;
 
+import com.redhat.ceylon.compiler.java.metadata.CaseTypes;
 import com.redhat.ceylon.compiler.java.metadata.Ceylon;
 import com.redhat.ceylon.compiler.java.metadata.Class;
 
 @Ceylon(major = 3)
 @Class(extendsType="")
+@CaseTypes({"ceylon.language::Nothing", "ceylon.language::Object"})
 public abstract class Void {
 }
