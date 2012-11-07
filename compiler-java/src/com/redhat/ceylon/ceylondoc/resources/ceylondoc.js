15dcae920c6c20fefa23032a7f68e9401b38c8ab
thradec <thradec@gmail.com>
2012-11-07 19:44:34 +0100

    ceylondoc: easter egg restored

--- compiler-java/src/com/redhat/ceylon/ceylondoc/resources/ceylondoc.js
+++ compiler-java/src/com/redhat/ceylon/ceylondoc/resources/ceylondoc.js
@@ -526,3 +526,20 @@ function getUrlVars() {
 	}
 	return vars;
 }
+
+
+/*
+ * easter egg
+ */
+var konami = 0;
+var konamiCode = [38,38,40,40,37,39,37,39,66,65,13];
+jQuery("html").keydown(function (event){
+    var evt = event || window.event;
+    var keyCode = evt.keyCode || evt.which;
+    if(keyCode == konamiCode[konami]){
+        konami++;
+        if(konami == konamiCode.length)
+            document.location = "http://ceylon-lang.org/easter-egg/";
+    }else
+        konami = 0;
+});
