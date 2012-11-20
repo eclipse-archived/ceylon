1082e9e420cd09d987bd2be9d8667c2bf7f5f495
Stephane Epardaud <stef@epardaud.fr>
2012-11-20 10:45:05 +0100

    Declaration: made equals faster by adding fast cases and comparing the qualified name rather than the container

--- typechecker/src/com/redhat/ceylon/compiler/typechecker/model/Declaration.java
+++ typechecker/src/com/redhat/ceylon/compiler/typechecker/model/Declaration.java
@@ -224,13 +224,17 @@ public abstract class Declaration
 
     @Override
     public boolean equals(Object object) {
+        if(this == object)
+            return true;
+        if(object == null || object.getClass() != getClass())
+            return false;
         if (object instanceof Declaration) {
             Declaration that = (Declaration) object;
-            return this==that || getName()!=null && that.getName()!=null &&
-                    that.getName().equals(getName()) &&
-                    that.getDeclarationKind()==getDeclarationKind() &&
-                    (getContainer()==null && that.getContainer()==null ||
-                            that.getContainer().equals(getContainer())); 
+            String myName = getQualifiedNameString();
+            String otherName = that.getQualifiedNameString();
+            return myName != null && otherName != null &&
+                    myName.equals(otherName) &&
+                    that.getDeclarationKind()==getDeclarationKind();
         }
         else {
             return false;
