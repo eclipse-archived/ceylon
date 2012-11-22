7d671f501d1f42b6702e08effa74a3d37eb3e551
Tako Schotanus <tako@ceylon-lang.org>
2012-11-22 02:00:01 +0100

    Fixed annotations for Java implementation of emptyIterator

--- language/runtime/ceylon/language/emptyIterator_.java
+++ language/runtime/ceylon/language/emptyIterator_.java
@@ -2,9 +2,11 @@ package ceylon.language;
 
 import com.redhat.ceylon.compiler.java.metadata.Ceylon;
 import com.redhat.ceylon.compiler.java.metadata.Object;
+import com.redhat.ceylon.compiler.java.metadata.SatisfiedTypes;
 
-@Ceylon(major = 3) @Object
-//TODO: extends Iterator<Bottom>
+@Ceylon(major = 3)
+@Object
+@SatisfiedTypes("ceylon.language::Iterator<ceylon.language::Bottom>")
 public class emptyIterator_ implements Iterator {
     
     private final static emptyIterator_ emptyIterator = new emptyIterator_();
