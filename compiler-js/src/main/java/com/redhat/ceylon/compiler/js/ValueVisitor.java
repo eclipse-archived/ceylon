e9fd7a30ea3dd7580e9d389fc67435564fc87661
Ivo Kasiuk <i.kasiuk@gmail.com>
2012-12-04 21:01:56 +0100

    Oops

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/ValueVisitor.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/ValueVisitor.java
@@ -6,14 +6,8 @@ import com.redhat.ceylon.compiler.typechecker.model.Setter;
 import com.redhat.ceylon.compiler.typechecker.model.TypedDeclaration;
 import com.redhat.ceylon.compiler.typechecker.model.Value;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.AttributeDeclaration;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree.LazySpecifierExpression;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.SpecifierExpression;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.SpecifierOrInitializerExpression;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.SpecifierStatement;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.MethodDeclaration;
-import com.redhat.ceylon.compiler.typechecker.tree.Tree.ParameterizedExpression;
 
 /**
  * Determines if a value is "captured" by 
@@ -134,12 +128,6 @@ public class ValueVisitor extends Visitor {
         boolean cs = enterCapturingScope();
         super.visit(that);
         exitCapturingScope(cs);
-    }
-    
-    @Override
-    public void visit(ParameterizedExpression that) {
-        // TODO Auto-generated method stub
-        super.visit(that);
     }
     
     @Override public void visit(Tree.ObjectArgument that) {
