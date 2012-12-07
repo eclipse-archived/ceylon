bad48ff9b726990301b4e7480523ba26438c09a3
Tako Schotanus <tako@ceylon-lang.org>
2012-12-07 13:47:51 +0100

    Made Iterator companion class more like it would be when generated by the compiler

--- language/runtime/ceylon/language/Iterator$impl.java
+++ language/runtime/ceylon/language/Iterator$impl.java
@@ -4,5 +4,8 @@ import com.redhat.ceylon.compiler.java.metadata.Ignore;
 
 @Ignore
 public class Iterator$impl<Element> {
-    public Iterator$impl(Iterator<Element> $this) {}
+    public Iterator$impl(Iterator<? extends Element> $this) {
+        this.$this = $this;
+    }
+    private final Iterator<? extends Element> $this;
 }
