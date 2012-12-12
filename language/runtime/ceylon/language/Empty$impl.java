245883717a1b0b71e11617420dcfd93118a67f5f
Tako Schotanus <tako@ceylon-lang.org>
2012-12-12 14:50:03 +0100

    Fixed Java implementation of `Empty`

--- language/runtime/ceylon/language/Empty$impl.java
+++ language/runtime/ceylon/language/Empty$impl.java
@@ -10,142 +10,140 @@ public final class Empty$impl {
         this.$this = $this;
     }
 
-    public long getSize() {
-        return _getSize($this);
-    }
-    private static long _getSize(Empty $this){
-        return 0;
-    }
-
-    public Empty getReversed() {
-        return _getReversed($this);
-    }
-    private static Empty _getReversed(Empty $this){
-        return $this;
-    }
-
     @SuppressWarnings("rawtypes")
     public Iterator getIterator() {
-        return _getIterator($this);
-    }
-    @SuppressWarnings("rawtypes")
-    private static Iterator _getIterator(Empty $this){
         return emptyIterator_.getEmptyIterator$();
     }
 
     public java.lang.Object item(java.lang.Object key){
-        return _item($this, key);
-    }
-    private static java.lang.Object _item(Empty $this, java.lang.Object key){
         return null;
     }
 
     public Empty segment(Integer from, long length) {
-        return _segment($this, from, length);
-    }
-    private static Empty _segment(Empty $this, Integer from, long length) {
         return $this;
     }
 
     public Empty span(Integer from, Integer to) {
-        return _span($this, from, to);
-    }
-    private static Empty _span(Empty $this, Integer from, Integer to) {
         return $this;
     }
 
     public Empty spanTo(Integer to) {
-        return _spanTo($this, to);
-    }
-    private static Empty _spanTo(Empty $this, Integer to) {
         return $this;
     }
 
     public Empty spanFrom(Integer from) {
-        return _spanFrom($this, from);
-    }
-    private static Empty _spanFrom(Empty $this, Integer from) {
         return $this;
     }
 
-    public long count(java.lang.Object element) {
-        return _count($this, element);
-    }
-    private static long _count(Empty $this, java.lang.Object element) {
+    public long getSize() {
         return 0;
     }
 
-    public boolean contains(java.lang.Object element) {
-        return _contains($this, element);
-    }
-    private static boolean _contains(Empty $this, java.lang.Object element) {
-        return false;
-    }
-
-    public boolean defines(Integer key) {
-        return _defines($this, key);
-    }
-    private static boolean _defines(Empty $this, Integer key) {
-        return false;
-    }
-
-    public Empty getClone() {
-        return _getClone($this);
-    }
-    private static Empty _getClone(Empty $this) {
+    public Empty getReversed() {
         return $this;
     }
 
     public Empty getSequence() {
-        return _getSequence($this);
-    }
-    private static Empty _getSequence(Empty $this) {
         return $this;
     }
 
     public java.lang.String toString() {
-        return _toString($this);
-    }
-    private static java.lang.String _toString(Empty $this) {
         return "{}";
     }
 
     public Integer getLastIndex(){
-        return _getLastIndex($this);
-    }
-    private static Integer _getLastIndex(Empty $this){
         return null;
     }
 
     public Integer getFirst(){
-        return _getFirst($this);
-    }
-    private static Integer _getFirst(Empty $this){
         return null;
     }
 
     public Integer getLast(){
-        return _getLast($this);
-    }
-    private static Integer _getLast(Empty $this){
         return null;
     }
 
+    public Empty getClone() {
+        return $this;
+    }
+
+    public Empty getCoalesced() {
+        return $this;
+    }
+
+    public boolean contains(java.lang.Object element) {
+        return false;
+    }
+
+    public long count(java.lang.Object element) {
+        return 0;
+    }
+    
+    public long count(Callable<? extends Boolean> f) {
+        return 0;
+    }
+
+    public boolean defines(Integer key) {
+        return false;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <Result> Iterable<Result> map(Callable<Result> collecting) {
+        return (Iterable<Result>) $this;
+    }
+    
+    public Empty filter(Callable<? extends Boolean> selecting) {
+        return $this;
+    }
+
+    public <Result> Result fold(Result initial, Callable<? extends Result> accumulating) {
+        return initial;
+    }
+
+    public java.lang.Object find(Callable<? extends Boolean> selecting) {
+        return null;
+    }
+
+    public Empty sort(Callable<? extends Comparison> comparing) {
+        return $this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public <Result> Sequential<? extends Result> collect(Callable<? extends Result> collecting) {
+        return (Sequential<? extends Result>) $this;
+    }
+    
+    public Empty select(Callable<? extends Boolean> selecting) {
+        return $this;
+    }
+
+    public boolean any(Callable<? extends Boolean> selecting) {
+        return false;
+    }
+
+    public boolean every(Callable<? extends Boolean> selecting) {
+        return false;
+    }
+
+    public Empty skipping(long skip) {
+        return $this;
+    }
+
+    public Empty taking(long take) {
+        return $this;
+    }
+
+    public Empty by(long step) {
+        return $this;
+    }
+    
     @SuppressWarnings({"rawtypes", "unchecked"})
     public <Other> Sequence withLeading(Other e) {
         return new ArraySequence<Other>(e);
     }
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    private static <Element,Other> Sequence _withLeading(Empty orig, Other e) {
-        return new ArraySequence<Other>(e);
-    }
+    
     @SuppressWarnings({"rawtypes", "unchecked"})
     public <Other> Sequence withTrailing(Other e) {
         return new ArraySequence<Other>(e);
     }
-    @SuppressWarnings({"rawtypes", "unchecked"})
-    private static <Element,Other> Sequence _withTrailing(Empty orig, Other e) {
-        return new ArraySequence<Other>(e);
-    }
-
 }
--- language/runtime/ceylon/language/Empty.java
+++ language/runtime/ceylon/language/Empty.java
@@ -6,6 +6,8 @@ import com.redhat.ceylon.compiler.java.metadata.Ceylon;
 import com.redhat.ceylon.compiler.java.metadata.Name;
 import com.redhat.ceylon.compiler.java.metadata.SatisfiedTypes;
 import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
+import com.redhat.ceylon.compiler.java.metadata.TypeParameter;
+import com.redhat.ceylon.compiler.java.metadata.TypeParameters;
 
 @Ceylon(major = 3)
 @SatisfiedTypes({
@@ -16,14 +18,11 @@ import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
 })
 public interface Empty 
         extends Sequential<java.lang.Object>, None<java.lang.Object> {
-	
-    @Annotations(@Annotation("actual"))
-    @Override
-    public long getSize(); 
     
     @Annotations(@Annotation("actual"))
     @Override
     @TypeInfo("ceylon.language::Iterator<ceylon.language::Bottom>")
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     public Iterator getIterator();
     
     @Annotations(@Annotation("actual"))
@@ -51,14 +50,26 @@ public interface Empty
     @Annotations(@Annotation("actual"))
     @Override
     @TypeInfo("ceylon.language::Empty")
+    public Empty spanTo(@Name("to") @TypeInfo("ceylon.language::Integer")
+    Integer to);
+    
+    @Annotations(@Annotation("actual"))
+    @Override
+    @TypeInfo("ceylon.language::Empty")
     public Empty spanFrom(@Name("from") @TypeInfo("ceylon.language::Integer")
     Integer from);
     
     @Annotations(@Annotation("actual"))
     @Override
-    @TypeInfo("ceylon.language::Empty")
-    public Empty spanTo(@Name("to") @TypeInfo("ceylon.language::Integer")
-    Integer to);
+    public long getSize(); 
+
+    @Annotations({@Annotation("actual")})
+    @Override
+    public Empty getReversed();
+    
+    @Annotations({@Annotation("actual")})
+    @Override
+    public Empty getSequence();
 
     @Annotations(@Annotation("actual"))
     @Override
@@ -78,18 +89,14 @@ public interface Empty
     @Override
     @TypeInfo("ceylon.language.Nothing")
     public java.lang.Object getLast();
-
-    @Annotations({@Annotation("actual")})
-    @Override
-    public Empty getReversed();
     
     @Annotations(@Annotation("actual"))
     @Override
     public Empty getClone();
     
-    @Annotations({@Annotation("actual")})
+    @Annotations(@Annotation("actual"))
     @Override
-    public Empty getSequence();
+    public Empty getCoalesced();
     
     @Annotations(@Annotation("actual"))
     @Override
@@ -97,8 +104,105 @@ public interface Empty
     java.lang.Object element);
 
     @Annotations(@Annotation("actual"))
+    @TypeInfo("ceylon.language::Integer")
+    public long count(@Name("selecting")
+        @TypeInfo("ceylon.language::Callable<ceylon.language::Boolean,ceylon.language::Tuple<ceylon.language::Bottom,ceylon.language::Bottom,ceylon.language::Empty>>")
+        Callable<? extends Boolean> selecting);
+
+    @Annotations(@Annotation("actual"))
     @Override
     public boolean defines(@Name("key") @TypeInfo("ceylon.language::Integer")
     Integer key);
+    
+    @Annotations(@Annotation("actual"))
+    @TypeInfo("ceylon.language::Empty")
+    public <Result> Iterable<? extends Result> map(@Name("collecting")
+        @TypeInfo("ceylon.language::Callable<Result,ceylon.language::Tuple<ceylon.language::Bottom,ceylon.language::Bottom,ceylon.language::Empty>>")
+        Callable<? extends Result> collecting);
+    
+    @Annotations(@Annotation("actual"))
+    @TypeInfo("ceylon.language::Empty")
+    public Empty filter(@Name("selecting")
+        @TypeInfo("ceylon.language::Callable<ceylon.language::Boolean,ceylon.language::Tuple<ceylon.language::Bottom,ceylon.language::Bottom,ceylon.language::Empty>>")
+        Callable<? extends Boolean> selecting);
+
+    @Annotations(@Annotation("actual"))
+    @TypeInfo("Result")
+    @TypeParameters(@TypeParameter("Result"))
+    public <Result> Result fold(@Name("initial")
+        @TypeInfo("Result") Result initial,
+        @Name("accumulating")
+        @TypeInfo("ceylon.language::Callable<Result,ceylon.language::Tuple<ceylon.language::Bottom,Result,ceylon.language::Tuple<ceylon.language::Bottom,ceylon.language::Bottom,ceylon.language::Empty>>>")
+        Callable<? extends Result> accumulating);
+
+    @Annotations(@Annotation("actual"))
+    @TypeInfo("ceylon.language::Nothing")
+    public java.lang.Object find(@Name("selecting")
+        @TypeInfo("ceylon.language::Callable<ceylon.language::Boolean,ceylon.language::Tuple<ceylon.language::Bottom,ceylon.language::Bottom,ceylon.language::Empty>>")
+        Callable<? extends Boolean> selecting);
+
+    @Annotations(@Annotation("actual"))
+    @TypeInfo("ceylon.language::Empty")
+    public Empty sort(@Name("comparing")
+        @TypeInfo("ceylon.language::Callable<ceylon.language::Nothing|ceylon.language::Comparison,ceylon.language::Tuple<ceylon.language::Bottom,ceylon.language::Bottom,ceylon.language::Tuple<ceylon.language::Bottom,ceylon.language::Bottom,ceylon.language::Empty>>>")
+        Callable<? extends Comparison> comparing);
+
+    @Annotations(@Annotation("actual"))
+    @TypeInfo("ceylon.language::Empty")
+    public <Result> Sequential<? extends Result> collect(@Name("collecting")
+        @TypeInfo("ceylon.language::Callable<Result,ceylon.language::Tuple<ceylon.language::Bottom,ceylon.language::Bottom,ceylon.language::Empty>>")
+        Callable<? extends Result> collecting);
+    
+    @Annotations(@Annotation("actual"))
+    @TypeInfo("ceylon.language::Empty")
+    public Empty select(@Name("selecting")
+        @TypeInfo("ceylon.language::Callable<ceylon.language::Boolean,ceylon.language::Tuple<ceylon.language::Bottom,ceylon.language::Bottom,ceylon.language::Empty>>")
+        Callable<? extends Boolean> selecting);
+
+    @Annotations(@Annotation("actual"))
+    @TypeInfo("ceylon.language::Boolean")
+    public boolean any(@Name("selecting")
+        @TypeInfo("ceylon.language::Callable<ceylon.language::Boolean,ceylon.language::Tuple<ceylon.language::Bottom,ceylon.language::Bottom,ceylon.language::Empty>>")
+        Callable<? extends Boolean> selecting);
+
+    @Annotations(@Annotation("actual"))
+    @TypeInfo("ceylon.language::Boolean")
+    public boolean every(@Name("selecting")
+        @TypeInfo("ceylon.language::Callable<ceylon.language::Boolean,ceylon.language::Tuple<ceylon.language::Bottom,ceylon.language::Bottom,ceylon.language::Empty>>")
+        Callable<? extends Boolean> selecting);
+
+    @Annotations(@Annotation("actual"))
+    @TypeInfo("ceylon.language::Empty")
+    public Empty skipping(@Name("skip")
+        @TypeInfo("ceylon.language::Integer")
+        long skip);
+
+    @Annotations(@Annotation("actual"))
+    @TypeInfo("ceylon.language::Empty")
+    public Empty taking(@Name("take")
+        @TypeInfo("ceylon.language::Integer")
+        long take);
+
+    @Annotations(@Annotation("actual"))
+    @TypeInfo("ceylon.language::Empty")
+    public Empty by(@Name("step")
+        @TypeInfo("ceylon.language::Integer")
+        long step);
+    
+    @SuppressWarnings("rawtypes")
+    @Annotations(@Annotation("actual"))
+    @TypeParameters(@TypeParameter("Other"))
+    @TypeInfo("ceylon.language::Sequence<Other>")
+    public <Other> Sequence withLeading(@Name("element")
+            @TypeInfo("Other")
+            Other element);
+
+    @SuppressWarnings("rawtypes")
+    @Annotations(@Annotation("actual"))
+    @TypeParameters(@TypeParameter("Other"))
+    @TypeInfo("ceylon.language::Sequence<Other>")
+    public <Other> Sequence withTrailing(@Name("element")
+            @TypeInfo("Other")
+            Other element);
 
 }
