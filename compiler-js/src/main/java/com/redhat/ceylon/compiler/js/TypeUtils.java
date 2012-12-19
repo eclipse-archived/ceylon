b5a9f154bc24ef9f63e877e809ccb10bcac7bfe5
Enrique Zamudio <alterbass@gmail.com>
2012-12-18 19:55:08 -0600

    A little progress on method type parameters for #2587

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/TypeUtils.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/TypeUtils.java
@@ -1,10 +1,14 @@
 package com.redhat.ceylon.compiler.js;
 
+import java.util.Iterator;
 import java.util.List;
 
 import com.redhat.ceylon.compiler.typechecker.model.ClassOrInterface;
 import com.redhat.ceylon.compiler.typechecker.model.IntersectionType;
+import com.redhat.ceylon.compiler.typechecker.model.Method;
 import com.redhat.ceylon.compiler.typechecker.model.Module;
+import com.redhat.ceylon.compiler.typechecker.model.Parameter;
+import com.redhat.ceylon.compiler.typechecker.model.ParameterList;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
 import com.redhat.ceylon.compiler.typechecker.model.Scope;
 import com.redhat.ceylon.compiler.typechecker.model.TypeDeclaration;
@@ -26,6 +30,7 @@ public class TypeUtils {
         sequential = (TypeDeclaration)pkg.getMember("Sequential", null, false);
     }
 
+    /** Prints the type arguments, usually for their reification. */
     public static void printTypeArguments(Node node, List<ProducedType> targs, GenerateJsVisitor gen) {
         gen.out("[");
         boolean first = true;
@@ -73,8 +78,8 @@ public class TypeUtils {
         if (type.isAlias()) {
             type = type.getExtendedTypeDeclaration();
         }
-        boolean unionIntersection = type instanceof com.redhat.ceylon.compiler.typechecker.model.UnionType
-                || type instanceof com.redhat.ceylon.compiler.typechecker.model.IntersectionType;
+        boolean unionIntersection = type instanceof UnionType
+                || type instanceof IntersectionType;
         if (unionIntersection) {
             outputTypeList(node, type, gen, typeReferences);
         } else if (typeReferences) {
@@ -92,7 +97,7 @@ public class TypeUtils {
     static void outputTypeList(Node node, TypeDeclaration type, GenerateJsVisitor gen, boolean typeReferences) {
         gen.out("{ t:'");
         final List<TypeDeclaration> subs;
-        if (type instanceof com.redhat.ceylon.compiler.typechecker.model.IntersectionType) {
+        if (type instanceof IntersectionType) {
             gen.out("i");
             subs = type.getSatisfiedTypeDeclarations();
         } else {
@@ -126,12 +131,51 @@ public class TypeUtils {
             //it has to be a method, right?
             //We need to find the index of the parameter where the argument occurs
             //...and it could be null...
+            int plistCount = -1;
+            int paramCount = -1;
+            ProducedType type = null;
+            for (Iterator<ParameterList> iter0 = ((Method)tp.getContainer()).getParameterLists().iterator();
+                    type == null && iter0.hasNext();) {
+                plistCount++;
+                for (Iterator<Parameter> iter1 = iter0.next().getParameters().iterator();
+                        type == null && iter1.hasNext();) {
+                    paramCount++;
+                    if (type == null) {
+                        type = typeContainsTypeParameter(iter1.next().getType(), tp);
+                    }
+                }
+            }
+            //The ProducedType that we find corresponds to a parameter, whose type can be:
+            //A type parameter in the method, in which case we just use the argument's type (may be null)
+            //A component of a union/intersection type, in which case we just use the argument's type (may be null)
+            //A type argument of the argument's type, in which case we must get the reified generic from the argument
             if (tp.getContainer() == parent) {
-                gen.out("/*SAME METHOD TYPEPARM*/'", tp.getQualifiedNameString(), "'");
+                gen.out("/*SAME METHOD TYPEPARM plist ", Integer.toString(plistCount), "#", Integer.toString(paramCount), "*/'", type.getProducedTypeQualifiedName(), "'");
             } else {
-                gen.out("/*METHOD TYPEPARM*/'", tp.getQualifiedNameString(), "'");
+                gen.out("/*METHOD TYPEPARM plist ", Integer.toString(plistCount), "#", Integer.toString(paramCount), "*/'", type.getProducedTypeQualifiedName(), "'");
             }
         }
+    }
+
+    static ProducedType typeContainsTypeParameter(ProducedType td, TypeParameter tp) {
+        TypeDeclaration d = td.getDeclaration();
+        if (d == tp) {
+            return td;
+        } else if (d instanceof UnionType || d instanceof IntersectionType) {
+            for (ProducedType sub : td.getCaseTypes()) {
+                td = typeContainsTypeParameter(sub, tp);
+                if (td != null) {
+                    return td;
+                }
+            }
+        } else if (d instanceof ClassOrInterface) {
+            for (ProducedType sub : td.getTypeArgumentList()) {
+                if (typeContainsTypeParameter(sub, tp) != null) {
+                    return td;
+                }
+            }
+        }
+        return null;
     }
 
 }
