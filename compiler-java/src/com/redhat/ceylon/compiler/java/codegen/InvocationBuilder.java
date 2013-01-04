03c5e9cccc72f0b6ee276f5b93769b68c6aeb7a1
Tom Bentley <tom.bentley@cantab.net>
2013-01-04 11:33:15 +0000

    InvocationBuilder: We no longer have arrayOfSome() or arrayOfNone(), so we don't need to worry about prepending implicit arguments for them

--- compiler-java/src/com/redhat/ceylon/compiler/java/codegen/InvocationBuilder.java
+++ compiler-java/src/com/redhat/ceylon/compiler/java/codegen/InvocationBuilder.java
@@ -286,7 +286,7 @@ abstract class InvocationBuilder {
         if (primaryDeclaration instanceof LazyMethod) {
             if ("ceylon.language".equals(primaryDeclaration.getContainer().getQualifiedNameString())) {
                 String name = primaryDeclaration.getName();
-                return ("array".equals(name) || "arrayOfSome".equals(name) || "arrayOfNone".equals(name) || "arrayOfSize".equals(name));
+                return ("array".equals(name) || "arrayOfSize".equals(name));
             }
         }
         return false;
