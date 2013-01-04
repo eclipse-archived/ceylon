42f39fc78a3889c1c709312e8e91cfe0cb3a64d2
Enrique Zamudio <alterbass@gmail.com>
2013-01-03 22:28:15 -0600

    extendsType(): Bottom extends everything.
    className(): add type args to the description

--- compiler-js/src/main/js/functions.js
+++ compiler-js/src/main/js/functions.js
@@ -103,7 +103,7 @@ function extendsType(t1, t2) {
         return _ints ? inters||unions : unions;
     }
     for (t in t1.t.$$.T$all) {
-        if (t === t2.t.$$.T$name) {
+        if (t === t2.t.$$.T$name || t === 'ceylon.language::Bottom') {
             return true;
         }
     }
@@ -111,8 +111,39 @@ function extendsType(t1, t2) {
 }
 
 function className(obj) {
+    function _typename(t) {
+        if (t.t==='i' || t.t==='u') {
+            var _sep = t.t==='i'?'&':'|';
+            var ct = '';
+            for (var i=0; i < t.l.length; i++) {
+                if (i>0) { ct+=_sep; }
+                ct += _typename(t.l[i]);
+            }
+            return String$(ct);
+        } else {
+            var tn = t.t.$$.T$name;
+            if (t.a) {
+                tn += '<';
+                for (var i = 0; i < t.a.length; i++) {
+                    if (i>0) { tn += ','; }
+                    tn += _typename(t.a[i]);
+                }
+                tn += '>';
+            }
+            return tn;
+        }
+    }
     if (obj === null) return String$('ceylon.language::Nothing');
-    return String$(obj.getT$name());
+    var tn = obj.getT$name();
+    if (obj.$$targs$$) {
+        tn += '<';
+        for (var i=0; i < obj.$$targs$$.length; i++) {
+            if (i>0) { tn += ','; }
+            tn += _typename(obj.$$targs$$[i]);
+        }
+        tn += '>';
+    }
+    return String$(tn);
 }
 
 function identityHash(obj) {
