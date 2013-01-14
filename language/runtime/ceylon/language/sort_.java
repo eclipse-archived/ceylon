d998d4d7f7e6bc322b78163ee939f0e9a1d11f65
Tako Schotanus <tako@ceylon-lang.org>
2013-01-14 20:21:19 +0100

    Added private internalSort() toplevel method that takes a comparing() method. Changed toplevel sort() to use it.

--- language/runtime/ceylon/language/sort_.java
+++ language/runtime/ceylon/language/sort_.java
@@ -3,6 +3,7 @@ package ceylon.language;
 import java.util.Comparator;
 
 import com.redhat.ceylon.compiler.java.Util;
+import com.redhat.ceylon.compiler.java.language.AbstractCallable;
 import com.redhat.ceylon.compiler.java.language.ArraySequence;
 import com.redhat.ceylon.compiler.java.metadata.Ceylon;
 import com.redhat.ceylon.compiler.java.metadata.Ignore;
@@ -27,6 +28,39 @@ public final class sort_ {
         @TypeInfo("ceylon.language::Sequential<Element>")
         final Sequential<? extends Element> elements) {
         
+        return internalSort_.internalSort(elements, new AbstractCallable<Comparison>("comparing") {
+            @Override
+            public Comparison $call(java.lang.Object arg0, java.lang.Object arg1) {
+                Element x = (Element)arg0;
+                Element y = (Element)arg1;
+                return x.compare(y);
+            }
+        });
+    }
+    
+    @Ignore
+    public static <Element> Sequential<? extends Element> sort() {
+        return (Sequential)empty_.getEmpty$();
+    }
+    
+}
+
+@Ceylon(major = 3)
+@Method
+final class internalSort_ {
+    
+    private internalSort_() {}
+    
+    @TypeParameters(@TypeParameter(value="Element"))
+    @TypeInfo("ceylon.language::Sequential<Element>")
+    public static <Element> Sequential<? extends Element> internalSort(
+        @Name("elements")
+        @TypeInfo("ceylon.language::Sequential<Element>")
+        final Sequential<? extends Element> elements,
+        @Name("comparing")
+        @TypeInfo("ceylon.language::Callable<ceylon.language::Comparison|ceylon.language::Null,ceylon.language::Tuple<Element,Element,ceylon.language::Tuple<Element,Element,ceylon.language::Empty>>>")
+        final Callable<Comparison> comparing) {
+        
         if( elements.getEmpty() ) {
             return (Sequential)empty_.getEmpty$();   
         }
@@ -35,7 +69,7 @@ public final class sort_ {
 
         java.util.Collections.sort(list, new Comparator<Element>() {
             public int compare(Element x, Element y) {
-                Comparison result = x.compare(y);
+                Comparison result = comparing.$call(x, y);
                 if (result.largerThan()) return 1;
                 if (result.smallerThan()) return -1;
                 return 0;
@@ -43,11 +77,6 @@ public final class sort_ {
         });
 
         return new ArraySequence<Element>(list);
-    }
-    
-    @Ignore
-    public static <Element> Sequential<? extends Element> sort() {
-        return (Sequential)empty_.getEmpty$();
     }
     
 }
--- language/src/ceylon/language/sort.ceylon
+++ language/src/ceylon/language/sort.ceylon
@@ -1,4 +1,10 @@
 doc "Sort a given elements, returning a new sequence."
 see (Comparable)
-shared native Element[] sort<Element>(Element* elements) 
-        given Element satisfies Comparable<Element>;
+shared Element[] sort<Element>(Element* elements) 
+        given Element satisfies Comparable<Element> {
+    return internalSort(elements, byIncreasing(x, y));
+}
+
+native Element[] internalSort<Element>(Element* elements, Comparison? comparing(Element x, Element y)) {
+    
+}
