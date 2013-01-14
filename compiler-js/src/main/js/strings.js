13da7431d0eadfc0965d7b426a9b4face093dd0e
Enrique Zamudio <alterbass@gmail.com>
2013-01-14 10:52:29 -0600

    Second type argument for String should be Null

--- compiler-js/src/main/js/strings.js
+++ compiler-js/src/main/js/strings.js
@@ -22,7 +22,7 @@ initType(SequenceString, "ceylon.language::SequenceString", String$, Sequence);
 function EmptyString() {}
 initType(EmptyString, "ceylon.language::EmptyString", String$, Empty);
 var String$proto = String$.$$.prototype;
-String$proto.$$targs$$=[{t:Character}, {t:Character}];
+String$proto.$$targs$$=[{t:Character}, {t:Null}];
 String$proto.getT$name = function() {
     return ((this.length!==0)?SequenceString:EmptyString).$$.T$name;
 }
