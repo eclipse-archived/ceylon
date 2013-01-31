927b21f2ab863065cdb27057371f29065edda27b
Tom Bentley <tom.bentley@cantab.net>
2013-01-31 11:53:01 +0000

    Fix #5070: Range: Re-enable the optimized version of Range<Integer>.by() (where we can use  a + instead of iterating doing ++) but using a native helper method. This is a hack until we have reified generics and can write the optimization in pure Ceylon.

---
+++ language/runtime/ceylon/language/integerRangeByIterable_.java
@@ -0,0 +1,80 @@
+package ceylon.language;
+
+import com.redhat.ceylon.compiler.java.language.AbstractIterable;
+import com.redhat.ceylon.compiler.java.metadata.Annotation;
+import com.redhat.ceylon.compiler.java.metadata.Annotations;
+import com.redhat.ceylon.compiler.java.metadata.Ceylon;
+import com.redhat.ceylon.compiler.java.metadata.Method;
+import com.redhat.ceylon.compiler.java.metadata.Name;
+import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
+import com.redhat.ceylon.compiler.java.metadata.TypeParameter;
+import com.redhat.ceylon.compiler.java.metadata.TypeParameters;
+
+@Ceylon(major = 4)
+@Method
+final class integerRangeByIterable_ {
+    
+    private integerRangeByIterable_() {}
+    
+    /**
+     * Optimized implementation of {@code Range<Integer>.iterator}. This is 
+     * necessary because we need reified generics in order to write 
+     * the optimized version in Ceylon. 
+     * @param range
+     * @param step
+     * @return
+     */
+    @Annotations(@Annotation("native"))
+    @TypeParameters(@TypeParameter(value="Element", 
+            satisfies={"ceylon.language::Ordinal<Element>", 
+                "ceylon.language::Comparable<Element>"}))
+    @TypeInfo(value="ceylon.language::Iterable<Element, Null>")
+    static <Element extends ceylon.language.Ordinal<? extends Element> & ceylon.language.Comparable<? super Element>> 
+            ceylon.language.Iterable<Element, Null> integerRangeByIterable(
+                    @Name("range")
+                    @TypeInfo(value="ceylon.language::Range<Element>")
+                    ceylon.language.Range<Element> range,
+                    @Name("step")
+                    final long step) {
+        final ceylon.language.Range<ceylon.language.Integer> r = (ceylon.language.Range)range; 
+        //Optimize for Integer ranges
+        return new AbstractIterable<Element,  Null>() {
+            @Override
+            @Annotations(@Annotation("formal"))
+            @TypeInfo("ceylon.language::Iterator<Element>")
+            public Iterator<? extends Element> getIterator() {
+
+                return new Iterator<Element>() {
+                    long current = r.getFirst().longValue();
+                    final long lim = r.getLast().longValue();
+                    boolean inverse = lim < current;
+
+                    @TypeInfo(value="Element|ceylon.language::Finished", erased=true)
+                    public java.lang.Object next() {
+                        long result = current;
+                        if (inverse) {
+                            // avoid current < lim etc: possibility of overflow
+                            if (current-lim < 0) return finished_.getFinished$();
+                            current-=step;
+                        } else {
+                            if (current-lim > 0) return finished_.getFinished$();
+                            current+=step;
+                        }
+                        return Integer.instance(result);
+                    }
+
+                    @Override
+                    public java.lang.String toString() {
+                        return "RangeIterator";
+                    }
+
+                    @Override
+                    public ceylon.language.Iterator$impl $ceylon$language$Iterator$impl() {
+                        return new ceylon.language.Iterator$impl(this);
+                    }
+                };
+            }
+        };
+    }
+}
+
---
+++ language/src/ceylon/language/integerRangeByIterable.ceylon
@@ -0,0 +1,6 @@
+doc "Provides an optimized implementation of `Range<Integer>.iterator`. 
+     This is necessary because we need reified generics in order to write 
+     the optimized version in pure Ceylon."
+native 
+{Element*} integerRangeByIterable<Element>(Range<Element> range, Integer step)
+        given Element satisfies Ordinal<Element> & Comparable<Element>;
