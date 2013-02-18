4865da08b38ee86eca0832cb6cbf7039524e864f
Tako Schotanus <tako@ceylon-lang.org>
2013-02-18 19:23:03 +0100

    Added test for #1037

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTest.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTest.java
@@ -1051,5 +1051,13 @@ public class IssuesTest extends CompilerTest {
     public void testBug1029() {
         compareWithJavaSource("bug10xx/Bug1029");
     }
+    
+    @Test
+    public void testBug1037() {
+        compile("bug10xx/bug1037/Bug1037Java.java");
+        assertErrors("bug10xx/bug1037/Bug1037",
+                new CompilerError(22, "CompilerBoxingDeclarationVisitor caused an exception visiting ValueParameterDeclaration node: java.lang.RuntimeException: Different sized parameter lists at com.redhat.ceylon.compiler.java.codegen.CodegenUtil.getTopmostRefinedDeclaration(CodegenUtil.java:208)"),
+                new CompilerError(23, "method test in class com.redhat.ceylon.compiler.java.test.issues.bug10xx.bug1037.Bug1037Java cannot be applied to given types;\n  required: no arguments\n  found: com.redhat.ceylon.compiler.java.language.ArraySequence<ceylon.language.String>\n  reason: actual and formal argument lists differ in length"));
+    }
 }
 
---
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/bug10xx/bug1037/Bug1037Java.java
@@ -0,0 +1,5 @@
+package com.redhat.ceylon.compiler.java.test.issues.bug10xx.bug1037;
+
+public class Bug1037Java {
+    void test() {}
+}
