700b70df5c69049f4ee4cb5f00049a460a8248c6
Tako Schotanus <tako@ceylon-lang.org>
2013-02-21 03:54:53 +0100

    Greatly simplified startup scripts due to the new launcher

--- compiler-java/bin/ceylon.bat
+++ compiler-java/bin/ceylon.bat
@@ -1,9 +1,18 @@
 @echo off
 setlocal
 
-call %~dp0\ceylon-cp.bat %*
+rem Find Java
+call %CEYLON_HOME%\bin\java.bat
 
-if "%exit%" == "true" (
+if "%JAVA_HOME%" == "" (
+    exit /b 1
+)
+
+set "JAVA=%JAVA_HOME%\bin\java.exe"
+
+rem Check that Java executable actually exists
+if not exist "%JAVA%" (
+    @echo "Cannot find java.exe at %JAVA%, check that your JAVA_HOME variable is pointing to the right place"
     exit /b 1
 )
 
@@ -16,18 +25,10 @@ if NOT "%PRESERVE_JAVA_OPTS%" == "true" (
 set "JAVA_OPTS=%PREPEND_JAVA_OPTS% %JAVA_OPTS%"
 
 set "LIB=%CEYLON_HOME%\lib"
-set "REPO=%CEYLON_HOME%\repo"
-set "RUNTIME_REPO=%CEYLON_HOME%\runtime-repo"
-
-set "JAVA_CP=%LIB%\ceylon-runtime-bootstrap.jar"
-set "JAVA_CP=%JAVA_CP%;%MINIMAL_CP%"
 
 "%JAVA%" ^
-    -cp %JAVA_CP% ^
-    "-Dceylon.home=%CEYLON_HOME%" ^
-    "-Dceylon.system.repo=%CEYLON_REPO%" ^
     %JAVA_OPTS% ^
-    com.redhat.ceylon.tools.CeylonTool ^
-    %ARGS%
+    -jar "%LIB%\ceylon-bootstrap.jar"
+    %*
 
 endlocal
