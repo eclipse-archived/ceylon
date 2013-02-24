db46d65a5afaf2380a0311d9e56c793e6f1bf2aa
Gavin King <gavin@hibernate.org>
2013-02-24 20:23:39 +0100

    unused import

--- typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/ModuleValidator.java
+++ typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/ModuleValidator.java
@@ -2,7 +2,6 @@ package com.redhat.ceylon.compiler.typechecker.analyzer;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
