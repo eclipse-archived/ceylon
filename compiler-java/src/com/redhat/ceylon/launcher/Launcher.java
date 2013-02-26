443b0dfe2dea682c04fa215d87909890aa8ddc18
Tako Schotanus <tako@ceylon-lang.org>
2013-02-26 01:36:58 +0100

    Temporary HACK to make multiple executions in the same JVM possible (WARNING: leaks a LOT of memory!)

--- compiler-java/src/com/redhat/ceylon/launcher/Launcher.java
+++ compiler-java/src/com/redhat/ceylon/launcher/Launcher.java
@@ -58,6 +58,17 @@ public class Launcher {
         System.setProperty("ceylon.home", CeylonClassLoader.determineHome().getAbsolutePath());
         System.setProperty("ceylon.system.repo", CeylonClassLoader.determineRepo().getAbsolutePath());
         
+        // Total HACK to make an error with JBoss Modules go away
+        Properties sp = System.getProperties();
+        sp.remove("javax.xml.parsers.DocumentBuilderFactory");
+        sp.remove("javax.xml.parsers.SAXParserFactory");
+        sp.remove("javax.xml.stream.XMLInputFactory");
+        sp.remove("javax.xml.stream.XMLEventFactory");
+        sp.remove("javax.xml.transform.TransformerFactory");
+        sp.remove("javax.xml.stream.XMLOutputFactory");
+        sp.remove("javax.xml.datatype.DatatypeFactory");
+        sp.remove("org.xml.sax.driver");
+        
         if (hasArgument(args, "--verbose")) {
             System.err.println("INFO: Ceylon home directory is '" + CeylonClassLoader.determineHome() + "'");
             for (File f : cp) {
