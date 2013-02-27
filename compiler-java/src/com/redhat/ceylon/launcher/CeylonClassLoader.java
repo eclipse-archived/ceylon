58e4f8d06c2d634dea604488f75aef80c95066de
Tako Schotanus <tako@ceylon-lang.org>
2013-02-27 20:20:59 +0100

    Making the Ceylon class loader child-first fixed #5699

--- compiler-java/src/com/redhat/ceylon/launcher/CeylonClassLoader.java
+++ compiler-java/src/com/redhat/ceylon/launcher/CeylonClassLoader.java
@@ -3,15 +3,29 @@ package com.redhat.ceylon.launcher;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 import com.redhat.ceylon.common.Versions;
 
+/**
+ * Ceylon-specific class loader that knows how to find and add
+ * all needed dependencies for compiler and runtime.
+ * Implements child-first class loading to prevent mix-ups with
+ * Java's own tool-chain.
+ * 
+ * @author Tako Schotanus
+ *
+ */
 public class CeylonClassLoader extends URLClassLoader {
     
     public CeylonClassLoader() throws URISyntaxException, MalformedURLException, FileNotFoundException {
@@ -145,4 +159,79 @@ public class CeylonClassLoader extends URLClassLoader {
             }
         }
     }
+    
+    @Override
+    protected synchronized Class<?> loadClass(String name, boolean resolve)
+            throws ClassNotFoundException {
+        // First, check if the class has already been loaded
+        Class<?> c = findLoadedClass(name);
+        if (c == null) {
+            try {
+                // checking local
+                c = findClass(name);
+            } catch (ClassNotFoundException e) {
+                // checking parent
+                // This call to loadClass may eventually call findClass again, in case the parent doesn't find anything.
+                c = super.loadClass(name, resolve);
+            }
+        }
+        if (resolve) {
+            resolveClass(c);
+        }
+        return c;
+    }
+
+    @Override
+    public URL getResource(String name) {
+        URL url = findResource(name);
+        if (url == null) {
+            // This call to getResource may eventually call findResource again, in case the parent doesn't find anything.
+            url = super.getResource(name);
+        }
+        return url;
+    }
+
+    @Override
+    public Enumeration<URL> getResources(String name) throws IOException {
+        /**
+        * Similar to super, but local resources are enumerated before parent resources
+        */
+        Enumeration<URL> localUrls = findResources(name);
+        Enumeration<URL> parentUrls = null;
+        if (getParent() != null) {
+            parentUrls = getParent().getResources(name);
+        }
+        final List<URL> urls = new ArrayList<URL>();
+        if (localUrls != null) {
+            while (localUrls.hasMoreElements()) {
+                urls.add(localUrls.nextElement());
+            }
+        }
+        if (parentUrls != null) {
+            while (parentUrls.hasMoreElements()) {
+                urls.add(parentUrls.nextElement());
+            }
+        }
+        return new Enumeration<URL>() {
+            Iterator<URL> iter = urls.iterator();
+
+            public boolean hasMoreElements() {
+                return iter.hasNext(); 
+            }
+            public URL nextElement() {
+                return iter.next();
+            }
+        };
+    }
+
+    @Override
+    public InputStream getResourceAsStream(String name) {
+        URL url = getResource(name);
+        try {
+            return url != null ? url.openStream() : null;
+        } catch (IOException e) {
+        }
+        return null;
+    }
+
 }
