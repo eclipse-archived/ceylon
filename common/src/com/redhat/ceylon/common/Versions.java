005950d3ee616bc1beea336c554ab54d5321582d
Stephane Epardaud <stef@epardaud.fr>
2013-03-12 11:33:24 +0100

    Nesa Pong!

--- common/src/com/redhat/ceylon/common/Versions.java
+++ common/src/com/redhat/ceylon/common/Versions.java
@@ -2,7 +2,7 @@ package com.redhat.ceylon.common;
 
 public class Versions {
     
-    // The current version is Ceylon 0.5 "Zaphod Beeblebrox"
+    // The current version is Ceylon 0.5 "Nesa Pong"
     // This comment is here so this file will show up in searches for the current version number
     
     // WARNING Don't forget to update ceylon.language/runtime-js/process.js when you change anything here!
@@ -11,7 +11,7 @@ public class Versions {
     public static final int CEYLON_VERSION_MINOR = 5;
     public static final int CEYLON_VERSION_RELEASE = 0;
     public static final String CEYLON_VERSION_NUMBER = CEYLON_VERSION_MAJOR + "." + CEYLON_VERSION_MINOR /* + "." + CEYLON_VERSION_RELEASE */;
-    public static final String CEYLON_VERSION_NAME = "Zaphod Beeblebrox";
+    public static final String CEYLON_VERSION_NAME = "Nesa Pong";
     public static final String CEYLON_VERSION = CEYLON_VERSION_NUMBER + " (" + CEYLON_VERSION_NAME + ")";
 
     /**
