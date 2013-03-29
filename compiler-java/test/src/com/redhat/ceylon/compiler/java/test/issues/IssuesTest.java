d92c3ee2ebd7a14cdd69202167211b27b9da5855
Tako Schotanus <tako@ceylon-lang.org>
2013-03-29 11:14:34 +0100

    Fixed .src because of ceylon/ceylon-spec@94f6c8342f85280502dea2e4f551cf66084d8ab9

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTest.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTest.java
@@ -926,7 +926,7 @@ public class IssuesTest extends CompilerTest {
     @Test
     public void testBug950() {
         assertErrors("bug09xx/Bug950",
-                new CompilerError(34, "member foo is inherited ambiguously from Bug950_Left and Bug950_Top and so must be refined by Bug950_Bottom"),
+                new CompilerError(34, "member foo is inherited ambiguously by Bug950_Bottom from Bug950_Left and another subtype of Bug950_Top and so must be refined by Bug950_Bottom"),
                 new CompilerError(34, "may not inherit two declarations with the same name unless redefined in subclass: foo is defined by supertypes Bug950_Left and Bug950_Right"));
 
     }
