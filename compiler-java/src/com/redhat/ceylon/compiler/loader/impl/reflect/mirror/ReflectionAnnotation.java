9c6a5a5405ce21cb3c9dc17d577f95e0209ea3e6
Tom Bentley <tom.bentley@cantab.net>
2013-04-18 13:06:57 +0100

    annotations: ReflectionAnnotation: Fix the ReflectionAnnotation so it properly handles primitive arrays

--- compiler-java/src/com/redhat/ceylon/compiler/loader/impl/reflect/mirror/ReflectionAnnotation.java
+++ compiler-java/src/com/redhat/ceylon/compiler/loader/impl/reflect/mirror/ReflectionAnnotation.java
@@ -47,18 +47,69 @@ public class ReflectionAnnotation implements AnnotationMirror {
     }
 
     private Object convertValue(Object value) {
-        if(value.getClass().isArray()){
-            Object[] array = (Object[])value;
-            List<Object> values = new ArrayList<Object>(array.length);
-            for(Object val : array)
-                values.add(convertValue(val));
-            return values;
+        Class<? extends Object> valueClass = value.getClass();
+        if(valueClass.isArray()){
+            if (valueClass.getComponentType().isPrimitive()) {
+                Class<?> componentType = valueClass.getComponentType();
+                if (componentType == short.class) {
+                    short[] array = (short[])value;
+                    List<Object> values = new ArrayList<Object>(array.length);
+                    for(short val : array)
+                        values.add(val);
+                    return values;
+                } else if (componentType == int.class) {
+                    int[] array = (int[])value;
+                    List<Object> values = new ArrayList<Object>(array.length);
+                    for(int val : array)
+                        values.add(val);
+                    return values;
+                } else if (componentType == long.class) {
+                    long[] array = (long[])value;
+                    List<Object> values = new ArrayList<Object>(array.length);
+                    for(long val : array)
+                        values.add(val);
+                    return values;
+                } else if (componentType == boolean.class) {
+                    boolean[] array = (boolean[])value;
+                    List<Object> values = new ArrayList<Object>(array.length);
+                    for(boolean val : array)
+                        values.add(val);
+                    return values;
+                } else if (componentType == char.class) {
+                    char[] array = (char[])value;
+                    List<Object> values = new ArrayList<Object>(array.length);
+                    for(char val : array)
+                        values.add(val);
+                    return values;
+                } else if (componentType == float.class) {
+                    float[] array = (float[])value;
+                    List<Object> values = new ArrayList<Object>(array.length);
+                    for(float val : array)
+                        values.add(val);
+                    return values;
+                } else if (componentType == double.class) {
+                    double[] array = (double[])value;
+                    List<Object> values = new ArrayList<Object>(array.length);
+                    for(double val : array)
+                        values.add(val);
+                    return values;
+                }
+            } else {
+                Object[] array = (Object[])value;
+                List<Object> values = new ArrayList<Object>(array.length);
+                for(Object val : array)
+                    values.add(convertValue(val));
+                return values;
+            }
         }
         if(value instanceof Annotation){
             return new ReflectionAnnotation((Annotation) value);
         }
         if(value instanceof Enum){
             return ((Enum<?>)value).name();
+        }
+        if(value instanceof Class){
+            return new ReflectionType((Class<?>)value);
         }
         return value;
     }
