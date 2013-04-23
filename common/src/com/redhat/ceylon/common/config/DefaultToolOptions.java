fdc575350f7dbf32a6fad50aca664316e0bb564c
Tako Schotanus <tako@ceylon-lang.org>
2013-04-24 00:05:10 +0200

    Added class to hold constants for common tool related configuration options

---
+++ common/src/com/redhat/ceylon/common/config/DefaultToolOptions.java
@@ -0,0 +1,10 @@
+package com.redhat.ceylon.common.config;
+
+public class DefaultToolOptions {
+    
+    public final static String DEFAULTS_ENCODING = "defaults.encoding";
+    
+    private DefaultToolOptions() {
+    }
+
+}
