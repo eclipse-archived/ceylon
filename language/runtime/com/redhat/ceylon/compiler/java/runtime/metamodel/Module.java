75a6ae7586f863a96075f4254461424bc2e02e36
Stephane Epardaud <stef@epardaud.fr>
2013-05-02 11:13:18 +0200

    metamodel: implement Module.members #5104

--- language/runtime/com/redhat/ceylon/compiler/java/runtime/metamodel/Module.java
+++ language/runtime/com/redhat/ceylon/compiler/java/runtime/metamodel/Module.java
@@ -1,9 +1,12 @@
 package com.redhat.ceylon.compiler.java.runtime.metamodel;
 
+import java.util.List;
+
 import ceylon.language.Sequential;
 import ceylon.language.metamodel.Module$impl;
 import ceylon.language.metamodel.Package;
 
+import com.redhat.ceylon.compiler.java.Util;
 import com.redhat.ceylon.compiler.java.metadata.Ignore;
 import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
 import com.redhat.ceylon.compiler.java.runtime.model.ReifiedType;
@@ -14,6 +17,7 @@ public class Module implements ceylon.language.metamodel.Module, ReifiedType{
     @Ignore
     public static final TypeDescriptor $TypeDescriptor = TypeDescriptor.klass(Module.class);
     private com.redhat.ceylon.compiler.typechecker.model.Module declaration;
+    private Sequential<Package> packages;
     
     public Module(com.redhat.ceylon.compiler.typechecker.model.Module declaration) {
         this.declaration = declaration;
@@ -29,8 +33,16 @@ public class Module implements ceylon.language.metamodel.Module, ReifiedType{
     @Override
     @TypeInfo("ceylon.language::Sequential<ceylon.language.metamodel::Package>")
     public Sequential<? extends Package> getMembers() {
-        // TODO Auto-generated method stub
-        return null;
+        // no need to synchronise as concurrent invocations should get the same array back
+        if(this.packages == null){
+            List<com.redhat.ceylon.compiler.typechecker.model.Package> modelPackages = declaration.getAllPackages();
+            Package[] packages = new Package[modelPackages.size()];
+            for(int i=0;i<packages.length;i++){
+                packages[i] = Metamodel.getOrCreateMetamodel(modelPackages.get(i));
+            }
+            this.packages = Util.sequentialInstance(Package.$TypeDescriptor, packages);
+        }
+        return this.packages;
     }
 
     @Override
