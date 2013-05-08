86e8bbf5ea798f3e71e2fbc7fca0cbde33615cb9
Tako Schotanus <tako@ceylon-lang.org>
2013-05-08 12:46:24 +0200

    Added "offline" option constant for #1121

--- common/src/com/redhat/ceylon/common/config/DefaultToolOptions.java
+++ common/src/com/redhat/ceylon/common/config/DefaultToolOptions.java
@@ -3,6 +3,7 @@ package com.redhat.ceylon.common.config;
 public class DefaultToolOptions {
     
     public final static String DEFAULTS_ENCODING = "defaults.encoding";
+    public final static String DEFAULTS_OFFLINE = "defaults.offline";
     
     private DefaultToolOptions() {
     }
