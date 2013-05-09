7bb71e94fa98bdaa4b3851c579a90bfb5c07a329
Enrique Zamudio <alterbass@gmail.com>
2013-05-09 15:01:33 -0500

    Work on #5080

--- language/runtime-js/metamodel.js
+++ language/runtime-js/metamodel.js
@@ -1,5 +1,29 @@
 function type(x) {
+    if (x === null) {
+        return getNothingType();
+    } else {
+        //Search for metamodel
+        var mm = x.$$metamodel$$;
+        if (mm === undefined && x.constructor && x.constructor.T$name && x.constructor.T$all) {
+            //It's probably an instance of a Ceylon type
+            var _x = x.constructor.T$all[x.constructor.T$name];
+            if (_x) {
+                mm = _x.$$metamodel$$;
+            }
+        }
+        if (mm && mm['$mt']) {
+            var metatype = mm['$mt'];
+            if (metatype === 'ifc') { //Interface
+                //
+            } else if (metatype === 'cls') { //Class
+            } else if (metatype === 'mthd') { //Method
+            } else {
+            }
+        } else {
+            throw Exception(String$("No metamodel available for "+x));
+        }
+    }
     return null;
 }
-type.$$metamodel$$={$nm:'type',$mt:'mthd',$an:function(){return[shared()];}};
+type.$$metamodel$$={$nm:'type',$mt:'mthd',$ps:[{t:Anything}],$an:function(){return[shared()];}};
 exports.type=type;
