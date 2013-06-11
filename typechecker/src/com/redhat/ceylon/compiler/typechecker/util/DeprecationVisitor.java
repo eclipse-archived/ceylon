48f5e1360d68f5f245ea978d29a80ad7e44f2376
Gavin King <gavin@hibernate.org>
2013-06-11 10:43:08 -0400

    ooops, fix npe

--- typechecker/src/com/redhat/ceylon/compiler/typechecker/util/DeprecationVisitor.java
+++ typechecker/src/com/redhat/ceylon/compiler/typechecker/util/DeprecationVisitor.java
@@ -1,5 +1,7 @@
 package com.redhat.ceylon.compiler.typechecker.util;
 
+import com.redhat.ceylon.compiler.typechecker.model.Declaration;
+import com.redhat.ceylon.compiler.typechecker.model.TypeDeclaration;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 
@@ -7,25 +9,28 @@ public class DeprecationVisitor extends Visitor {
     @Override
     public void visit(Tree.MemberOrTypeExpression that) {
         super.visit(that);
-        if (that.getDeclaration().isDeprecated()) {
+        Declaration d = that.getDeclaration();
+		if (d!=null && d.isDeprecated()) {
         	that.addUsageWarning("declaration is deprecated: " + 
-        			that.getDeclaration().getName());
+        			d.getName());
         }
     }
     @Override
     public void visit(Tree.SimpleType that) {
     	super.visit(that);
-        if (that.getDeclarationModel().isDeprecated()) {
+        TypeDeclaration d = that.getDeclarationModel();
+		if (d!=null && d.isDeprecated()) {
         	that.addUsageWarning("type is deprecated: " + 
-        			that.getDeclarationModel().getName());
+        			d.getName());
         }
     }
     @Override
     public void visit(Tree.ImportMemberOrType that) {
         super.visit(that);
-        if (that.getDeclarationModel().isDeprecated()) {
+        Declaration d = that.getDeclarationModel();
+		if (d!=null && d.isDeprecated()) {
         	that.addUsageWarning("imported declaration is deprecated: " + 
-        			that.getDeclarationModel().getName());
+        			d.getName());
         }
     }
 }
