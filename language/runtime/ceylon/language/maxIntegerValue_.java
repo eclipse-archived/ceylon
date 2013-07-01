c77c110fa3471396585e0c779086bd1d374cb677
Loic Rouchon <loic.rouchon@gmail.com>
2013-07-01 20:02:38 +0200

    #5074 Migration to @Ceylon(major=5)

--- language/runtime/ceylon/language/maxIntegerValue_.java
+++ language/runtime/ceylon/language/maxIntegerValue_.java
@@ -2,14 +2,11 @@ package ceylon.language;
 
 import com.redhat.ceylon.compiler.java.metadata.Ceylon;
 import com.redhat.ceylon.compiler.java.metadata.Attribute;
-import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
 
-@Ceylon(major = 4)
-@Attribute
-public final class maxIntegerValue_ {
+@Ceylon(major = 5) @Attribute
+public class maxIntegerValue_ {
 
-    @TypeInfo("ceylon.language::Integer")
-    public static Integer getMaxIntegerValue$() {
+    public static Integer $get() {
         return Integer.instance(Long.MAX_VALUE);
     }
 }
--- language/runtime/ceylon/language/minIntegerValue_.java
+++ language/runtime/ceylon/language/minIntegerValue_.java
@@ -2,14 +2,11 @@ package ceylon.language;
 
 import com.redhat.ceylon.compiler.java.metadata.Ceylon;
 import com.redhat.ceylon.compiler.java.metadata.Attribute;
-import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
 
-@Ceylon(major = 4)
-@Attribute
-public final class minIntegerValue_ {
+@Ceylon(major = 5) @Attribute
+public class minIntegerValue_ {
 
-    @TypeInfo("ceylon.language::Integer")
-    public static Integer getMinIntegerValue$() {
-        return Integer.instance(Long.MIN_VALUE);
-    }
+	public static Integer $get() {
+		return Integer.instance(Long.MIN_VALUE);
+	}
 }
