b7f8c24f0d2b8d66324567fe3a20a4e1fe93f73e
Stephane Epardaud <stef@epardaud.fr>
2013-07-16 14:04:00 +0200

    metamodel #5104: fixed a typo in a name

--- language/runtime/com/redhat/ceylon/compiler/java/runtime/metamodel/FreeClassWithAppliedMemberClass.java
+++ language/runtime/com/redhat/ceylon/compiler/java/runtime/metamodel/FreeClassWithAppliedMemberClass.java
@@ -34,7 +34,7 @@ import com.redhat.ceylon.compiler.typechecker.model.Class;
     @TypeParameter(value = "Type", variance = Variance.OUT),
     @TypeParameter(value = "Arguments", variance = Variance.IN, satisfies = "ceylon.language::Sequential<ceylon.language::Anything>"),
 })
-public class FreeClassWithAppliedClassMember<Container, Type, Arguments extends Sequential<? extends Object>> 
+public class FreeClassWithAppliedMemberClass<Container, Type, Arguments extends Sequential<? extends Object>> 
     extends FreeClass
     implements ceylon.language.metamodel.MemberClass<Container, Type, Arguments> {
 
@@ -46,7 +46,7 @@ public class FreeClassWithAppliedClassMember<Container, Type, Arguments extends 
     @Ignore
     private TypeDescriptor $reifiedArguments;
 
-    public FreeClassWithAppliedClassMember(@Ignore TypeDescriptor $reifiedContainer,
+    public FreeClassWithAppliedMemberClass(@Ignore TypeDescriptor $reifiedContainer,
             @Ignore TypeDescriptor $reifiedType,
             @Ignore TypeDescriptor $reifiedArguments,
             Class declaration) {
@@ -285,6 +285,6 @@ public class FreeClassWithAppliedClassMember<Container, Type, Arguments extends 
     @Override
     public TypeDescriptor $getType() {
         checkInit();
-        return TypeDescriptor.klass(FreeClassWithAppliedClassMember.class, $reifiedContainer, $reifiedType, $reifiedArguments);
+        return TypeDescriptor.klass(FreeClassWithAppliedMemberClass.class, $reifiedContainer, $reifiedType, $reifiedArguments);
     }
 }
