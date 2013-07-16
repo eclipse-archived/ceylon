d8eb48c1eff2eb469ab18a16eb63b51fd958f7eb
Enrique Zamudio <alterbass@gmail.com>
2013-07-16 18:34:54 -0500

    Test JS compilation with Java imports

---
+++ compiler-js/src/test/java/com/redhat/ceylon/compiler/js/TestJavaDeps.java
@@ -0,0 +1,27 @@
+package com.redhat.ceylon.compiler.js;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+
+import org.junit.Test;
+
+import com.redhat.ceylon.compiler.Options;
+import com.redhat.ceylon.compiler.typechecker.TypeChecker;
+import com.redhat.ceylon.compiler.typechecker.TypeCheckerBuilder;
+
+public class TestJavaDeps {
+
+    @Test
+    public void testJavaDependencies() throws IOException {
+        final TypeCheckerBuilder builder = new TypeCheckerBuilder();
+        builder.addSrcDirectory(new File("src/test/resources/javadeps"));
+        final TypeChecker tc = builder.getTypeChecker();
+        tc.process();
+        final Options opts = new Options(Collections.<String>emptyList(), Collections.singletonList("src/test/resources/javadeps"),
+                null, "./build", null, null, false, true, false, true, false, false, false, false, "UTF-8", false);
+        final JsCompiler comp = new JsCompiler(tc, opts);
+        comp.generate();
+    }
+
+}
---
+++ compiler-js/src/test/resources/javadeps/javadeps/module.ceylon
@@ -0,0 +1,4 @@
+"Test that units depending on Java modules generate errors"
+module javadeps '0.1' {
+    import jdk.base '7';
+}
