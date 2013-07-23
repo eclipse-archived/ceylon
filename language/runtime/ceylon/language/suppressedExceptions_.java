1da1fbbe8ad6eb5c27ff7f4c7c27c08699074a22
Tako Schotanus <tako@ceylon-lang.org>
2013-07-23 18:02:56 +0200

    Added suppressedExceptions() toplevel function (#5127)

---
+++ language/runtime/ceylon/language/suppressedExceptions_.java
@@ -0,0 +1,27 @@
+package ceylon.language;
+
+import com.redhat.ceylon.compiler.java.metadata.Ceylon;
+import com.redhat.ceylon.compiler.java.metadata.Method;
+import com.redhat.ceylon.compiler.java.metadata.Name;
+import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
+
+@Ceylon(major = 5)
+@Method
+public final class suppressedExceptions_ {
+    
+    private suppressedExceptions_() {}
+    
+    @TypeInfo("ceylon.language::Sequential<ceylon.language::Exception>")
+    public static ceylon.language.Sequential<? extends java.lang.Throwable> suppressedExceptions(
+            @Name("exception")
+            @TypeInfo("ceylon.language::Exception")
+            final java.lang.Throwable exception) {
+        Throwable[] sup = exception.getSuppressed();
+        if (sup.length > 0) {
+            return ArraySequence.backedBy$hidden(ceylon.language.Exception.$TypeDescriptor, sup, 0, sup.length);
+        } else {
+            return (ceylon.language.Sequential)empty_.$get();
+        }
+    }
+
+}
---
+++ language/src/ceylon/language/suppressedExceptions.ceylon
@@ -0,0 +1,2 @@
+"Returns the exceptions that were suppressed (if any) during the handling of the given exception."
+shared native Exception[] suppressedExceptions(Exception exception);
