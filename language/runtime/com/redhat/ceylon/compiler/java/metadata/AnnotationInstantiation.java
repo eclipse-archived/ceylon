63392a5ab92b50d9541f5322a4b89ad28560d22c
Tom Bentley <tom.bentley@cantab.net>
2013-08-07 11:13:56 +0100

    metadata/AnnotationInstantiation: Change the @Target elements of @AnnotationInstantiation so we can annotation default parameter methods with @AnnotationInstantiation if they invoke annotation classes or constructors. See #1221.

--- language/runtime/com/redhat/ceylon/compiler/java/metadata/AnnotationInstantiation.java
+++ language/runtime/com/redhat/ceylon/compiler/java/metadata/AnnotationInstantiation.java
@@ -7,12 +7,12 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /** 
- * Annotates the class containing an annotation constructor method
+ * Annotates an annotation constructor method
  * (i.e. adjacent to {@link Method @Method}) 
  * as being a trivial instantiation of an annotation class. 
  */
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
+@Target({ElementType.TYPE, ElementType.METHOD})
 public @interface AnnotationInstantiation {
     /** 
      * The annotation class (not the annotation type for that class) 
@@ -24,8 +24,13 @@ public @interface AnnotationInstantiation {
     // TODO Can also support a constructor which returns a value using some 
     // other annotation @AnnotationValue
     /** 
-     * A permutation of the annotation constructor parameters which produces the 
-     * argument list for the instantiation of the annotation class.
+     * <p>A permutation of the annotation constructor parameters which produces the 
+     * argument list for the instantiation of the annotation class.</p>
+     * <table><tbody>
+     * <tr><td>-32768</td>  <td>int, char, boolean, float or String literal</td></tr>
+     * <tr><td>0-255</td>   <td>the corresponding parameter</td></tr>
+     * <tr><td>256-511</td> <td>the corresponding parameter spread</td></tr>
+     * </tbody></table>
      */
     short[] arguments() default {};
 }
