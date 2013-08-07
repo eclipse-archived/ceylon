420855451b831fc8e3c881541e07628dae5c1cd4
Tom Bentley <tom.bentley@cantab.net>
2013-08-07 11:14:58 +0100

    Add @AnnotationInstantiationTree so we can serialize nested invocations of annotation constructors and classes. See #1221.

---
+++ language/runtime/com/redhat/ceylon/compiler/java/metadata/AnnotationInstantiationTree.java
@@ -0,0 +1,12 @@
+package com.redhat.ceylon.compiler.java.metadata;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface AnnotationInstantiationTree {
+    AnnotationInstantiation[] value();
+}
