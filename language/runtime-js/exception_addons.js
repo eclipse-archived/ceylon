0d910b1fdf6d81cb2dd67cc0b8787d78c91a40ce
Enrique Zamudio <alterbass@gmail.com>
2013-08-12 19:21:39 -0500

    Fix #5149 although without line numbers or source file

---
+++ language/runtime-js/exception_addons.js
@@ -0,0 +1,21 @@
+function $init$native$Exception$before(exc) {
+  var _caller=arguments.callee.caller.caller;
+  exc.stack_trace=[];
+  while(_caller) {
+    exc.stack_trace.push(_caller);
+    _caller = _caller.caller;
+  }
+}
+Exception.$$.prototype.printStackTrace = function() {
+  for (var i=0; i<this.stack_trace.length; i++) {
+    var f = this.stack_trace[i];
+    var mm = f.$$metamodel$$;
+    if (typeof(mm)==='function') {
+      mm = mm();
+      f.$$metamodel$$=mm;
+    }
+    if (mm) {
+        print("  at " + mm.pkg + "::" + mm.d.$nm);
+    }
+  }
+}
