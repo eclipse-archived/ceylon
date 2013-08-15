3fef14d42e05be51c8ed6a60adc1cae2efb9fba1
Enrique Zamudio <alterbass@gmail.com>
2013-08-14 20:22:19 -0500

    Implement AnnotatedDeclaration.annotations() natively in JS

---
+++ language/runtime-js/annotated_declaration.js
@@ -0,0 +1,22 @@
+//Add-on to AnnotatedDeclaration            //MethodDeclaration annotations at caca.ceylon (82:2-83:72)
+AnnotatedDeclaration$model$declaration.$$.prototype.annotations=function ($$$mptypes) {
+    var $$openInterface=this;
+    var ans = [];
+    var _mdl = $$openInterface.tipo.$$metamodel$$;
+    if (typeof(_mdl)==='function') {
+      _mdl = _mdl();
+      $$openInterface.tipo.$$metamodel$$=_mdl;
+    }
+    var _ans = _mdl.$an;
+    if (typeof(_ans)==='function') {
+      _ans = _ans();
+      _mdl.$an=_ans;
+    }
+    for (var i=0; i<_ans.length;i++) {
+      if (isOfType(_ans[i], $$$mptypes.Annotation)) {
+        ans.push(_ans[i]);
+      }
+    }
+    return ans.length == 0 ? getEmpty() : ans.reifyCeylonType({Element:$$$mptypes.Annotation});
+};
+AnnotatedDeclaration$model$declaration.$$.prototype.annotations.$$metamodel$$=function(){return{mod:$$METAMODEL$$,$t:{t:Sequential,a:{Element:'Annotation'}},$ps:[],$cont:AnnotatedDeclaration$model$declaration,$tp:{Annotation:{'var':'out','satisfies':[{t:Annotation$model,a:{Value:'Annotation'}}]}},$an:function(){return[shared(),formal()];},pkg:'ceylon.language.model.declaration',d:$$METAMODEL$$['ceylon.language.model.declaration']['AnnotatedDeclaration']['$m']['annotations']};};
