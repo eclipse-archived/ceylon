2c1451dd661d0f5ddee4c483d2b21403e9f6202f
Stephane Epardaud <stef@epardaud.fr>
2013-09-03 14:49:28 +0200

    metamodel: Fixed Eclipse errors in AppliedVariableAttribute

--- language/runtime/com/redhat/ceylon/compiler/java/runtime/metamodel/AppliedVariableAttribute.java
+++ language/runtime/com/redhat/ceylon/compiler/java/runtime/metamodel/AppliedVariableAttribute.java
@@ -1,11 +1,11 @@
 package com.redhat.ceylon.compiler.java.runtime.metamodel;
 
+import ceylon.language.Sequential;
 import ceylon.language.model.Member$impl;
 import ceylon.language.model.VariableAttribute$impl;
 
 import com.redhat.ceylon.compiler.java.metadata.Ignore;
 import com.redhat.ceylon.compiler.java.runtime.model.TypeDescriptor;
-import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedTypedReference;
 
 /**
@@ -35,7 +35,61 @@ public class AppliedVariableAttribute<Container, Type>
         // TODO Auto-generated method stub
         return null;
     }
-    
+
+    @Override
+    @Ignore
+    public ceylon.language.model.Variable<Type> $call$variadic() {
+        return (ceylon.language.model.Variable<Type>)super.$call$variadic();
+    }
+
+    @Override
+    @Ignore
+    public ceylon.language.model.Variable<Type> $call$variadic(Object arg0) {
+        return (ceylon.language.model.Variable<Type>)super.$call$variadic(arg0);
+    }
+
+    @Override
+    @Ignore
+    public ceylon.language.model.Variable<Type> $call$variadic(Object arg0, Object arg1) {
+        return (ceylon.language.model.Variable<Type>)super.$call$variadic(arg0, arg1);
+    }
+
+    @Override
+    @Ignore
+    public ceylon.language.model.Variable<Type> $call$variadic(Object arg0, Object arg1, Object arg2) {
+        return (ceylon.language.model.Variable<Type>)super.$call$variadic(arg0, arg1, arg2);
+    }
+
+    @Override
+    @Ignore
+    public ceylon.language.model.Variable<Type> $call$variadic(Object... args) {
+        return (ceylon.language.model.Variable<Type>)super.$call$variadic(args);
+    }
+
+    @Override
+    @Ignore
+    public ceylon.language.model.Variable<Type> $call$variadic(Sequential<?> varargs) {
+        return (ceylon.language.model.Variable<Type>)super.$call$variadic(varargs);
+    }
+
+    @Override
+    @Ignore
+    public ceylon.language.model.Variable<Type> $call$variadic(Object arg0, Sequential<?> varargs) {
+        return (ceylon.language.model.Variable<Type>)super.$call$variadic(arg0, varargs);
+    }
+
+    @Override
+    @Ignore
+    public ceylon.language.model.Variable<Type> $call$variadic(Object arg0, Object arg1, Sequential<?> varargs) {
+        return (ceylon.language.model.Variable<Type>)super.$call$variadic(arg0, arg1, varargs);
+    }
+
+    @Override
+    @Ignore
+    public ceylon.language.model.Variable<Type> $call$variadic(Object arg0, Object arg1, Object arg2, Sequential<?> varargs) {
+        return (ceylon.language.model.Variable<Type>)super.$call$variadic(arg0, arg1, arg2, varargs);
+    }
+
     @Override
     public ceylon.language.model.Variable<Type> $call() {
         return (ceylon.language.model.Variable<Type>)super.$call();
