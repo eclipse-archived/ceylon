0f5cd083b814da8fd608fa9450299b989da73631
Stephane Epardaud <stef@epardaud.fr>
2013-09-06 13:31:16 +0200

    Added @LocalContainer for #5138 to make the model loader aware of the fact that a type is a local type

---
+++ language/runtime/com/redhat/ceylon/compiler/java/metadata/LocalContainer.java
@@ -0,0 +1,11 @@
+package com.redhat.ceylon.compiler.java.metadata;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Annotation applied to local types
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LocalContainer {
+}
