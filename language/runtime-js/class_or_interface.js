de116e500e357b5e2fcc908161d52894eb47d9aa
Enrique Zamudio <alterbass@gmail.com>
2013-09-11 21:56:46 -0500

    Partial impl of FunctionalDeclaration.parameterDeclarations

--- language/runtime-js/class_or_interface.js
+++ language/runtime-js/class_or_interface.js
@@ -18,3 +18,20 @@ ClassOrInterfaceDeclaration$model$declaration.$$.prototype.getMemberDeclaration=
 };
 ClassOrInterfaceDeclaration$model$declaration.$$.prototype.getMemberDeclaration.$$metamodel$$=function(){return{mod:$$METAMODEL$$,$t:{ t:'u', l:[{t:Null},'Kind']},$ps:[{$nm:'name',$mt:'prm',$t:{t:String$}}],$cont:OpenClass,$tp:{Kind:{'satisfies':[{t:TopLevelOrMemberDeclaration$model$declaration}]}},$an:function(){return[shared(),actual()];},d:['ceylon.language.model.declaration','ClassOrInterfaceDeclaration','$m','getMemberDeclaration']};};
 
+defineAttr(FunctionalDeclaration$model$declaration.$$.prototype,'parameterDeclarations',function(){
+  var that=this;
+  var parms = that.meta['$ps'];
+  if (parms === null || parms === undefined || parms.length === 0)return getEmpty();
+  parms = parms[0];
+  if (parms === null || parms === undefined || parms.length === 0)return getEmpty();
+  var rv = [];
+  for (var i=0; i<parms.length;i++) {
+    var p = parms[i];
+    if (p['$pt'] === 'f') {
+      console.log("parametro funcional");
+    } else {
+      rv.push(OpenValue(p['$nm'], that.packageContainer, false, p));
+    }
+  }
+  return rv.reifyCeylonType({Element:{t:FunctionOrValueDeclaration$model$declaration},Absent:{t:Null}});
+},undefined,function(){return{mod:$$METAMODEL$$,$t:{t:Sequential,a:{Element:{t:FunctionOrValueDeclaration$model$declaration}}},$cont:FunctionalDeclaration$model$declaration,$an:function(){return[shared(),formal()];},d:['ceylon.language.model.declaration','FunctionalDeclaration','$at','parameterDeclarations']};});
