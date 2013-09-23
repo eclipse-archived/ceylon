b6d164591101ea900bcf1f54b15aba1bbbb6b876
Tako Schotanus <tako@ceylon-lang.org>
2013-09-23 22:07:35 +0200

    Fixed Ceylon release name and reset the binary version numbers

--- language/runtime-js/process.js
+++ language/runtime-js/process.js
@@ -16,9 +16,9 @@ defineAttr(lang$proto, 'minorVersion', function(){ return 6; },undefined,
   {$t:{t:Integer}, $cont:lang$proto, $an:function(){return[shared(),actual()]},mod:$$METAMODEL$$,d:['ceylon.language','language','$at','minorVersion']});
 defineAttr(lang$proto, 'releaseVersion', function(){ return 1; },undefined,
   {$t:{t:Integer}, $cont:lang$proto, $an:function(){return[shared(),actual()]},mod:$$METAMODEL$$,d:['ceylon.language','language','$at','releaseVersion']});
-defineAttr(lang$proto, 'versionName', function(){ return String$("Transmogrifier",14); },undefined,
+defineAttr(lang$proto, 'versionName', function(){ return String$("Virtual Boy",11); },undefined,
   {$t:{t:String$}, $cont:lang$proto, $an:function(){return[shared(),actual()]},mod:$$METAMODEL$$,d:['ceylon.language','language','$at','versionName']});
-defineAttr(lang$proto, 'majorVersionBinary', function(){ return 5; },undefined,
+defineAttr(lang$proto, 'majorVersionBinary', function(){ return 0; },undefined,
   {$t:{t:Integer}, $cont:lang$proto, $an:function(){return[shared(),actual()]},mod:$$METAMODEL$$,d:['ceylon.language','language','$at','majorVersionBinary']});
 defineAttr(lang$proto, 'minorVersionBinary', function(){ return 0; },undefined,
   {$t:{t:Integer}, $cont:lang$proto, $an:function(){return[shared(),actual()]},mod:$$METAMODEL$$,d:['ceylon.language','language','$at','minorVersionBinary']});
