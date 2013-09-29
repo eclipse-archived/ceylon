8913bc6495206d97c32f5ed6314198bc3b713e17
Akber Choudhry <akber@dgwave.com>
2013-09-30 00:21:45 +0100

    Allow empty groups in DocLink, fixes #3905

--- typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/LiteralVisitor.java
+++ typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/LiteralVisitor.java
@@ -29,7 +29,7 @@ import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 public class LiteralVisitor extends Visitor {
 	
     private int indent;
-    static final Pattern DOC_LINK_PATTERN = Pattern.compile("\\[\\[([^\"`|\\[\\]]*\\|)?((((\\w|\\.)+)::)?(\\w+)(\\.(\\w+))*)\\]\\]");
+    static final Pattern DOC_LINK_PATTERN = Pattern.compile("\\[\\[([^\"`|\\[\\]]*\\|)?((((\\w|\\.)+)::)?(\\w*)(\\.(\\w*))*)\\]\\]");
     private static Pattern CHARACTER_ESCAPE_PATTERN = Pattern.compile("\\\\(\\{#([^}]*)\\}|\\{([^}^#]*)\\}|(.))");
     
     
