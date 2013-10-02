efec5d395ef790b5992a627b6a02333fff1505bc
Tako Schotanus <tako@ceylon-lang.org>
2013-10-02 10:59:59 +0200

    Added constants for environment variable and property names

--- common/src/com/redhat/ceylon/common/Constants.java
+++ common/src/com/redhat/ceylon/common/Constants.java
@@ -6,5 +6,17 @@ public abstract class Constants {
     }
 
     public static final String CEYLON_SUFFIX = ".ceylon";
+
+    public static final String ENV_CEYLON_HOME_DIR = "CEYLON_HOME";
+
+    public static final String PROP_CEYLON_SYSTEM_VERSION = "ceylon.system.version";
+
+    public static final String PROP_CEYLON_HOME_DIR = "ceylon.home";
+    public static final String PROP_CEYLON_CONFIG_DIR = "ceylon.config.dir";
+    public static final String PROP_CEYLON_USER_DIR = "ceylon.user.dir";
+    public static final String PROP_CEYLON_SYSLIBS_DIR = "ceylon.system.libs";
     
+    public static final String PROP_CEYLON_SYSTEM_REPO = "ceylon.system.repo";
+    public static final String PROP_CEYLON_USER_REPO = "ceylon.user.repo";
+    public static final String PROP_CEYLON_CACHE_REPO = "ceylon.cache.repo";
 }
