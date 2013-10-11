662a63a9b44627ec6284540eb9c0953cc6b2b729
Tako Schotanus <tako@ceylon-lang.org>
2013-10-11 02:40:46 +0200

    The CompilerConfig now properly handles the `cwd` option if it was set in the options (see #4606)

--- compiler-java/src/com/redhat/ceylon/compiler/java/tools/CompilerConfig.java
+++ compiler-java/src/com/redhat/ceylon/compiler/java/tools/CompilerConfig.java
@@ -3,14 +3,21 @@ package com.redhat.ceylon.compiler.java.tools;
 import java.io.File;
 
 import com.redhat.ceylon.common.config.CeylonConfig;
+import com.sun.tools.javac.main.OptionName;
 import com.sun.tools.javac.util.Context;
+import com.sun.tools.javac.util.Options;
 
 public class CompilerConfig {
 
     public static CeylonConfig instance(Context context) {
         CeylonConfig instance = context.get(CeylonConfig.class);
         if (instance == null) {
-            instance = CeylonConfig.createFromLocalDir(new File("."));
+            Options options = Options.instance(context);
+            String cwd = options.get(OptionName.CEYLONCWD);
+            if (cwd == null) {
+                cwd = ".";
+            }
+            instance = CeylonConfig.createFromLocalDir(new File(cwd));
             context.put(CeylonConfig.class, instance);
         }
         return instance;
