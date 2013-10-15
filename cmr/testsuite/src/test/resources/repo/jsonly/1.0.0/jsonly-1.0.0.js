f83d2201b830de528bb8c540960d1973633f1960
Tako Schotanus <tako@ceylon-lang.org>
2013-10-15 18:54:41 +0200

    Updated the jsonly module to one compiled with the latest compiler to be able to test binary version retrieval

--- cmr/testsuite/src/test/resources/repo/jsonly/1.0.0/jsonly-1.0.0.js
+++ cmr/testsuite/src/test/resources/repo/jsonly/1.0.0/jsonly-1.0.0.js
@@ -1,17 +1,15 @@
 (function(define) { define(function(require, exports, module) {
-var $$METAMODEL$$={"$mod-deps":["ceylon.language\/0.6"],"$mod-name":"jsonly","$mod-version":"1.0.0","jsonly":{"$pkg-shared":"1","run":{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Anything"},"$mt":"mthd","$an":{"shared":[]},"$nm":"run"}}};
+var $$METAMODEL$$={"$mod-deps":["ceylon.language\/1.0.0"],"$mod-name":"jsonly","$mod-version":"1.0.0","jsonly":{"$pkg-shared":"1","run":{"$t":{"$md":"ceylon.language","$pk":"ceylon.language","$nm":"Anything"},"$mt":"mthd","$an":{"shared":[]},"$nm":"run"}},"$mod-bin":"6.0"};
 exports.$$METAMODEL$$=function(){return $$METAMODEL$$;};
-var $$$cl3887=require('ceylon/language/0.6/ceylon.language-0.6');
-$$$cl3887.$addmod$($$$cl3887,'ceylon.language/0.6');
-exports.$pkg$ans$jsonly=function(){return[$$$cl3887.shared()];};
-exports.$mod$ans$=function(){return[$$$cl3887.doc($$$cl3887.String("A test",6)),$$$cl3887.by([$$$cl3887.String("The Ceylon Team",15)].reifyCeylonType({Absent:{t:$$$cl3887.Null},Element:{t:$$$cl3887.String}})),$$$cl3887.license($$$cl3887.String("Apache Software License",23))];};
-
-//MethodDefinition run at jsonly.ceylon (2:0-4:0)
+var $$$cl1=require('ceylon/language/1.0.0/ceylon.language-1.0.0');
+$$$cl1.$addmod$($$$cl1,'ceylon.language/1.0.0');
+exports.$pkg$ans$jsonly=function(){return[$$$cl1.shared()];};
+exports.$mod$ans$=function(){return[$$$cl1.doc($$$cl1.String("A test",6)),$$$cl1.by([$$$cl1.String("The Ceylon Team",15)].reifyCeylonType({Absent:{t:$$$cl1.Null},Element:{t:$$$cl1.String}})),$$$cl1.license($$$cl1.String("Apache Software License",23))];};
 function run(){
-    $$$cl3887.print($$$cl3887.String("jsonly",6));
+    $$$cl1.print($$$cl1.String("jsonly",6));
 }
 exports.run=run;
-run.$$metamodel$$=function(){return{mod:$$METAMODEL$$,$t:{t:$$$cl3887.Anything},$ps:[],$an:function(){return[$$$cl3887.shared()];},d:['jsonly','run']};};
+run.$$metamodel$$=function(){return{mod:$$METAMODEL$$,$t:{t:$$$cl1.Anything},$ps:[],$an:function(){return[$$$cl1.shared()];},d:['jsonly','run']};};
 });
 }(typeof define==='function' && define.amd ? define : function (factory) {
 if (typeof exports!=='undefined') { factory(require, exports, module);
