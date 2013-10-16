ab8c387c3c1079e6a1d2c5c222e8ce8d976d69ce
Stephane Epardaud <stef@epardaud.fr>
2013-10-16 13:52:33 +0200

    AbstractCallable: fixed inheritance of type parameters for Callable #5198

--- language/runtime/com/redhat/ceylon/compiler/java/language/AbstractCallable.java
+++ language/runtime/com/redhat/ceylon/compiler/java/language/AbstractCallable.java
@@ -1,15 +1,19 @@
 package com.redhat.ceylon.compiler.java.language;
 
-import java.util.Arrays;
-
-import com.redhat.ceylon.compiler.java.Util;
-import com.redhat.ceylon.compiler.java.metadata.Ignore;
-import com.redhat.ceylon.compiler.java.runtime.model.ReifiedType;
-import com.redhat.ceylon.compiler.java.runtime.model.TypeDescriptor;
-
 import ceylon.language.Callable;
 import ceylon.language.Sequential;
 import ceylon.language.empty_;
+
+import com.redhat.ceylon.compiler.java.Util;
+import com.redhat.ceylon.compiler.java.metadata.Ceylon;
+import com.redhat.ceylon.compiler.java.metadata.Class;
+import com.redhat.ceylon.compiler.java.metadata.Ignore;
+import com.redhat.ceylon.compiler.java.metadata.SatisfiedTypes;
+import com.redhat.ceylon.compiler.java.metadata.TypeParameter;
+import com.redhat.ceylon.compiler.java.metadata.TypeParameters;
+import com.redhat.ceylon.compiler.java.metadata.Variance;
+import com.redhat.ceylon.compiler.java.runtime.model.ReifiedType;
+import com.redhat.ceylon.compiler.java.runtime.model.TypeDescriptor;
 
 /**
  * Stub implementation for {@link Callable}, so the anonymous inner classes 
@@ -20,37 +24,57 @@ import ceylon.language.empty_;
 // Note: The compiler sometimes generates anonymous subclasses with 
 // an extra $call$typed() method. This is used when dealing with 
 // defaulted parameters so as to avoid multiple boxings and unboxings.
-
-public class AbstractCallable<Return> implements Callable<Return>, ReifiedType {
+@Ceylon(major = 6)
+@Class
+@TypeParameters({
+    @TypeParameter(value="Return", variance=Variance.OUT),
+    @TypeParameter(value="Arguments", satisfies="ceylon.language::Sequential<ceylon.language::Anything>", variance=Variance.IN)
+})
+@SatisfiedTypes("ceylon.language::Callable<Return,Arguments>")
+public abstract class AbstractCallable<Return> implements Callable<Return>, ReifiedType {
     
-    private final String string;
+    @Ignore
     private final TypeDescriptor $reifiedArguments;
+    @Ignore
     private final TypeDescriptor $reifiedReturn;
+    private final String string;
     private final short variadicParameterIndex;
 
-    public AbstractCallable(TypeDescriptor $reifiedReturn, TypeDescriptor $reifiedArguments, String string, short variadicParameterIndex) {
+    public AbstractCallable(@Ignore TypeDescriptor $reifiedReturn, 
+            @Ignore TypeDescriptor $reifiedArguments, 
+            String string, short variadicParameterIndex) {
         this.string = string;
         this.$reifiedReturn = $reifiedReturn;
         this.$reifiedArguments = $reifiedArguments;
         this.variadicParameterIndex = variadicParameterIndex;
     }
     
+    @Override
+    @Ignore
     public Return $call() {
         throw new UnsupportedOperationException();
     }
     
+    @Override
+    @Ignore
     public Return $call(java.lang.Object arg0) {
         throw new UnsupportedOperationException();
     }
     
+    @Override
+    @Ignore
     public Return $call(java.lang.Object arg0, java.lang.Object arg1) {
         throw new UnsupportedOperationException();
     }
     
+    @Override
+    @Ignore
     public Return $call(java.lang.Object arg0, java.lang.Object arg1, java.lang.Object arg2) {
         throw new UnsupportedOperationException();
     }
     
+    @Override
+    @Ignore
     public Return $call(java.lang.Object... args) {
         throw new UnsupportedOperationException();
     }
