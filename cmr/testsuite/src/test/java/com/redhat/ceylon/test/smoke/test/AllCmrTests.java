2b6b9f9d6a572b7f67830523344a5169e07017b7
Tako Schotanus <tako@ceylon-lang.org>
2013-10-17 20:09:02 +0200

    Added `AllCmrTests`

---
+++ cmr/testsuite/src/test/java/com/redhat/ceylon/test/smoke/test/AllCmrTests.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the authors tag. All rights reserved.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU General Public License version 2.
+ * 
+ * This particular file is subject to the "Classpath" exception as provided in the 
+ * LICENSE file that accompanied this code.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License,
+ * along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package com.redhat.ceylon.test.smoke.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.redhat.ceylon.test.maven.test.AetherTestCase;
+
+@RunWith(Suite.class) 
+@SuiteClasses({
+    SmokeTestCase.class,
+    HerdTestCase.class,
+    AetherTestCase.class
+})
+public class AllCmrTests {
+}
