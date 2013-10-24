5aa7bc83821861cacfda461a792d04f9faaefa9b
Enrique Zamudio <alterbass@gmail.com>
2013-10-24 15:14:28 -0500

    retuple for #2708

--- language/runtime-js/functions3.js
+++ language/runtime-js/functions3.js
@@ -10,3 +10,23 @@ function suppressedExceptions(/*Exception*/e) {
     return e.$sups$===undefined?getEmpty():e.$sups$;
 }
 exports.suppressedExceptions=suppressedExceptions;
+function $retuple(t) { //receives {t:'T',l:[...]}
+  if (t.t!=='T')return t;
+  var e;
+  var r={t:Empty};
+  for (var i=t.l.length-1;i>=0;i--){
+    var f=$retuple(t.l[i]);
+    var e=(r.a&&r.a.Element)||f;
+    if (r.a&&r.a.Element) {
+      if (e.l) {
+        var l2=[];for(var j=0;j<e.l.length;j++)l2.push(e.l[j]);
+        l2.unshift(f);
+        e={t:'u',l:l2};
+      } else {
+        e = {t:'u',l:[f,e]};
+      }
+    }
+    r={t:Tuple,a:{First:f,Element:e,Rest:r}};
+  }
+  return r;
+}
