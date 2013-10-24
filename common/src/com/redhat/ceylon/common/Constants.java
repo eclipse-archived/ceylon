58bbde58d9f1462bb40a9f3ad7bd4a5f5ffa2055
Tako Schotanus <tako@ceylon-lang.org>
2013-10-24 12:56:55 +0200

    Added constants for the default folders we use: "source, "resource" and "modules"

--- common/src/com/redhat/ceylon/common/Constants.java
+++ common/src/com/redhat/ceylon/common/Constants.java
@@ -19,4 +19,8 @@ public abstract class Constants {
     public static final String PROP_CEYLON_SYSTEM_REPO = "ceylon.system.repo";
     public static final String PROP_CEYLON_USER_REPO = "ceylon.user.repo";
     public static final String PROP_CEYLON_CACHE_REPO = "ceylon.cache.repo";
+    
+    public static final String DEFAULT_SOURCE_DIR = "source";
+    public static final String DEFAULT_RESOURCE_DIR = "resource";
+    public static final String DEFAULT_MODULE_DIR = "modules";
 }
