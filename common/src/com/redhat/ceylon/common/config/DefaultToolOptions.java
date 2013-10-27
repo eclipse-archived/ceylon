51681c493ffba58e95fa9fcb20c6080d0de1aba1
Tako Schotanus <tako@ceylon-lang.org>
2013-10-27 02:16:27 +0200

    Added helper methods to easily get at the configured or default source, resource and output directories, as well as the offline and encoding options.

--- common/src/com/redhat/ceylon/common/config/DefaultToolOptions.java
+++ common/src/com/redhat/ceylon/common/config/DefaultToolOptions.java
@@ -1,11 +1,71 @@
 package com.redhat.ceylon.common.config;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import com.redhat.ceylon.common.Constants;
+import com.redhat.ceylon.common.FileUtil;
+
 public class DefaultToolOptions {
     
     public final static String DEFAULTS_ENCODING = "defaults.encoding";
     public final static String DEFAULTS_OFFLINE = "defaults.offline";
     
+    public final static String COMPILER_SOURCE = "compiler.source";
+    public final static String COMPILER_RESOURCE = "compiler.resource";
+    
     private DefaultToolOptions() {
     }
+    
+    public static String getDefaultEncoding() {
+        return getDefaultEncoding(CeylonConfig.get());
+    }
+    
+    public static String getDefaultEncoding(CeylonConfig config) {
+        return config.getOption(DEFAULTS_ENCODING);
+    }
+    
+    public static boolean getDefaultOffline() {
+        return getDefaultOffline(CeylonConfig.get());
+    }
+    
+    public static boolean getDefaultOffline(CeylonConfig config) {
+        return config.getBoolOption(DEFAULTS_OFFLINE, false);
+    }
+    
+    public static List<File> getCompilerSourceDirs() {
+        return getCompilerSourceDirs(CeylonConfig.get());
+    }
+    
+    public static List<File> getCompilerSourceDirs(CeylonConfig config) {
+        String[] dirs = config.getOptionValues(COMPILER_SOURCE);
+        if (dirs != null) {
+            return Arrays.asList(FileUtil.pathsToFileArray(dirs));
+        } else {
+            return Collections.singletonList(new File(Constants.DEFAULT_SOURCE_DIR));
+        }
+    }
+    
+    public static List<File> getCompilerResourceDirs() {
+        return getCompilerResourceDirs(CeylonConfig.get());
+    }
 
+    public static List<File> getCompilerResourceDirs(CeylonConfig config) {
+        String[] dirs = config.getOptionValues(COMPILER_RESOURCE);
+        if (dirs != null) {
+            return Arrays.asList(FileUtil.pathsToFileArray(dirs));
+        } else {
+            return Collections.singletonList(new File(Constants.DEFAULT_RESOURCE_DIR));
+        }
+    }
+    
+    public static File getCompilerOutDir() {
+        return getCompilerOutDir(CeylonConfig.get());
+    }
+
+    public static File getCompilerOutDir(CeylonConfig config) {
+        return new File(Repositories.withConfig(config).getOutputRepository().getUrl());
+    }
 }
