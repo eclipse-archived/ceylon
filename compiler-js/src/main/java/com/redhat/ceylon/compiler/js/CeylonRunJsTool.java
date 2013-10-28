465d0f866e89d084490cd0b5a4090066dd085ef7
Tako Schotanus <tako@ceylon-lang.org>
2013-10-28 14:19:28 +0100

    Added "check" flag to argument description

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/CeylonRunJsTool.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/CeylonRunJsTool.java
@@ -167,7 +167,7 @@ public class CeylonRunJsTool extends RepoUsingTool {
     @Option
     @OptionArgument(argumentName = "flags")
     @Description("Determines if and how compilation should be handled." +
-            "Allowed flags include: `never`, `once`, `force`.")
+            "Allowed flags include: `never`, `once`, `force`, `check`.")
     public void setCompile(String compile) {
         this.compileFlags = compile;
     }
