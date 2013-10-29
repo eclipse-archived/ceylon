2f5614878dfed388cbbde3b8e08d1b31c7a79a50
Stephane Epardaud <stef@epardaud.fr>
2013-10-29 10:49:43 +0100

    Revert "model loader #1392: set the visible scope everywhere we can"
    This reverts commit bd68589d7f62d51fc975052a8d60cb29ad940a21.

--- compiler-java/src/com/redhat/ceylon/compiler/loader/AbstractModelLoader.java
+++ compiler-java/src/com/redhat/ceylon/compiler/loader/AbstractModelLoader.java
@@ -559,7 +559,6 @@ public abstract class AbstractModelLoader implements ModelCompleter, ModelLoader
         if(!classMirror.isInnerClass() && !classMirror.isLocalClass()){
             d.setContainer(pkg);
             pkg.addCompiledMember(d);
-            DeclarationVisitor.setVisibleScope(d);
         }else if(classMirror.isLocalClass()){
             // set its container to the package for now, but don't add it to the package as a member because it's not
             d.setContainer(pkg);
@@ -572,14 +571,12 @@ public abstract class AbstractModelLoader implements ModelCompleter, ModelLoader
                 d.setContainer(container);
                 // let's not trigger lazy-loading
                 ((LazyContainer)container).addMember(d);
-                DeclarationVisitor.setVisibleScope(d);
                 // now we can do overloads
                 if(d instanceof Class && ((Class)d).getOverloads() != null){
                     for(Declaration overload : ((Class)d).getOverloads()){
                         overload.setContainer(container);
                         // let's not trigger lazy-loading
                         ((LazyContainer)container).addMember(d);
-                        DeclarationVisitor.setVisibleScope(overload);
                     }
                 }
             }
@@ -1796,8 +1793,7 @@ public abstract class AbstractModelLoader implements ModelCompleter, ModelLoader
         setAnnotations(method, methodMirror);
         
         klass.getMembers().add(method);
-        DeclarationVisitor.setVisibleScope(method);
-
+        
         return method;
     }
 
@@ -1965,7 +1961,6 @@ public abstract class AbstractModelLoader implements ModelCompleter, ModelLoader
         markTypeErased(value, fieldMirror, fieldMirror.getType());
         setAnnotations(value, fieldMirror);
         klass.getMembers().add(value);
-        DeclarationVisitor.setVisibleScope(value);
     }
     
     private boolean isRaw(Module module, TypeMirror type) {
@@ -2070,7 +2065,6 @@ public abstract class AbstractModelLoader implements ModelCompleter, ModelLoader
         markTypeErased(value, methodMirror, methodMirror.getReturnType());
         setAnnotations(value, methodMirror);
         klass.getMembers().add(value);
-        DeclarationVisitor.setVisibleScope(value);
     }
 
     private boolean isUncheckedNull(AnnotatedMirror methodMirror) {
