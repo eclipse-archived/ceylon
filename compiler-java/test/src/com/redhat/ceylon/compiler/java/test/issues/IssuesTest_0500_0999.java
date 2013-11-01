4dcbf0a26a0787fed621555b02f5551534ed9ce8
Stephane Epardaud <stef@epardaud.fr>
2013-11-01 16:23:53 +0100

    Updated test for #713 since we now allow overriding a single overloaded method

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTest_0500_0999.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTest_0500_0999.java
@@ -377,8 +377,7 @@ public class IssuesTest_0500_0999 extends CompilerTest {
     
     @Test
     public void testBug713() {
-        assertErrors("bug07xx/Bug713",
-                new CompilerError(26, "member does not have the same number of parameters as the member it refines: getComponentAt declared by Bug713 refining getComponentAt declared by Container"));
+        compareWithJavaSource("bug07xx/Bug713");
     }
     
     @Ignore("M6: https://github.com/ceylon/ceylon-compiler/issues/714")
