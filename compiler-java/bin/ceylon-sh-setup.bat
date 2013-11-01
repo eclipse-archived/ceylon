cb5fe32de7405a9167116d85f1b085bfd36517da
Tako Schotanus <tako@ceylon-lang.org>
2013-11-01 13:51:51 +0100

    Implemented Windows version of the ceylon-sh-setup script (#750)

--- compiler-java/bin/ceylon-sh-setup.bat
+++ compiler-java/bin/ceylon-sh-setup.bat
@@ -1,1 +1,21 @@
-THIS FILE NEEDS CONTENT
+
+:loop_args
+    if "%~1" == "" goto :done_args
+    
+    if "%~1" == "--_print-summary" (
+        echo "%DESCRIPTION%"
+        exit
+    )
+    if "%~1" == "--_print-description" (
+        echo "%LONG_USAGE%"
+        exit
+    )
+    if "%~1" == "--_print-usage" (
+        echo "%USAGE%"
+        exit
+    )
+    shift
+    goto :loop_args
+)
+:done_args
+
