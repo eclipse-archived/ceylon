41239d13e4ac9ccbe66f63aa1a41e3c64310e298
Stephane Epardaud <stef@epardaud.fr>
2013-11-01 22:07:14 +0100

    Updated errors due to typechecker changes

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTest_1000_1499.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTest_1000_1499.java
@@ -711,8 +711,7 @@ public class IssuesTest_1000_1499 extends CompilerTest {
     @Test
     public void testBug1405() {
         assertErrors("bug14xx/Bug1405",
-                new CompilerError(24, "value type could not be inferred"),
-                new CompilerError(24, "ambiguous reference to overloaded method or class: there must be exactly one overloaded declaration of State that accepts the given argument types")
+                new CompilerError(24, "member type is not visible: State of type Thread")
         );
     }
 
