c5e6ae221f5133a5d54efaab94ebeb25d9d411f2
Tako Schotanus <tako@codejive.org>
2013-11-01 16:58:37 +0100

    Fixed Windows setup script (#750)

--- compiler-java/bin/ceylon-sh-setup.bat
+++ compiler-java/bin/ceylon-sh-setup.bat
@@ -1,21 +1,22 @@
+@echo off
+setlocal enabledelayedexpansion
 
 :loop_args
     if "%~1" == "" goto :done_args
     
     if "%~1" == "--_print-summary" (
-        echo "%DESCRIPTION%"
-        exit
+        echo !DESCRIPTION!
+        exit /b 1
     )
     if "%~1" == "--_print-description" (
-        echo "%LONG_USAGE%"
-        exit
+        echo !LONG_USAGE!
+        exit /b 1
     )
     if "%~1" == "--_print-usage" (
-        echo "%USAGE%"
-        exit
+        echo !USAGE!
+        exit /b 1
     )
     shift
     goto :loop_args
 )
 :done_args
-
