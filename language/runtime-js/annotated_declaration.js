93938818506c032ca61b2409348a02c1b225de18
Enrique Zamudio <alterbass@gmail.com>
2013-11-04 23:43:30 -0600

    simplify code

--- language/runtime-js/annotated_declaration.js
+++ language/runtime-js/annotated_declaration.js
@@ -1,22 +1,21 @@
 //Add-on to AnnotatedDeclaration
 AnnotatedDeclaration$meta$declaration.$$.prototype.annotations=function ($$$mptypes) {
-    var $$openInterface=this;
-    var ans = [];
-    var _mdl = $$openInterface.tipo.$$metamodel$$;
-    if (typeof(_mdl)==='function') {
-      _mdl = _mdl();
-      $$openInterface.tipo.$$metamodel$$=_mdl;
+  var ans = [];
+  var _mdl = this.tipo.$$metamodel$$;
+  if (typeof(_mdl)==='function') {
+    _mdl = _mdl();
+    this.tipo.$$metamodel$$=_mdl;
+  }
+  var _ans = _mdl.$an;
+  if (typeof(_ans)==='function') {
+    _ans = _ans();
+    _mdl.$an=_ans;
+  }
+  for (var i=0; i<_ans.length;i++) {
+    if (isOfType(_ans[i], $$$mptypes.Annotation)) {
+      ans.push(_ans[i]);
     }
-    var _ans = _mdl.$an;
-    if (typeof(_ans)==='function') {
-      _ans = _ans();
-      _mdl.$an=_ans;
-    }
-    for (var i=0; i<_ans.length;i++) {
-      if (isOfType(_ans[i], $$$mptypes.Annotation)) {
-        ans.push(_ans[i]);
-      }
-    }
-    return ans.length == 0 ? getEmpty() : ans.reifyCeylonType({Element:$$$mptypes.Annotation});
+  }
+  return ans.length == 0 ? getEmpty() : ans.reifyCeylonType({Element:$$$mptypes.Annotation});
 };
 AnnotatedDeclaration$meta$declaration.$$.prototype.annotations.$$metamodel$$=function(){return{mod:$$METAMODEL$$,$t:{t:Sequential,a:{Element:'Annotation'}},$ps:[],$cont:AnnotatedDeclaration$meta$declaration,$tp:{Annotation:{'var':'out','satisfies':[{t:Annotation,a:{Value:'Annotation'}}]}},$an:function(){return[shared(),formal()];},d:['ceylon.language.meta.declaration','AnnotatedDeclaration','$m','annotations']};};
