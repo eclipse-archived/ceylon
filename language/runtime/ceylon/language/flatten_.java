c668ca8a4808157a60858027070a407d5b752c52
Gavin King <gavin@hibernate.org>
2013-11-06 17:17:45 +0100

    after all that work, a simpler and much more efficient impl!

--- language/runtime/ceylon/language/flatten_.java
+++ language/runtime/ceylon/language/flatten_.java
@@ -19,7 +19,6 @@ public final class flatten_ {
     @TypeParameters({@TypeParameter(value="Return"),
                      @TypeParameter(value="Args", satisfies="ceylon.language::Sequential<ceylon.language::Anything>")})
     @TypeInfo("ceylon.language::Callable<Return,Args>")
-    @SuppressWarnings({"unchecked", "rawtypes"})
     public static <Return,Args> Callable<Return> flatten(
             @Ignore final TypeDescriptor $reifiedReturn,
             @Ignore final TypeDescriptor $reifiedArgs, 
@@ -29,6 +28,10 @@ public final class flatten_ {
         
         return new AbstractCallable<Return>($reifiedReturn, $reifiedArgs, null, (short)-1) {
 
+            private TypeDescriptor getElementType() {
+                return ((TypeDescriptor.Class) $reifiedArgs).getTypeArguments()[0];
+            }
+
             @Override
             public Return $call$() {
                 return tupleFunction.$call$(empty_.get_());
@@ -36,42 +39,22 @@ public final class flatten_ {
 
             @Override
             public Return $call$(java.lang.Object arg0) {
-                TypeDescriptor[] typeArguments = ((TypeDescriptor.Class)$reifiedArgs).getTypeArguments();
-                return tupleFunction.$call$(new Tuple(typeArguments[0], typeArguments[1], typeArguments[2], arg0, empty_.get_()));
+                return tupleFunction.$call$(new Tuple(getElementType(), new java.lang.Object[] { arg0 }));
             }
 
             @Override
             public Return $call$(java.lang.Object arg0, java.lang.Object arg1) {
-                TypeDescriptor[] typeArguments0 = ((TypeDescriptor.Class)$reifiedArgs).getTypeArguments();
-                TypeDescriptor[] typeArguments1 = ((TypeDescriptor.Class)typeArguments0[2]).getTypeArguments();
-                return tupleFunction.$call$(new Tuple(typeArguments0[0], typeArguments0[1], typeArguments0[2], 
-                                           arg0, new Tuple(typeArguments1[0], typeArguments1[1], typeArguments1[2], 
-                                                           arg1, empty_.get_())));
+                return tupleFunction.$call$(new Tuple(getElementType(), new java.lang.Object[] { arg0, arg1 }));
             }
 
             @Override
             public Return $call$(java.lang.Object arg0, java.lang.Object arg1, java.lang.Object arg2) {
-                TypeDescriptor[] typeArguments0 = ((TypeDescriptor.Class)$reifiedArgs).getTypeArguments();
-                TypeDescriptor[] typeArguments1 = ((TypeDescriptor.Class)typeArguments0[2]).getTypeArguments();
-                TypeDescriptor[] typeArguments2 = ((TypeDescriptor.Class)typeArguments1[2]).getTypeArguments();
-                return tupleFunction.$call$(new Tuple(typeArguments0[0], typeArguments0[1], typeArguments0[2], 
-                                           arg0, new Tuple(typeArguments1[0], typeArguments1[1], typeArguments1[2], 
-                                                           arg1, new Tuple(typeArguments2[0], typeArguments2[1], typeArguments2[2], 
-                                                                           arg2, empty_.get_()))));
+                return tupleFunction.$call$(new Tuple(getElementType(), new java.lang.Object[] { arg0, arg1, arg2 }));
             }
 
             @Override
             public Return $call$(java.lang.Object... args) {
-                Sequential t = empty_.get_();
-                TypeDescriptor[][] typeArguments = new TypeDescriptor[args.length][];
-                typeArguments[0] = ((TypeDescriptor.Class)$reifiedArgs).getTypeArguments();
-                for (int i=1; i<args.length; i++) {
-                    typeArguments[i] = ((TypeDescriptor.Class)typeArguments[i-1][2]).getTypeArguments();
-                }
-                for (int i=args.length-1;i>=0;i--) {
-                    t = new Tuple(typeArguments[i][0], typeArguments[i][1], typeArguments[i][2], args[i], t);
-                }
-                return tupleFunction.$call$(t);
+                return tupleFunction.$call$(new Tuple(getElementType(), args));
             }
             
             @Override
