6f9405fa278c335fa4715ff510d401e9a7414c6c
Gavin King <gavin@hibernate.org>
2013-11-12 15:06:41 +0100

    add missing space

--- runtime/bootstrap/src/main/java/ceylon/modules/bootstrap/CeylonTestTool.java
+++ runtime/bootstrap/src/main/java/ceylon/modules/bootstrap/CeylonTestTool.java
@@ -64,7 +64,7 @@ public class CeylonTestTool extends RepoUsingTool {
 
     @Option
     @OptionArgument(argumentName = "flags")
-    @Description("Determines if and how compilation should be handled." +
+    @Description("Determines if and how compilation should be handled. " +
             "Allowed flags include: `never`, `once`, `force`, `check`.")
     public void setCompile(String compile) {
         this.compileFlags = compile;
