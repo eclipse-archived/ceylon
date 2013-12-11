8e594a4053378f0217c83e131fc5be862eda5460
Stephane Epardaud <stef@epardaud.fr>
2013-12-11 12:06:04 +0100

    Optim: made ProducedTypeCache thread-safe
    I'm sure the IDE will appreciate that

--- typechecker/src/com/redhat/ceylon/compiler/typechecker/context/ProducedTypeCache.java
+++ typechecker/src/com/redhat/ceylon/compiler/typechecker/context/ProducedTypeCache.java
@@ -1,14 +1,20 @@
 package com.redhat.ceylon.compiler.typechecker.context;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
 import com.redhat.ceylon.compiler.typechecker.model.TypeDeclaration;
+import com.redhat.ceylon.compiler.typechecker.model.UnknownType;
 
 public class ProducedTypeCache {
 
-    private final Map<ProducedType, Map<TypeDeclaration, ProducedType>> superTypes = new HashMap<>();
+    // need a special value for null because ConcurrentHashMap does not support null
+    private final static ProducedType NULL_VALUE = new UnknownType(null).getType();
+    
+    // need ConcurrentHashMap even for the cache, otherwise get/put/containsKey can get info infinite loops
+    // on concurrent operations
+    private final Map<ProducedType, Map<TypeDeclaration, ProducedType>> superTypes = new ConcurrentHashMap<>();
     
     public boolean containsKey(ProducedType producedType, TypeDeclaration dec) {
         Map<TypeDeclaration, ProducedType> cache = superTypes.get(producedType);
@@ -21,15 +27,20 @@ public class ProducedTypeCache {
         Map<TypeDeclaration, ProducedType> cache = superTypes.get(producedType);
         if(cache == null)
             return null;
-        return cache.get(dec);
+        ProducedType ret = cache.get(dec);
+        return ret == NULL_VALUE ? null : ret;
     }
 
     public void put(ProducedType producedType, TypeDeclaration dec, ProducedType superType) {
         Map<TypeDeclaration, ProducedType> cache = superTypes.get(producedType);
         if(cache == null){
-            cache = new HashMap<>();
+            // need ConcurrentHashMap even for the cache, otherwise get/put/containsKey can get info infinite loops
+            // on concurrent operations
+            cache = new ConcurrentHashMap<>();
             superTypes.put(producedType, cache);
         }
+        if(superType == null)
+            superType = NULL_VALUE;
         cache.put(dec, superType);
     }
 
