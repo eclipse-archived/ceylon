a50dd0a610d7c3f63f626f871b11791688e9c27c
Enrique Zamudio <alterbass@gmail.com>
2014-03-05 11:35:50 -0600

    refactor some of the work on #2757 for reuse

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/BmeGenerator.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/BmeGenerator.java
@@ -2,10 +2,13 @@ package com.redhat.ceylon.compiler.js;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.model.Method;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
+import com.redhat.ceylon.compiler.typechecker.model.TypeDeclaration;
 import com.redhat.ceylon.compiler.typechecker.model.TypeParameter;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 
@@ -37,24 +40,11 @@ public class BmeGenerator {
             if (who != null && who.isEmpty()) {
                 who=null;
             }
-            final boolean hasTparms = bme.getTypeArguments() != null && !bme.getTypeArguments().getTypeModels().isEmpty();
+            final boolean hasTparms = hasTypeParameters(bme);
             if (isCallable && (who != null || hasTparms)) {
                 if (hasTparms) {
                     //Method refs with type arguments must be passed as a special function
-                    final String tmpargs = gen.getNames().createTempVariable();
-                    final String tmpi = gen.getNames().createTempVariable();
-                    gen.out("function(){var ", tmpargs, "=[];for(var ", tmpi, "=0;",
-                            tmpi, "<arguments.length;", tmpi, "++)", tmpargs, "[", tmpi, "]=arguments[", tmpi, "];",
-                            tmpargs, ".push(");
-                    //Create type arguments map
-                    HashMap<TypeParameter, ProducedType> targs = new HashMap<>();
-                    Iterator<ProducedType> iter = bme.getTypeArguments().getTypeModels().iterator();
-                    for (TypeParameter tp : ((Method)decl).getTypeParameters()) {
-                        ProducedType pt = iter.hasNext() ? iter.next() : tp.getDefaultTypeArgument();
-                        targs.put(tp, pt);
-                    }
-                    TypeUtils.printTypeArguments(bme, targs, gen, true);
-                    gen.out(");return ", exp, ".apply(", who==null?"null":who, ",", tmpargs, ");}");
+                    printGenericMethodReference(gen, bme, who, exp);
                 } else {
                     //Member methods must be passed as JsCallables
                     gen.out(GenerateJsVisitor.getClAlias(), "JsCallable(/*ESTEMERO*/", who, ",", exp, ")");
@@ -63,6 +53,44 @@ public class BmeGenerator {
                 gen.out(exp);
             }
         }
+    }
+
+    static boolean hasTypeParameters(final Tree.StaticMemberOrTypeExpression expr) {
+        return expr.getTypeArguments() != null && !expr.getTypeArguments().getTypeModels().isEmpty();
+    }
+
+    /** Create a map with type arguments from the type parameter list in the expression's declaration and the
+     *  type argument list in the expression itself. */
+    static Map<TypeParameter, ProducedType> createTypeArguments(final Tree.StaticMemberOrTypeExpression expr) {
+        List<TypeParameter> tparams = null;
+        if (expr.getDeclaration() instanceof Method) {
+            tparams = ((Method)expr.getDeclaration()).getTypeParameters();
+        } else if (expr.getDeclaration() instanceof TypeDeclaration) {
+            tparams = ((TypeDeclaration)expr.getDeclaration()).getTypeParameters();
+        } else {
+            expr.addUnexpectedError("Getting type parameters from unidentified declaration type "
+                    + expr.getDeclaration());
+            return null;
+        }
+        final HashMap<TypeParameter, ProducedType> targs = new HashMap<>();
+        final Iterator<ProducedType> iter = expr.getTypeArguments().getTypeModels().iterator();
+        for (TypeParameter tp : tparams) {
+            ProducedType pt = iter.hasNext() ? iter.next() : tp.getDefaultTypeArgument();
+            targs.put(tp, pt);
+        }
+        return targs;
+    }
+
+    static void printGenericMethodReference(final GenerateJsVisitor gen,
+            final Tree.StaticMemberOrTypeExpression expr, final String who, final String member) {
+        //Method refs with type arguments must be passed as a special function
+        final String tmpargs = gen.getNames().createTempVariable();
+        final String tmpi = gen.getNames().createTempVariable();
+        gen.out("function(){var ", tmpargs, "=[];for(var ", tmpi, "=0;",
+                tmpi, "<arguments.length;", tmpi, "++)", tmpargs, "[", tmpi, "]=arguments[", tmpi, "];",
+                tmpargs, ".push(");
+        TypeUtils.printTypeArguments(expr, createTypeArguments(expr), gen, true);
+        gen.out(");return ", member, ".apply(", who==null?"null":who, ",", tmpargs, ");}");
     }
 
 }
