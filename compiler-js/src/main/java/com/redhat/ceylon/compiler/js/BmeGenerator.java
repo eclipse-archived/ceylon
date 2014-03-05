8f2278b0e55c07327ecf727725327d9d0756a2d8
Enrique Zamudio <alterbass@gmail.com>
2014-03-04 18:18:55 -0600

    partial fix for #2757 (doesn't work with direct nested invokes yet)

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/BmeGenerator.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/BmeGenerator.java
@@ -1,7 +1,12 @@
 package com.redhat.ceylon.compiler.js;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.model.Method;
+import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
+import com.redhat.ceylon.compiler.typechecker.model.TypeParameter;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 
 public class BmeGenerator {
@@ -26,20 +31,36 @@ public class BmeGenerator {
             gen.generateThrow("Undefined or null reference: " + exp, bme);
             gen.out(":", exp, ")");
         } else {
-            final boolean isCallable = !forInvoke && decl instanceof Method && decl.isMember()
+            final boolean isCallable = !forInvoke && decl instanceof Method
                     && gen.getTypeUtils().callable.equals(bme.getTypeModel().getDeclaration());
-            String who = null;
-            if (isCallable) {
-                who = gen.getMember(bme, decl, null);
-                if (who != null && who.length() > 0) {
-                    gen.out(GenerateJsVisitor.getClAlias(), "JsCallable(/*ESTEMERO*/", who, ",");
-                } else {
-                    who = null;
-                }
+            String who = isCallable && decl.isMember() ? gen.getMember(bme, decl, null) : null;
+            if (who != null && who.isEmpty()) {
+                who=null;
             }
-            gen.out(exp);
-            if (who != null) {
-                gen.out(")");
+            final boolean hasTparms = bme.getTypeArguments() != null && !bme.getTypeArguments().getTypeModels().isEmpty();
+            if (isCallable && (who != null || hasTparms)) {
+                if (hasTparms) {
+                    //Method refs with type arguments must be passed as a special function
+                    final String tmpargs = gen.getNames().createTempVariable();
+                    final String tmpi = gen.getNames().createTempVariable();
+                    gen.out("function(){var ", tmpargs, "=[];for(var ", tmpi, "=0;",
+                            tmpi, "<arguments.length;", tmpi, "++)", tmpargs, "[", tmpi, "]=arguments[", tmpi, "];",
+                            tmpargs, ".push(");
+                    //Create type arguments map
+                    HashMap<TypeParameter, ProducedType> targs = new HashMap<>();
+                    Iterator<ProducedType> iter = bme.getTypeArguments().getTypeModels().iterator();
+                    for (TypeParameter tp : ((Method)decl).getTypeParameters()) {
+                        ProducedType pt = iter.hasNext() ? iter.next() : tp.getDefaultTypeArgument();
+                        targs.put(tp, pt);
+                    }
+                    TypeUtils.printTypeArguments(bme, targs, gen, true);
+                    gen.out(");return ", exp, ".apply(", who==null?"null":who, ",", tmpargs, ");}");
+                } else {
+                    //Member methods must be passed as JsCallables
+                    gen.out(GenerateJsVisitor.getClAlias(), "JsCallable(/*ESTEMERO*/", who, ",", exp, ")");
+                }
+            } else {
+                gen.out(exp);
             }
         }
     }
