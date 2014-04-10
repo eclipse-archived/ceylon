f2732e0ecdb538106471823caa18ecdada487524
Stephane Epardaud <stef@epardaud.fr>
2014-04-10 16:32:45 +0200

    Bumped version to 1.1.0

--- common/src/com/redhat/ceylon/common/Versions.java
+++ common/src/com/redhat/ceylon/common/Versions.java
@@ -2,22 +2,27 @@ package com.redhat.ceylon.common;
 
 public class Versions {
     
-    // The current version is Ceylon 1.0.0 "No More Mr Nice Guy"
+    // The current version is Ceylon 1.0.1 "Unflatzburged"
     // This comment is here so this file will show up in searches for the current version number
     
-    //****************************************************************************************************
-    // WARNING Don't forget to update ceylon.language/runtime-js/process.js when you change anything here!
-    // WARNING Don't forget to update ceylon.language/runtime-js/process.js when you change anything here!
-    // WARNING Don't forget to update ceylon.language/runtime-js/process.js when you change anything here!
-    // WARNING Don't forget to update ceylon.language/runtime-js/process.js when you change anything here!
-    // This includes version NAMES and NUMBERS but NOT the JVM binary version!
-    //****************************************************************************************************
+    /****************************************************************************************************
+    * WARNING Don't forget to update:
+    * - ceylon.language/runtime-js/process.js (versions, name, binary version)
+    * - ceylon-spec/src/com/redhat/ceylon/compiler/typechecker/TypeChecker.java (language module version)
+    * - *.java,*.src @(\.?com.redhat.ceylon.compiler.java.metadata.)?Ceylon\(\s*major\s*=\s*6 -> @$1Version(major = 7
+    * - ceylon-dist/build.properties (versions)
+    * - ceylon-common/common-build.properties (version)
+    * - ceylon-ide-eclipse/plugins/com.redhat.ceylon.eclipse.ui/about.ini (version, name)
+    * - ceylon.language/test/process.ceylon (versions, name, binary version)
+    * - ceylon-compiler Eclipse build path (ceylon.language/ide-dist/ceylon.language-XXX.car)
+    * - ceylon.language Eclipse build path (ceylon.language/ide-dist/ceylon.language-XXX.car)
+    ****************************************************************************************************/
 
     public static final int CEYLON_VERSION_MAJOR = 1;
-    public static final int CEYLON_VERSION_MINOR = 0;
+    public static final int CEYLON_VERSION_MINOR = 1;
     public static final int CEYLON_VERSION_RELEASE = 0;
     public static final String CEYLON_VERSION_NUMBER = CEYLON_VERSION_MAJOR + "." + CEYLON_VERSION_MINOR + "." + CEYLON_VERSION_RELEASE;
-    public static final String CEYLON_VERSION_NAME = "No More Mr Nice Guy";
+    public static final String CEYLON_VERSION_NAME = "Unflatzburged";
     public static final String CEYLON_VERSION = CEYLON_VERSION_NUMBER + " (" + CEYLON_VERSION_NAME + ")";
 
     /**
@@ -28,8 +33,9 @@ public class Versions {
      * M5 is 4.0
      * M6 is 5.0
      * 1.0 is 6.0
+     * 1.1 is 7.0
      */
-    public static final int JVM_BINARY_MAJOR_VERSION = 6;
+    public static final int JVM_BINARY_MAJOR_VERSION = 7;
     public static final int JVM_BINARY_MINOR_VERSION = 0;
     public static final int JS_BINARY_MAJOR_VERSION = 7;
     public static final int JS_BINARY_MINOR_VERSION = 0;
