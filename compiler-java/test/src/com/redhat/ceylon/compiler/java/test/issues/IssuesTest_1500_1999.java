8b28838c81cbaad90a52346dd6c959cc9a333008
Stephane Epardaud <stef@epardaud.fr>
2014-04-29 16:26:51 +0200

    Test for #1604: Import from java.lang with wildcard doesn't import types like IntArray

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTest_1500_1999.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTest_1500_1999.java
@@ -264,6 +264,11 @@ public class IssuesTest_1500_1999 extends CompilerTest {
     }
 
     @Test
+    public void testBug1604() {
+        compareWithJavaSource("bug16xx/Bug1604");
+    }
+
+    @Test
     public void testBug1608() {
         compareWithJavaSource("bug16xx/Bug1608");
     }
