f786193d360bc4893815d339fa050b1ea054814d
Tom Bentley <tom.bentley@cantab.net>
2014-05-13 14:35:29 +0100

    Fix #5316

--- language/runtime/com/redhat/ceylon/compiler/java/language/ObjectArray.java
+++ language/runtime/com/redhat/ceylon/compiler/java/language/ObjectArray.java
@@ -18,6 +18,7 @@ import ceylon.language.empty_;
 import ceylon.language.finished_;
 
 import com.redhat.ceylon.compiler.java.Util;
+import com.redhat.ceylon.compiler.java.language.IntArray.IntArrayIterable;
 import com.redhat.ceylon.compiler.java.metadata.Ceylon;
 import com.redhat.ceylon.compiler.java.metadata.Class;
 import com.redhat.ceylon.compiler.java.metadata.Defaulted;
@@ -213,6 +214,12 @@ public final class ObjectArray<T> implements ReifiedType {
         
         private final T[] array;
         private TypeDescriptor $reified$Element;
+        /** The index where iteration starts */
+        private final int start;
+        /** The step size of iteration */
+        private final int step;
+        /** The index one beyond where iteration ends */
+        private final int end;
         
         @Override
         public TypeDescriptor $getType$() {
@@ -221,10 +228,27 @@ public final class ObjectArray<T> implements ReifiedType {
         
         @Ignore
         public ObjectArrayIterable(T[] array) {
+            this(array, 0, array.length, 1);
+        }
+        @Ignore
+        private ObjectArrayIterable(T[] array, int start, int end, int step) {
+            if (start < 0) {
+                throw new ceylon.language.AssertionError("start must be positive");
+            }
+            if (end < 0) {
+                throw new ceylon.language.AssertionError("end must be positive");
+            }
+            if (step <= 0) {
+                throw new ceylon.language.AssertionError("step size must be greater than zero");
+            }
+            
             this.array = array;
             this.$reified$Element = TypeDescriptor.klass(array.getClass().getComponentType());
             this.$ceylon$language$Iterable$this = new Iterable$impl<T, Null>(
                     this.$reified$Element, Null.$TypeDescriptor$, this);
+            this.start = start;
+            this.end = end;
+            this.step = step;
         }
         
         @Ignore
@@ -244,48 +268,26 @@ public final class ObjectArray<T> implements ReifiedType {
     
         @Override
         public boolean containsAny(Iterable<? extends Object, ? extends Object> arg0) {
-            Iterator<? extends Object> iter = arg0.iterator();
-            Object item;
-            while (!((item = iter.next()) instanceof Finished)) {
-                for (int ii = 0; ii < array.length; ii++) {
-                    if (array[ii] == item) {// TODO equals
-                        return true;
-                    }
-                }
-            }
-            return false;
+            return $ceylon$language$Category$this.containsAny(arg0);
         }
     
         @Override
         public boolean containsEvery(
                 Iterable<? extends Object, ? extends Object> arg0) {
-            Iterator<? extends Object> iter = arg0.iterator();
-            Object item;
-            OUTER: while (!((item = iter.next()) instanceof Finished)) {
-                for (int ii = 0; ii < array.length; ii++) {
-                    if (array[ii] == item) { // TODO equals()
-                        continue OUTER;
-                    }
-                }
-                return false;
-            }
-            return true;
+            return $ceylon$language$Category$this.containsEvery(arg0);
         }
     
         @Override
         public boolean any(Callable<? extends ceylon.language.Boolean> arg0) {
-            for (int ii=0; ii < array.length; ii++) {
-                if (arg0.$call$(array[ii]).booleanValue()) {
-                    return true;
-                }
-            }
-            return false;
+            return $ceylon$language$Iterable$this.any(arg0);
         }
     
         @Override
-        public Iterable<? extends T, ? extends Null> by(long step) {
-            // TODO optimizable by allocating a new array, and filling it in a for loop
-            return $ceylon$language$Iterable$this.by(step);
+        public ObjectArrayIterable<T> by(long step) {
+            return new ObjectArrayIterable<T>(this.array, 
+                    this.start, 
+                    this.end, 
+                    this.step*(int)step);
         }
     
         @Override
@@ -308,12 +310,7 @@ public final class ObjectArray<T> implements ReifiedType {
         
         @Override
         public boolean contains(Object item) {
-            for (int ii = 0; ii < array.length; ii++) {
-                if (array[ii] == item) {// TODO equals
-                    return true;
-                }
-            }
-            return false;
+            return $ceylon$language$Iterable$this.contains(item);
         }
         
         @Override
@@ -381,72 +378,79 @@ public final class ObjectArray<T> implements ReifiedType {
         public Iterable<? extends T, ? extends Null> getCycled() {
             return $ceylon$language$Iterable$this.getCycled();
         }
-    
+        
         @Override
         public boolean getEmpty() {
-            return array.length == 0;
+            return this.end <= this.start;
         }
-    
+        
         @Override
-        public Object getFirst() {
-            return array.length == 0 ? null : array[0];
+        public T getFirst() {
+            return this.getEmpty() ? null : this.array[this.start];
         }
-    
+        
         @Override
         public Iterable<? extends Entry<? extends ceylon.language.Integer, ? extends T>, ? extends Object> getIndexed() {
             return $ceylon$language$Iterable$this.getIndexed();
         }
-    
+        
         @Override
-        public Object getLast() {
-            return array.length == 0 ? null : array[array.length-1];
+        public T getLast() {
+            return this.getEmpty() ? null : this.array[this.end-1];
         }
-    
+        
         @Override
-        public Iterable<? extends T, ? extends Object> getRest() {
-            return $ceylon$language$Iterable$this.getRest();
+        public ObjectArrayIterable<T> getRest() {
+            return new ObjectArrayIterable<T>(this.array, this.start+1, this.end, this.step);
         }
-    
+        
         @Override
         public Sequential<? extends T> getSequence() {
-            return array.length == 0 ? empty_.get_() : new ArraySequence(ceylon.language.Integer.$TypeDescriptor$, this);
+         // Note: Sequential is immutable, and we don't know where the array
+            // came from, so however we create the sequence we must take a copy
+            return this.getEmpty() ? empty_.get_() : new ArraySequence($reified$Element, this);
         }
     
         @Ignore
         @Override
         public long getSize() {
-            return array.length;
+            return java.lang.Math.max(0, (this.end-this.start+this.step-1)/this.step);
         }
     
         @Override
         public Iterator<? extends T> iterator() {
+            if (this.getEmpty()) {
+                return (Iterator)ceylon.language.emptyIterator_.get_();
+            }
             return new Iterator<T>() {
-    
-                private int index = 0;
                 
-                private final Iterator$impl<T> $ceylon$language$Iterator$this = new Iterator$impl<T>(ceylon.language.Integer.$TypeDescriptor$, this);
+                private int index = ObjectArrayIterable.this.start;
+                
+                private final Iterator$impl<T> $ceylon$language$Iterator$this = new Iterator$impl<T>($reified$Element, this);
                 
                 @Override
                 public Iterator$impl<? extends T> $ceylon$language$Iterator$impl() {
                     return $ceylon$language$Iterator$this;
                 }
-    
+                
                 @Override
                 public Object next() {
-                    if (index < array.length) {
-                        return array[index++];
+                    if (index < ObjectArrayIterable.this.end) {
+                        T result = ObjectArrayIterable.this.array[index];
+                        index += ObjectArrayIterable.this.step;
+                        return result;
                     } else {
                         return finished_.get_();
                     }
                 }
             };
         }
-    
+        
         @Override
         public boolean longerThan(long length) {
-            return array.length > length;
+            return this.getSize() > length;
         }
-    
+        
         @Override
         public <Result> Iterable<? extends Result, ? extends Null> map(
                 @Ignore
@@ -472,15 +476,21 @@ public final class ObjectArray<T> implements ReifiedType {
         public Sequential<? extends T> select(Callable<? extends ceylon.language.Boolean> selecting) {
             return $ceylon$language$Iterable$this.select(selecting);
         }
-    
+        
         @Override
         public boolean shorterThan(long length) {
-            return array.length < length;
+            return this.getSize() < length;
         }
     
         @Override
-        public Iterable<? extends T, ? extends Object> skip(long skip) {
-            return $ceylon$language$Iterable$this.skip(skip);
+        public ObjectArrayIterable<T> skip(long skip) {
+            if (skip <= 0) {
+                return this;
+            }
+            return new ObjectArrayIterable<T>(this.array, 
+                    this.start+(int)skip*this.step, 
+                    this.end, 
+                    this.step);
         }
     
         @Override
@@ -496,8 +506,14 @@ public final class ObjectArray<T> implements ReifiedType {
         }
     
         @Override
-        public Iterable<? extends T, ? extends Object> take(long take) {
-            return $ceylon$language$Iterable$this.take(take);
+        public ObjectArrayIterable<T> take(long take) {
+            if (take >= this.getSize()) {
+                return this;
+            }
+            return new ObjectArrayIterable<T>(this.array, 
+                    this.start, 
+                    (int)take*this.step+1, 
+                    this.step);
         }
     
         @Override
@@ -505,8 +521,7 @@ public final class ObjectArray<T> implements ReifiedType {
                 Callable<? extends ceylon.language.Boolean> take) {
             return $ceylon$language$Iterable$this.takeWhile(take);
         }
-    }
    
-
+    }
 
 }
--- language/test-jvm/ceylon/language/PrimitiveArrayIterableTest.java
+++ language/test-jvm/ceylon/language/PrimitiveArrayIterableTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import com.redhat.ceylon.compiler.java.language.AbstractCallable;
 import com.redhat.ceylon.compiler.java.language.IntArray;
 import com.redhat.ceylon.compiler.java.language.IntArray.IntArrayIterable;
+import com.redhat.ceylon.compiler.java.language.ObjectArray;
+import com.redhat.ceylon.compiler.java.language.ObjectArray.ObjectArrayIterable;
 
 public class PrimitiveArrayIterableTest {
 
@@ -162,6 +164,168 @@ public class PrimitiveArrayIterableTest {
         
         Assert.assertEquals("[2, 4, 6, 8]", zero_ten_evens.take(5).skip(1).getSequence().toString());
         
+    }
+    
+    ObjectArrayIterable<ceylon.language.Integer> ints(int... ints) {
+        ceylon.language.Integer[] result = new ceylon.language.Integer[ints.length];
+        for (int ii = 0; ii < ints.length; ii++) {
+            result[ii] = ceylon.language.Integer.instance(ints[ii]);
+        }
+        return ObjectArray.<ceylon.language.Integer>getIterable(result);
+    }
+    
+    @Test
+    public void testObjectArrayIterable() {
+        ObjectArrayIterable<ceylon.language.Integer> zero_ten = ints(-1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11).skip(1).take(11);
+        ObjectArrayIterable<ceylon.language.Integer> zero_five = ints(0, 1, 2, 3, 4, 5);
+        ObjectArrayIterable<ceylon.language.Integer> five_ten = ints(5, 6, 7, 8, 9, 10);
+        ObjectArrayIterable<ceylon.language.Integer> empty = ints();
+        ObjectArrayIterable<ceylon.language.Integer> ten = ints(10);
+        ObjectArrayIterable<ceylon.language.Integer> zero = ints(0);
+        
+        // getSize
+        Assert.assertEquals(11, zero_ten.getSize());
+        Assert.assertEquals(6, zero_five.getSize());
+        Assert.assertEquals(6, five_ten.getSize());
+        Assert.assertEquals(0, empty.getSize());
+        Assert.assertEquals(1, ten.getSize());
+        Assert.assertEquals(1, zero.getSize());
+        
+        // getEmpty
+        Assert.assertFalse(zero_ten.getEmpty());
+        Assert.assertFalse(zero_five.getEmpty());
+        Assert.assertFalse(five_ten.getEmpty());
+        Assert.assertTrue(empty.getEmpty());
+        Assert.assertFalse(ten.getEmpty());
+        Assert.assertFalse(zero.getEmpty());
+        
+        // getFirst
+        Assert.assertEquals(Integer.instance(0), zero_ten.getFirst());
+        Assert.assertEquals(Integer.instance(10), ten.getFirst());
+        Assert.assertEquals(Integer.instance(0), zero.getFirst());
+        Assert.assertEquals(null, empty.getFirst());
+        
+        // getLast
+        Assert.assertEquals(Integer.instance(10), zero_ten.getLast());
+        Assert.assertEquals(Integer.instance(10), ten.getLast());
+        Assert.assertEquals(Integer.instance(0), zero.getLast());
+        Assert.assertEquals(null, empty.getLast());
+        
+        // getRest
+        Assert.assertEquals("[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", 
+                zero_ten.getRest().getSequence().toString());
+        Assert.assertEquals(Integer.instance(1), zero_ten.getRest().getFirst());
+        Assert.assertEquals(Integer.instance(10), zero_ten.getRest().getLast());
+        Assert.assertEquals("[2, 3, 4, 5, 6, 7, 8, 9, 10]", 
+                zero_ten.getRest().getRest().getSequence().toString());
+        Assert.assertEquals("[]", ten.getRest().getSequence().toString());
+        Assert.assertEquals("[]", ten.getRest().getRest().getSequence().toString());
+        
+        // getSequence
+        Assert.assertEquals("[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]", zero_ten.getSequence().toString());
+        
+        // longerThan
+        Assert.assertTrue(zero_ten.longerThan(10));
+        Assert.assertFalse(zero_ten.longerThan(11));
+        Assert.assertFalse(zero_ten.longerThan(12));
+        
+        // shorterThan
+        Assert.assertFalse(zero_ten.shorterThan(10));
+        Assert.assertFalse(zero_ten.shorterThan(11));
+        Assert.assertTrue(zero_ten.shorterThan(12));
+
+        // iterator
+        Iterator<? extends Integer> iterator = zero_ten.iterator();
+        Assert.assertEquals(ceylon.language.Integer.instance(0), iterator.next());
+        Assert.assertEquals(ceylon.language.Integer.instance(1), iterator.next());
+        Assert.assertEquals(ceylon.language.Integer.instance(2), iterator.next());
+        Assert.assertEquals(ceylon.language.Integer.instance(3), iterator.next());
+        Assert.assertEquals(ceylon.language.Integer.instance(4), iterator.next());
+        Assert.assertEquals(ceylon.language.Integer.instance(5), iterator.next());
+        Assert.assertEquals(ceylon.language.Integer.instance(6), iterator.next());
+        Assert.assertEquals(ceylon.language.Integer.instance(7), iterator.next());
+        Assert.assertEquals(ceylon.language.Integer.instance(8), iterator.next());
+        Assert.assertEquals(ceylon.language.Integer.instance(9), iterator.next());
+        Assert.assertEquals(ceylon.language.Integer.instance(10), iterator.next());
+        Assert.assertEquals(ceylon.language.finished_.get_(), iterator.next());
+        Assert.assertEquals(ceylon.language.finished_.get_(), iterator.next());
+        
+        // containsAny
+        Assert.assertTrue(zero_ten.containsAny(zero_ten));
+        Assert.assertTrue(zero_ten.containsAny(zero));
+        Assert.assertTrue(zero_ten.containsAny(ten));
+        Assert.assertTrue(zero_ten.containsAny(zero_five));
+        Assert.assertTrue(zero_ten.containsAny(five_ten));
+        Assert.assertFalse(zero_ten.containsAny(empty));
+        Assert.assertTrue(zero_five.containsAny(zero_ten));
+        
+        // containsEvery
+        Assert.assertTrue(zero_ten.containsEvery(zero_ten));
+        Assert.assertTrue(zero_ten.containsEvery(zero));
+        Assert.assertTrue(zero_ten.containsEvery(ten));
+        Assert.assertTrue(zero_ten.containsEvery(empty));
+        Assert.assertTrue(zero_ten.containsEvery(zero_five));
+        Assert.assertTrue(zero_ten.containsEvery(five_ten));
+        Assert.assertFalse(zero_five.containsEvery(zero_ten));
+        Assert.assertFalse(five_ten.containsEvery(zero_ten));
+        
+        // any
+        class BetweenPred extends AbstractCallable<ceylon.language.Boolean> {
+
+            private final long lower;
+            private final long upper;
+            
+            public BetweenPred(long lower, long upper) {
+                super(ceylon.language.Boolean.$TypeDescriptor$, 
+                        ceylon.language.Integer.$TypeDescriptor$, "Boolean(Integer)", (short)-1);
+                this.lower = lower;
+                this.upper = upper;
+            }
+            
+            public ceylon.language.Boolean $call$(java.lang.Object arg0) {
+                ceylon.language.Integer arg = (ceylon.language.Integer)arg0;
+                boolean result = this.lower <= arg.value
+                        && arg.value <= this.upper;
+                return ceylon.language.Boolean.instance(result);
+            }
+        }
+        BetweenPred between3And4 = new BetweenPred(3,4);
+        Assert.assertTrue(zero_ten.any(between3And4));
+        Assert.assertTrue(zero_five.any(between3And4));
+        Assert.assertFalse(five_ten.any(between3And4));
+        Assert.assertFalse(zero.any(between3And4));
+        Assert.assertFalse(ten.any(between3And4));
+
+        // contains
+        Assert.assertTrue(zero_ten.contains(ceylon.language.Integer.instance(0)));
+        Assert.assertTrue(zero_ten.contains(ceylon.language.Integer.instance(10)));
+        Assert.assertFalse(zero_ten.contains(ceylon.language.Integer.instance(11)));
+        Assert.assertFalse(zero_ten.contains(ceylon.language.Integer.instance(-1)));
+        
+        // taking
+        ObjectArrayIterable<Integer> zero_seven = zero_ten.take(8);
+        Assert.assertEquals("[0, 1, 2, 3, 4, 5, 6, 7]", zero_seven.getSequence().toString());
+        
+        // skipping
+        ObjectArrayIterable<Integer> two_seven = zero_ten.take(8).skip(2);
+        Assert.assertEquals("[2, 3, 4, 5, 6, 7]", two_seven.getSequence().toString());
+        
+        // by
+        ObjectArrayIterable<Integer> zero_ten_evens = zero_ten.by(2);
+        Assert.assertEquals("[0, 2, 4, 6, 8, 10]", zero_ten_evens.getSequence().toString());
+        Assert.assertEquals(6, zero_ten_evens.getSize());
+        Assert.assertTrue(zero_ten_evens.longerThan(5));
+        Assert.assertFalse(zero_ten_evens.longerThan(6));
+        Assert.assertFalse(zero_ten_evens.longerThan(7));
+        
+        Assert.assertFalse(zero_ten_evens.shorterThan(5));
+        Assert.assertFalse(zero_ten_evens.shorterThan(6));
+        Assert.assertTrue(zero_ten_evens.shorterThan(7));
+        
+        Assert.assertEquals("[0, 4, 8]", zero_ten_evens.by(2).getSequence().toString());
+        Assert.assertEquals(3, zero_ten_evens.by(2).getSize());
+        
+        Assert.assertEquals("[2, 4, 6, 8]", zero_ten_evens.take(5).skip(1).getSequence().toString());
     }
     
 }
