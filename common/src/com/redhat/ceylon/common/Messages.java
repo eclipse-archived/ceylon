393dfc0445721193e9622ea30e4fc9fe1c85ec3b
Stephane Epardaud <stef@epardaud.fr>
2014-05-26 12:22:42 +0200

    Messages: default to the key name if the value is missing

--- common/src/com/redhat/ceylon/common/Messages.java
+++ common/src/com/redhat/ceylon/common/Messages.java
@@ -20,12 +20,18 @@
 package com.redhat.ceylon.common;
 
 import java.text.MessageFormat;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
 
     public static String msg(ResourceBundle bundle, String msgKey, Object... msgArgs) {
-        String msg = bundle.getString(msgKey);
+        String msg;
+        try{
+            msg = bundle.getString(msgKey);
+        }catch(MissingResourceException x){
+            msg = msgKey;
+        }
         if (msgArgs != null) {
             msg = MessageFormat.format(msg, msgArgs);
         }
