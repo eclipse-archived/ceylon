6fb020f9745e67c11a0473fa87dd1f9a4d030368
Tako Schotanus <tako@ceylon-lang.org>
2014-05-28 17:59:15 +0200

    The method `copy()` now accepts references to a file as first parameter (#4629)

--- common/src/com/redhat/ceylon/common/FileUtil.java
+++ common/src/com/redhat/ceylon/common/FileUtil.java
@@ -374,15 +374,20 @@ public class FileUtil {
      * Recursively copy every file/folder from root to dest
      */
     public static void copy(File root, File dest) throws IOException {
-        for(File child : root.listFiles()){
-            File childDest = new File(dest, child.getName());
-            if(child.isDirectory()){
-                if(!childDest.mkdirs())
-                    throw new IOException("Failed to create dir "+childDest.getPath());
-                copy(child, childDest);
-            }else{
-                Files.copy(child.toPath(), childDest.toPath());
+        if(root.isDirectory()){
+            for(File child : root.listFiles()){
+                File childDest = new File(dest, child.getName());
+                if(child.isDirectory()){
+                    if(!childDest.mkdirs())
+                        throw new IOException("Failed to create dir "+childDest.getPath());
+                    copy(child, childDest);
+                }else{
+                    Files.copy(child.toPath(), childDest.toPath());
+                }
             }
+        }else{
+            File childDest = new File(dest, root.getName());
+            Files.copy(root.toPath(), childDest.toPath());
         }
     }
 }
