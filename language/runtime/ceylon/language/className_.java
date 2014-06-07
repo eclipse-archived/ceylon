cf98a9a6b7461d3bc0d888a8a0fa24f619109c34
gavinking <gavin@ceylon-lang.org>
2014-06-07 09:09:33 +0200

    cleanup commented code

--- language/runtime/ceylon/language/className_.java
+++ language/runtime/ceylon/language/className_.java
@@ -15,42 +15,6 @@ public final class className_ {
     @TypeInfo("ceylon.language::Object")
     final java.lang.Object object) {
         return object.getClass().getName();
-        //return name(object.getClass());
     }
     
-    /*private static java.lang.String name(Class<?> c) {
-        Name annot = c.getAnnotation(Name.class);
-        java.lang.String cn;
-        if (annot == null) {
-            cn = mangle(c.getSimpleName(), 
-                    c.isAnnotationPresent(Ceylon.class));
-        } 
-        else {
-            cn = annot.value();
-        }
-        if (c.isLocalClass()) {
-            return cn;
-        }
-        else if (c.isMemberClass()) {
-            return name(c.getEnclosingClass()) + '.' + cn;
-        }
-        else {
-            return mangle(c.getPackage().getName(), 
-                    c.isAnnotationPresent(Ceylon.class)) + 
-                "::" + cn;
-        }
-    }
-
-    private final static java.lang.String mangle(java.lang.String s, 
-            boolean ceylon) {
-        int i = s.indexOf("$");
-        if (i>0) {
-            s = s.substring(0, i) + s.substring(i+1);
-        }
-        if (s.endsWith("_") && ceylon) {
-            s = s.substring(0, s.length()-1);
-        }
-        return s;
-    }*/
-
 }
