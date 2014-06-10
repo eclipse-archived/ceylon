3d988602f96cfa1b853efc8c7edc3f3aaee72f90
Enrique Zamudio <alterbass@gmail.com>
2014-06-10 12:43:25 -0500

    move printStackTrace to its own file for #2786

--- language/runtime-js/exception_addons.js
+++ language/runtime-js/exception_addons.js
@@ -18,34 +18,3 @@ function $init$native$Throwable$before(exc) {
     }
   }
 }
-function printStackTrace(exc, _write) {
-  if (_write===undefined)_write=getProcess().writeError;
-  var _c = className(exc);
-  if (exc.message.size > 0) {
-    _c += ' "' + exc.message + '"';
-  }
-  _write(_c);_write(getOperatingSystem().newline);
-  for (var i=0; i<exc.stack_trace.length; i++) {
-    var f = exc.stack_trace[i];
-    if (f == "CIRCULAR") {
-      _write("    (Infinite loop detected)");
-      _write(getOperatingSystem().newline);
-      return;
-    }
-    var mm = getrtmm$$(f);
-    if (mm) {
-      var _src = '';
-      if (i==0) {
-        if (exc.$loc && exc.$file) _src = ' (' + exc.$file + " " + exc.$loc + ')';
-      }
-      var pkn=mm.d[0];
-      if (pkn==='$')pkn='ceylon.language';
-      _write("    at " + pkn + "::" + mm.d[mm.d.length-1] + _src);_write(getOperatingSystem().newline);
-    }
-  }
-}
-printStackTrace.$crtmm$=function(){
-  return{mod:$CCMM$,d:['ceylon.language','printStackTrace'],$t:{t:Anything},$ps:[
-    {$nm:'exception',$t:{t:Throwable},$mt:'prm'},{$nm:'write',$t:{t:Callable,a:{Return:{t:Anything},Arguments:{t:'T',l:[{t:String$}]}}},$mt:'prm','$def':1}
-  ]};};
-ex$.printStackTrace=printStackTrace;
