1b178d1d028dc103b1b0ab001d65704bcbca742d
Enrique Zamudio <alterbass@gmail.com>
2014-06-20 15:59:44 -0500

    fix String.trim() for unicode chars

--- language/runtime-js/String/trim.js
+++ language/runtime-js/String/trim.js
@@ -1,15 +1,16 @@
 function(/*Category*/chars) {
     var from = 0;
-    while (from<this.length && chars(this.$_get(from))) {++from}
-    var to = this.length;
+    var size=this.size;
+    while (from<size && chars(this.$_get(from))) {++from}
+    var to = size;
     if (from < to) {
         do {--to} while (from<to && chars(this.$_get(to)));
         ++to;
     }
-    if (from===0 && to===this.length) {return this;}
+    if (from===0 && to===size) {return this;}
     var result = this.substring(from, to);
     if (this.codePoints !== undefined) {
-        result.codePoints = this.codePoints - from - this.length + to;
+        result.codePoints = this.codePoints - from - size + to;
     }
     return result;
 }
