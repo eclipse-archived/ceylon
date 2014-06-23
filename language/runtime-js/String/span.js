ba503a7adf491a98c0d29c1faf447846dc9c06a9
Enrique Zamudio <alterbass@gmail.com>
2014-06-23 12:52:10 -0500

    reverse is now a reserved word

--- language/runtime-js/String/span.js
+++ language/runtime-js/String/span.js
@@ -1,6 +1,6 @@
 function(from, to) {
     if (from > to) {
-        return this.segment(to, from-to+1).reverse();
+        return this.segment(to, from-to+1).$_reverse();
     }
     return this.segment(from, to-from+1);
 }
