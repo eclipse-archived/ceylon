4c195d05ccef710d6aff0c5ae90e44843f0d5321
Enrique Zamudio <alterbass@gmail.com>
2014-06-23 12:06:34 -0500

    use reverse() instead of reversed

--- language/runtime-js/String/span.js
+++ language/runtime-js/String/span.js
@@ -1,6 +1,6 @@
 function(from, to) {
     if (from > to) {
-        return this.segment(to, from-to+1).reversed;
+        return this.segment(to, from-to+1).reverse();
     }
     return this.segment(from, to-from+1);
 }
