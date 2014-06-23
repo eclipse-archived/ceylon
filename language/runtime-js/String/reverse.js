caebeb25abefc97bb492b4978595e44c9e677550
gavinking <gavin@ceylon-lang.org>
2014-06-23 14:07:48 +0200

    fix reverse()

--- language/runtime-js/String/reverse.js
+++ language/runtime-js/String/reverse.js
@@ -1,10 +1,12 @@
-var result = "";
-for (var i=this.length; i>0;) {
+function() {
+  var result = "";
+  for (var i=this.length; i>0;) {
     var cc = this.charCodeAt(--i);
     if ((cc&0xfc00)!==0xdc00 || i===0) {
-        result += this.charAt(i);
+      result += this.charAt(i);
     } else {
-        result += this.substr(--i, 2);
+      result += this.substr(--i, 2);
     }
+  }
+  return result;
 }
-return result;
