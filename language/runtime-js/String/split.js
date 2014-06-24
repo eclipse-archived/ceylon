226138220ca4b10214eaf0f23e9993d32c7a24ee
gavinking <gavin@ceylon-lang.org>
2014-06-24 13:11:43 +0200

    update js impl of String.split

--- language/runtime-js/String/split.js
+++ language/runtime-js/String/split.js
@@ -1,13 +1,18 @@
 function(sep, discard, group) {
+
+    //TODO: return a stream
+    var tokens = [];
+
     // shortcut for empty input
-    if (this.length === 0) {return Singleton(this, {Element$Singleton:{t:$_String}}); }
+    if (this.length === 0) {
+        tokens.push(this);
+        return tokens.reifyCeylonType({t:$_String});
+    }
 
     if (sep === undefined) {sep = function(c){return c.value in Character.WS$;}}
     if (discard === undefined) {discard = true}
     if (group === undefined) {group = true}
 
-    //TODO: return an iterable which determines the next token on demand
-    var tokens = [];
     var tokenBegin = 0;
     var tokenBeginCount = 0;
     var count = 0;
@@ -17,100 +22,54 @@ function(sep, discard, group) {
     function pushToken(tokenEnd) {
         tokens.push($_String(value.substring(tokenBegin, tokenEnd), count-tokenBeginCount));
     }
-    if (is$(sep, {t:Iterable})) {
-        var sepChars = {}
-        var it = sep.iterator();
-        var c; while ((c=it.next()) !== getFinished()) {sepChars[c.value] = true}
-        for (var i=0; i<this.length;) {
-            var j = i;
-            var cp = this.charCodeAt(i++);
-            if ((cp&0xfc00)===0xd800 && i<this.length) {
-                cp = (cp<<10) + this.charCodeAt(i++) - 0x35fdc00;
-            }
-            if (cp in sepChars) {
-                if (!group) {
-                    // ungrouped separator: store preceding token
-                    pushToken(j);
-                    if (!discard) {
-                        // store separator as token
-                        tokens.push($_String(this.substring(j, i), 1));
-                    }
-                    // next token begins after this character
-                    tokenBegin = i;
-                    tokenBeginCount = count + 1;
-                } else if (!separator || (j == 0)) {
-                    // begin of grouped separator: store preceding token
-                    pushToken(j);
-                    // separator token begins at this character
-                    tokenBegin = j;
-                    tokenBeginCount = count;
-                }
-                separator = true;
+    
+    for (var i=0; i<this.length; ++count) {
+        var j = i;
+        var cp = this.charCodeAt(i++);
+        if ((cp&0xfc00)===0xd800 && i<this.length) {
+            cp = (cp<<10) + this.charCodeAt(i++) - 0x35fdc00;
+        }
 
-            } else if (separator) {
-                // first non-separator after separators or at beginning
-                if (!discard && (tokenBegin != j)) {
-                    // store preceding grouped separator (if group=false then tokenBegin=j)
-                    pushToken(j);
+        if (sep(Character(cp))) {
+            if (!group) {
+                // ungrouped separator: store preceding token
+                pushToken(j);
+                if (!discard) {
+                    // store separator as token
+                    tokens.push($_String(this.substring(j, i), 1));
                 }
-                // non-separator token begins at this character
+                // next token begins after this character
+                tokenBegin = i;
+                tokenBeginCount = count + 1;
+            } else if (!separator || (j == 0)) {
+                // begin of grouped separator: store preceding token
+                pushToken(j);
+                // separator token begins at this character
                 tokenBegin = j;
                 tokenBeginCount = count;
-                separator = false;
             }
-        }
-        if (tokenBegin != i) {
-            pushToken(i);
-        }
-    } else {
-        for (var i=0; i<this.length; ++count) {
-            var j = i;
-            var cp = this.charCodeAt(i++);
-            if ((cp&0xfc00)===0xd800 && i<this.length) {
-                cp = (cp<<10) + this.charCodeAt(i++) - 0x35fdc00;
+            separator = true;
+
+        } else if (separator) {
+            // first non-separator after separators or at beginning
+            if (!discard && (tokenBegin != j)) {
+                // store preceding grouped separator (if group=false then tokenBegin=j)
+                pushToken(j);
             }
-
-            if (sep(Character(cp))) {
-                if (!group) {
-                    // ungrouped separator: store preceding token
-                    pushToken(j);
-                    if (!discard) {
-                        // store separator as token
-                        tokens.push($_String(this.substring(j, i), 1));
-                    }
-                    // next token begins after this character
-                    tokenBegin = i;
-                    tokenBeginCount = count + 1;
-                } else if (!separator || (j == 0)) {
-                    // begin of grouped separator: store preceding token
-                    pushToken(j);
-                    // separator token begins at this character
-                    tokenBegin = j;
-                    tokenBeginCount = count;
-                }
-                separator = true;
-
-            } else if (separator) {
-                // first non-separator after separators or at beginning
-                if (!discard && (tokenBegin != j)) {
-                    // store preceding grouped separator (if group=false then tokenBegin=j)
-                    pushToken(j);
-                }
-                // non-separator token begins at this character
-                tokenBegin = j;
-                tokenBeginCount = count;
-                separator = false;
-            }
+            // non-separator token begins at this character
+            tokenBegin = j;
+            tokenBeginCount = count;
+            separator = false;
         }
+    }
 
-        if ((tokenBegin != i) && !(separator && discard)) {
-            // store preceding token (may be a grouped separator)
-            pushToken(i);
-        }
-        if (separator) {
-            // if last character was a separator then there's another empty token
-            tokens.push("");
-        }
+    if ((tokenBegin != i) && !(separator && discard)) {
+        // store preceding token (may be a grouped separator)
+        pushToken(i);
+    }
+    if (separator) {
+        // if last character was a separator then there's another empty token
+        tokens.push("");
     }
 
     this.codePoints = count;
