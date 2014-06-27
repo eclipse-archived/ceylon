13e62e0228dced53a087874ec9454a3904b301fb
Enrique Zamudio <alterbass@gmail.com>
2014-06-27 16:47:41 -0500

    work on #2806

--- language/runtime-js/meta/annotations.js
+++ language/runtime-js/meta/annotations.js
@@ -1,6 +1,9 @@
 /*Native Implementation of annotations() */
 function annotations$meta(anntype, progelem, $$$mptypes) {
-  if (progelem.tipo)progelem=progelem.tipo;
+  if (progelem.tipo)
+    progelem=progelem.tipo;
+  else if (progelem._alias)
+    progelem=progelem._alias;
   var mm = getrtmm$$(progelem);
   if (progelem.$anns)mm={an:typeof(progelem.$anns)==='function'?progelem.$anns():progelem.$anns};
   else if (progelem._anns)mm={an:typeof(progelem._anns)==='function'?progelem._anns():progelem._anns};
@@ -30,9 +33,10 @@ function annotations$meta(anntype, progelem, $$$mptypes) {
     for (var i=0; i < anns.length; i++) {
       if (is$(anns[i], {t:anntype.tipo}))r.push(anns[i]);
     }
-    return r.length==0?getEmpty():ArraySequence(r,{Element$ArraySequence:{t:ConstrainedAnnotation,
-      a:{Value$ConstrainedAnnotation:$$$mptypes.Value$annotations,Values$ConstrainedAnnotation:$$$mptypes.Values$annotations,
-      ProgramElement$ConstrainedAnnotation:$$$mptypes.ProgramElement$annotations}}});
+    var targ={t:ConstrainedAnnotation,a:{Value$ConstrainedAnnotation:$$$mptypes.Value$annotations,
+      Values$ConstrainedAnnotation:$$$mptypes.Values$annotations,
+      ProgramElement$ConstrainedAnnotation:$$$mptypes.ProgramElement$annotations}};
+    return r.length==0?getEmpty():ArraySequence(r.reifyCeylonType(targ),{Element$ArraySequence:targ});
   }
   return null;
 }
