81a062bb253c0f2fcde6f128b6f4a7e2fa770011
Enrique Zamudio <alterbass@gmail.com>
2014-06-27 20:20:17 -0500

    fix buggy impls

--- language/runtime-js/String/firstInclusion.js
+++ language/runtime-js/String/firstInclusion.js
@@ -1,7 +1,7 @@
 function (e) {
   if (e.size===0)return 0;
   if (is$(e, {t:$_String})) {
-    for (var i=0; i < this.size-e.size; i++) {
+    for (var i=0; i < this.size-e.size+1; i++) {
       if (cmpSubString(this,e,i))return i;
     }
     return null;
--- language/runtime-js/String/includes.js
+++ language/runtime-js/String/includes.js
@@ -1,7 +1,8 @@
 function (e) {
   if (e.size===0)return true;
   if (is$(e, {t:$_String})) {
-    return this.firstInclusion(e) >= 0;
+    var fi=this.firstInclusion(e);
+    return fi!==null && fi>=0;
   }
   else {
     return List.$$.prototype.includes.call(this,e);
