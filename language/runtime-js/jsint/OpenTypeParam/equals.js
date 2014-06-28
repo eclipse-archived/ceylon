42740c022bc280e123905328efbe5eb3b1d0cb9d
Enrique Zamudio <alterbass@gmail.com>
2014-06-27 19:55:06 -0500

    add option to avoid circularity

--- language/runtime-js/jsint/OpenTypeParam/equals.js
+++ language/runtime-js/jsint/OpenTypeParam/equals.js
@@ -1,3 +1,10 @@
-function(o) {
-  return is$(o,{t:OpenTypeParam$jsint}) && o.container==this.container && o._fname==this._fname;
+function(o,skip) {
+  if (is$(o,{t:OpenTypeParam$jsint}) && o._fname===this._fname) {
+    if (!skip) {
+      var mc=this.container,oc=o.container;
+      return mc.equals(oc);
+    }
+    return true;
+  }
+  return false;
 }
