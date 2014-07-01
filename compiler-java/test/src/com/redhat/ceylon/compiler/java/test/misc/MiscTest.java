3449d221a49b7132c9cebe8bcac705aab6e111cf
Stephane Epardaud <stef@epardaud.fr>
2014-07-01 12:08:19 +0200

    cosmetic: imports cleanup

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/misc/MiscTest.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/misc/MiscTest.java
@@ -22,14 +22,10 @@ package com.redhat.ceylon.compiler.java.test.misc;
 import java.io.File;
 import java.io.FileFilter;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.lang.ProcessBuilder.Redirect;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.tools.JavaFileObject;
 
