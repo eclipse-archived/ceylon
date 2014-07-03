05f8b5203e7cd40b6ea9903d5dcf27961f539b20
Enrique Zamudio <alterbass@gmail.com>
2014-07-03 17:18:06 -0500

    fix #5261

--- language/runtime-js/numbers.js
+++ language/runtime-js/numbers.js
@@ -61,21 +61,38 @@ atr$(JSNum$proto,'$$targs$$',function(){
 JSNum$proto.toString = origNumToString;
 atr$(JSNum$proto, 'string', function(){ return this.toString(); },
   undefined,function(){return{an:function(){return[shared(),actual()]},mod:$CCMM$,$cont:$_Object,d:['$','Object','$at','string']};});
+function $addnm$(nm,s) {
+  var t={t:JSNumber};
+  if (s===undefined)s=$_Number.$$.prototype[nm];
+  var d=JSNum$proto[nm];
+  d.$crtmm$=s.$crtmm$();
+  d=d.$crtmm$;
+  if (typeof(d.$t)==='string')d.$t=t;
+  if (d.ps) for (var i=0; i < d.ps.length; i++) {
+    if (typeof(d.ps[i].$t)==='string')d.ps[i].$t=t;
+  }
+}
+
 JSNum$proto.plus = function(other) {
     return (nflt$(this)||nflt$(other)) ? Float(this+other) : (this+other);
 }
+$addnm$('plus');
 JSNum$proto.plusInteger = function(other) {
     return nflt$(this) ? Float(this+other) : (this+other);
 }
+$addnm$('plusInteger');
 JSNum$proto.minus = function(other) {
     return (nflt$(this)||nflt$(other)) ? Float(this-other) : (this-other);
 }
+$addnm$('minus');
 JSNum$proto.times = function(other) {
     return (nflt$(this)||nflt$(other)) ? Float(this*other) : (this*other);
 }
+$addnm$('times');
 JSNum$proto.timesInteger = function(other) {
     return nflt$(this) ? Float(this*other) : (this*other);
 }
+$addnm$('timesInteger');
 JSNum$proto.divided = function(other) {
     if (nflt$(this)||nflt$(other)) { return Float(this/other); }
     if (other == 0) {
@@ -83,7 +100,10 @@ JSNum$proto.divided = function(other) {
     }
     return toInt(this/other);
 }
+$addnm$('divided');
 JSNum$proto.remainder = function(other) { return this%other; }
+$addnm$('remainder',Integral.$$.prototype.remainder);
+JSNum$proto.remainder.$crtmm$=function(){return{mod:$CCMM$,$t:{t:Integer},ps:[{$t:{t:Integer},nm:'other'}],d:['$','Integer','remainder']};}
 JSNum$proto.power = function(exp) {
     if (nflt$(this)||nflt$(exp)) { return Float(Math.pow(this, exp)); }
     if (exp<0 && this!=1 && this!=-1) {
@@ -91,6 +111,7 @@ JSNum$proto.power = function(exp) {
     }
     return toInt(Math.pow(this, exp));
 }
+$addnm$('power',Exponentiable.$$.prototype.power);
 atr$(JSNum$proto, 'negated', function() {
     return nflt$(this) ? Float(-this) : -this;
 },undefined,function(){return{an:function(){return[shared(),actual()]},mod:$CCMM$,$cont:Invertible,d:['$','Invertible','$at','negated']};});
@@ -101,22 +122,28 @@ atr$(JSNum$proto, 'positive', function(){
   return nflt$(this) ? this > 0.0 : this.valueOf() > 0;
 },undefined,function(){return{$t:{t:$_Boolean},an:function(){return[shared(),actual()]},mod:$CCMM$,$cont:$_Number,d:['$','Number','$at','positive']};});
 JSNum$proto.equals = function(other) { return (typeof(other)==='number' || other.constructor===Number) && other==this.valueOf(); }
+$addnm$('equals',$_Object.$$.prototype.equals);
 JSNum$proto.compare = function(other) {
     var value = this.valueOf();
     return value==other ? getEqual() : (value<other ? getSmaller():getLarger());
 }
+$addnm$('compare',Comparable.$$.prototype.compare);
 JSNum$proto.smallerThan=function(o) {
   return Comparable.$$.prototype.smallerThan.call(this,o);
 }
+$addnm$('smallerThan',Comparable.$$.prototype.smallerThan);
 JSNum$proto.largerThan=function(o) {
   return Comparable.$$.prototype.largerThan.call(this,o);
 }
+$addnm$('largerThan',Comparable.$$.prototype.largerThan);
 JSNum$proto.notSmallerThan=function(o) {
   return Comparable.$$.prototype.notSmallerThan.call(this,o);
 }
+$addnm$('notSmallerThan',Comparable.$$.prototype.notSmallerThan);
 JSNum$proto.notLargerThan=function(o) {
   return Comparable.$$.prototype.notLargerThan.call(this,o);
 }
+$addnm$('notLargerThan',Comparable.$$.prototype.notLargerThan);
 atr$(JSNum$proto, '$_float', function(){ return Float(this.valueOf()); },
   undefined,function(){return{an:function(){return[shared(),actual()]},mod:$CCMM$,$cont:$_Number,d:['$','Number','$at','float']};});
 atr$(JSNum$proto, 'integer', function(){ return toInt(this); },
@@ -151,11 +178,17 @@ JSNum$proto.distanceFrom = function(other) {
 atr$(JSNum$proto, 'not', function(){ return ~this; },
   undefined,function(){return{an:function(){return[shared(),actual()]},mod:$CCMM$,$cont:Binary,d:['$','Binary','$at','not']};});
 JSNum$proto.leftLogicalShift = function(i) { return this << i; }
+$addnm$('leftLogicalShift',Binary.$$.prototype.leftLogicalShift);
 JSNum$proto.rightLogicalShift = function(i) { return this >>> i; }
+$addnm$('rightLogicalShift',Binary.$$.prototype.rightLogicalShift);
 JSNum$proto.rightArithmeticShift = function(i) { return this >> i; }
+$addnm$('rightArithmeticShift',Binary.$$.prototype.rightArithmeticShift);
 JSNum$proto.and = function(x) { return this & x; }
+$addnm$('and',Binary.$$.prototype.and);
 JSNum$proto.or = function(x) { return this | x; }
+$addnm$('or',Binary.$$.prototype.or);
 JSNum$proto.xor = function(x) { return this ^ x; }
+$addnm$('xor',Binary.$$.prototype.xor);
 JSNum$proto.$_get = function(idx) {
     if (idx < 0 || idx >31) {
         return false;
@@ -163,6 +196,7 @@ JSNum$proto.$_get = function(idx) {
     var mask = 1 << idx;
     return (this & mask) != 0 ? true : false;
 }
+$addnm$('$_get',Binary.$$.prototype.$_get);
 JSNum$proto.set = function(idx,bit) {
     if (idx < 0 || idx >31) {
         return this;
@@ -171,6 +205,7 @@ JSNum$proto.set = function(idx,bit) {
         var mask = idx > 1 ? 1 << idx : 1;
     return (bit === true) ? this | mask : this & ~mask;
 }
+$addnm$('set',Binary.$$.prototype.set);
 JSNum$proto.flip = function(idx) {
     if (idx < 0 || idx >31) {
         return this;
@@ -178,18 +213,23 @@ JSNum$proto.flip = function(idx) {
     var mask = 1 << idx;
     return this ^ mask;
 }
+$addnm$('flip',Binary.$$.prototype.flip);
 JSNum$proto.clear = function(index) {
     return this.set(index, false);
 }
+$addnm$('clear',Binary.$$.prototype.clear);
 JSNum$proto.neighbour=function(offset) {
   return this+offset;
 }
+$addnm$('neighbour',Enumerable.$$.prototype.neighbour);
 JSNum$proto.offset=function(other) {
   return this-other;
 }
+$addnm$('offset',Enumerable.$$.prototype.offset);
 JSNum$proto.offsetSign=function(other) {
   return this-other;
 }
+$addnm$('offsetSign',Enumerable.$$.prototype.offsetSign);
 
 atr$(JSNum$proto, 'magnitude', function(){ return Math.abs(this); },
   undefined,function(){return{an:function(){return[shared(),actual()]},mod:$CCMM$,$cont:$_Number,d:['$','Number','$at','magnitude']};});
