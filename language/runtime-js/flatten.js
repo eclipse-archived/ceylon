8ebf66410434bffc3330dd3721e2724c709e14a8
Enrique Zamudio <alterbass@gmail.com>
2014-07-07 22:51:48 -0500

    skip last argument if it's type arguments

--- language/runtime-js/flatten.js
+++ language/runtime-js/flatten.js
@@ -2,8 +2,16 @@ function flatten(tf, $$$mptypes) {
   function rf() {
     var argc = arguments.length;
     var last = argc>0 ? arguments[argc-1] : undefined;
-    if (typeof(last) === 'object' && typeof(last.Args$flatten) === 'object' && (last.Args$flatten.t==='T'||typeof(last.Args$flatten.t) === 'function')) {
+    var tlast = typeof(last)==='object';
+    if (tlast && typeof(last.Args$flatten) === 'object' && (last.Args$flatten.t==='T'||typeof(last.Args$flatten.t) === 'function')) {
       argc--;
+    } else if (tf.$$targs$$ && tlast) {
+      var ks=Object.keys(tf.$$targs$$);
+      var all=true;
+      for (var i=0;i<ks.length;i++) {
+        if (last[ks[i]]===undefined)all=false;
+      }
+      if (all)argc--;
     }
     var t = [];
     for (var i=0;i<argc;i++) {
