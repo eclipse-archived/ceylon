83797cc5b1210fc85637bd55fdebfad9c4b853b9
Enrique Zamudio <alterbass@gmail.com>
2014-07-21 16:14:32 -0500

    tests for #2820

--- compiler-js/src/test/java/com/redhat/ceylon/compiler/js/CompileJsToolTest.java
+++ compiler-js/src/test/java/com/redhat/ceylon/compiler/js/CompileJsToolTest.java
@@ -1,7 +1,11 @@
 package com.redhat.ceylon.compiler.js;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -35,5 +39,59 @@ public class CompileJsToolTest {
         Assert.assertNotNull(tool);
         pluginFactory.bindArguments(tool, args());
     }
-    
+
+    @Test(expected=CompilerErrorException.class)
+    public void testInvalidResource() throws Exception {
+        ToolModel<CeylonCompileJsTool> tool = pluginLoader.loadToolModel("compile-js");
+        Assert.assertNotNull(tool);
+        CeylonCompileJsTool jsc = pluginFactory.bindArguments(tool, args(
+                "--rep=build/runtime",
+                "--source=src/test/resources/doc",
+                "--resource=src/test/resources/res_test",
+                "src/test/resources/doc/calls.ceylon",
+                "src/test/resources/res_test/invalid.txt"));
+        jsc.run();
+    }
+
+    @Test
+    public void testValidResource() throws Exception {
+        ToolModel<CeylonCompileJsTool> tool = pluginLoader.loadToolModel("compile-js");
+        Assert.assertNotNull(tool);
+        CeylonCompileJsTool jsc = pluginFactory.bindArguments(tool, args(
+                "--rep=build/runtime",// "--verbose",
+                "--source=src/test/resources/doc",
+                "--resource=src/test/resources/res_test",
+                "src/test/resources/doc/calls.ceylon",
+                "src/test/resources/res_test/test.txt"));
+        jsc.run();
+        checkResources("modules/default/default-resources.zip", "test.txt");
+    }
+
+    @Test
+    public void testAllResources() throws Exception {
+        ToolModel<CeylonCompileJsTool> tool = pluginLoader.loadToolModel("compile-js");
+        Assert.assertNotNull(tool);
+        CeylonCompileJsTool jsc = pluginFactory.bindArguments(tool, args(
+                "--rep=build/runtime",// "--verbose",
+                "--source=src/test/resources/doc",
+                "--resource=src/test/resources/res_test",
+                "src/test/resources/doc/calls.ceylon"));
+        jsc.run();
+        checkResources("modules/default/default-resources.zip",
+                "test.txt", "another_test.txt", "subdir/third.txt");
+    }
+
+    void checkResources(String path, String... paths) throws IOException {
+        File res = new File(path);
+        Assert.assertTrue("Resources file missing", res.exists() && res.isFile());
+        try (ZipFile zip = new ZipFile(res)) {
+            for (String r : paths) {
+                ZipEntry e = zip.getEntry(r);
+                Assert.assertTrue("Missing resource " + r, e != null);
+            }
+        } finally {
+            //nothing
+        }
+    }
+
 }
