32020bf2885936fcf0de7c4550862b4b8ce1ae83
Enrique Zamudio <alterbass@gmail.com>
2014-08-04 23:46:32 -0500

    fix some metamodel issues

--- language/runtime-js/opentypes.js
+++ language/runtime-js/opentypes.js
@@ -299,9 +299,11 @@ atr$($$openValue,'containingPackage',function(){return this._pkg;},undefined,fun
 atr$($$openValue,'containingModule',function(){return this._pkg.container;},undefined,function(){return{mod:$CCMM$,$t:{t:Module$meta$declaration},$cont:OpenValue,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','containingModule']};});
 atr$($$openValue,'toplevel',function(){return this.toplevel_;},undefined,function(){return{mod:$CCMM$,$t:{t:$_Boolean},$cont:OpenValue,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','toplevel']};});
 
-  $$openValue.equals=function(other) {
-    if (is$(other, {t:OpenValue}) && other.name.equals(this.name) && other.toplevel===this.toplevel && other.containingPackage.equals(this.containingPackage)) {
-      return other.meta==this.meta;
+  $$openValue.equals=function(o) {
+    if (is$(o,{t:OpenValue}))return this.meta===o.meta;
+    if (is$(o,{t:ValueDeclaration$meta$declaration})&&o.qualifiedName.equals(this.qualifiedName)&&o.shared==this.shared&&o.actual==this.actual&&o.formal==this.formal&&o.$_default==this.$_default&&o.variable==this.variable&&o.toplevel==this.toplevel&&o.openType.equals(this.openType)) {
+      var _mc=this.container,_oc=o.container;
+      return _mc===null?_oc===null:_mc.equals(_oc);
     }
     return false;
   }
@@ -565,12 +567,6 @@ function ValParamDecl(cont,param,$$valParamDecl){
   $$valParamDecl.param=param;
   $$valParamDecl.tipo={$crtmm$:{$cont:cont.tipo,$t:param.$t,mt:'prm',d:cont.tipo.$crtmm$.d,an:param.an}};
 
-  $$valParamDecl.$prop$getParameter.get=function(){return true;};
-  $$valParamDecl.$prop$getShared.get=function(){return false;};
-  $$valParamDecl.$prop$getToplevel.get=function(){return false;};
-  $$valParamDecl.$prop$getFormal.get=function(){return false;};
-  $$valParamDecl.$prop$getDefault.get=function(){return false;};
-  $$valParamDecl.$prop$getActual.get=function(){return false;};
   return $$valParamDecl;
 }
 ValParamDecl.$crtmm$=function(){return{mod:$CCMM$,'super':{t:Basic},ps:[],sts:[{t:ValueDeclaration$meta$declaration}],d:['ceylon.language.meta.declaration','ValParamDecl']};};
@@ -587,8 +583,20 @@ function $init$ValParamDecl(){
         return this.param.seq!==undefined;
       },undefined,function(){return{mod:$CCMM$,$t:{t:$_Boolean},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','variadic']};});
       atr$($$valParamDecl,'variable',function(){
-        return (getrtmm$(this.param).pa & 1024) > 0;
+        return (this.param.pa & 1024) > 0;
       },undefined,function(){return{mod:$CCMM$,$t:{t:$_Boolean},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','variable']};});
+      atr$($$valParamDecl,'shared',function(){
+        return (this.param.pa & 1) > 0;
+      },undefined,function(){return{mod:$CCMM$,$t:{t:$_Boolean},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','shared']};});
+      atr$($$valParamDecl,'formal',function(){
+        return (this.param.pa & 4) > 0;
+      },undefined,function(){return{mod:$CCMM$,$t:{t:$_Boolean},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','formal']};});
+      atr$($$valParamDecl,'$_default',function(){
+        return (this.param.pa & 8) > 0;
+      },undefined,function(){return{mod:$CCMM$,$t:{t:$_Boolean},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','default']};});
+      atr$($$valParamDecl,'actual',function(){
+        return (this.param.pa & 2) > 0;
+      },undefined,function(){return{mod:$CCMM$,$t:{t:$_Boolean},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','actual']};});
       atr$($$valParamDecl,'container',function(){
         return this.cont;
       },undefined,function(){return{mod:$CCMM$,$t:{t:'u', l:[{t:Package$meta$declaration},{t:NestableDeclaration$meta$declaration}]},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','container']};});
@@ -603,11 +611,7 @@ function $init$ValParamDecl(){
         if (typeof(t)==='string')return OpenTvar$jsint(OpenTypeParam$jsint(this.cont,t));
         return _openTypeFromTarg(t);
       },undefined,function(){return{mod:$CCMM$,$t:{t:OpenType$meta$declaration},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','openType']};});
-      atr$($$valParamDecl,'shared',function(){return false;},undefined,function(){return{mod:$CCMM$,$t:{t:$_Boolean},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','shared']};});
       atr$($$valParamDecl,'toplevel',function(){return false;},undefined,function(){return{mod:$CCMM$,$t:{t:$_Boolean},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','toplevel']};});
-      atr$($$valParamDecl,'formal',function(){return false;},undefined,function(){return{mod:$CCMM$,$t:{t:$_Boolean},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','formal']};});
-      atr$($$valParamDecl,'$_default',function(){return false;},undefined,function(){return{mod:$CCMM$,$t:{t:$_Boolean},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','default']};});
-      atr$($$valParamDecl,'actual',function(){return false;},undefined,function(){return{mod:$CCMM$,$t:{t:$_Boolean},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','actual']};});
       atr$($$valParamDecl,'qualifiedName',function(){
         return qname$(this.cont.tipo.$crtmm$)+"."+this.param.nm;
       },undefined,function(){return{mod:$CCMM$,$t:{t:$_String},$cont:ValParamDecl,pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$at','qualifiedName']};});
@@ -624,6 +628,15 @@ function $init$ValParamDecl(){
           var $$valParamDecl=this;
           throw Exception("IMPL ValParamDecl.memberApply");
       };$$valParamDecl.memberApply.$crtmm$=function(){return{mod:$CCMM$,$t:{t:Attribute$meta$model,a:{Type:'Type',Container:'Container'}},ps:[{nm:'containerType',mt:'prm',$t:{t:Type$meta$model,a:{Type:'Container'}},an:function(){return[];}}],$cont:ValParamDecl,tp:{Container:{'def':{t:Nothing}},Type:{'def':{t:Anything}}},pa:3,d:['ceylon.language.meta.declaration','ValueDeclaration','$m','memberApply']};};
+$$valParamDecl.equals=function(o){
+  if (is$(o,{t:ValParamDecl}))return o.param===this.param;
+  var eq=is$(o,{t:ValueDeclaration$meta$declaration})&&o.qualifiedName.equals(this.qualifiedName)&&o.shared==this.shared&&o.actual==this.actual&&o.formal==this.formal&&o.$_default==this.$_default&&o.variable==this.variable&&!o.toplevel&&o.openType.equals(this.openType)&&this.container.equals(o.container);
+  if (eq && o.parameter) {
+    return o.variadic==this.variadic&&o.defaulted==this.defaulted;
+  }
+  return eq;
+};$$valParamDecl.equals.$crtmm$=function(){return{mod:$CCMM$,$t:{t:$_Boolean},ps:[{nm:'other',mt:'prm',$t:{t:$_Object}}],$cont:ValParamDecl,pa:3,d:['$','Object','$m','equals']};};
+atr$($$valParamDecl,'string',function(){return 'value '+this.qualifiedName;},undefined,function(){return{mod:$CCMM$,$t:{t:$_String},pa:3,d:['$','Object','$at','string']};});
     })(ValParamDecl.$$.prototype);
   }
   return ValParamDecl;
