d47aec9988c402be102455fb695c61f827aba8e5
Tako Schotanus <tako@ceylon-lang.org>
2014-08-07 18:03:45 +0200

    Mkae sure `applyCwd()` doesn't add unnecessary `./` to the start of a file

--- common/src/com/redhat/ceylon/common/FileUtil.java
+++ common/src/com/redhat/ceylon/common/FileUtil.java
@@ -142,7 +142,7 @@ public class FileUtil {
      * @return An absolute file
      */
     public static File applyCwd(File cwd, File file) {
-        if (cwd != null && file != null && !file.isAbsolute()) {
+        if (cwd != null && !cwd.getPath().equals(".") && file != null && !file.isAbsolute()) {
             File absCwd = absoluteFile(cwd);
             file = new File(absCwd, file.getPath());
         }
