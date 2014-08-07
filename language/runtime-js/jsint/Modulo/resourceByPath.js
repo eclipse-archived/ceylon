a101988788993eaedc605626ba15941efabb6c25
Tako Schotanus <tako@ceylon-lang.org>
2014-08-07 22:58:47 +0200

    Implemented #5405 "`resourceByPath()` should behave differently with relative and absolute paths" for the JS backend

--- language/runtime-js/jsint/Modulo/resourceByPath.js
+++ language/runtime-js/jsint/Modulo/resourceByPath.js
@@ -1,23 +1,32 @@
 function resourceByPath(_path) {
   var mpath;
-  var sep = getOperatingSystem().fileSeparator;
   if (this.name==='default' && this.version==='unversioned') {
     mpath = this.name;
   } else {
-    mpath = this.name.replace(/\./g,sep) + sep + this.version;
+    mpath = this.name.replace(/\./g, '/');
   }
-  if (_path[0]===sep) {
-    mpath += _path;
+  var path = 'module-resources';
+  if (_path[0]==='/') {
+    path += _path;
   } else {
-    mpath += sep + _path;
+    if (this.name==='default' && this.version==='unversioned') {
+      path += '';
+    } else {
+      path += '/';
+      path += mpath;
+    }
+    path += '/' + _path;
   }
   if (getRuntime().name==='node.js') {
+    var sep = getOperatingSystem().fileSeparator;
+    path = mpath + '/' + this.version + '/' + path;
+    path = path.replace(/\\/g,sep);
     var _fr=require;//this is so that requirejs leaves us alone
     var pm=_fr('path');
     var mods=process.env.NODE_PATH.split(getOperatingSystem().pathSeparator);
     var fs=_fr('fs');
     for (var i=0; i<mods.length; i++) {
-      var fp = pm.resolve(mods[i], mpath);
+      var fp = pm.resolve(mods[i], path);
       if (fs.existsSync(fp)) {
         var f = fs.statSync(fp);
         if (f && f.isFile()) {
