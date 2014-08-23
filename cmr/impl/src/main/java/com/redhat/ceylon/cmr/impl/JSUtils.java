2e02b189b9ce73711037e328efd2cdb67fc2eb2b
Enrique Zamudio <alterbass@gmail.com>
2014-08-23 00:08:19 -0500

    new variant due to #2834

--- cmr/impl/src/main/java/com/redhat/ceylon/cmr/impl/JSUtils.java
+++ cmr/impl/src/main/java/com/redhat/ceylon/cmr/impl/JSUtils.java
@@ -267,7 +267,8 @@ public final class JSUtils extends AbstractDependencyResolver implements ModuleI
         try (BufferedReader reader = new BufferedReader(new FileReader(jsFile))) {
             String line = null;
             while ((line = reader.readLine()) != null) {
-                if ((line.startsWith("exports.$CCMM$=") || line.startsWith("var $CCMM$=")
+                if ((line.startsWith("ex$.$CCMM$=") || line.startsWith("exports.$CCMM$=")
+                        || line.startsWith("var $CCMM$=")
                         || line.startsWith("var $METAMODEL$=")) && line.endsWith("};")) {
                     line = line.substring(line.indexOf("{"), line.length()-1);
                     @SuppressWarnings("unchecked")
