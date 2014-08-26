9e89430285e55ab8c924680517a16eaf87970280
Enrique Zamudio <alterbass@gmail.com>
2014-08-26 16:42:56 -0500

    fix #2836

--- language/runtime-js/jsint/Paquete/members.js
+++ language/runtime-js/jsint/Paquete/members.js
@@ -1,8 +1,8 @@
 function members($$$mptypes){
   var filter=[];
   if (extendsType({t:FunctionDeclaration$meta$declaration},$$$mptypes.Kind$members))filter.push('m');
-  if (extendsType({t:ValueDeclaration$meta$declaration},$$$mptypes.Kind$members))filter.push('a','g','o', 's');
-  if (extendsType({t:ClassDeclaration$meta$declaration},$$$mptypes.Kind$members))filter.push('c');
+  if (extendsType({t:ValueDeclaration$meta$declaration},$$$mptypes.Kind$members))filter.push('a','g','s');
+  if (extendsType({t:ClassDeclaration$meta$declaration},$$$mptypes.Kind$members))filter.push('c','o');
   if (extendsType({t:InterfaceDeclaration$meta$declaration},$$$mptypes.Kind$members))filter.push('i');
   if (extendsType({t:AliasDeclaration$meta$declaration},$$$mptypes.Kind$members))filter.push('als');
   var r=[];
@@ -12,11 +12,11 @@ function members($$$mptypes){
     if (filter.indexOf(mt)>=0 && (m.pa&1)) {
       if (mt === 'm') {
         r.push(OpenFunction(this, m));
-      } else if (mt==='c') {
+      } else if (mt==='c'||mt==='o') {
         r.push(OpenClass$jsint(this, m));
       } else if (mt==='i') {
         r.push(OpenInterface$jsint(this, m));
-      } else if (mt==='a'||mt==='g'||mt==='o') {
+      } else if (mt==='a'||mt==='g') {
         r.push(OpenValue$jsint(this, m));
       } else if (mt==='s') {
         r.push(OpenSetter(OpenValue$jsint(this, m)));
