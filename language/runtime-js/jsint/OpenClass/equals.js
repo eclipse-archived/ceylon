5a438a0c0a0d4a9647406bd0c5191adcfc16610e
Enrique Zamudio <alterbass@gmail.com>
2014-08-26 16:42:42 -0500

    fix equals method for anonymous classes

--- language/runtime-js/jsint/OpenClass/equals.js
+++ language/runtime-js/jsint/OpenClass/equals.js
@@ -1,3 +1,11 @@
 function(other) {
-  return is$(other, {t:OpenClass$jsint}) && other.tipo===this.tipo;
+  if (is$(other, {t:OpenClass$jsint})) {
+    if (other.tipo===this.tipo)return true;
+    if (this.anonymous && other.anonymous) {
+      //Anonymous classes sometimes have a ref to the class constructor
+      //and sometimes to the value getter, depending on how it was obtained
+      return this.qualifiedName.equals(other.qualifiedName);
+    }
+  }
+  return false;
 }
