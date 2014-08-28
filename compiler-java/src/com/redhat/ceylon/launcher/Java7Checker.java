9412eabbeea224552d740dd1623f44a4d16bbfbe
Tako Schotanus <tako@ceylon-lang.org>
2014-08-28 10:56:33 +0200

    Fixed #4625 Java7Checker is too strict, should allow 7+

--- compiler-java/src/com/redhat/ceylon/launcher/Java7Checker.java
+++ compiler-java/src/com/redhat/ceylon/launcher/Java7Checker.java
@@ -21,17 +21,23 @@ public class Java7Checker {
 
     public static void check() {
         String version = System.getProperty("java.version");
-        if(version == null || version.isEmpty()){
-            System.err.println("Unable to determine Java version (java.version property missing or empty). Aborting.");
-            System.exit(1);
+        String[] elems = (version != null) ? version.split("\\.|_|-") : null;
+        if (version != null && !version.isEmpty() && elems != null && elems.length >= 3) {
+            try {
+                int major = Integer.parseInt(elems[0]);
+                int minor = Integer.parseInt(elems[1]);
+                int release = Integer.parseInt(elems[2]);
+                if (major != 1 || minor < 7) {
+                    System.err.println("Your Java version is not supported: " + version);
+                    System.err.println("Ceylon needs Java 7 or newer. Please install it from http://www.java.com");
+                    System.err.println("Aborting.");
+                    System.exit(1);
+                }
+                return;
+            } catch (NumberFormatException ex) {}
         }
-        // TODO Do something better here
-        if(!version.startsWith("1.7") && !version.startsWith("1.8")){
-            System.err.println("Your Java version is not supported: "+version);
-            System.err.println("Ceylon needs Java 7 or newer. Please install it from http://www.java.com");
-            System.err.println("Aborting.");
-            System.exit(1);
-        }
+        System.err.println("Unable to determine Java version (java.version property missing, empty or has unexpected format: '" + version +"'). Aborting.");
+        System.exit(1);
     }
     
 }
