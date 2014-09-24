c57bf61a9363c34a5b218159a2833671f53536c7
Enrique Zamudio <alterbass@gmail.com>
2014-09-23 21:33:06 -0500

    container

--- language/runtime-js/AppliedAttribute.js
+++ language/runtime-js/AppliedAttribute.js
@@ -1,42 +1,43 @@
-function AppliedAttribute(pname, atr,$$targs$$,$$appliedAttribute){
+function AppliedAttribute(pname, atr,$$targs$$,that){
   if (!$$targs$$.Get$AppliedAttribute)$$targs$$.Get$AppliedAttribute=$$targs$$.Get$Attribute;
   if (!$$targs$$.Set$AppliedAttribute)$$targs$$.Set$AppliedAttribute=$$targs$$.Set$Attribute;
   if (!$$targs$$.Container$AppliedAttribute)$$targs$$.Container$AppliedAttribute=$$targs$$.Container$Attribute;
   $init$AppliedAttribute();
-  if ($$appliedAttribute===undefined) {
-    $$appliedAttribute=function(x){return AppliedValue$jsint(x,atr,
+  if (that===undefined) {
+    that=function(x){return AppliedValue$jsint(x,atr,
       {Get$AppliedValue:$$targs$$.Get$AppliedAttribute,Set$AppliedValue:$$targs$$.Set$AppliedAttribute,
       Container$AppliedValue:$$targs$$.Container$AppliedAttribute});};
-    $$appliedAttribute.$$=AppliedAttribute.$$;
+    that.$$=AppliedAttribute.$$;
     var dummy=new AppliedAttribute.$$;
-    $$appliedAttribute.getT$all=function(){return dummy.getT$all();};
-    $$appliedAttribute.getT$name=function(){return dummy.getT$name();};
+    that.getT$all=function(){return dummy.getT$all();};
+    that.getT$name=function(){return dummy.getT$name();};
   }
-  Attribute$meta$model($$appliedAttribute.$$targs$$===undefined?$$targs$$:{Get$Attribute:$$appliedAttribute.$$targs$$.Get$AppliedAttribute,
-    Set$Attribute:$$appliedAttribute.$$targs$$.Set$AppliedAttribute,
-    Container$Attribute:$$appliedAttribute.$$targs$$.Container$AppliedAttribute},$$appliedAttribute);
-  set_type_args($$appliedAttribute,$$targs$$,AppliedAttribute);
-  $$appliedAttribute.tipo=atr;
-  $$appliedAttribute.pname=pname;
-  atr$($$appliedAttribute,'type',function(){
+  Attribute$meta$model(that.$$targs$$===undefined?$$targs$$:{Get$Attribute:that.$$targs$$.Get$AppliedAttribute,
+    Set$Attribute:that.$$targs$$.Set$AppliedAttribute,
+    Container$Attribute:that.$$targs$$.Container$AppliedAttribute},that);
+  set_type_args(that,$$targs$$,AppliedAttribute);
+  that.tipo=atr;
+  that.pname=pname;
+  atr$(that,'type',function(){
     var t = getrtmm$$(atr);
     if (t===undefined)throw Exception("Attribute reference not found. Metamodel doesn't work in modules compiled in lexical scope style.");
     t=t.$t;
+    //TODO resolve type arguments
     return typeLiteral$meta({Type$typeLiteral:t});
   },undefined,function(){return{mod:$CCMM$,$t:{t:Type$meta$model,a:{Type$Type:'Get$Attribute'}},$cont:AppliedAttribute,an:function(){return[shared(),actual()];},d:['ceylon.language.meta.model','Attribute','$at','type']};});
   //AttributeGetterDefinition declaration at X (100:4-100:83)
-  atr$($$appliedAttribute,'declaration',function(){
+  atr$(that,'declaration',function(){
     var mm = getrtmm$$(atr);
     var _m = typeof(mm.mod)==='function'?mm.mod():mm.mod;
     var pkg = getModules$meta().find(_m['$mod-name'],_m['$mod-version']).findPackage(mm.d[0]);
     return OpenValue$jsint(pkg, atr);
   },undefined,function(){return{mod:$CCMM$,$t:{t:ValueDeclaration$meta$declaration},$cont:AppliedAttribute,an:function(){return[shared(),actual()];},d:['ceylon.language.meta.model','Attribute','$at','declaration']};});
-  $$appliedAttribute.$_bind=function(cont){
+  that.$_bind=function(cont){
     return AppliedValue$jsint(cont,atr,{Get$AppliedValue:$$targs$$.Get$AppliedAttribute,
       Set$AppliedValue:$$targs$$.Set$AppliedAttribute,
       Container$AppliedValue:$$targs$$.Container$AppliedAttribute});
   }
-  atr$($$appliedAttribute,'string',function(){
+  atr$(that,'string',function(){
     var c=getrtmm$$(atr).$cont;
     if (typeof(c.$crtmm$)==='function')c.$crtmm$=c.$crtmm$();
     if (!c)return qname$(atr);
@@ -57,10 +58,13 @@ function AppliedAttribute(pname, atr,$$targs$$,$$appliedAttribute){
     qn+="."+pname;
     return qn;
   },undefined,function(){return{mod:$CCMM$,$t:{t:$_String},d:['$','Object','$at','string']};});
-  $$appliedAttribute.equals=function(o) {
+  atr$(that,'container',function(){
+    return AppliedValue$jsint.$$.prototype.$prop$getContainer.get.call(this);
+  },undefined,function(){return{mod:$CCMM$,$t:{t:Type$meta$model,a:{Type$Type:{t:Anything}}},$cont:AppliedAttribute,pa:67,d:['ceylon.language.jsint','Member','$at','container']};});
+  that.equals=function(o) {
     return is$(o,{t:AppliedAttribute}) && o.tipo===atr;
   }
-  return $$appliedAttribute;
+  return that;
 }
 AppliedAttribute.$crtmm$=function(){return{mod:$CCMM$,'super':{t:Basic},tp:{Get:{dv:'out','def':{t:Anything}},Set:{dv:'in','def':{t:Nothing}},Container:{dv:'in'}},sts:[{t:Attribute$meta$model,a:{Get:'Get',Set:'Set',Container:'Container'}}],pa:1,d:['ceylon.language.meta.model','Attribute']};};
 ex$.AppliedAttribute=AppliedAttribute;
