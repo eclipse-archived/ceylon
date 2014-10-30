9be4005abc27e4c5a95945ef2a2e269db6be241f
Enrique Zamudio <alterbass@gmail.com>
2014-10-30 17:21:04 -0600

    fix some union/intersection impls

--- language/runtime-js/jsint/AppliedIntersectionType/intersection.js
+++ language/runtime-js/jsint/AppliedIntersectionType/intersection.js
@@ -1,4 +1,8 @@
 function(t,$mpt){
   if (this.subtypeOf(t))return this;
-  return AppliedIntersectionType$jsint(this.tipo,this.satisfiedTypes.sequence().withTrailing(t,{Other$withTrailing:$mpt.Other$intersection}));
+  var _t={t:'i',l:[]};
+  for (var i=0;i<_t.l.length;i++)_t.l.push(this.tipo.l[i]);
+  _t.l.push($mpt.Other$intersection);
+  return AppliedIntersectionType$jsint(this.tipo,this.satisfiedTypes.sequence().withTrailing(t,{Other$withTrailing:$mpt.Other$intersection}),
+    {Union$AppliedIntersectionType:_t});
 }
--- language/runtime-js/jsint/AppliedUnionType/union.js
+++ language/runtime-js/jsint/AppliedUnionType/union.js
@@ -1,4 +1,8 @@
 function(t,$mpt){
   if (this.supertypeOf(t))return this;
-  return AppliedUnionType$jsint(this.tipo,this.caseTypes.sequence().withTrailing(t,{Other$withTrailing:$mpt.Other$Union}));
+  var _t={t:'u',l:[]};
+  for (var i=0; i<this.tipo.l.length;i++)_t.l.push(this.tipo.l[i]);
+  _t.l.push($mpt.Other$union);
+  return AppliedUnionType$jsint(this.tipo,this.caseTypes.sequence().withTrailing(t,{Other$withTrailing:$mpt.Other$union}),
+    {Union$AppliedUnionType:_t});
 }
