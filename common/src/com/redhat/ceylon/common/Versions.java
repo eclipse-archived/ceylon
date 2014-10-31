5b9eba8339670157b78c5cfdd0bcb54b1705b956
Stephane Epardaud <stef@epardaud.fr>
2014-10-31 11:26:07 +0100

    Bumped versions to 1.1.1

--- common/src/com/redhat/ceylon/common/Versions.java
+++ common/src/com/redhat/ceylon/common/Versions.java
@@ -2,25 +2,37 @@ package com.redhat.ceylon.common;
 
 public class Versions {
     
-    // The current version is Ceylon 1.1.0 "Ultimate Ship The Second"
+    // The current version is Ceylon 1.1.1 "Pif Gadget"
     // This comment is here so this file will show up in searches for the current version number
     
     /****************************************************************************************************
     * WARNING Don't forget to update:
-    * - ceylon.language/runtime-js/process.js (versions, name, binary version)
-    * - ceylon-spec/src/com/redhat/ceylon/compiler/typechecker/TypeChecker.java (language module version)
-    * - *.java,*.src @(\.?com.redhat.ceylon.compiler.java.metadata.)?Ceylon\(\s*major\s*=\s*6 -> @$1Version(major = 7
+    * - ceylon.language/runtime-js/language/releaseVersion.js
+    * - ceylon.language/runtime-js/language/version.js
+    * - ceylon.language/runtime-js/language/versionName.js
+    * - ceylon.language/test/process.ceylon (versions, name, binary version)
     * - ceylon-dist/build.properties (versions)
+    * - ceylon-dist/osgi/module.properties (versions)
+    * - ceylon-dist/osgi/META-INF/MANIFEST.MF (versions)
     * - ceylon-common/common-build.properties (version)
     * - ceylon-ide-eclipse/plugins/com.redhat.ceylon.eclipse.ui/about.ini (version, name)
-    * - ceylon.language/test/process.ceylon (versions, name, binary version)
+    *   Find all files on IDE which need to be updated:
+    *    grep -rl --exclude-dir 'target' --exclude-dir required-bundle-proxies --exclude-dir .git --exclude '*.class' --exclude '*.car' --exclude '*.idx' '1\.1\.0' .
     * - ceylon-compiler Eclipse build path (ceylon.language/ide-dist/ceylon.language-XXX.car)
     * - ceylon.language Eclipse build path (ceylon.language/ide-dist/ceylon.language-XXX.car)
+    * 
+    * If you bump the binary version:
+    * - *.java,*.src @(\.?com.redhat.ceylon.compiler.java.metadata.)?Ceylon\(\s*major\s*=\s*6 -> @$1Version(major = 7
+    *
+    * If you bump the SDK:
+    * - ceylon-sdk/source
+    * - ceylon-sdk/source-js
+    * - ceylon-sdk/test-source
     ****************************************************************************************************/
 
     public static final int CEYLON_VERSION_MAJOR = 1;
     public static final int CEYLON_VERSION_MINOR = 1;
-    public static final int CEYLON_VERSION_RELEASE = 0;
+    public static final int CEYLON_VERSION_RELEASE = 1;
     /**
      * The MAJOR.MINOR.RELEASE version.
      */
@@ -28,7 +40,7 @@ public class Versions {
     /**
      * The release code name.
      */
-    public static final String CEYLON_VERSION_NAME = "Ultimate Ship The Second";
+    public static final String CEYLON_VERSION_NAME = "Pif Gadget";
     /**
      * The version number + code name description string.
      */
