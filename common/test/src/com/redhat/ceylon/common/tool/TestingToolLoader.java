25537aa8cb954998f285138e42547e261293bca3
Tako Schotanus <tako@ceylon-lang.org>
2014-11-05 17:57:48 +0100

    Fixed the way the test finds the "example" tool so it won't confuse it with any example scripts the user might have installed in their path

--- common/test/src/com/redhat/ceylon/common/tool/TestingToolLoader.java
+++ common/test/src/com/redhat/ceylon/common/tool/TestingToolLoader.java
@@ -10,7 +10,7 @@ public class TestingToolLoader extends ServiceToolLoader {
 
     @Override
     public String getToolName(String className) {
-        return classNameToToolName(className);
+        return camelCaseToDashes(className.replaceAll("^(.*\\.)?Test(.*)Tool$", "$2"));
     }
 
 }
