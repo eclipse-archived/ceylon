eac0b15de1bc15ece05b4d8dd62b45512d1f24ba
Enrique Zamudio <alterbass@gmail.com>
2014-11-08 17:21:00 -0600

    work on #2881

--- language/runtime-js/callables.js
+++ language/runtime-js/callables.js
@@ -2,17 +2,17 @@ function $JsCallable(callable,parms,targs) {
     if (callable.getT$all === undefined) {
         callable.getT$all=Callable.getT$all;
     }
-    var set_meta = callable.$crtmm$ === undefined;
+    var set_meta = getrtmm$$(callable) === undefined;
     if (set_meta) {
         callable.$crtmm$={ps:[],mod:$CCMM$,d:['$','Callable']};
         if (parms !== undefined) {
-            callable.$crtmm$['ps']=parms;
+            callable.$crtmm$.ps=parms;
         }
     }
     if (targs !== undefined && callable.$$targs$$ === undefined) {
         callable.$$targs$$=targs;
         if (set_meta) {
-            callable.$crtmm$['$t']=targs['Return'];
+            callable.$crtmm$.$t=targs.Return$Callable;
         }
     }
     return callable;
@@ -21,31 +21,31 @@ initExistingTypeProto($JsCallable, Function, 'ceylon.language::JsCallable', Call
 
 function nop$() { return null; }
 
-//This is used for plain method references
+//This is used for method references
 function JsCallable(o,f,targs) {
   if (o===null || o===undefined) return nop$;
   var f2 = function() {
     var arg=[].slice.call(arguments,0);
     //if we get only 1 arg and it's a tuple...
     if (arg.length===1 && is$(arg[0],{t:Tuple})) {
-      //Possible spread
+      //Possible spread, check the metamodel
       var mm=getrtmm$$(f);
       //If f has only 1 param and it's not sequenced, get its type
       var a1t=mm && mm.ps && mm.ps.length===1 && mm.ps[0].seq===undefined ? mm.ps[0].$t : undefined;
-      //If its type is a type param, get the type arg
+      //If it's a type param, get the type argument
       if (typeof(a1t)==='string')a1t=targs && targs[a1t];
       //If the tuple type matches the param type, it's NOT a spread
+      //(it's just a regular 1-param func which happens to receive a tuple)
       if (!(a1t && is$(arg[0],a1t))) {
         var typecheck;
         if (a1t && arg[0].$$targs$$) {
           if (arg[0].$$targs$$.First$Tuple) {
-            typecheck=arg[0].$$targs$$.First$Tuple;
+            typecheck={t:Tuple,a:arg[0].$$targs$$};
           } else if (arg[0].$$targs$$.t==='T') {
-            typecheck=arg[0].$$targs$$.l[0];
+            typecheck=arg[0].$$targs$$;
+          } else if (arg[0].$$targs$$.Element$Iterable) {
+            typecheck={t:Iterable,a:arg[0].$$targs$$.Element$Iterable};
           }
-        } else {
-          typecheck={t:Iterable};
-          if (arg[0].$$targs$$ && arg[0].$$targs$$.Element$Iterable)typecheck.a={Element$Iterable:arg[0].$$targs$$.Element$Iterable};
         }
         if (mm && mm.ps && (mm.ps.length>1 || (mm.ps.length===1
             && (mm.ps[0].seq || !extendsType(a1t, typecheck))))) {
