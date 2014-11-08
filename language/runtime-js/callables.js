f9d47582971d5cbc62bdc21f6ac18616f68bfb07
Enrique Zamudio <alterbass@gmail.com>
2014-11-07 22:28:08 -0600

    work on #2881

--- language/runtime-js/callables.js
+++ language/runtime-js/callables.js
@@ -22,19 +22,31 @@ initExistingTypeProto($JsCallable, Function, 'ceylon.language::JsCallable', Call
 function nop$() { return null; }
 
 //This is used for plain method references
-function JsCallable(o,f) {
+function JsCallable(o,f,targs) {
   if (o===null || o===undefined) return nop$;
   var f2 = function() {
     var arg=[].slice.call(arguments,0);
-    if (arg.length==1 && is$(arg[0],{t:Tuple})) {
+    if (arg.length===1 && is$(arg[0],{t:Tuple})) {
       //Possible spread
       var mm=getrtmm$$(f);
-      var typecheck={t:Iterable};
-      if (arg[0].$$targs$$ && arg[0].$$targs$$.Element$Iterable)typecheck.a={Element$Iterable:arg[0].$$targs$$.Element$Iterable};
-      if (mm && mm.ps!==undefined
-          && (mm.ps.length>1 || (mm.ps.length==1
-          && (mm.ps[0].seq || !extendsType(mm.ps[0].$t, typecheck))))) {
-        var a=arg[0].elem$;
+      //If f has only 1 param and it's not sequenced, get its type
+      var a1t=mm && mm.ps && mm.ps.length===1 && mm.ps[0].seq===undefined ? mm.ps[0].$t : undefined;
+      var typecheck;
+      if (a1t && arg[0].$$targs$$) {
+        if (arg[0].$$targs$$.First$Tuple) {
+          typecheck=arg[0].$$targs$$.First$Tuple;
+        } else if (arg[0].$$targs$$.t==='T') {
+          typecheck=arg[0].$$targs$$.l[0];
+        }
+      } else {
+        typecheck={t:Iterable};
+        if (arg[0].$$targs$$ && arg[0].$$targs$$.Element$Iterable)typecheck.a={Element$Iterable:arg[0].$$targs$$.Element$Iterable};
+      }
+      //If its type is a type param, get the type arg
+      if (typeof(a1t)==='string' && targs && targs[a1t])a1t=targs[a1t];
+      if (mm && mm.ps && (mm.ps.length>1 || (mm.ps.length===1
+          && (mm.ps[0].seq || !extendsType(a1t, typecheck))))) {
+        var a=arg[0].nativeArray ? arg[0].nativeArray():undefined;
         if (a===undefined) {
           a=[];
           for (var i=0;i<arg[0].size;i++)a.push(arg[0].$_get(i));
@@ -42,6 +54,7 @@ function JsCallable(o,f) {
         arg=a;
       }
     }
+    if (targs)arg.push(targs);
     return f.apply(o, arg);
   };
   f2.$crtmm$=f.$crtmm$===undefined?Callable.$crtmm$:f.$crtmm$;
