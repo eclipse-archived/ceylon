3d27074272e449844f118de846cc76f2674d084f
Enrique Zamudio <alterbass@gmail.com>
2014-11-08 14:07:30 -0600

    metamodel of flattened func

--- language/runtime-js/flatten.js
+++ language/runtime-js/flatten.js
@@ -1,5 +1,6 @@
 function flatten(tf, $$$mptypes) {
   if (tf.$unflattened$)return tf.$unflattened$;
+  var mm=getrtmm$$(tf);
   function rf() {
     var argc = arguments.length;
     var last = argc>0 ? arguments[argc-1] : undefined;
@@ -20,6 +21,14 @@ function flatten(tf, $$$mptypes) {
     }
     return tf(tpl$(t));
   };
+  if (mm) {
+    rf.$crtmm$={$t:mm.$t,ps:[]};
+    if (mm.ps.length===1 && mm.ps[0].$t.t==='T') {
+      for(var i=0;i<mm.ps[0].$t.l.length;i++){
+        rf.$crtmm$.ps.push(mm.ps[0].$t.l[i]);
+      }
+    }
+  }
   rf.$$targs$$={Return$Callable:$$$mptypes.Return$flatten,Arguments$Callable:$$$mptypes.Args$flatten};
   rf.$flattened$=tf;
   return rf;
