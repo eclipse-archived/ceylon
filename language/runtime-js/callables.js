7074d7a67d2794da894909b64d0f3aef206d574b
Enrique Zamudio <alterbass@gmail.com>
2014-11-08 09:18:48 -0600

    #2881 - fix some cases, break some others...

--- language/runtime-js/callables.js
+++ language/runtime-js/callables.js
@@ -26,32 +26,36 @@ function JsCallable(o,f,targs) {
   if (o===null || o===undefined) return nop$;
   var f2 = function() {
     var arg=[].slice.call(arguments,0);
+    //if we get only 1 arg and it's a tuple...
     if (arg.length===1 && is$(arg[0],{t:Tuple})) {
       //Possible spread
       var mm=getrtmm$$(f);
       //If f has only 1 param and it's not sequenced, get its type
       var a1t=mm && mm.ps && mm.ps.length===1 && mm.ps[0].seq===undefined ? mm.ps[0].$t : undefined;
-      var typecheck;
-      if (a1t && arg[0].$$targs$$) {
-        if (arg[0].$$targs$$.First$Tuple) {
-          typecheck=arg[0].$$targs$$.First$Tuple;
-        } else if (arg[0].$$targs$$.t==='T') {
-          typecheck=arg[0].$$targs$$.l[0];
-        }
-      } else {
-        typecheck={t:Iterable};
-        if (arg[0].$$targs$$ && arg[0].$$targs$$.Element$Iterable)typecheck.a={Element$Iterable:arg[0].$$targs$$.Element$Iterable};
-      }
       //If its type is a type param, get the type arg
-      if (typeof(a1t)==='string' && targs && targs[a1t])a1t=targs[a1t];
-      if (mm && mm.ps && (mm.ps.length>1 || (mm.ps.length===1
-          && (mm.ps[0].seq || !extendsType(a1t, typecheck))))) {
-        var a=arg[0].nativeArray ? arg[0].nativeArray():undefined;
-        if (a===undefined) {
-          a=[];
-          for (var i=0;i<arg[0].size;i++)a.push(arg[0].$_get(i));
+      if (typeof(a1t)==='string')a1t=targs && targs[a1t];
+      //If the tuple type matches the param type, it's NOT a spread
+      if (!(a1t && is$(arg[0],a1t))) {
+        var typecheck;
+        if (a1t && arg[0].$$targs$$) {
+          if (arg[0].$$targs$$.First$Tuple) {
+            typecheck=arg[0].$$targs$$.First$Tuple;
+          } else if (arg[0].$$targs$$.t==='T') {
+            typecheck=arg[0].$$targs$$.l[0];
+          }
+        } else {
+          typecheck={t:Iterable};
+          if (arg[0].$$targs$$ && arg[0].$$targs$$.Element$Iterable)typecheck.a={Element$Iterable:arg[0].$$targs$$.Element$Iterable};
         }
-        arg=a;
+        if (mm && mm.ps && (mm.ps.length>1 || (mm.ps.length===1
+            && (mm.ps[0].seq || !extendsType(a1t, typecheck))))) {
+          var a=arg[0].nativeArray ? arg[0].nativeArray():undefined;
+          if (a===undefined) {
+            a=[];
+            for (var i=0;i<arg[0].size;i++)a.push(arg[0].$_get(i));
+          }
+          arg=a;
+        }
       }
     }
     if (targs)arg.push(targs);
