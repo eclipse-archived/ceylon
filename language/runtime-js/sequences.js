1e8f76e48b9185e8f26fbb122dd1d6ec56044bc7
Enrique Zamudio <alterbass@gmail.com>
2014-11-11 16:09:40 -0600

    fix a couple of things

--- language/runtime-js/sequences.js
+++ language/runtime-js/sequences.js
@@ -1,11 +1,12 @@
 var arrprot$=Array.prototype;
 arrprot$.ser$$=function(d){
-  d=d(AppliedClass($_Array,{Type$AppliedClass:$_Array,Arguments$AppliedClass:
-    {t:'T',l:[{t:Iterable,a:{Element$Iterable:this.$$targs$$.Element$Iterable}}]}}));
-  d.putTypeArgument(OpenTypeParam$jsint($_Array,'Element$Array'),typeLiteral$meta({Type$typeLiteral:this.$$targs$$.Element$Iterable}));
+  var targ=this.$$targs$$.Element$Iterable;
+  d=d(AppliedClass($_Array,{Type$AppliedClass:{t:$_Array,a:{Element$Array:targ}},
+    Arguments$AppliedClass:{t:'T',l:[{t:Iterable,a:{Element$Iterable:targ}}]}}));
+  d.putTypeArgument(OpenTypeParam$jsint($_Array,'Element$Array'),typeLiteral$meta({Type$typeLiteral:targ}));
   d.putValue(OpenValue$jsint(lmp$(ex$,'$'),this.$prop$getSize),this.length,{Instance$putValue:{t:Integer}});
   for (var i=0; i < this.length; i++) {
-    d.putElement(i,this[i],{Instance$putElement:this.$$targs$$.Element$Iterable});
+    d.putElement(i,this[i],{Instance$putElement:targ});
   }
 }
 var origArrToString = arrprot$.toString;
