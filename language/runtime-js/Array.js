4f35d5d596050e97b09ec06a558f91c8ed5f374f
Enrique Zamudio <alterbass@gmail.com>
2014-11-11 16:09:01 -0600

    simplify the code a bit

--- language/runtime-js/Array.js
+++ language/runtime-js/Array.js
@@ -2,15 +2,13 @@ function $_Array(elems,$$targs$$) {
     $init$$_Array();
     var e=[];
     if (!(elems === null || elems === undefined)) {
-        var iter=elems.iterator();
-        var item;while((item=iter.next())!==getFinished()) {
+        var item;for(var iter=elems.iterator();(item=iter.next())!==getFinished();) {
             e.push(item);
         }
     }
-    e.$$targs$$=$$targs$$;
     var t=$$targs$$.Element$Array;
     List({Element$List:t}, e);
-    return e;
+    return e.rt$(t);
 }
 $_Array.deser$$=function(a){
   var targ=ser$et$(a.getTypeArgument(OpenTypeParam$jsint($_Array,'Element$Array')));
