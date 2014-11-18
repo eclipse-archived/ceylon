446daed73b926a588ab438f5b1ccfae6eec8e871
Stephane Epardaud <stef@epardaud.fr>
2014-11-18 17:39:51 +0100

    Test for #1914 which passes
    We must have already fixed it

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTest_1500_1999.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTest_1500_1999.java
@@ -882,6 +882,11 @@ public class IssuesTest_1500_1999 extends CompilerTest {
     }
 
     @Test
+    public void testBug1914() {
+        compareWithJavaSource("bug19xx/Bug1914");
+    }
+
+    @Test
     public void testBug1916() {
         compareWithJavaSource("bug19xx/Bug1916");
     }
