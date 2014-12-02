8d0b6bd32d29d54b5d9a019c2c5a3007c1cc1796
Enrique Zamudio <alterbass@gmail.com>
2014-12-02 17:31:30 -0600

    ConstructorDeclaration seems to be working now #5461

--- language/runtime-js/jsint/OpenConstructor/equals.js
+++ language/runtime-js/jsint/OpenConstructor/equals.js
@@ -1,7 +1,7 @@
 function(other) {
   if (is$(other, {t:OpenConstructor$jsint})) {
     if (other.tipo===this.tipo)return true;
-    return (this.container.equals(other.container) &&
+    return this.container.equals(other.container) &&
       this.name.equals(other.name);
   }
   return false;
