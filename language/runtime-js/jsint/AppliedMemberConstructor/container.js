7073663a84f4a19ccba64682485bd01ad0b61f40
Enrique Zamudio <alterbass@gmail.com>
2014-12-03 13:20:30 -0600

    member class #5461

--- language/runtime-js/jsint/AppliedMemberConstructor/container.js
+++ language/runtime-js/jsint/AppliedMemberConstructor/container.js
@@ -1,6 +1,9 @@
 if (this.cont$===undefined) {
   var cc=getrtmm$$(this.tipo).$cont;
   var mm=getrtmm$$(cc);
-  this.cont$=coicont$(this);
+  var _t={t:cc};
+  if (this.$targs)_t.a=this.$targs;
+  this.cont$=AppliedMemberClass(cc,{Type$AppliedMemberClass:_t,Arguments$AppliedMemberClass:{t:Nothing},
+    Container$AppliedMemberClass:this.$$targs$$.Container$AppliedMemberConstructor},undefined,this.$targs);
 }
 return this.cont$;
