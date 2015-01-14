aa42e8e0c486e9c4bb744243a515e77b30fb10bd
Stephane Epardaud <stef@epardaud.fr>
2015-01-14 17:16:46 +0100

    Ceylon help tool: support pagination #1506

--- common/src/com/redhat/ceylon/common/tools/help/CeylonHelpTool.java
+++ common/src/com/redhat/ceylon/common/tools/help/CeylonHelpTool.java
@@ -19,6 +19,10 @@
  */
 package com.redhat.ceylon.common.tools.help;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.lang.ProcessBuilder.Redirect;
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -33,6 +37,7 @@ import com.redhat.ceylon.common.tool.Summary;
 import com.redhat.ceylon.common.tool.Tool;
 import com.redhat.ceylon.common.tool.ToolLoader;
 import com.redhat.ceylon.common.tool.ToolModel;
+import com.redhat.ceylon.common.tool.ToolUsageError;
 import com.redhat.ceylon.common.tool.WordWrap;
 import com.redhat.ceylon.common.tools.CeylonTool;
 import com.redhat.ceylon.common.tools.help.model.Doc;
@@ -60,6 +65,7 @@ public class CeylonHelpTool implements Tool {
     private ToolModel<?> tool;
     private boolean synopsis = false;
     private String options = null;
+    private boolean wantsPager;
     
     public final void setToolLoader(ToolLoader toolLoader) {
         this.toolLoader = toolLoader;
@@ -97,6 +103,34 @@ public class CeylonHelpTool implements Tool {
 
     @Override
     public void initialize(CeylonTool mainTool) {
+        Boolean wantsPager = mainTool.getWantsPager();
+        // tool-specific option
+        if(wantsPager == null){
+            String option = CeylonConfig.get().getOption("help.pager");
+            if(option != null){
+                if(CeylonConfig.isFalsish(option))
+                    wantsPager = Boolean.FALSE;
+                else // any other value is either true or a pager command
+                    wantsPager = Boolean.TRUE;
+            }
+        }
+        // general option
+        if(wantsPager == null){
+            String option = CeylonConfig.get().getOption("defaults.pager");
+            if(option != null){
+                if(CeylonConfig.isFalsish(option))
+                    wantsPager = Boolean.FALSE;
+                else // any other value is either true or a pager command
+                    wantsPager = Boolean.TRUE;
+            }
+        }
+        // allow pager if it's unspecified (default) or if it's not explicitly disabled
+        if((wantsPager == null 
+                || wantsPager.booleanValue())
+                // and if stdout is on a tty
+                && Boolean.getBoolean("com.redhat.ceylon.common.tool.terminal.isatty")){
+            this.wantsPager = true;
+        }
     }
     
     @Override
@@ -109,21 +143,97 @@ public class CeylonHelpTool implements Tool {
             final ToolModel<CeylonTool> root = toolLoader.loadToolModel("");
             doc = docBuilder.buildDoc(root, true);
         }
-        final WordWrap wrap = getWrap();
-        Visitor plain = new PlainVisitor(wrap);
-        if (synopsis) {
-            plain = new SynopsisOnlyVisitor(plain);
-        } else if (options != null) {
-            plain = new OptionsOnlyVisitor(plain, 
-                    new HashSet<String>(Arrays.asList(options.trim().split("\\s*,\\s*"))));
+        Process pagerProcess = null;
+        if(wantsPager){
+            pagerProcess = startPager();
         }
-        doc.accept(plain);
-        wrap.flush();
+        OutputStream pagerPipe = null;
+        if(pagerProcess != null)
+            pagerPipe = pagerProcess.getOutputStream();
+        try{
+            final WordWrap wrap = getWrap(pagerPipe);
+            Visitor plain = new PlainVisitor(wrap);
+            if (synopsis) {
+                plain = new SynopsisOnlyVisitor(plain);
+            } else if (options != null) {
+                plain = new OptionsOnlyVisitor(plain, 
+                        new HashSet<String>(Arrays.asList(options.trim().split("\\s*,\\s*"))));
+            }
+            doc.accept(plain);
+            wrap.flush();
+        }finally{
+            // shutdown pager
+            if(pagerPipe != null){
+                try {
+                    pagerPipe.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                // wait for pager to be done, there's no point doing anything else meanwhile
+                try {
+                    int errorCode = pagerProcess.waitFor();
+                    if(errorCode != 0){
+                        throw new ToolUsageError("Pager process returned an error exit code: "+errorCode+". Try fixing your $CEYLON_PAGER or $PAGER environment variable or invoke with the --no-pager command-line option.");
+                    }
+                } catch (InterruptedException e) {
+                    throw new ToolUsageError("Pager process interrupted. Try fixing your $CEYLON_PAGER or $PAGER environment variable or invoke with the --no-pager command-line option.");
+                }
+            }
+        }
     }
 
-    private WordWrap getWrap() {
-        return new WordWrap(out != null ? out 
-                : synopsis || options != null ? System.err 
-                : System.out);
+    private Process startPager() {
+        // find out the pager process, same process as in git
+        // first try ceylon-specific pager env var
+        String pager = System.getenv("CEYLON_PAGER");
+        if(pager == null){
+            // first try tool-specific option
+            String option = CeylonConfig.get().getOption("help.pager");
+            // if it's neither true nor false it's a pager program
+            if(option != null 
+                    && !CeylonConfig.isTrueish(option)
+                    && !CeylonConfig.isFalsish(option))
+                pager = option;
+        }
+        if(pager == null){
+            // then try general option
+            String option = CeylonConfig.get().getOption("defaults.pager");
+            // if it's neither true nor false it's a pager program
+            if(option != null 
+                    && !CeylonConfig.isTrueish(option)
+                    && !CeylonConfig.isFalsish(option))
+                pager = option;
+        }
+        // then try general pager env var
+        if(pager == null)
+            pager = System.getenv("PAGER");
+        // default to less
+        if(pager == null)
+            pager = "less";
+        // make sure we don't page for cat
+        if(!pager.equals("cat")){
+            // start the pager process
+            ProcessBuilder pb = new ProcessBuilder(pager);
+            pb.redirectError(Redirect.INHERIT);
+            pb.redirectOutput(Redirect.INHERIT);
+            try {
+                return pb.start();
+            } catch (IOException e) {
+                throw new ToolUsageError("Could not start pager process '"+pager+"'. Try fixing your $CEYLON_PAGER or $PAGER environment variable or invoke with the --no-pager command-line option.", e);
+            }
+        }
+        return null;
+    }
+
+    private WordWrap getWrap(OutputStream pagerPipe) {
+        Appendable destination = null;
+        if(pagerPipe != null)
+            destination = new PrintStream(pagerPipe);
+        if(destination == null)
+            destination = out;
+        if(destination == null)
+            destination = synopsis || options != null ? System.err : System.out;
+        return new WordWrap(destination);
     }
 }
