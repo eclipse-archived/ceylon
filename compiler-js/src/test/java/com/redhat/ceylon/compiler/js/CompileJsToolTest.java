f63db7f227b09af1425edf73e5de660976463863
Stephane Epardaud <stef@epardaud.fr>
2015-01-15 15:27:41 +0100

    Cosmetic: imports cleanup

--- compiler-js/src/test/java/com/redhat/ceylon/compiler/js/CompileJsToolTest.java
+++ compiler-js/src/test/java/com/redhat/ceylon/compiler/js/CompileJsToolTest.java
@@ -14,12 +14,7 @@ import org.junit.Test;
 import com.redhat.ceylon.common.FileUtil;
 import com.redhat.ceylon.common.tool.AbstractToolTest;
 import com.redhat.ceylon.common.tool.OptionArgumentException;
-import com.redhat.ceylon.common.tool.ServiceToolLoader;
-import com.redhat.ceylon.common.tool.Tool;
-import com.redhat.ceylon.common.tool.ToolFactory;
-import com.redhat.ceylon.common.tool.ToolLoader;
 import com.redhat.ceylon.common.tool.ToolModel;
-import com.redhat.ceylon.common.tools.CeylonTool;
 
 public class CompileJsToolTest extends AbstractToolTest {
 
