d32553cbac852534af4ca8590eb98f4702f8b0c9
Enrique Zamudio <alterbass@gmail.com>
2015-01-20 10:14:16 -0600

    more renaming for #2914

--- language/runtime-js/jsint/OpenTypeParam/variance.js
+++ language/runtime-js/jsint/OpenTypeParam/variance.js
@@ -1,7 +1,7 @@
 var tp=getrtmm$$(this.container.tipo).tp[this._fname];
 if (tp) {
-  if (tp.dv==='out')return getCovariant$meta$declaration();
-  if (tp.dv=== 'in')return getContravariant$meta$declaration();
-  return getInvariant$meta$declaration();
+  if (tp.dv==='out')return covariant$meta$declaration();
+  if (tp.dv=== 'in')return contravariant$meta$declaration();
+  return invariant$meta$declaration();
 }
 throw new Error("Invalid Type Parameter");
