27d417748f73d806b421f4559d32d718dc5fb0f8
Enrique Zamudio <alterbass@gmail.com>
2015-01-23 14:14:51 -0600

    simplify

--- language/runtime-js/unflatten.js
+++ language/runtime-js/unflatten.js
@@ -5,7 +5,7 @@ function unflatten(ff, $$$mptypes) {
   if (mm && mm.ps) {
     var ru=function ru(seq,$mptypes) {
       if (seq===undefined || seq.size === 0) { return ff(); }
-      var pmeta = ff.$crtmm$.ps;
+      var pmeta = mm.ps;
       var _lim=Math.max(pmeta.length,seq.size);
       var a = [];
       for (var i = 0; i < _lim; i++) {
@@ -16,7 +16,7 @@ function unflatten(ff, $$$mptypes) {
           a.push(seq.$_get(i));
         }
       }
-      if ($mptypes && ff.$crtmm$.tp)a.push($mptypes);
+      if ($mptypes && mm.tp)a.push($mptypes);
       return ff.apply(ru, a);
     }
     ru.$crtmm$={$t:mm.$t,ps:[{$t:{t:'T',l:[]}}]};
