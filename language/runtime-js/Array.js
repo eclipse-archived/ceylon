d53a03e93bc419edec111057738ecc45e2599ab1
Enrique Zamudio <alterbass@gmail.com>
2015-02-04 23:08:25 -0600

    improve deser #2869

--- language/runtime-js/Array.js
+++ language/runtime-js/Array.js
@@ -11,14 +11,12 @@ function $_Array(elems,$$targs$$) {
     return e.rt$(t);
 }
 $_Array.inst$$=function(cm){
-  return [];
+  return $_Array(undefined,cm.$$targs$$.Type$Class.a);
 }
 $_Array.deser$$=function(a,cm,b){
-  var targ=cm.$$targs$$.Type$Class.a.Element$Array;
   var tam=a.getValue(OpenValue$jsint(lmp$(ex$,'$'),$_Array.$$.prototype.$prop$getSize),{Instance$getValue:{t:Integer}});
   for (var i=0; i < tam; i++) {
-    b.push(a.getElement(i,{Instance$getElement:targ}));
-    if (is$(b[i],{t:Reference$serialization}))b[i]=b[i].leak();
+    var e=a.getElement(i,{Instance$getElement:targ});
+    b.push(is$(e,{t:Reference$serialization})?e.leak():e);
   }
-  b.rt$(targ);
 }
