a17d9fbb6600138aead9d3a5e8b74dd7f1fdf8ba
Enrique Zamudio <alterbass@gmail.com>
2015-02-04 23:41:59 -0600

    duh

--- language/runtime-js/Array.js
+++ language/runtime-js/Array.js
@@ -16,7 +16,7 @@ $_Array.inst$$=function(cm){
 $_Array.deser$$=function(a,cm,b){
   var tam=a.getValue(OpenValue$jsint(lmp$(ex$,'$'),$_Array.$$.prototype.$prop$getSize),{Instance$getValue:{t:Integer}});
   for (var i=0; i < tam; i++) {
-    var e=a.getElement(i,{Instance$getElement:targ});
+    var e=a.getElement(i,{Instance$getElement:b.$$targs$$.Element$Array});
     b.push(is$(e,{t:Reference$serialization})?e.leak():e);
   }
 }
