759deccfdbca48db0a17cfb6b6498f927253d184
Tako Schotanus <tako@ceylon-lang.org>
2015-02-06 16:35:19 +0100

    Moved a couple of enumeration utility functions from the compiler to `EnumUtil`

--- common/src/com/redhat/ceylon/common/tool/EnumUtil.java
+++ common/src/com/redhat/ceylon/common/tool/EnumUtil.java
@@ -1,7 +1,7 @@
 package com.redhat.ceylon.common.tool;
 
-import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.List;
 
 public abstract class EnumUtil {
@@ -22,19 +22,45 @@ public abstract class EnumUtil {
     }
     
     public static <ENUM extends Enum<ENUM>> Iterable<String> possibilities(Class<ENUM> enumClass) {
-        ENUM[] values;
-        try {
-            Method method = enumClass.getMethod("values");
-            method.setAccessible(true);
-            values = (ENUM[]) method.invoke(null);
-        } catch (ReflectiveOperationException e) {
-            // Should never happen
-            throw new RuntimeException(e);
-        }
-        List<String> result = new ArrayList<>(values.length);
+        EnumSet<ENUM> values = EnumSet.allOf(enumClass);
+        List<String> result = new ArrayList<>(values.size());
         for (ENUM value : values) {
             result.add(value.toString());
         }
         return result;
+    }
+    
+    public static <ENUM extends Enum<ENUM>> EnumSet<ENUM> enumsFromStrings(Class<ENUM> enumClass, List<String> elems) {
+        if (elems != null) {
+            EnumSet<ENUM> result = EnumSet.noneOf(enumClass);
+            for (String elem : elems) {
+                elem = elem.trim();
+                elem = elem.replace('-', '_');
+                for (ENUM e : EnumSet.allOf(enumClass)) {
+                    if (e.name().equalsIgnoreCase(elem)) {
+                        elem = e.name();
+                    }
+                }
+                result.add(EnumUtil.valueOf(enumClass, elem));
+            }
+            return result;
+        } else {
+            return null;
+        }
+    }
+    
+    public static <ENUM extends Enum<ENUM>> String enumsToString(Class<ENUM> enumClass, EnumSet<ENUM> elems) {
+        if (elems != null) {
+            StringBuilder buf = new StringBuilder();
+            for (ENUM e : EnumSet.allOf(enumClass)) {
+                if (buf.length() > 0) {
+                    buf.append(",");
+                }
+                buf.append(e.name());
+            }
+            return buf.toString();
+        } else {
+            return null;
+        }
     }
 }
