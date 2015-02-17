faff0f305b3b11a9d47817d2a5e7509ebc725a5e
Tom Bentley <tom.bentley@cantab.net>
2015-02-17 14:07:01 +0000

    Fix #2054 by using a visitor to determine whether we can trust the type attributed to the variable in the else clause of if statements and if expressions.

--- compiler-java/src/com/redhat/ceylon/compiler/java/codegen/ExpressionTransformer.java
+++ compiler-java/src/com/redhat/ceylon/compiler/java/codegen/ExpressionTransformer.java
@@ -5999,7 +5999,7 @@ public class ExpressionTransformer extends AbstractTransformer {
         Tree.Expression elsePart = op.getElseClause() != null ? op.getElseClause().getExpression() : null;
         Tree.Variable elseVar = op.getElseClause() != null ? op.getElseClause().getVariable() : null;
         java.util.List<Tree.Condition> conditions = op.getIfClause().getConditionList().getConditions();
-        List<JCStatement> statements = statementGen().transformIf(conditions, thenPart, elseVar, elsePart, tmpVar, op);
+        List<JCStatement> statements = statementGen().transformIf(conditions, thenPart, elseVar, elsePart, tmpVar, op, statementGen().ifElseVisitor.trustElseType(op));
         at(op);
         JCExpression vartype = makeJavaType(typeFact().denotableType(op.getTypeModel()), CodegenUtil.getBoxingStrategy(op) == BoxingStrategy.UNBOXED ? 0 : JT_NO_PRIMITIVES);
         return make().LetExpr(make().VarDef(make().Modifiers(0), names().fromString(tmpVar), vartype , null), statements, makeUnquotedIdent(tmpVar));
