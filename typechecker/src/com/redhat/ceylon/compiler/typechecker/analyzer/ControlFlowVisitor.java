58ec686c607211aebe2cdc3c67e26f09a6d5e043
gavinking <gavin@ceylon-lang.org>
2015-03-11 18:57:50 +0100

    test and fix for #4358

--- typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/ControlFlowVisitor.java
+++ typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/ControlFlowVisitor.java
@@ -233,6 +233,16 @@ public class ControlFlowVisitor extends Visitor {
     }
     
     @Override
+    public void visit(Tree.Constructor that) {
+        checkReachable(that);
+        boolean c = beginReturnScope(true);
+        boolean d = beginDefiniteReturnScope();
+        super.visit(that);
+        endReturnScope(c);
+        endDefiniteReturnScope(d);
+    }
+    
+    @Override
     public void visit(Tree.ClassDefinition that) {
         boolean c = beginReturnScope(true);
         boolean d = beginDefiniteReturnScope();
