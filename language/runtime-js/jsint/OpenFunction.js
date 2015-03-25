c2e32f547d79243956f4173dae2b026c69426fe4
Enrique Zamudio <alterbass@gmail.com>
2015-03-24 18:27:09 -0600

    fix some bugs with OpenFunction #5237

--- language/runtime-js/jsint/OpenFunction.js
+++ language/runtime-js/jsint/OpenFunction.js
@@ -6,13 +6,13 @@ function OpenFunction$jsint(pkg,meta,that){
     var _mm=getrtmm$$(meta);
     if (_mm === undefined) {
       //it's a metamodel
-      that.meta=meta;
+      that.meta_=meta;
       that.tipo=_findTypeFromModel(pkg,meta);
       _mm=getrtmm$$(that.tipo);
     } else {
       //it's a type
       that.tipo = meta;
-      that.meta = get_model(_mm);
+      that.meta_ = get_model(_mm);
     }
     that.name_=(that.meta&&that.meta.nm)||'?';
     that.toplevel_=_mm===undefined||_mm.$cont===undefined;
