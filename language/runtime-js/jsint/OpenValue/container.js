0c963174c01fd697c436740b6d986b7140a37d96
Enrique Zamudio <alterbass@gmail.com>
2015-03-24 20:30:28 -0600

    cache result

--- language/runtime-js/jsint/OpenValue/container.js
+++ language/runtime-js/jsint/OpenValue/container.js
@@ -1,3 +1,4 @@
 if (this.$parent)return this.$parent;
 if (this.toplevel || this.tipo.$crtmm$.$cont===0)return this.containingPackage;
-return typeLiteral$meta({Type$typeLiteral:this.tipo.$crtmm$.$cont});
+this.$parent=typeLiteral$meta({Type$typeLiteral:this.tipo.$crtmm$.$cont});
+return this.$parent;
