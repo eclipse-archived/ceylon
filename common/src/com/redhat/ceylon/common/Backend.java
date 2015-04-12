b29a4fa1ae00a0699c6d08fc3770a024451216fd
Tako Schotanus <tako@ceylon-lang.org>
2015-04-12 19:40:29 +0200

    Made the `Backend` items more descriptive and added their abbreviation as an attribute. Added the `doc` "backend" to `Backend`. (in preparation for my #3606 proposal)

--- common/src/com/redhat/ceylon/common/Backend.java
+++ common/src/com/redhat/ceylon/common/Backend.java
@@ -1,3 +1,13 @@
 package com.redhat.ceylon.common;
 
-public enum Backend { JVM, JS }
+public enum Backend {
+    Java("jvm"),
+    JavaScript("js"),
+    Documentation("doc");
+    
+    public final String compilerAnnotation;
+    
+    Backend(String compilerAnnotation) {
+        this.compilerAnnotation = compilerAnnotation;
+    }
+}
--- common/src/com/redhat/ceylon/common/Platform.java
+++ common/src/com/redhat/ceylon/common/Platform.java
@@ -1,7 +1,7 @@
 package com.redhat.ceylon.common;
 
 public enum Platform {
-    JVM(Backend.JVM), NODE(Backend.JS), BROWSER(Backend.JS);
+    JVM(Backend.Java), NODE(Backend.JavaScript), BROWSER(Backend.JavaScript);
     
     public final Backend backend;
     
