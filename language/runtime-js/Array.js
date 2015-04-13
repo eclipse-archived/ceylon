77bf04307f96ff93bb27f4574ac0672a80be94ed
Enrique Zamudio <alterbass@gmail.com>
2015-04-13 12:52:59 -0500

    changes related to #2955

--- language/runtime-js/Array.js
+++ language/runtime-js/Array.js
@@ -1,23 +1,6 @@
-function $_Array(elems,$$targs$$) {
-    $init$$_Array();
-    var e=[];
-    if (!(elems === null || elems === undefined)) {
-        var item;for(var iter=elems.iterator();(item=iter.next())!==finished();) {
-            e.push(item);
-        }
-    }
-    var t=$$targs$$.Element$Array;
-    List({Element$List:t}, e);
-    return e.rt$(t);
+function $_Array$$c(a$,t$){
+  var t=t$.Element$Array;
+  List({Element$List:t}, a$);
+  return a$.rt$(t);
 }
-$_Array.inst$$=function(cm){
-  return $_Array(undefined,cm.$$targs$$.Type$Class.a);
-}
-$_Array.deser$$=function(a,cm,b){
-  var tam=a.getValue(OpenValue$jsint(lmp$(ex$,'$'),$_Array.$$.prototype.$prop$getSize),{Instance$getValue:{t:Integer}});
-  var elemtarg={Instance$getElement:b.$$targs$$.Element$Array};
-  for (var i=0; i < tam; i++) {
-    var e=a.getElement(i,elemtarg);
-    b.push(is$(e,{t:Reference$serialization})?e.leak():e);
-  }
-}
+function $_Array(e$,t$){return $_Array_$_Array(e$,t$);}
---
+++ language/runtime-js/Array/Array.js
@@ -0,0 +1,21 @@
+function $_Array_$_Array(elems,$$targs$$) {
+    $init$$_Array();
+    var e=[];
+    if (!(elems === null || elems === undefined)) {
+        var item;for(var iter=elems.iterator();(item=iter.next())!==finished();) {
+            e.push(item);
+        }
+    }
+    return $_Array$$c(e,$$targs$$);
+}
+$_Array.inst$$=function(cm){
+  return $_Array(undefined,cm.$$targs$$.Type$Class.a);
+}
+$_Array.deser$$=function(a,cm,b){
+  var tam=a.getValue(OpenValue$jsint(lmp$(ex$,'$'),$_Array.$$.prototype.$prop$getSize),{Instance$getValue:{t:Integer}});
+  var elemtarg={Instance$getElement:b.$$targs$$.Element$Array};
+  for (var i=0; i < tam; i++) {
+    var e=a.getElement(i,elemtarg);
+    b.push(is$(e,{t:Reference$serialization})?e.leak():e);
+  }
+}
