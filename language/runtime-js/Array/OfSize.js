c97749d440ba28d9c75753b3725948480fec0d77
Enrique Zamudio <alterbass@gmail.com>
2015-04-15 10:30:04 -0500

    fix #5535

--- language/runtime-js/Array/OfSize.js
+++ language/runtime-js/Array/OfSize.js
@@ -1,4 +1,6 @@
 function $_Array_OfSize(s,e,t$) {
+  asrt$(s>=0 && s<=runtime().maxArraySize,
+        "Invalid array size","0:0","Array.ceylon");
   $init$$_Array();
   var a=new Array(s);
   for (var i=0;i<s;i++) {
