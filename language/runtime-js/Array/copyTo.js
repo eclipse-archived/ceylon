3deeef5e909ffb4c0bb3cc162dec7f42a7aef8bb
Enrique Zamudio <alterbass@gmail.com>
2015-04-17 11:38:01 -0500

    fix copyTo

--- language/runtime-js/Array/copyTo.js
+++ language/runtime-js/Array/copyTo.js
@@ -1,8 +1,8 @@
 function(other,srcpos,dstpos,length){
     if(srcpos===undefined)srcpos=0;
     if(dstpos===undefined)dstpos=0;
-    if (length===0)return;
     if(length===undefined)length=Math.min(this.size-srcpos,other.size-dstpos);
+    if (length<=0)return;
     var endpos=srcpos+length-1;
     if (srcpos<0||srcpos>=this.size||length<1||dstpos+length>other.size||endpos>=this.size)throw Exception("Array index out of bounds");
     if (other===this && dstpos>srcpos) {
