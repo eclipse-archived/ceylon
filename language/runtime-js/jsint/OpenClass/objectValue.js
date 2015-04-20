21f0903c603eab1c6a3631be637def74cc0b2b46
Enrique Zamudio <alterbass@gmail.com>
2015-04-20 11:27:52 -0500

    objectValue of local anon class is null

--- language/runtime-js/jsint/OpenClass/objectValue.js
+++ language/runtime-js/jsint/OpenClass/objectValue.js
@@ -3,4 +3,4 @@ if (this.container && this.container.getMember) {
   var m=this.container.getMember(this.name,{getMember$Kind:{t:ValueDeclaration$meta$declaration}});
   if (m)return m;
 }
-return OpenValue$jsint(this.containingPackage,this.tipo);
+return null
