8ac43764c9a59e5b617be2cbbf889fb297314cd7
Tom Bentley <tom.bentley@cantab.net>
2015-04-20 12:49:53 +0100

    Fix #2134 (which addresses #4360), though it's not pretty. We now wait until we've transformed the whole class before transforming constructors, so we know about the executable statements that occur after each constructor declaration.

--- compiler-java/src/com/redhat/ceylon/compiler/java/codegen/InitializerBuilder.java
+++ compiler-java/src/com/redhat/ceylon/compiler/java/codegen/InitializerBuilder.java
@@ -1,19 +1,16 @@
 package com.redhat.ceylon.compiler.java.codegen;
 
-import static com.sun.tools.javac.code.Flags.FINAL;
-import static com.sun.tools.javac.code.Flags.ABSTRACT;
 import static com.sun.tools.javac.code.Flags.PRIVATE;
 import static com.sun.tools.javac.code.Flags.PROTECTED;
 import static com.sun.tools.javac.code.Flags.PUBLIC;
 
-import javax.management.RuntimeErrorException;
-
+import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.sun.tools.javac.code.Flags;
-import com.sun.tools.javac.tree.TreeCopier;
 import com.sun.tools.javac.tree.JCTree.JCAnnotation;
 import com.sun.tools.javac.tree.JCTree.JCMethodDecl;
 import com.sun.tools.javac.tree.JCTree.JCStatement;
 import com.sun.tools.javac.tree.JCTree.JCThrow;
+import com.sun.tools.javac.tree.TreeCopier;
 import com.sun.tools.javac.util.List;
 import com.sun.tools.javac.util.ListBuffer;
 
@@ -23,18 +20,26 @@ public class InitializerBuilder implements ParameterizedBuilder<InitializerBuild
     private long modifiers = 0;
     private JCStatement superCall;
     private final ListBuffer<ParameterDefinitionBuilder> params = ListBuffer.lb();
-    private final ListBuffer<JCStatement> init = ListBuffer.lb();
+    /** 
+     * For classes with parameter lists this is a {@code List<JCStatement>}.
+     * For classes with constructors it's a 
+     * {@code List<JCStatement|Tree.Constructor>} and this we know which 
+     * statements need to be prepended to each transformed constructor body
+     * and which need to be appended. 
+     */
+    private final java.util.List<Object/* JCStatement|Tree.Constructor*/> init = new java.util.ArrayList<Object>();
     private final ListBuffer<JCAnnotation> userAnnos = ListBuffer.lb();
     
     public InitializerBuilder(AbstractTransformer gen) {
         this.gen = gen;
     }
     
+    /** Only called for classes with parameter lists */
     JCMethodDecl build() {
         if (superCall != null/* && !isAlias*/) {
-            init.prepend(superCall);
+            init.add(0, superCall);
         }
-        List<JCStatement> body = init.toList();
+        List<JCStatement> body = List.from(init.toArray(new JCStatement[init.size()]));
         int index = 0;
         for (JCStatement stmt : body) {
             if (stmt instanceof JCThrow) {
@@ -76,14 +81,22 @@ public class InitializerBuilder implements ParameterizedBuilder<InitializerBuild
     
     public InitializerBuilder init(JCStatement statement) {
         if (statement != null) {
-            this.init.append(statement);
+            this.init.add(statement);
         }
         return this;
     }
     
     public InitializerBuilder init(List<JCStatement> init) {
         if (init != null) {
-            this.init.appendList(init);
+            this.init.addAll(init);
+        }
+        return this;
+    }
+    
+    public InitializerBuilder constructor(
+            com.redhat.ceylon.compiler.typechecker.tree.Tree.Constructor ctor) {
+        if (ctor != null) {
+            this.init.add(ctor);
         }
         return this;
     }
@@ -97,13 +110,44 @@ public class InitializerBuilder implements ParameterizedBuilder<InitializerBuild
     public boolean isEmptyInit() {
         return init.isEmpty();
     }
-
-    public List<JCStatement> getBodyCopy() {
+    
+    List<JCStatement> statementsBefore(Tree.Constructor ctor) {
+        ListBuffer<JCStatement> result = ListBuffer.lb();
+        for (Object o : this.init) {
+            if (o instanceof JCStatement) {
+                result.add((JCStatement)o);
+            } else if (ctor.equals(o)){
+                break;
+            }
+        }
+        return result.toList();
+    }
+    
+    List<JCStatement> statementsAfter(Tree.Constructor ctor) {
+        ListBuffer<JCStatement> result = ListBuffer.lb();
+        boolean found = false;
+        for (Object o : this.init) {
+            if (found && o instanceof JCStatement) {
+                result.add((JCStatement)o);
+            } else if (ctor.equals(o)){
+                found = true;
+            }
+        }
+        return result.toList();
+    }
+    
+    public List<JCStatement> getPreBodyCopy(Tree.Constructor ctor) {
         TreeCopier<Object> copier = new TreeCopier<Object>(gen.make());
-        List<JCStatement> body = this.init.toList();
+        List<JCStatement> body = statementsBefore(ctor);
         if (superCall != null) {
             body = body.prepend(superCall);
         }
+        return copier.copy(body);
+    }
+    
+    public List<JCStatement> getPostBodyCopy(Tree.Constructor ctor) {
+        TreeCopier<Object> copier = new TreeCopier<Object>(gen.make());
+        List<JCStatement> body = statementsAfter(ctor);
         return copier.copy(body);
     }
     
