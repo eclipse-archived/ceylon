02e8c2e76df7d7e7dbb482cf024b3443dfafbfa5
Tako Schotanus <tako@ceylon-lang.org>
2015-05-06 18:54:59 +0200

    Improved checking for missing native implementations (see #3606 and #4392)

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/MissingNativeVisitor.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/MissingNativeVisitor.java
@@ -1,12 +1,11 @@
 package com.redhat.ceylon.compiler.js;
 
-import java.util.HashMap;
+import static com.redhat.ceylon.compiler.typechecker.model.Util.getNativeDeclaration;
 
 import com.redhat.ceylon.common.Backend;
 import com.redhat.ceylon.compiler.typechecker.model.Class;
 import com.redhat.ceylon.compiler.typechecker.model.Declaration;
 import com.redhat.ceylon.compiler.typechecker.model.Method;
-import com.redhat.ceylon.compiler.typechecker.model.Overloadable;
 import com.redhat.ceylon.compiler.typechecker.model.Package;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
 import com.redhat.ceylon.compiler.typechecker.model.Scope;
@@ -26,11 +25,8 @@ import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 public class MissingNativeVisitor extends Visitor {
     private final Backend forBackend;
 
-    private final HashMap<Declaration, Boolean> checked;
-    
     public MissingNativeVisitor(Backend forBackend) {
         this.forBackend = forBackend;
-        checked = new HashMap<Declaration, Boolean>();
     }
 
     //
@@ -52,95 +48,90 @@ public class MissingNativeVisitor extends Visitor {
     // Declaration
     
     public void visit(Tree.ClassOrInterface decl) {
-        checkNativeExistence(decl);
-        super.visit(decl);
+        if (checkNativeExistence(decl)) {
+            super.visit(decl);
+        }
     }
 
     public void visit(Tree.ObjectDefinition decl) {
-        checkNativeExistence(decl);
-        super.visit(decl);
+        if (checkNativeExistence(decl)) {
+            super.visit(decl);
+        }
     }
 
     public void visit(Tree.AttributeDeclaration decl){
-        checkNativeExistence(decl);
-        super.visit(decl);
+        if (checkNativeExistence(decl)) {
+            super.visit(decl);
+        }
     }
 
     public void visit(Tree.AttributeGetterDefinition decl){
-        checkNativeExistence(decl);
-        super.visit(decl);
+        if (checkNativeExistence(decl)) {
+            super.visit(decl);
+        }
     }
     
     public void visit(Tree.AttributeSetterDefinition decl) {
         Setter model = (Setter)decl.getDeclarationModel();
-        checkNativeExistence(decl, model.getGetter());
-        super.visit(decl);
+        if (checkNativeExistence(decl, model.getGetter())) {
+            super.visit(decl);
+        }
     }
 
     public void visit(Tree.AnyMethod decl) {
-        checkNativeExistence(decl);
-        super.visit(decl);
+        if (checkNativeExistence(decl)) {
+            super.visit(decl);
+        }
     }
 
-    private void checkNativeExistence(Tree.Declaration decl) {
+    private boolean checkNativeExistence(Tree.Declaration decl) {
         Declaration model = decl.getDeclarationModel();
-        checkNativeExistence(decl, model);
+        return checkNativeExistence(decl, model);
     }
     
-    private void checkNativeExistence(Node node, Declaration model) {
+    private boolean checkNativeExistence(Node node, Declaration model) {
         if(model == null)
-            return;
+            return true;
         if(!model.isToplevel() || !model.isNative())
-            return;
+            return true;
         Package pkg = getPackage(model);
         if(pkg == null)
-            return;
+            return true;
         
         if ((node instanceof Tree.ClassOrInterface || node instanceof Tree.AnyMethod || node instanceof Tree.AnyAttribute)
                 && !model.getNative().isEmpty()
                 && !forBackend.nativeAnnotation.equals(model.getNative())) {
             // We don't care about declarations for other backends
-            return;
+            return false;
         }
         
         boolean ok = true;
-        if (checked.containsKey(model)) {
-            ok = checked.get(model);
-        } else {
-            try {
-                if (model instanceof Method || model instanceof Class || model instanceof Value) {
-                    Declaration m = pkg.getDirectMember(model.getName(), null, false);
-                    if (m != null) {
-                        if (!m.isNative()) {
-                            // An error will already have been added by the typechecker
-                            return;
-                        }
-                        // Native declarations are a bit weird, if there are multiple they
-                        // will all have the same list of overloads containing each of them.
-                        // We here check to see if any of them are for this backend
-                        if (forBackend.nativeAnnotation.equals(m.getNative())) {
-                            return;
-                        } else if (m instanceof Overloadable && ((Overloadable)m).getOverloads() != null) {
-                            for (Declaration o : ((Overloadable)m).getOverloads()) {
-                                if (forBackend.nativeAnnotation.equals(o.getNative())) {
-                                    return;
-                                }
-                            }
-                        }
-                        ok = false;
-                    }
+        if (model instanceof Method || model instanceof Class || model instanceof Value) {
+            Declaration m = pkg.getDirectMember(model.getName(), null, false);
+            if (m != null) {
+                if (!m.isNative()) {
+                    // An error will already have been added by the typechecker
+                    return true;
                 }
-                
-                // TODO If we got here it might be that we're dealing with
-                // a true native implementation in JavaScript. If possible
-                // we should detect if it actually exists
-                //ok = ok && checkNativeImplementationExists(model);
-            } finally {
-                checked.put(model, ok);
+                // Native declarations are a bit weird, if there are multiple they
+                // will all have the same list of overloads containing each of them.
+                // We here check to see if any of them are for this backend
+                m = getNativeDeclaration(m, forBackend);
+                if (m != null) {
+                    return true;
+                }
+                ok = false;
             }
         }
+        
+        // TODO If we got here it might be that we're dealing with
+        // a true native implementation in JavaScript. If possible
+        // we should detect if it actually exists
+        //ok = ok && checkNativeImplementationExists(model);
         if(!ok)
-            node.addError("native implementation not found");
+            node.addError("native implementation not found for: '" + model.getName() + "'");
+        
+        return true;
     }
 
     private static Package getPackage(Declaration decl){
