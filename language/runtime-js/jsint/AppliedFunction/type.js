e526e8de3f7d40ec28c0506ed52b16971e08db71
Enrique Zamudio <alterbass@gmail.com>
2015-05-13 02:11:59 -0500

    work on #5552

--- language/runtime-js/jsint/AppliedFunction/type.js
+++ language/runtime-js/jsint/AppliedFunction/type.js
@@ -1,2 +1,5 @@
 var mm=getrtmm$$(this.tipo);
-return typeLiteral$meta({Type$typeLiteral:restype$(this.$$targs$$.Container$Method,this.$$targs$$.Type$Method||mm.$t)});
+if (this.$$targs$$.Container$Method) {
+  return typeLiteral$meta({Type$typeLiteral:restype$(this.$$targs$$.Container$Method,this.$$targs$$.Type$Method||mm.$t)});
+}
+return typeLiteral$meta({Type$typeLiteral:this.$$targs$$.Type$Function||mm.$t});
