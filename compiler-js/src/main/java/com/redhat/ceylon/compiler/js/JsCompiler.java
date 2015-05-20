ccf75f9bd339256cca436d5973c5237b1ac84a8f
Tako Schotanus <tako@ceylon-lang.org>
2015-05-20 03:56:50 +0200

    Be more precise in our error messages for `native` annotations on module descriptors (see #4052)

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/JsCompiler.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/JsCompiler.java
@@ -424,8 +424,15 @@ public class JsCompiler {
     private void checkInvalidNativePUs(List<PhasedUnit> phasedUnits) {
         for (PhasedUnit pu : phasedUnits) {
             ModuleDescriptor md = pu.findModuleDescriptor();
-            if (md != null && !isForBackend(md.getAnnotationList(), Backend.JavaScript, md.getUnit())) {
-                md.addError("Module not meant for this backend: " + formatPath(md.getImportPath().getIdentifiers()), Backend.JavaScript);
+            if (md != null) {
+                String be = getNativeBackend(md.getAnnotationList(), md.getUnit());
+                if (be != null) {
+                    if (be.isEmpty()) {
+                        md.addError("Missing backend argument for native annotation on module: " + formatPath(md.getImportPath().getIdentifiers()), Backend.Java);
+                    } else if (!isForBackend(be, Backend.Java)) {
+                        md.addError("Module not meant for this backend: " + formatPath(md.getImportPath().getIdentifiers()), Backend.Java);
+                    }
+                }
             }
         }
     }
