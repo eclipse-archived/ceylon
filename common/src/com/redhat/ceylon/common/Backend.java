874127807068024017e4094c2b4a0d235d2605be
Tako Schotanus <tako@ceylon-lang.org>
2015-05-21 17:47:20 +0200

    Revert "Annotation for Java backend renamed from "jvm" to "java""
    This reverts commit d965d39bd33cbb8efde7c815537bdf02d4c287c8.

--- common/src/com/redhat/ceylon/common/Backend.java
+++ common/src/com/redhat/ceylon/common/Backend.java
@@ -2,7 +2,7 @@ package com.redhat.ceylon.common;
 
 public enum Backend {
     None("", BackendSupport.HEADER),
-    Java("java", BackendSupport.JAVA),
+    Java("jvm", BackendSupport.JAVA),
     JavaScript("js", BackendSupport.JAVASCRIPT);
     
     public final String nativeAnnotation;
