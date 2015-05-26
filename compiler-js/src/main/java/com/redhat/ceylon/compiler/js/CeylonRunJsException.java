04a60ded1cd78b2016c0d9534f9d8966bed070a6
gavinking <gavin@ceylon-lang.org>
2015-05-26 23:32:44 +0200

    unused import

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/CeylonRunJsException.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/CeylonRunJsException.java
@@ -1,7 +1,6 @@
 package com.redhat.ceylon.compiler.js;
 
 import com.redhat.ceylon.common.tool.ToolError;
-import com.redhat.ceylon.common.tool.WordWrap;
 
 
 public class CeylonRunJsException extends ToolError {
