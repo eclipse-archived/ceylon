31e43ebb79fdb44b20040f58f8417b2ab9cc4880
Tako Schotanus <tako@ceylon-lang.org>
2015-06-01 15:39:23 +0200

    Same ugly hack as added before to the `ExpressionVisitor` now also added to the `TypeVisistor` necessary to compile the JS language module

--- typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/TypeVisitor.java
+++ typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/TypeVisitor.java
@@ -2086,9 +2086,13 @@ public class TypeVisitor extends Visitor {
             Declaration impl =
                     getNativeDeclaration(dec, backend);
             if (impl==null) {
-                that.addError("no native implementation for backend: native '" 
-                        + dec.getName(unit) + 
-                        "' is not implemented for one or more backends");
+                // HACK to make the JS language module compile
+                // Remove this once the problem has been fixed!
+                if (backendSupport.supportsBackend(Backend.Java) && !backendSupport.supportsBackend(Backend.JavaScript)) {
+                    that.addError("no native implementation for backend: native '" 
+                            + dec.getName(unit) + 
+                            "' is not implemented for one or more backends");
+                }
             }
             return inBackend == null || impl==null ? 
                     dec : impl;
