04dad96205060e2e77fb9a9be148c799e00c44df
gavinking <gavin@ceylon-lang.org>
2015-06-03 20:57:58 +0200

    clean imports

--- language/runtime/com/redhat/ceylon/compiler/java/Util.java
+++ language/runtime/com/redhat/ceylon/compiler/java/Util.java
@@ -3,8 +3,6 @@ package com.redhat.ceylon.compiler.java;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Arrays;
 
 import ceylon.language.ArraySequence;
@@ -32,7 +30,6 @@ import com.redhat.ceylon.compiler.java.metadata.TypeInfo;
 import com.redhat.ceylon.compiler.java.runtime.metamodel.Metamodel;
 import com.redhat.ceylon.compiler.java.runtime.model.TypeDescriptor;
 import com.redhat.ceylon.model.cmr.ArtifactResult;
-import com.redhat.ceylon.model.loader.NamingBase;
 
 /**
  * Helper class for generated Ceylon code that needs to call implementation logic.
