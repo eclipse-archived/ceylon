36a9981f0ebef9b584d69808d72b61da008970a1
Tako Schotanus <tako@ceylon-lang.org>
2015-06-09 17:31:51 +0200

    Don't assume that `backend` is an empty string for headers

--- typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/DeclarationVisitor.java
+++ typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/DeclarationVisitor.java
@@ -204,7 +204,7 @@ public abstract class DeclarationVisitor extends Visitor implements NaturalVisit
         Tree.Annotation a = 
                 getAnnotation(al, "native", unit);
         if (a != null) {
-            String backend = getAnnotationArgument(a, "");
+            String backend = getAnnotationArgument(a, Backend.None.nativeAnnotation);
             String name = model.getName();
             boolean canBeNative = 
                     that instanceof Tree.AnyMethod ||
@@ -212,7 +212,7 @@ public abstract class DeclarationVisitor extends Visitor implements NaturalVisit
                     that instanceof Tree.AnyAttribute;
             if (canBeNative && 
                     (model.isToplevel() || model.isMember())) {
-                if (!backend.isEmpty() && 
+                if (!backend.equals(Backend.None.nativeAnnotation) && 
                         !Backend.validAnnotation(backend)) {
                     a.addError("illegal native backend name: '\"" + 
                             backend + 
@@ -222,7 +222,7 @@ public abstract class DeclarationVisitor extends Visitor implements NaturalVisit
                         unit.getPackage()
                             .getModule()
                             .getNative();
-                if (!backend.isEmpty() && 
+                if (!backend.equals(Backend.None.nativeAnnotation) && 
                         moduleBackend != null && 
                         !backend.equals(moduleBackend)) {
                     that.addError("native backend name on declaration conflicts with module descriptor: '\"" + 
@@ -277,7 +277,7 @@ public abstract class DeclarationVisitor extends Visitor implements NaturalVisit
                                 member.getOverloads();
                         if (hasOverload(backend, model, 
                                 overloads)) {
-                            if (backend.isEmpty()) {
+                            if (backend.equals(Backend.None.nativeAnnotation)) {
                                 that.addError("duplicate native header: '" + 
                                         name + "'");
                             }
@@ -304,7 +304,7 @@ public abstract class DeclarationVisitor extends Visitor implements NaturalVisit
                         }
                     }
                     else {
-                        if (backend.isEmpty()) {
+                        if (backend.equals(Backend.None.nativeAnnotation)) {
                             that.addError("native header for non-native declaration: '" + 
                                     name + "'");
                         }
@@ -332,7 +332,7 @@ public abstract class DeclarationVisitor extends Visitor implements NaturalVisit
 //              }
             }
             else if (!(model instanceof Setter) && 
-                    !backend.isEmpty()) {
+                    !backend.equals(Backend.None.nativeAnnotation)) {
                 if (!canBeNative) {
                     that.addError("native declaration is not a class, method or attribute: '" + 
                             name + "'");
