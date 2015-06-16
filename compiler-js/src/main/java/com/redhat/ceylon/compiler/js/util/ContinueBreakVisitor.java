371c5e154c4540f7c8a58f6e6041ba5910091b7e
Enrique Zamudio <alterbass@gmail.com>
2015-06-16 02:39:02 -0500

    refactor for #3020

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/util/ContinueBreakVisitor.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/util/ContinueBreakVisitor.java
@@ -1,6 +1,8 @@
 package com.redhat.ceylon.compiler.js.util;
 
-import com.redhat.ceylon.compiler.typechecker.tree.Node;
+import java.util.IdentityHashMap;
+
+import com.redhat.ceylon.compiler.js.BlockWithCaptureVisitor;
 import com.redhat.ceylon.compiler.typechecker.tree.Tree;
 import com.redhat.ceylon.compiler.typechecker.tree.Visitor;
 import com.redhat.ceylon.model.typechecker.model.Scope;
@@ -15,36 +17,63 @@ public class ContinueBreakVisitor extends Visitor {
     private String bname;
     private String cname;
     private String rname;
+    private int level;
+    private boolean ignore;
+    private final IdentityHashMap<Tree.Directive, Boolean> dirs = new IdentityHashMap<>();
 
-    public ContinueBreakVisitor(Node n, JsIdentifierNames names) {
+    public ContinueBreakVisitor(Tree.Block n, JsIdentifierNames names) {
         scope = n.getScope();
         this.names = names;
         n.visit(this);
     }
 
+    public void visit(Tree.Block that) {
+        level++;
+        if (level>1) {
+            ignore=new BlockWithCaptureVisitor(that).hasCapture();
+        }
+        super.visit(that);
+        ignore=false;
+        level--;
+    }
     public void visit(Tree.Break n) {
-        if (n.getScope() == scope) {
+        if (ignore) {
+            return;
+        }
+        if (level < 3) {
             if (bname == null) {
                 bname = names.createTempVariable();
             }
             breaks = true;
+            dirs.put(n, true);
         }
         super.visit(n);
     }
     public void visit(Tree.Continue n) {
-        if (n.getScope() == scope) {
+        if (ignore) {
+            return;
+        }
+        if (level < 3) {
             if (cname == null) {
                 cname = names.createTempVariable();
             }
             continues = true;
+            dirs.put(n, true);
         }
         super.visit(n);
     }
     public void visit(Tree.Return n) {
-        if (n.getScope() == scope) {
+        if (ignore) {
+            return;
+        }
+        if (level < 3) {
             returns = true;
         }
         super.visit(n);
+    }
+
+    public boolean belongs(Tree.Directive dir) {
+        return dirs.containsKey(dir);
     }
 
     public Scope getScope() {
