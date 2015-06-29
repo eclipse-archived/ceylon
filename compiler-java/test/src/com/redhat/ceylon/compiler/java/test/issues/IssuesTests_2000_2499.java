4934a1d55e746bb9b5e269509caa032ca9e6750a
Stephane Epardaud <stef@epardaud.fr>
2015-06-29 15:57:21 +0200

    Test for #2214: error with local mixin types

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTests_2000_2499.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTests_2000_2499.java
@@ -365,4 +365,9 @@ public class IssuesTests_2000_2499 extends CompilerTests {
     public void testBug2198() {
         compareWithJavaSource("bug21xx/Bug2198");
     }
+
+    @Test
+    public void testBug2214() {
+        compareWithJavaSource("bug22xx/Bug2214");
+    }
 }
