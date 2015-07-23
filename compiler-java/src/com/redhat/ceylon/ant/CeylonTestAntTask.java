f82cfbaf06a83a6102323cd2f0db9a10cda41273
John Vasileff <john@vasileff.com>
2015-07-23 13:37:41 -0400

    add ant support for ceylon-test --test

--- compiler-java/src/com/redhat/ceylon/ant/CeylonTestAntTask.java
+++ compiler-java/src/com/redhat/ceylon/ant/CeylonTestAntTask.java
@@ -25,21 +25,36 @@
  */
 package com.redhat.ceylon.ant;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.types.Commandline;
 
-
 public class CeylonTestAntTask extends RepoUsingCeylonAntTask {
 
     static final String FAIL_MSG = "Test failed; see the error output for details.";
-    
+
+    public static class Test {
+        private String test;
+
+        public String getTest() {
+            return test;
+        }
+
+        public void setTest(String test) {
+            this.test = test;
+        }
+    }
+
     private final ModuleSet moduleSet = new ModuleSet();
     private String compileFlags;
     private String version;
     private Boolean tap = false;
     private Boolean report = false;
-    
+    private List<Test> tests = new ArrayList<Test>(0);
+
     public CeylonTestAntTask() {
         super("test");
     }
@@ -96,7 +111,15 @@ public class CeylonTestAntTask extends RepoUsingCeylonAntTask {
     public void setReport(Boolean report) {
         this.report = report;
     }
-    
+
+    /**
+     * Adds a test to run.
+     * @param test
+     */
+    public void addTest(Test test) {
+        this.tests.add(test);
+    }
+
     /**
      * Check that all required attributes have been set and nothing silly has
      * been entered.
@@ -127,7 +150,10 @@ public class CeylonTestAntTask extends RepoUsingCeylonAntTask {
         if(report) {
             appendOption(cmd, "--report");
         }
-        
+        for (Test test : tests) {
+            appendOptionArgument(cmd, "--test", test.getTest());
+        }
+
         for (Module module : moduleSet.getModules()) {
             log("Adding module: "+module, Project.MSG_VERBOSE);
             cmd.createArgument().setValue(module.toSpec());
