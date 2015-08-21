814371d2ab9d3aa484899bf92c8875d2f1c5b019
Enrique Zamudio <alterbass@gmail.com>
2015-08-21 11:44:37 -0500

    recovered impls

--- language/runtime-js/serialization/PartialImpl/instantiate.js
+++ language/runtime-js/serialization/PartialImpl/instantiate.js
@@ -1,3 +1,21 @@
-function instantiate() {
-  console.log("PartialImpl.instantiate THIS");
+function instantiate(){
+  var classModel=this.clazz;
+  if (!classModel) {
+    throw DeserializationException("no class specified for instance with id " + this.id);
+  }
+  var outer,outerClass;
+  if (is$(classModel,{t:AppliedClass$jsint})) {
+    outer=null;
+    outerClass=null;
+  } else if (is$(classModel,{t:AppliedMemberClass$jsint})) {
+    outer=this.container;
+    if (is$(outer,{t:Partial$serialization}))outer=outer.instance_;
+    outerClass=type$meta(outer,{Type$type:classModel.$$targs$$.Container$AppliedMemberClass});
+  } else {
+    throw AssertionError("unexpected class model " + (classModel&&classModel.string||"NULL"));
+  }
+  this.instance_=classModel.tipo.inst$$(classModel);
+  if (outer) {
+    this.instance_.outer$=outer;
+  }
 }
