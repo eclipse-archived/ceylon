4acd4d24c7e46762042892ba0bf10e6ebbee8b48
Enrique Zamudio <alterbass@gmail.com>
2015-08-21 11:42:55 -0500

    recovered impls

--- language/runtime-js/impl/reach/references.js
+++ language/runtime-js/impl/reach/references.js
@@ -1,15 +1,14 @@
 function references(o) {
   var isarr=is$(o,{t:$_Array});
-  var refs=o.getT$all()[getT$name()].ser$refs$();
-  var i=0,ii=0;
+  var idx1=-1,idx2=0;
+  var refs = o.getT$all()[o.getT$name()].ser$refs$(o);
   return for$iter(function(){
-    if (i<refs.length) {
-      var r=refs[i];
-      i++;
-      return r;
-    } else if (isarr && ii<o.size) {
-      var r=ElementImpl$impl(ii);
-      ii++;
+    idx1++;
+    if (refs.length>idx1) {
+      return refs[idx1];
+    } else if (isarr && idx2<o.size) {
+      var r=ElementImpl$impl(idx2);
+      idx2++;
       return r;
     }
     return finished();
