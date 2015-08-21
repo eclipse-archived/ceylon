fd20288e2646c15275de65c66d34a1fad2399de2
Enrique Zamudio <alterbass@gmail.com>
2015-08-21 11:40:33 -0500

    recovered impl

--- language/runtime-js/meta/classDeclaration.js
+++ language/runtime-js/meta/classDeclaration.js
@@ -1,4 +1,18 @@
 function classDeclaration$meta(inst) {
-  //TODO get package
-  return openClass$jsint(pkg,inst.getT$all()[inst.getT$name()]);
+  if (o==null) {
+    return openClass$jsint(m$1.lmp$(ex$,'$'),Null);
+  }
+  var pkn=o.getT$name();
+  var meta=o.getT$all()[pkn];
+  var mm=getrtmm$$(meta);
+  var mod=mm.mod;
+  if (typeof(mod)==='function')mod=mod();
+  mod=modules$meta().find(mod['$mod-name'],mod['$mod-version']);
+  if (pkn.indexOf('::')) {
+    pkn = pkn.substring(0,pkn.indexOf('::'));
+  } else {
+    pkn = '';
+  }
+  var pkg=mod.findPackage(pkn);
+  return openClass$jsint(pkg,meta);
 }
