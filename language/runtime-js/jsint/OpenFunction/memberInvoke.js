7e6cc58d0c28d859e383ccce4881c1f70037db49
Enrique Zamudio <alterbass@gmail.com>
2015-08-26 13:35:23 -0500

    work on #5596

--- language/runtime-js/jsint/OpenFunction/memberInvoke.js
+++ language/runtime-js/jsint/OpenFunction/memberInvoke.js
@@ -1,3 +1,21 @@
-function memberInvoke() {
-  throw new TypeError("IMPL OpenFunction.memberInvoke");
+function memberInvoke(cont,targs,args) {
+  if (targs===undefined)targs=empty();
+  if (args===undefined)args=empty();
+  var a=[];
+  for (var i=0;i<args.size;i++)a.push(args.$_get(i));
+  if (targs.size) {
+    var mm=getrtmm$$(this.tipo);
+    mm=get_model(mm);
+    var oname=mm.nm;
+    mm=mm.tp;
+    if (mm.length !== targs.size)throw IncompatibleTypeException$meta$model("Expected " + mm.length + " type arguments");
+    var ta={};
+    for (i=0;i<mm.length;i++) {
+      var k=mm[i].nm + '$' + oname;
+      ta[k] = {t:targs.$_get(i).tipo};
+      if (targs.$_get(i).$$targs$$.Target$Type.a)ta[k].a=targs.$_get(i).$$targs$$.Target$Type.a;
+    }
+    a.push(ta);
+  }
+  return this.tipo.apply(cont,a);
 }
