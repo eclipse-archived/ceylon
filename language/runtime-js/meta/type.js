955995f5fb0a08b27aa6ef088ea4ddd8c5609eed
Enrique Zamudio <alterbass@gmail.com>
2015-09-11 17:42:49 -0500

    check for member constructor as well

--- language/runtime-js/meta/type.js
+++ language/runtime-js/meta/type.js
@@ -22,7 +22,7 @@ function type$meta(x,$$targs$$) {
   }
   if (mm===undefined&&x.rt$)mm=$_Array.$crtmm$;
   if (mm===undefined)throw new Error("Cannot retrieve metamodel for " + x);
-  var _cntr=mm.$cont && !is$(x,{t:AppliedCallableConstructor$jsint}) ? get_model(getrtmm$$(mm.$cont)) : undefined;
+  var _cntr=mm.$cont && !is$(x,{t:'u',l:[{t:AppliedCallableConstructor$jsint},{t:AppliedMemberClassCallableConstructor$jsint}]}) ? get_model(getrtmm$$(mm.$cont)) : undefined;
   var _classOrInterfaceMember = _cntr && (_cntr.mt==='c' || _cntr.mt==='i' || _cntr.mt==='o');
   if (mm.$t) { //it's a value
     if (typeof(x)==='function') { //It's a callable
