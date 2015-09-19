ecd774f34698bb48c4f0aad30b37e354b0ecb214
Paŭlo Ebermann <paul@fencing-game.de>
2015-09-19 23:46:26 +0200

    Better overflow message for Float.integer.

--- language/runtime/ceylon/language/Float.java
+++ language/runtime/ceylon/language/Float.java
@@ -412,7 +412,7 @@ public final class Float
             return (long)value;
         }
         else {
-            throw new OverflowException("Numeric overflow converting Float " + value + " to Integer.");
+            throw new OverflowException(value + " cannot be coerced to a 64 bit integer");
         }
     }
     
