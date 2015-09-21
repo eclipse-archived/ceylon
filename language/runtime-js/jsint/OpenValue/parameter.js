9e33f6d71c3ee415c717a68a0c04d6fff9bd4eb2
Enrique Zamudio <alterbass@gmail.com>
2015-09-21 17:05:52 -0500

    update to new hierarchy

--- language/runtime-js/jsint/OpenValue/parameter.js
+++ language/runtime-js/jsint/OpenValue/parameter.js
@@ -1,5 +1,5 @@
 if (this.param$$!==undefined)return this.param$$!==null;
-if (is$(this.container,{t:FunctionalDeclaration$meta$declaration})) {
+if (is$(this.container,{t:'u',l:[{t:FunctionalDeclaration$meta$declaration},{t:ClassWithInitializerDeclaration$meta$declaration}]})) {
   var param;
   for (var iter=this.container.parameterDeclarations.iterator();(param=iter.next())!==finished();) {
     if (param.name.equals(this.name)) {
