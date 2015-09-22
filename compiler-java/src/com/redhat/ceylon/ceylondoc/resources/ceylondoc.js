46bc937b46f1eb74f3fd1885c84c213e4fa09a8f
Stephane Epardaud <stef@epardaud.fr>
2015-09-22 14:02:04 +0200

    Ceylondoc: fix search and spaces

--- compiler-java/src/com/redhat/ceylon/ceylondoc/resources/ceylondoc.js
+++ compiler-java/src/com/redhat/ceylon/ceylondoc/resources/ceylondoc.js
@@ -547,8 +547,9 @@ function getUrlVars() {
 	var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
 	for ( var i = 0; i < hashes.length; i++) {
 		hash = hashes[i].split('=');
-		vars.push(hash[0]);
-		vars[hash[0]] = hash[1];
+		var name = decodeURIComponent(hash[0]);
+		vars.push(name);
+		vars[name] = decodeURIComponent(hash[1]);
 	}
 	return vars;
 }
