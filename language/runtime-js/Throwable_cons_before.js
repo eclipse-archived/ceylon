5e97b0fbfa7b5902dda355d0e0dafdc20666ba5f
Tako Schotanus <tako@ceylon-lang.org>
2015-09-24 18:00:28 +0200

    Make sure that strict mode doesn't cause any problems (#5510)

--- language/runtime-js/Throwable_cons_before.js
+++ language/runtime-js/Throwable_cons_before.js
@@ -1,24 +1,28 @@
 function(msg,cause,exc) {
   exc.stack_trace=[];
   exc.toString=function(){return this.string;}
-  var _caller=arguments.callee.caller.caller;
-  var ilc=0;
-  var ilf=null;
-  var noms=[];
-  while(_caller) {
-    exc.stack_trace.push(_caller);
-    noms.push(_caller.string);
-    _caller = _caller.caller;
-    if (!_caller)return;
-    if (_caller.string===ilf) {
-      ilc++;
-      if (ilc>2) {
-        exc.stack_trace.push("CIRCULAR");
-        _caller = null;
-      }
-    } else if (ilc===0 && noms.contains(_caller.string)) {
-      ilc=1;
-      ilf=_caller.string;
+  try {
+    var _caller=arguments.callee.caller.caller;
+    var ilc=0;
+    var ilf=null;
+    var noms=[];
+    while(_caller) {
+      exc.stack_trace.push(_caller);
+      noms.push(_caller.string);
+      try {
+        _caller = _caller.caller;
+        if (!_caller)return;
+        if (_caller.string===ilf) {
+          ilc++;
+          if (ilc>2) {
+            exc.stack_trace.push("CIRCULAR");
+            _caller = null;
+          }
+        } else if (ilc===0 && noms.contains(_caller.string)) {
+          ilc=1;
+          ilf=_caller.string;
+        }
+      } catch(e) { _caller = null; }
     }
-  }
+  } catch(e) {}
 }
