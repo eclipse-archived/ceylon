d8faf0c1a084339d12077dd518bbb0d42c878bf9
Enrique Zamudio <alterbass@gmail.com>
2015-10-09 16:24:21 -0500

    fix bug related to #3054

--- language/runtime-js/callables.js
+++ language/runtime-js/callables.js
@@ -57,29 +57,36 @@ function spread$(a,f,targs) {
   var arg=[].slice.call(a,0);
   //if we get only 1 arg and it's a tuple...
   if (arg.length===1 && is$(arg[0],{t:Tuple})) {
+    var tuple0=arg[0];
     //Possible spread, check the metamodel
     var mm=getrtmm$$(f);
-    if (arg[0].size===mm.ps.length && mm.ps.length>=1) {
+    var params = mm && mm.ps;
+    if (params.length===1 && params[0].seq>0 && is$(tuple0,{t:Sequence,a:{Element$Sequence:params[0].$t}})){
+      return arg;
+    }
+    if (tuple0.size===params.length && params.length>=1) {
       //Simple mapping
       var all=[];
-      for (var i=0; i<mm.ps.length;i++){
-        var tet=mm.ps[i].$t;
+      for (var i=0; i<params.length;i++){
+        var tet=params[i].$t;
         if (typeof(tet)==='string'&&targs)tet=targs[tet];
-        if (is$(arg[0].$_get(i),tet)) {
-          all.push(arg[0].$_get(i));
+        if (is$(tuple0.$_get(i),tet)) {
+          all.push(tuple0.$_get(i));
         }
       }
-      if (all.length===mm.ps.length) {
+      if (all.length===params.length) {
         return all;
       }
     }
     //If f has only 1 param and it's not sequenced, get its type
-    var a1t=mm && mm.ps && mm.ps.length===1 && mm.ps[0].seq===undefined ? mm.ps[0].$t : undefined;
+    var a1t=params.length===1 && params[0].seq===undefined ? params[0].$t : undefined;
     //If it's a type param, get the type argument
     if (typeof(a1t)==='string')a1t=targs && targs[a1t];
     //If the tuple type matches the param type, it's NOT a spread
     //(it's just a regular 1-param func which happens to receive a tuple)
-    if (!a1t || is$(arg[0],a1t))return arg;
+    if (!a1t || is$(tuple0,a1t)) {
+      return arg;
+    }
     var typecheck;
     if (a1t && targs && targs.Arguments$Callable) {
       typecheck=targs.Arguments$Callable;
@@ -88,21 +95,21 @@ function spread$(a,f,targs) {
         //after all, it is NOT a spread
         return arg;
       }
-    } else if (a1t && arg[0].$$targs$$) {
-      if (arg[0].$$targs$$.First$Tuple) {
-        typecheck={t:Tuple,a:arg[0].$$targs$$};
-      } else if (arg[0].$$targs$$.t==='T') {
-        typecheck=arg[0].$$targs$$;
-      } else if (arg[0].$$targs$$.Element$Iterable) {
-        typecheck={t:Iterable,a:arg[0].$$targs$$.Element$Iterable};
+    } else if (a1t && tuple0.$$targs$$) {
+      if (tuple0.$$targs$$.First$Tuple) {
+        typecheck={t:Tuple,a:tuple0.$$targs$$};
+      } else if (tuple0.$$targs$$.t==='T') {
+        typecheck=tuple0.$$targs$$;
+      } else if (tuple0.$$targs$$.Element$Iterable) {
+        typecheck={t:Iterable,a:tuple0.$$targs$$.Element$Iterable};
       }
     }
-    if (mm && mm.ps && (mm.ps.length>1 || (mm.ps.length===1
-        && (mm.ps[0].seq || !extendsType(a1t, typecheck))))) {
-      var a=arg[0].nativeArray ? arg[0].nativeArray():undefined;
+    if (params && (params.length>1 || (params.length===1
+        && (params[0].seq || !extendsType(a1t, typecheck))))) {
+      var a=tuple0.nativeArray ? tuple0.nativeArray():undefined;
       if (a===undefined) {
         a=[];
-        for (var i=0;i<arg[0].size;i++)a.push(arg[0].$_get(i));
+        for (var i=0;i<tuple0.size;i++)a.push(tuple0.$_get(i));
       }
       arg=a;
     }
