392f0b85f6e8373e83edfe78bdcece02db4da5f0
Enrique Zamudio <alterbass@gmail.com>
2015-11-07 11:39:19 -0600

    unneeded "new"

--- language/runtime-js/Array/iterator.js
+++ language/runtime-js/Array/iterator.js
@@ -1,7 +1,7 @@
 function() {
     var $idx$=0;
     var $arr$=this;
-    return new for$iter(function() {
+    return for$iter(function() {
         return ($idx$===$arr$.length) ? finished() : $arr$[$idx$++];
     },{Element$Iterable:this._elemTarg(),Absent$Iterable:{t:Null}});
 }
