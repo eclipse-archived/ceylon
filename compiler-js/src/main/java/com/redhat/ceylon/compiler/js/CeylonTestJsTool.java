5c5182165eba070dd81a8f299697157aa89b9158
thradec <thradec@gmail.com>
2015-11-13 12:21:01 +0100

    ceylon test: inspect the tested module to determine version of the test framework to load #2426

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/CeylonTestJsTool.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/CeylonTestJsTool.java
@@ -3,24 +3,17 @@ package com.redhat.ceylon.compiler.js;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import com.redhat.ceylon.cmr.api.ModuleQuery;
-import com.redhat.ceylon.cmr.api.ModuleVersionDetails;
 import com.redhat.ceylon.cmr.api.RepositoryManager;
-import com.redhat.ceylon.cmr.ceylon.RepoUsingTool;
+import com.redhat.ceylon.cmr.ceylon.AbstractTestTool;
 import com.redhat.ceylon.common.ModuleUtil;
 import com.redhat.ceylon.common.Versions;
-import com.redhat.ceylon.common.config.DefaultToolOptions;
-import com.redhat.ceylon.common.tool.Argument;
 import com.redhat.ceylon.common.tool.Description;
-import com.redhat.ceylon.common.tool.Option;
 import com.redhat.ceylon.common.tool.OptionArgument;
 import com.redhat.ceylon.common.tool.RemainingSections;
-import com.redhat.ceylon.common.tool.Rest;
 import com.redhat.ceylon.common.tool.Summary;
-import com.redhat.ceylon.common.tools.CeylonTool;
 
 @Summary("Executes tests")
 @Description(
@@ -38,7 +31,7 @@ import com.redhat.ceylon.common.tools.CeylonTool;
         "The following would execute tests in the `com.example.foobar` module:" +
         "\n\n" +
         "    ceylon test-js com.example.foobar/1.0.0")
-public class CeylonTestJsTool extends RepoUsingTool {
+public class CeylonTestJsTool extends AbstractTestTool {
 
     private static final String TEST_MODULE_NAME = "com.redhat.ceylon.testjs";
     private static final String TEST_RUN_FUNCTION = "com.redhat.ceylon.testjs.run";
@@ -47,47 +40,11 @@ public class CeylonTestJsTool extends RepoUsingTool {
     private static final String COLOR_GREEN = "com.redhat.ceylon.common.tool.terminal.color.green";
     private static final String COLOR_RED = "com.redhat.ceylon.common.tool.terminal.color.red";
 
-    private List<String> moduleNameOptVersionList;
-    private List<String> testList;
-    private List<String> argumentList;
-    private String version;
-    private String compileFlags;
     private String nodeExe;
     private boolean debug = true;
-    private boolean tap = false;
-    private boolean report = false;
 
     public CeylonTestJsTool() {
-        super(CeylonRunJsMessages.RESOURCE_BUNDLE);
-    }
-
-    @Override
-    public void initialize(CeylonTool mainTool) throws Exception {
-        // noop
-    }
-
-    @Argument(argumentName = "modules", multiplicity = "+")
-    public void setModules(List<String> moduleNameOptVersionList) {
-        this.moduleNameOptVersionList = moduleNameOptVersionList;
-    }
-
-    @OptionArgument(longName = "test", argumentName = "test")
-    @Description("Specifies which tests will be run.")
-    public void setTests(List<String> testList) {
-        this.testList = testList;
-    }
-
-    @Option
-    @OptionArgument(argumentName = "flags")
-    @Description("Determines if and how compilation should be handled. Allowed flags include: `never`, `once`, `force`, `check`.")
-    public void setCompile(String compile) {
-        this.compileFlags = compile;
-    }
-
-    @OptionArgument(argumentName = "version")
-    @Description("Specifies which version of the test module to use, defaults to " + Versions.CEYLON_VERSION_NUMBER + ".")
-    public void setVersion(String version) {
-        this.version = version;
+        super(CeylonRunJsMessages.RESOURCE_BUNDLE, ModuleQuery.Type.JS, Versions.JS_BINARY_MAJOR_VERSION, Versions.JS_BINARY_MINOR_VERSION);
     }
 
     @OptionArgument(argumentName = "node-exe")
@@ -101,93 +58,21 @@ public class CeylonTestJsTool extends RepoUsingTool {
     public void setDebug(boolean debug) {
         this.debug = debug;
     }
-    
-    @Option(longName = "tap")
-    @Description("Enables the Test Anything Protocol v13.")
-    public void setTap(boolean tap) {
-        this.tap = tap;
-    }
-
-    @Option(longName = "report")
-    @Description("Generates the test results report into HTML format, output directory is `reports/test-js` (experimental).")
-    public void setReport(boolean report) {
-        this.report = report;
-    }
-
-    @Rest
-    public void setArgs(List<String> argumentList) {
-        this.argumentList = argumentList;
-    }
 
     @Override
     public void run() throws Exception {
         final List<String> args = new ArrayList<String>();
         final List<String> moduleAndVersionList = new ArrayList<String>();
 
-        if (version == null) {
-            Collection<ModuleVersionDetails> versions = getModuleVersions(
-                    getRepositoryManager(),
-                    TEST_MODULE_NAME,
-                    null,
-                    ModuleQuery.Type.JS,
-                    Versions.JS_BINARY_MAJOR_VERSION,
-                    Versions.JS_BINARY_MINOR_VERSION);
-
-            if (versions == null || versions.isEmpty()) {
-                version = Versions.CEYLON_VERSION_NUMBER;
-            } else {
-                ModuleVersionDetails mdv = versions.toArray(new ModuleVersionDetails[] {})[versions.size() - 1];
-                version = mdv.getVersion();
-            }
-        }
-
-        if (moduleNameOptVersionList != null) {
-            for (String moduleNameOptVersion : moduleNameOptVersionList) {
-                String moduleAndVersion = resolveModuleAndVersion(moduleNameOptVersion);
-                if (moduleAndVersion == null) {
-                    return;
-                }
-                args.add("--module");
-                args.add(moduleAndVersion);
-                moduleAndVersionList.add(moduleAndVersion);
-            }
-        }
-        if (testList != null) {
-            for (String test : testList) {
-                args.add("--test");
-                args.add(test);
-            }
-        }
-        if (argumentList != null) {
-            args.addAll(argumentList);
-        }
-
-        if (compileFlags == null) {
-            compileFlags = DefaultToolOptions.getTestToolCompileFlags();
-            if (compileFlags.isEmpty()) {
-                compileFlags = COMPILE_NEVER;
-            }
-        } else if (compileFlags.isEmpty()) {
-            compileFlags = COMPILE_ONCE;
-        }
+        processModuleNameOptVersionList(args, moduleAndVersionList);
+        processTestList(args);
+        processArgumentList(args);
+        processCompileFlags();
+        processTapOption(args);
+        processReportOption(args);
+        processColors(args);
         
-        if (tap) {
-            args.add("--tap");
-        }
-        if (report) {
-            args.add("--report");
-        }
-        
-        if (System.getProperties().containsKey(COLOR_RESET)
-                && System.getProperties().containsKey(COLOR_GREEN)
-                && System.getProperties().containsKey(COLOR_RED)) {
-            args.add("--" + COLOR_RESET);
-            args.add(System.getProperty(COLOR_RESET));
-            args.add("--" + COLOR_GREEN);
-            args.add(System.getProperty(COLOR_GREEN));
-            args.add("--" + COLOR_RED);
-            args.add(System.getProperty(COLOR_RED));
-        }
+        resolveVersion(moduleAndVersionList);
 
         CeylonRunJsTool ceylonRunJsTool = new CeylonRunJsTool() {
             @Override
@@ -218,19 +103,17 @@ public class CeylonTestJsTool extends RepoUsingTool {
         ceylonRunJsTool.run();
     }
 
-    private String resolveModuleAndVersion(String moduleNameOptVersion) throws IOException {
-        String modName = ModuleUtil.moduleName(moduleNameOptVersion);
-        String modVersion = ModuleUtil.moduleVersion(moduleNameOptVersion);
-
-        modVersion = checkModuleVersionsOrShowSuggestions(
-                getRepositoryManager(),
-                modName,
-                modVersion,
-                ModuleQuery.Type.JS,
-                Versions.JS_BINARY_MAJOR_VERSION,
-                Versions.JS_BINARY_MINOR_VERSION,
-                compileFlags);
-
-        return modVersion != null ? modName + "/" + modVersion : null;
+    private void processColors(final List<String> args) {
+        if (System.getProperties().containsKey(COLOR_RESET)
+                && System.getProperties().containsKey(COLOR_GREEN)
+                && System.getProperties().containsKey(COLOR_RED)) {
+            args.add("--" + COLOR_RESET);
+            args.add(System.getProperty(COLOR_RESET));
+            args.add("--" + COLOR_GREEN);
+            args.add(System.getProperty(COLOR_GREEN));
+            args.add("--" + COLOR_RED);
+            args.add(System.getProperty(COLOR_RED));
+        }
     }
+
 }
