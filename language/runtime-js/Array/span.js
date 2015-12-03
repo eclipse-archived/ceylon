fd2cb3728c3d2c3ce4597a65a7d69e19474a53a8
Enrique Zamudio <alterbass@gmail.com>
2015-12-03 16:45:52 -0600

    don't overwrap

--- language/runtime-js/Array/span.js
+++ language/runtime-js/Array/span.js
@@ -2,7 +2,7 @@ function(from, to) {
     if (from > to) {
         var arr = this.measure(to, from-to+1);
         arr.reverseInPlace();
-        return $arr$(arr,this.$$targs$$.Element$Array);
+        return arr;
     }
     return this.measure(from, to-from+1);
 }
