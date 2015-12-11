056ccde7fedc5b3a09219fa5ad2695e5e2d78ec4
Stephane Epardaud <stef@epardaud.fr>
2015-12-11 12:11:25 +0100

    Fix build for model depending on classfile, add methods to module reader

--- model/src/com/redhat/ceylon/model/loader/Java9ModuleReader.java
+++ model/src/com/redhat/ceylon/model/loader/Java9ModuleReader.java
@@ -4,41 +4,89 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
 import com.redhat.ceylon.langtools.classfile.Attribute;
 import com.redhat.ceylon.langtools.classfile.ClassFile;
-import com.redhat.ceylon.langtools.classfile.ConstantPool.InvalidIndex;
-import com.redhat.ceylon.langtools.classfile.ConstantPool.UnexpectedEntry;
 import com.redhat.ceylon.langtools.classfile.ConstantPoolException;
+import com.redhat.ceylon.langtools.classfile.MainClass_attribute;
 import com.redhat.ceylon.langtools.classfile.Module_attribute;
 import com.redhat.ceylon.langtools.classfile.Module_attribute.ExportsEntry;
+import com.redhat.ceylon.langtools.classfile.Version_attribute;
 
 public class Java9ModuleReader {
 
-    public static List<String> getExportedPackages(File jar){
-        ClassFile classFile = getClassFile(jar);
-        if(classFile == null)
-            return null;
-        Module_attribute moduleAttribute = (Module_attribute) classFile.getAttribute(Attribute.Module);
-        try {
-            if(moduleAttribute != null){
-                List<String> ret = new ArrayList<String>(moduleAttribute.exports_count);
+    public static class Java9Module{
+        public final String name, version, mainClass;
+        public final Set<String> exports = new HashSet<>();
+        
+        public Java9Module(Module_attribute moduleAttribute, ClassFile classFile){
+            try {
+                String nameAttr = classFile.getName();
+                if(nameAttr.endsWith("/module-info"))
+                    nameAttr = nameAttr.substring(0, nameAttr.length()-12);
+                name = nameAttr.replace('/', '.');
                 for(ExportsEntry export : moduleAttribute.exports){
                     if(export.exports_to_count == 0){
                         String ex;
                         ex = classFile.constant_pool.getUTF8Value(export.exports_index).replace('/', '.');
-                        ret.add(ex);
+                        exports.add(ex);
                     }
                 }
-                return ret;
+                Version_attribute versionAttribute = (Version_attribute) classFile.getAttribute(Attribute.Version);
+                if(versionAttribute != null){
+                    version = classFile.constant_pool.getUTF8Value(versionAttribute.version_index);
+                }else{
+                    // or throw?
+                    version = null;
+                }
+                MainClass_attribute mainAttribute = (MainClass_attribute) classFile.getAttribute(Attribute.MainClass);
+                if(mainAttribute != null){
+                    mainClass = mainAttribute.getMainClassName(classFile.constant_pool).replace('/', '.');
+                }else{
+                    mainClass = null;
+                }
+            } catch (ConstantPoolException x) {
+                throw new RuntimeException(x);
             }
-            return null;
-        } catch (InvalidIndex | UnexpectedEntry x) {
-            throw new RuntimeException(x);
+            
         }
+    }
+    
+    public static List<String> getExportedPackages(File jar){
+        Java9Module module = getJava9Module(jar);
+        if(module != null){
+            List<String> ret = new ArrayList<String>(module.exports.size());
+            ret.addAll(module.exports);
+            return ret;
+        }
+        return null;
+    }
+
+    public static Java9Module getJava9Module(ZipFile zipFile, ZipEntry entry){
+        ClassFile classFile = getClassFile(zipFile, entry);
+        if(classFile == null)
+            return null;
+        Module_attribute moduleAttribute = (Module_attribute) classFile.getAttribute(Attribute.Module);
+        if(moduleAttribute != null){
+            return new Java9Module(moduleAttribute, classFile);
+        }
+        return null;
+    }
+
+    public static Java9Module getJava9Module(File jar){
+        ClassFile classFile = getClassFile(jar);
+        if(classFile == null)
+            return null;
+        Module_attribute moduleAttribute = (Module_attribute) classFile.getAttribute(Attribute.Module);
+        if(moduleAttribute != null){
+            return new Java9Module(moduleAttribute, classFile);
+        }
+        return null;
     }
 
     private static ClassFile getClassFile(File jar){
@@ -46,12 +94,20 @@ public class Java9ModuleReader {
             try(ZipFile zipFile = new ZipFile(jar)){
                 ZipEntry entry = zipFile.getEntry("module-info.class");
                 if(entry != null){
-                    try(InputStream in = zipFile.getInputStream(entry)){
-                        return ClassFile.read(in);
-                    }
+                    return getClassFile(zipFile, entry);
                 }
             }
             return null;
+        }catch(IOException x){
+            throw new RuntimeException(x);
+        }
+    }
+
+    private static ClassFile getClassFile(ZipFile zipFile, ZipEntry entry){
+        try{
+            try(InputStream in = zipFile.getInputStream(entry)){
+                return ClassFile.read(in);
+            }
         }catch(IOException | ConstantPoolException x){
             throw new RuntimeException(x);
         }
