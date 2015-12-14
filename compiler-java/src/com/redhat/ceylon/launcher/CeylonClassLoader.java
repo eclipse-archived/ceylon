c0dc6206cd763be12e83fa15b037f42d0d24e818
Stephane Epardaud <stef@epardaud.fr>
2015-12-14 16:30:23 +0100

    Added tool.provier to CeylonClassLoader

--- compiler-java/src/com/redhat/ceylon/launcher/CeylonClassLoader.java
+++ compiler-java/src/com/redhat/ceylon/launcher/CeylonClassLoader.java
@@ -109,6 +109,7 @@ public class CeylonClassLoader extends URLClassLoader {
         archives.add(getRepoJar(ceylonRepo, "com.redhat.ceylon.model", version));
         archives.add(getRepoJar(ceylonRepo, "com.redhat.ceylon.module-resolver", version));
         archives.add(getRepoJar(ceylonRepo, "com.redhat.ceylon.langtools.classfile", version));
+        archives.add(getRepoJar(ceylonRepo, "com.redhat.ceylon.tool.provider", version));
         archives.add(getRepoJar(ceylonRepo, "org.jboss.jandex", Versions.DEPENDENCY_JANDEX_VERSION));
         archives.add(getRepoJar(ceylonRepo, "org.jboss.modules", Versions.DEPENDENCY_JBOSS_MODULES_VERSION));
         archives.add(getRepoJar(ceylonRepo, "org.jboss.logmanager", Versions.DEPENDENCY_LOGMANAGER_VERSION));
