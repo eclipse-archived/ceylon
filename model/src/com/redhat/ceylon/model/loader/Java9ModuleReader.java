63ff75945ea315fed456e859a5a50b864dba9329
Stephane Epardaud <stef@epardaud.fr>
2015-12-15 16:24:02 +0100

    Java9ModuleReader: add dependencies

--- model/src/com/redhat/ceylon/model/loader/Java9ModuleReader.java
+++ model/src/com/redhat/ceylon/model/loader/Java9ModuleReader.java
@@ -16,6 +16,7 @@ import com.redhat.ceylon.langtools.classfile.ConstantPoolException;
 import com.redhat.ceylon.langtools.classfile.MainClass_attribute;
 import com.redhat.ceylon.langtools.classfile.Module_attribute;
 import com.redhat.ceylon.langtools.classfile.Module_attribute.ExportsEntry;
+import com.redhat.ceylon.langtools.classfile.Module_attribute.RequiresEntry;
 import com.redhat.ceylon.langtools.classfile.Version_attribute;
 
 public class Java9ModuleReader {
@@ -23,6 +24,7 @@ public class Java9ModuleReader {
     public static class Java9Module{
         public final String name, version, mainClass;
         public final Set<String> exports = new HashSet<>();
+        public final List<Java9ModuleDependency> dependencies = new ArrayList<>();
         
         public Java9Module(Module_attribute moduleAttribute, ClassFile classFile){
             try {
@@ -36,6 +38,10 @@ public class Java9ModuleReader {
                         ex = classFile.constant_pool.getUTF8Value(export.exports_index).replace('/', '.');
                         exports.add(ex);
                     }
+                }
+                for(RequiresEntry requires : moduleAttribute.requires){
+                    dependencies.add(new Java9ModuleDependency(requires.getRequires(-1/*ignored*/, classFile.constant_pool),
+                            (requires.requires_flags & Module_attribute.ACC_PUBLIC) != 0));
                 }
                 Version_attribute versionAttribute = (Version_attribute) classFile.getAttribute(Attribute.Version);
                 if(versionAttribute != null){
@@ -54,6 +60,15 @@ public class Java9ModuleReader {
                 throw new RuntimeException(x);
             }
             
+        }
+    }
+    
+    public static class Java9ModuleDependency {
+        public final String module;
+        public final boolean shared;
+        public Java9ModuleDependency(String module, boolean shared) {
+            this.module = module;
+            this.shared = shared;
         }
     }
     
@@ -89,6 +104,17 @@ public class Java9ModuleReader {
         return null;
     }
 
+    public static Java9Module getJava9Module(InputStream is){
+        ClassFile classFile = getClassFile(is);
+        if(classFile == null)
+            return null;
+        Module_attribute moduleAttribute = (Module_attribute) classFile.getAttribute(Attribute.Module);
+        if(moduleAttribute != null){
+            return new Java9Module(moduleAttribute, classFile);
+        }
+        return null;
+    }
+
     private static ClassFile getClassFile(File jar){
         try{
             try(ZipFile zipFile = new ZipFile(jar)){
@@ -106,9 +132,17 @@ public class Java9ModuleReader {
     private static ClassFile getClassFile(ZipFile zipFile, ZipEntry entry){
         try{
             try(InputStream in = zipFile.getInputStream(entry)){
-                return ClassFile.read(in);
+                return getClassFile(in);
             }
-        }catch(IOException | ConstantPoolException x){
+        }catch(IOException x){
+            throw new RuntimeException(x);
+        }
+    }
+
+    private static ClassFile getClassFile(InputStream in){
+        try {
+            return ClassFile.read(in);
+        } catch (IOException | ConstantPoolException x) {
             throw new RuntimeException(x);
         }
     }
