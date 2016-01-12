2a5eb0cc17ff87a3949ccc1ce6b9c9c0e0a75a89
Enrique Zamudio <alterbass@gmail.com>
2016-01-12 12:20:30 -0600

    according to a langmod test, order of validations matters

--- language/runtime-js/jsint/OpenFunction/apply.js
+++ language/runtime-js/jsint/OpenFunction/apply.js
@@ -1,10 +1,10 @@
 function(types,$mptypes){
   var mm=this.tipo.$crtmm$;
   var ta={t:this.tipo};
+  validate$typeparams(ta,mm.tp,types);
   if (!extendsType(mm.$t,$mptypes.Return$apply)){
     throw IncompatibleTypeException$meta$model("actual type of applied declaration is not compatible with expected type");
   }
-  validate$typeparams(ta,mm.tp,types);
   validate$params(mm.ps,$mptypes.Arguments$apply,"Wrong number of arguments when applying function");
   return ta.a?AppliedFunction$jsint(this.tipo,{Type$AppliedFunction:mm.$t,Arguments$AppliedFunction:tupleize$params(mm.ps,ta.a)},undefined,ta.a):
     AppliedFunction$jsint(this.tipo,{Type$AppliedFunction:mm.$t,Arguments$AppliedFunction:tupleize$params(mm.ps)});
