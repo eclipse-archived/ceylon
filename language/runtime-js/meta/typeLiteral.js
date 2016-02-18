04579ac0ce8f7daa8ce1df5426607cf04f55614e
Enrique Zamudio <alterbass@gmail.com>
2016-02-17 20:34:35 -0600

    fix #5962

--- language/runtime-js/meta/typeLiteral.js
+++ language/runtime-js/meta/typeLiteral.js
@@ -27,24 +27,25 @@ function typeLiteral$meta($$targs$$,targ$2) {
     } else {
       var mm = getrtmm$$(t);
       var mdl = get_model(mm);
-      if (mdl.mt==='c' || mdl.mt==='o') {
+      var mtype=mdl.mt;
+      if (mtype==='c' || mtype==='o') {
         //TODO tupleize Arguments
         var acargs={t:Sequential,a:{Element$Iterable:{t:Anything}}};
         var r=mm.$cont?AppliedMemberClass$jsint(t,{Container$AppliedMemberClass:{t:mm.$cont},Type$AppliedMemberClass:$$targs$$.Type$typeLiteral,Arguments$AppliedMemberClass:acargs})
           : AppliedClass$jsint(t,{Type$AppliedClass:$$targs$$.Type$typeLiteral,Arguments$AppliedClass:acargs});
         if ($$targs$$.Type$typeLiteral.a)r.$targs=$$targs$$.Type$typeLiteral.a;
         return r;
-      } else if (mdl['mt'] === 'i') {
+      } else if (mtype==='i') {
         var r=AppliedInterface$jsint(t,{Type$Interface:$$targs$$.Type$typeLiteral});
         if ($$targs$$.Type$typeLiteral.a)r.$targs=$$targs$$.Type$typeLiteral.a;
         return r;
-      } else if (mdl['mt'] === 'm') {
+      } else if (mtype==='m') {
         return AppliedFunction$jsint(t,{Type$AppliedFunction:$$targs$$.Type$typeLiteral,
                Arguments$AppliedFunction:{t:Sequential,a:{Element$Iterable:{t:Anything}}}});
-      } else if (mdl['mt'] === 'a' || mdl['mt'] === 'g' || mdl.mt==='s') {
+      } else if (mtype==='a' || type==='g' || mtype==='s') {
         return AppliedValue$jsint(undefined,t,{Container$Value:{t:mm.$cont},Get$Value:mm.$t,Set$Value:mdl['var']?mm.$t:{t:Nothing}});
       } else {
-        console.log("WTF is a metatype " + mdl['mt'] + " on a closed type???????");
+        console.log("WTF is a metatype " + mtype + " on a closed type???????");
       }
       console.log("typeLiteral<" + t.getT$name() + "> (closed type)");
     }
@@ -53,22 +54,29 @@ function typeLiteral$meta($$targs$$,targ$2) {
     var t = $$targs$$.Type$typeLiteral;
     var mm = getrtmm$$(t);
     var mdl = get_model(mm);
+    var mtype=mdl.mt;
+    if (mtype==='als') {
+      //resolve type alias
+      t=t.t;
+      mm=getrtmm$$(t);
+      mdl=get_model(mm);
+    }
     var _m = typeof(mm.mod)==='function'?mm.mod():mm.mod;
     //We need the module
     var _mod = modules$meta().find(_m['$mod-name'],_m['$mod-version']);
     var _pkg = _mod.findPackage(mm.d[0]);
     if (mdl.mt==='c' || mdl.mt==='o') {
       return openClass$jsint(_pkg, t);
-    } else if (mdl['mt'] === 'i') {
+    } else if (mtype==='i') {
       return OpenInterface$jsint(_pkg, t);
-    } else if (mdl['mt'] === 'm') {
+    } else if (mtype==='m') {
       return OpenFunction$jsint(_pkg, t);
-    } else if (mdl['mt'] === 'a' || mdl['mt'] === 'g') {
+    } else if (mtype==='a' || mtype==='g') {
       return OpenValue$jsint(_pkg, t);
-    } else if (mdl.mt==='s') {
+    } else if (mtype==='s') {
       return OpenSetter(OpenValue$jsint(_pkg, t));
     } else {
-      console.log("WTF is a metatype " + mdl['mt'] + " on an open type???????");
+      console.log("WTF is a metatype " + mtype + " on an open type???????");
     }
     console.log("typeLiteral<" + t.getT$name() + "> (open type)");
   }
