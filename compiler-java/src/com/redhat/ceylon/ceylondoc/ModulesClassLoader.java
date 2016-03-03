46a325d069e0df879c43c7e15a0493a0c0e1b9dd
Stephane Epardaud <stef@epardaud.fr>
2016-03-03 15:26:28 +0100

    Android: ModulesClassLoader: use jdkProvider to get JDK package list, not rt.jar #5883
    Which does not exist in Android and is superfluous

--- compiler-java/src/com/redhat/ceylon/ceylondoc/ModulesClassLoader.java
+++ compiler-java/src/com/redhat/ceylon/ceylondoc/ModulesClassLoader.java
@@ -19,15 +19,8 @@
  */
 package com.redhat.ceylon.ceylondoc;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.jar.JarEntry;
-import java.util.jar.JarInputStream;
 
 import com.redhat.ceylon.model.cmr.ArtifactResult;
 import com.redhat.ceylon.model.loader.JdkProvider;
@@ -62,63 +55,27 @@ class ModulesClassLoader extends ClassLoader {
 
     public boolean packageExists(Module module, String name) {
         String moduleName = module.getNameAsString();
-        if(JDKUtils.isJDKModule(moduleName)){
-            return JDKUtils.isJDKPackage(moduleName, name);
-        }
-        if(JDKUtils.isOracleJDKModule(moduleName)){
-            return JDKUtils.isOracleJDKPackage(moduleName, name);
+        if(jdkProvider.isJDKModule(moduleName)){
+            return jdkProvider.isJDKPackage(moduleName, name);
         }
         return jars.packageExists(module, name);
     }
 
     public List<String> getPackageList(Module module, String name) {
         String moduleName = module.getNameAsString();
-        if(JDKUtils.isJDKModule(moduleName)
-                || JDKUtils.isOracleJDKModule(moduleName)){
+        if(jdkProvider.isJDKModule(moduleName)){
             return getJDKPackageList(name);
         }
         return jars.getPackageList(module, name);
     }
 
     private List<String> getJDKPackageList(String name) {
-        // Currently limit to rt.jar to avoid scanning everything
-        // Try the boot classpath first, since that's where rt.jar is supposed to sit
-        File rtJar = findRtJar("sun.boot.class.path");
-        if(rtJar == null)
-            rtJar = findRtJar("java.class.path");
-        if(rtJar == null)
-            return Collections.emptyList();
-        String path = name.replace('.', '/') + "/";
-        List<String> ret = new LinkedList<String>();
-
-        try(JarInputStream is = new JarInputStream(new FileInputStream(rtJar))){
-
-            JarEntry entry;
-            while( (entry = is.getNextJarEntry()) != null) {
-                String entryName = entry.getName();
-                if(entryName.endsWith(".class") && entryName.startsWith(path)) {
-                    ret.add(entryName);
-                }
-            }
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException("Failed to load rt.jar", e);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load rt.jar", e);
-        }
-        return ret;
+    	List<String> ret = new LinkedList<String>();
+    	String prefix = name+".";
+    	for(String pkg : jdkProvider.getJDKPackageList()){
+    		if(pkg.equals(name) || pkg.startsWith(prefix))
+    			ret.add(pkg);
+    	}
+    	return ret;
     }
-
-    private File findRtJar(String property) {
-        String path = System.getProperty(property);
-        if(path == null)
-            return null;
-        for(String entry : path.split(File.pathSeparator)){
-            File file = new File(entry);
-            if(file.exists() && file.isFile() && file.getName().toLowerCase().equals("rt.jar")){
-                return file;
-            }
-        }
-        return null;
-    }
-
 }
