9455545495e6a6b6d676267346d87258baf8f12c
Enrique Zamudio <alterbass@gmail.com>
2016-03-28 13:04:24 -0600

    avoid instantiating StringBuilder if possible #5740

--- compiler-js/src/main/java/com/redhat/ceylon/compiler/js/util/JsUtils.java
+++ compiler-js/src/main/java/com/redhat/ceylon/compiler/js/util/JsUtils.java
@@ -4,6 +4,17 @@ public class JsUtils {
 
     /** Escapes a StringLiteral (needs to be quoted). */
     public static String escapeStringLiteral(String s) {
+        //First check if we really need to do something about it
+        boolean clean = true;
+        for (int i=0; i < s.length(); i++) {
+            final char c = s.charAt(i);
+            if (c < 32 || c > 127 || c == 34 || c == 39 || c == 92) {
+                clean = false;
+            }
+        }
+        if (clean) {
+            return s;
+        }
         StringBuilder text = new StringBuilder(s);
         //Escape special chars
         for (int i=0; i < text.length();i++) {
