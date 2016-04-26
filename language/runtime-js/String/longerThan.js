ff025b67dac74a0ef8adbfd73e3ad327c9f3b294
Enrique Zamudio <alterbass@gmail.com>
2016-04-25 20:09:53 -0500

    fix #6230

--- language/runtime-js/String/longerThan.js
+++ language/runtime-js/String/longerThan.js
@@ -1,6 +1,3 @@
 function(length) {
-    //TODO: OPTIMIZE THIS!
-    if (this.size <= length) {return false}
-    if (this.size<<1 > length) {return true}
     return this.size>length;
 }
--- language/runtime-js/String/shorterThan.js
+++ language/runtime-js/String/shorterThan.js
@@ -1,6 +1,3 @@
 function(length) {
-    //TODO: OPTIMIZE THIS!
-    if (this.size < length) {return true}
-    if (this.size<<1 >= length) {return false}
     return this.size<length;
 }
