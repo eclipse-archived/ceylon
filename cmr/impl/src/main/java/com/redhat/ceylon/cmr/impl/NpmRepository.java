d7e7913bb901d136ab8a28ae8d9e42baa214f0ec
Enrique Zamudio <alterbass@gmail.com>
2016-05-19 12:47:40 -0500

    fugly hack to install npm module and find proper file #3106

--- cmr/impl/src/main/java/com/redhat/ceylon/cmr/impl/NpmRepository.java
+++ cmr/impl/src/main/java/com/redhat/ceylon/cmr/impl/NpmRepository.java
@@ -1,11 +1,11 @@
 package com.redhat.ceylon.cmr.impl;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
 import com.redhat.ceylon.cmr.api.ArtifactContext;
 import com.redhat.ceylon.cmr.api.CmrRepository;
@@ -15,6 +15,8 @@ import com.redhat.ceylon.cmr.spi.OpenNode;
 import com.redhat.ceylon.model.cmr.ArtifactResult;
 import com.redhat.ceylon.model.cmr.ArtifactResultType;
 import com.redhat.ceylon.model.cmr.RepositoryException;
+
+import net.minidev.json.JSONValue;
 
 public class NpmRepository extends AbstractRepository {
 
@@ -30,10 +32,63 @@ public class NpmRepository extends AbstractRepository {
     }
 
     protected List<String> getDefaultParentPathInternal(ArtifactContext context) {
+        //npm simply creates a dir with the module name and puts files inside
         final String name = context.getName();
-        final List<String> tokens = new ArrayList<String>();
-        tokens.addAll(Arrays.asList(name.split("\\.")));
-        return tokens;
+        //TODO is it ok to run npm install here?
+        //This is such an ugly hack. I'm pretty sure this doesn't belong here
+        //but I have no idea where this code goes
+        Node kid = getRoot().getChild(name);
+        if (kid == null) {
+            try {
+                final File parent = getRoot().getContent(File.class).getParentFile();
+                System.out.println("installing " + name + "@" + context.getVersion() + " in " + parent);
+                ProcessBuilder pb = new ProcessBuilder().command("/usr/local/bin/npm", "install", name + "@" + context.getVersion())
+                    .directory(parent).inheritIO();
+                String path = pb.environment().get("PATH");
+                if (path == null) {
+                    path = "/usr/local/bin";
+                } else if (!path.contains("/usr/local/bin")) {
+                    path = path + ":/usr/local/bin";
+                }
+                pb.environment().put("PATH", path);
+                pb.start().waitFor();
+                kid = getRoot().addNode(name);
+            } catch (InterruptedException | IOException ex) {
+                System.out.println("npm died or something");
+                ex.printStackTrace();
+            }
+        }
+        return Collections.singletonList(name);
+    }
+
+    public String[] getArtifactNames(ArtifactContext context) {
+        final String name = context.getName();
+        Node kid = getRoot().getChild(name);
+        try {
+            File dir = kid.getContent(File.class);
+            File json = new File(dir, "package.json");
+            if (json.exists() && json.isFile() && json.canRead()) {
+                //TODO parse json, get "main", that's the file we need
+                try (FileReader reader = new FileReader(json)){
+                    @SuppressWarnings("unchecked")
+                    Map<String,Object> descriptor = (Map<String,Object>)JSONValue.parse(reader);
+                    Object main = descriptor.get("main");
+                    if (main instanceof String) {
+                        File mainFile = new File(dir, (String)main);
+                        kid = kid.getChild(mainFile.getName());
+                        if (kid != null) {
+                            context.toNode(kid);
+                            return new String[]{ mainFile.getName() };
+                        }
+                    } else {
+                        System.out.println("WTF 'main' is " + main);
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println("Trying to read package.json");
+        }
+        return new String[0];
     }
 
     @Override
@@ -54,6 +109,7 @@ public class NpmRepository extends AbstractRepository {
         private NpmArtifactResult(CmrRepository repository, RepositoryManager manager, String name, String version, Node node) {
             super(repository, name, version);
             this.node = node;
+            System.out.println("NPM artifact " + repository + ", " + name + " , " + version + ": " + node);
         }
 
         @Override
@@ -77,6 +133,7 @@ public class NpmRepository extends AbstractRepository {
 
         @Override
         public List<ArtifactResult> dependencies() throws RepositoryException {
+            //Get the package.json file
             return Collections.emptyList(); // dunno how to grab deps
         }
         
