2ac53775e3e544fa4fb62ef8895a35bc6f88f31b
davidfestal <david.festal@serli.com>
2016-06-02 17:32:22 +0200

    Allow using `InputStream`s (for the IDEs) in `SourceArchiveCreator`

---
+++ cmr/api/src/main/java/com/redhat/ceylon/cmr/api/SourceArtifactCreator.java
@@ -0,0 +1,16 @@
+package com.redhat.ceylon.cmr.api;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Set;
+
+/**
+ * Contract for a component that can create a compilation artifact.
+ * 
+ * @author Enrique Zamudio
+ */
+public interface SourceArtifactCreator extends ArtifactCreator {
+
+    /** Copy the specified source streams, avoiding duplicate entries. */
+    public Set<String> copyStreams(Collection<SourceStream> sourceStreams) throws IOException;
+}
