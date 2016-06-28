a282b544b293ef216e9d4f8c3dba386e7cb3fb67
Stephane Epardaud <stef@epardaud.fr>
2016-06-28 10:27:27 +0200

    Test for #5919: APT

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTests_5500_5999.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTests_5500_5999.java
@@ -19,9 +19,29 @@
  */
 package com.redhat.ceylon.compiler.java.test.issues;
 
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.jar.JarFile;
+
+import javax.xml.bind.DatatypeConverter;
+
+import org.junit.Assert;
 import org.junit.Test;
 
+import com.redhat.ceylon.cmr.impl.IOUtils;
 import com.redhat.ceylon.compiler.java.test.CompilerTests;
+import com.redhat.ceylon.compiler.java.tools.CeyloncTaskImpl;
+import com.redhat.ceylon.javax.tools.DiagnosticListener;
+import com.redhat.ceylon.javax.tools.FileObject;
 
 
 public class IssuesTests_5500_5999 extends CompilerTests {
@@ -52,6 +72,58 @@ public class IssuesTests_5500_5999 extends CompilerTests {
     public void testBug5868() {
         compileAndRun("com.redhat.ceylon.compiler.java.test.issues.bug58xx.bug5868",
                 "bug58xx/bug5868.ceylon");
+    }
+
+    @Test
+    public void testBug5919() throws IOException {
+        File mrepo = new File(System.getProperty("user.home"), ".m2/repository");
+        File annot = new File(mrepo, "com/android/support/support-annotations/23.0.1");
+        File jar = new File(annot, "support-annotations-23.0.1.jar");
+        File pom = new File(annot, "support-annotations-23.0.1.pom");
+        downloadAndroidAnnotations(pom);
+        downloadAndroidAnnotations(jar);
+        File overridesFile = new File(getPackagePath(), "bug59xx/bug5919/overrides.xml");
+        compile(Arrays.asList(
+                "-overrides", overridesFile.getAbsolutePath(),
+                "-apt", "com.jakewharton:butterknife-compiler/8.1.0"), 
+                "bug59xx/bug5919/test.ceylon");
+        
+        File carFile = getModuleArchive("com.redhat.ceylon.compiler.java.test.issues.bug59xx.bug5919", "1");
+        assertTrue(carFile.exists());
+
+        try(JarFile car = new JarFile(carFile)){
+            Assert.assertNotNull(car.getEntry("com/redhat/ceylon/compiler/java/test/issues/bug59xx/bug5919/Foo$$ViewBinder.class"));
+        }
+    }
+
+    private void downloadAndroidAnnotations(File file) throws IOException {
+        String repoUrl = "https://android.googlesource.com/platform/prebuilts/maven_repo/android/+/android-6.0.1_r46/com/android/support/support-annotations/23.0.1/";
+        // support-annotations-23.0.1.pom?format=TEXT
+        if(!file.exists()){
+            File folder = file.getParentFile();
+            folder.mkdirs();
+            URL url = new URL(repoUrl+file.getName()+"?format=TEXT");
+            File b64 = new File(folder, file.getName()+".base64");
+            try{
+                try(InputStream is = url.openStream();
+                        OutputStream out = new FileOutputStream(b64)){
+                    IOUtils.copyStream(is, out, false, false);
+                    out.flush();
+                }
+                char[] buffer = new char[(int) b64.length()];
+                try(Reader r = new FileReader(b64)){
+                    r.read(buffer);
+                }
+                byte[] bytes = DatatypeConverter.parseBase64Binary(new String(buffer));
+                try(OutputStream os = new FileOutputStream(file)){
+                    os.write(bytes);
+                    os.flush();
+                }
+            }finally{
+                b64.delete();
+            }
+        }
+        
     }
 
     @Test
