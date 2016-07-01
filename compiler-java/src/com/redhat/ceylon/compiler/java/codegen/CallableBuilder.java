ceylon/ceylon-compiler

WIP for #1617: Java 8: support auto-SMI of our Callable types
Detect SMI in model loader and start generating SMI rather than Callable when generating code

This is far from working.

src/com/redhat/ceylon/compiler/java/codegen/CallableBuilder.java
@@ -41,6 +41,7 @@ import com.redhat.ceylon.compiler.typechecker.model.Parameter;
 import com.redhat.ceylon.compiler.typechecker.model.ParameterList;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedReference;
 import com.redhat.ceylon.compiler.typechecker.model.ProducedType;
+import com.redhat.ceylon.compiler.typechecker.model.ProducedTypedReference;
 import com.redhat.ceylon.compiler.typechecker.model.TypeDeclaration;
 import com.redhat.ceylon.compiler.typechecker.model.TypedDeclaration;
 import com.redhat.ceylon.compiler.typechecker.model.Value;
@@ -1142,14 +1143,23 @@ public class CallableBuilder {
         JCClassDecl classDef = gen.make().AnonymousClassDef(gen.make().Modifiers(0), classBody.toList());
         
         int variadicIndex = isVariadic ? numParams - 1 : -1;
-        JCNewClass instance = gen.make().NewClass(null, 
-                null, 
-                gen.makeJavaType(typeModel, JT_EXTENDS | JT_CLASS_NEW), 
-                List.<JCExpression>of(gen.makeReifiedTypeArgument(typeModel.getTypeArgumentList().get(0)),
-                                      gen.makeReifiedTypeArgument(typeModel.getTypeArgumentList().get(1)),
-                                      gen.make().Literal(typeModel.getProducedTypeName(true)),
-                                      gen.make().TypeCast(gen.syms().shortType, gen.makeInteger(variadicIndex))),
-                classDef);
+        JCNewClass instance;
+        if(functionalInterface == null){
+            instance = gen.make().NewClass(null, 
+                    null, 
+                    gen.makeJavaType(typeModel, JT_EXTENDS | JT_CLASS_NEW), 
+                    List.<JCExpression>of(gen.makeReifiedTypeArgument(typeModel.getTypeArgumentList().get(0)),
+                            gen.makeReifiedTypeArgument(typeModel.getTypeArgumentList().get(1)),
+                            gen.make().Literal(typeModel.getProducedTypeName(true)),
+                            gen.make().TypeCast(gen.syms().shortType, gen.makeInteger(variadicIndex))),
+                    classDef);
+        }else{
+            instance = gen.make().NewClass(null, 
+                    null, 
+                    gen.makeJavaType(functionalInterface, JT_EXTENDS | JT_CLASS_NEW), 
+                    List.<JCExpression>nil(),
+                    classDef);
+        }
         return instance;
     }
 
@@ -1341,6 +1351,10 @@ public class CallableBuilder {
     }
 
     Target target = new Target(null);
+
+    private ProducedTypedReference functionalInterfaceMethod;
+
+    private ProducedType functionalInterface;
     
     class Target {
         Tree.Term forwardCallTo;
@@ -1447,5 +1461,14 @@ public class CallableBuilder {
             return gen.makeEmptyAsSequential(true);
         }
         throw Assert.fail("compiler bug: " + defaultedParam.getName() + " is not a defaulted parameter");
+    }
+
+    /**
+     * @param other
+     * @param functionalInterface
+     */
+    public void functionalInterface(ProducedType interfaceType, ProducedTypedReference method) {
+        this.functionalInterface = interfaceType;
+        this.functionalInterfaceMethod = method;
     }
 }

src/com/redhat/ceylon/compiler/java/codegen/ExpressionTransformer.java
@@ -367,6 +367,7 @@ public class ExpressionTransformer extends AbstractTransformer {
     }
     
     JCExpression transform(Tree.FunctionArgument functionArg, ProducedType expectedType) {
+        System.err.println("Function "+functionArg+" return "+expectedType);
         Method model = functionArg.getDeclarationModel();
         List<JCStatement> body;
         boolean prevNoExpressionlessReturn = statementGen().noExpressionlessReturn;
@@ -400,11 +401,61 @@ public class ExpressionTransformer extends AbstractTransformer {
                 functionArg.getParameterLists().get(0),
                 classGen().transformMplBody(functionArg.getParameterLists(), model, body));
         
+        TypeDeclaration expectedDeclaration = expectedType.getDeclaration();
+        if(expectedDeclaration instanceof UnionType){
+            // ignore Callable and Null
+            ProducedType other = null;
+            boolean skip = false;
+            for(ProducedType caseType : expectedDeclaration.getCaseTypes()){
+                // FIXME: fast-case
+                if(caseType.isExactly(typeFact().getNullDeclaration().getType())
+                        || caseType.isSubtypeOf(typeFact().getCallableDeclaration().getType()))
+                    continue;
+                if(other == null)
+                    other = caseType;
+                else{
+                    skip = true;
+                    break;
+                }
+            }
+            if(!skip && other != null){
+                ProducedTypedReference functionalInterface = isFunctionalInterface(other);
+                if(functionalInterface != null){
+                    System.err.println("Got functional interface: "+other+" / "+functionalInterface);
+                    callableBuilder.functionalInterface(other, functionalInterface);
+                }
+            }
+        }
+        
         JCExpression result = callableBuilder.build();
         result = applyErasureAndBoxing(result, callableType, true, BoxingStrategy.BOXED, expectedType);
         return result;
     }
     
+    private ProducedTypedReference isFunctionalInterface(ProducedType type) {
+        TypeDeclaration declaration = type.getDeclaration();
+        if(declaration instanceof Interface == false)
+            return null;
+        if(!declaration.getSatisfiedTypes().isEmpty())
+            return null;
+        Method method = null;
+        for(Declaration d : declaration.getMembers()){
+            if(d instanceof Method == false)
+                return null;
+            // ignore non-formal methods
+            if(!d.isFormal())
+                continue;
+            // allow only one
+            if(method == null)
+                method = (Method) d;
+            else
+                return null;
+        }
+        if(method == null)
+            return null;
+        return method.getProducedTypedReference(type, Collections.<ProducedType>emptyList());
+    }
+
     //
     // Boxing and erasure of expressions
     

src/com/redhat/ceylon/compiler/loader/AbstractModelLoader.java
@@ -64,6 +64,7 @@ import com.redhat.ceylon.compiler.loader.mirror.AnnotatedMirror;
 import com.redhat.ceylon.compiler.loader.mirror.AnnotationMirror;
 import com.redhat.ceylon.compiler.loader.mirror.ClassMirror;
 import com.redhat.ceylon.compiler.loader.mirror.FieldMirror;
+import com.redhat.ceylon.compiler.loader.mirror.FunctionalInterface;
 import com.redhat.ceylon.compiler.loader.mirror.MethodMirror;
 import com.redhat.ceylon.compiler.loader.mirror.PackageMirror;
 import com.redhat.ceylon.compiler.loader.mirror.TypeMirror;
@@ -3804,9 +3805,45 @@ public abstract class AbstractModelLoader implements ModelCompleter, ModelLoader
         if (ret.getUnderlyingType() == null) {
             ret.setUnderlyingType(getUnderlyingType(originalType, location));
         }
+        if(location == TypeLocation.TOPLEVEL 
+                && variance == VarianceLocation.CONTRAVARIANT){
+            ProducedType callableType = getFunctionalInterfaceType(moduleScope, scope, type);
+            if(callableType != null){
+                System.err.println(type+" is a FunctionalInterface: "+callableType);
+                UnionType pt = new UnionType(typeFactory);
+                List<ProducedType> caseTypes = new ArrayList<ProducedType>(2);
+                caseTypes.add(ret);
+                caseTypes.add(callableType);
+                pt.setCaseTypes(caseTypes);
+                ret = pt.getType();
+            }
+        }
         return ret;
     }
     
+    private ProducedType getFunctionalInterfaceType(Module moduleScope, Scope scope, TypeMirror type) {
+//        System.err.println("getfunctional for "+type);
+        if(type instanceof SimpleReflType)
+            return null;
+        FunctionalInterface functionalInterface = getFunctionalInterface(type);
+        if(functionalInterface == null)
+            return null;
+        // we found one
+        ProducedType returnType = obtainType(moduleScope, functionalInterface.returnType, scope, TypeLocation.TOPLEVEL, VarianceLocation.COVARIANT);
+        List<TypeMirror> parameters = functionalInterface.parameterTypes;
+        List<ProducedType> parameterTypes = new ArrayList<ProducedType>(parameters.size());
+        for(TypeMirror parameter : parameters){
+            ProducedType parameterType = obtainType(moduleScope, parameter, scope, 
+                    TypeLocation.TOPLEVEL, VarianceLocation.CONTRAVARIANT);
+            parameterTypes.add(parameterType);
+        }
+        ProducedType parameterTuple = typeFactory.getTupleType(parameterTypes, false, false, -1);
+        ProducedType callableType = typeFactory.getCallableDeclaration().getProducedType(null, Arrays.asList(returnType, parameterTuple));
+        return callableType;
+    }
+    
+    protected abstract FunctionalInterface getFunctionalInterface(TypeMirror type);
+
     private TypeMirror applyTypeMapping(TypeMirror type, TypeLocation location) {
         // don't erase to c.l.String if in a type param location
         if (sameType(type, STRING_TYPE) && location != TypeLocation.TYPE_PARAM) {

src/com/redhat/ceylon/compiler/loader/mirror/FunctionalInterface.java
@@ -1,0 +1,42 @@
+/*
+ * Copyright Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the authors tag. All rights reserved.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU General Public License version 2.
+ * 
+ * This particular file is subject to the "Classpath" exception as provided in the 
+ * LICENSE file that accompanied this code.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License,
+ * along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+package com.redhat.ceylon.compiler.loader.mirror;
+
+import java.util.List;
+
+/**
+ *
+ * @author Stéphane Épardaud <stef@epardaud.fr>
+ */
+public class FunctionalInterface {
+
+    public final ClassMirror klass;
+    public final MethodMirror method;
+    public final TypeMirror returnType;
+    public final List<TypeMirror> parameterTypes;
+
+    public FunctionalInterface(ClassMirror klass, MethodMirror method, TypeMirror returnType, List<TypeMirror> parameterTypes) {
+        this.klass = klass;
+        this.method = method;
+        this.returnType = returnType;
+        this.parameterTypes = parameterTypes;
+    }
+}

eclipse-archived/ceylon

compiler-java/src/com/redhat/ceylon/compiler/java/codegen/CallableBuilder.java
@@ -1548,15 +1548,24 @@ public class CallableBuilder {
             callableType = typeModel;
         }
         
-        JCNewClass callableInstance = gen.at(node).NewClass(null, 
-                null, 
-                gen.makeJavaType(callableType, JT_EXTENDS | JT_CLASS_NEW), 
-                List.<JCExpression>of(gen.makeReifiedTypeArgument(callableType.getTypeArgumentList().get(0)),
-                        gen.makeReifiedTypeArgument(callableType.getTypeArgumentList().get(1)),
-                                      gen.make().Literal(callableType.asString(true)),
-                                      gen.make().TypeCast(gen.syms().shortType, gen.makeInteger(variadicIndex))),
-                classDef);
-        
+        JCNewClass callableInstance;
+        if(functionalInterface == null){
+            callableInstance = gen.at(node).NewClass(null, 
+                    null, 
+                    gen.makeJavaType(callableType, JT_EXTENDS | JT_CLASS_NEW), 
+                    List.<JCExpression>of(gen.makeReifiedTypeArgument(callableType.getTypeArgumentList().get(0)),
+                            gen.makeReifiedTypeArgument(callableType.getTypeArgumentList().get(1)),
+                                          gen.make().Literal(callableType.asString(true)),
+                                          gen.make().TypeCast(gen.syms().shortType, gen.makeInteger(variadicIndex))),
+                    classDef);
+        }else{
+            callableInstance = gen.make().NewClass(null, 
+                    null, 
+                    gen.makeJavaType(functionalInterface, JT_EXTENDS | JT_CLASS_NEW), 
+                    List.<JCExpression>nil(),
+                    classDef);
+        }
+
         JCExpression result;
         if (typeModel.isTypeConstructor()) {
             result = buildTypeConstructor(callableType, callableInstance);
@@ -1749,6 +1758,11 @@ public class CallableBuilder {
 
 
     //Target target = new Target(null);
+    Target target = new Target(null);
+
+    private TypedReference functionalInterfaceMethod;
+
+    private Type functionalInterface;
     
     class Target {
         Tree.Term forwardCallTo;
@@ -1759,6 +1773,12 @@ public class CallableBuilder {
         
     }
     
-    
-    
+    /**
+     * @param other
+     * @param functionalInterface
+     */
+    public void functionalInterface(Type interfaceType, TypedReference method) {
+        this.functionalInterface = interfaceType;
+        this.functionalInterfaceMethod = method;
+    }
 }

compiler-java/src/com/redhat/ceylon/compiler/java/codegen/ExpressionTransformer.java
@@ -105,6 +105,7 @@ import com.redhat.ceylon.model.typechecker.model.TypeDeclaration;
 import com.redhat.ceylon.model.typechecker.model.TypeParameter;
 import com.redhat.ceylon.model.typechecker.model.TypedDeclaration;
 import com.redhat.ceylon.model.typechecker.model.TypedReference;
+import com.redhat.ceylon.model.typechecker.model.UnionType;
 import com.redhat.ceylon.model.typechecker.model.Value;
 
 /**
@@ -360,6 +361,7 @@ public class ExpressionTransformer extends AbstractTransformer {
     }
     
     JCExpression transform(Tree.FunctionArgument functionArg, Type expectedType) {
+        System.err.println("Function "+functionArg+" return "+expectedType);
         Function model = functionArg.getDeclarationModel();
         List<JCStatement> body;
         boolean prevNoExpressionlessReturn = statementGen().noExpressionlessReturn;
@@ -394,11 +396,61 @@ public class ExpressionTransformer extends AbstractTransformer {
                 Collections.singletonList(functionArg.getParameterLists().get(0)),
                 classGen().transformMplBody(functionArg.getParameterLists(), model, body));
         
+        TypeDeclaration expectedDeclaration = expectedType.getDeclaration();
+        if(expectedDeclaration instanceof UnionType){
+            // ignore Callable and Null
+            Type other = null;
+            boolean skip = false;
+            for(Type caseType : expectedDeclaration.getCaseTypes()){
+                // FIXME: fast-case
+                if(caseType.isExactly(typeFact().getNullDeclaration().getType())
+                        || caseType.isSubtypeOf(typeFact().getCallableDeclaration().getType()))
+                    continue;
+                if(other == null)
+                    other = caseType;
+                else{
+                    skip = true;
+                    break;
+                }
+            }
+            if(!skip && other != null){
+                TypedReference functionalInterface = isFunctionalInterface(other);
+                if(functionalInterface != null){
+                    System.err.println("Got functional interface: "+other+" / "+functionalInterface);
+                    callableBuilder.functionalInterface(other, functionalInterface);
+                }
+            }
+        }
+        
         JCExpression result = callableBuilder.build();
         result = applyErasureAndBoxing(result, callableType, true, BoxingStrategy.BOXED, expectedType);
         return result;
     }
     
+    private TypedReference isFunctionalInterface(Type type) {
+        TypeDeclaration declaration = type.getDeclaration();
+        if(declaration instanceof Interface == false)
+            return null;
+        if(!declaration.getSatisfiedTypes().isEmpty())
+            return null;
+        Function method = null;
+        for(Declaration d : declaration.getMembers()){
+            if(d instanceof Function == false)
+                return null;
+            // ignore non-formal methods
+            if(!d.isFormal())
+                continue;
+            // allow only one
+            if(method == null)
+                method = (Function) d;
+            else
+                return null;
+        }
+        if(method == null)
+            return null;
+        return method.appliedTypedReference(type, Collections.<Type>emptyList());
+    }
+
     //
     // Boxing and erasure of expressions
     

model/src/com/redhat/ceylon/model/loader/AbstractModelLoader.java
@@ -33,6 +33,7 @@ import com.redhat.ceylon.model.loader.mirror.AnnotatedMirror;
 import com.redhat.ceylon.model.loader.mirror.AnnotationMirror;
 import com.redhat.ceylon.model.loader.mirror.ClassMirror;
 import com.redhat.ceylon.model.loader.mirror.FieldMirror;
+import com.redhat.ceylon.model.loader.mirror.FunctionalInterface;
 import com.redhat.ceylon.model.loader.mirror.MethodMirror;
 import com.redhat.ceylon.model.loader.mirror.PackageMirror;
 import com.redhat.ceylon.model.loader.mirror.TypeKind;
@@ -85,6 +86,7 @@ import com.redhat.ceylon.model.typechecker.model.TypeAlias;
 import com.redhat.ceylon.model.typechecker.model.TypeDeclaration;
 import com.redhat.ceylon.model.typechecker.model.TypeParameter;
 import com.redhat.ceylon.model.typechecker.model.TypedDeclaration;
+import com.redhat.ceylon.model.typechecker.model.UnionType;
 import com.redhat.ceylon.model.typechecker.model.Unit;
 import com.redhat.ceylon.model.typechecker.model.UnknownType;
 import com.redhat.ceylon.model.typechecker.model.Value;
@@ -5104,9 +5106,45 @@ public abstract class AbstractModelLoader implements ModelCompleter, ModelLoader
         if (ret.getUnderlyingType() == null) {
             ret.setUnderlyingType(getUnderlyingType(originalType, location));
         }
+        if(location == TypeLocation.TOPLEVEL 
+                && variance == VarianceLocation.CONTRAVARIANT){
+            Type callableType = getFunctionalInterfaceType(moduleScope, scope, type);
+            if(callableType != null){
+                System.err.println(type+" is a FunctionalInterface: "+callableType);
+                UnionType pt = new UnionType(typeFactory);
+                List<Type> caseTypes = new ArrayList<Type>(2);
+                caseTypes.add(ret);
+                caseTypes.add(callableType);
+                pt.setCaseTypes(caseTypes);
+                ret = pt.getType();
+            }
+        }
         return ret;
     }
     
+    private Type getFunctionalInterfaceType(Module moduleScope, Scope scope, TypeMirror type) {
+        //        System.err.println("getfunctional for "+type);
+        if(type instanceof SimpleReflType)
+            return null;
+        FunctionalInterface functionalInterface = getFunctionalInterface(type);
+        if(functionalInterface == null)
+            return null;
+        // we found one
+        Type returnType = obtainType(moduleScope, functionalInterface.returnType, scope, TypeLocation.TOPLEVEL, VarianceLocation.COVARIANT);
+        List<TypeMirror> parameters = functionalInterface.parameterTypes;
+        List<Type> parameterTypes = new ArrayList<Type>(parameters.size());
+        for(TypeMirror parameter : parameters){
+            Type parameterType = obtainType(moduleScope, parameter, scope, 
+                    TypeLocation.TOPLEVEL, VarianceLocation.CONTRAVARIANT);
+            parameterTypes.add(parameterType);
+        }
+        Type parameterTuple = typeFactory.getTupleType(parameterTypes, false, false, -1);
+        Type callableType = typeFactory.getCallableDeclaration().appliedType(null, Arrays.asList(returnType, parameterTuple));
+        return callableType;
+    }
+
+    protected abstract FunctionalInterface getFunctionalInterface(TypeMirror type);
+        
     private TypeMirror applyTypeMapping(TypeMirror type, TypeLocation location) {
         // don't erase to c.l.String if in a type param location
         if (sameType(type, STRING_TYPE) && location != TypeLocation.TYPE_PARAM) {

model/src/com/redhat/ceylon/model/loader/mirror/FunctionalInterface.java
@@ -1,0 +1,42 @@
+/*
+ * Copyright Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the authors tag. All rights reserved.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU General Public License version 2.
+ * 
+ * This particular file is subject to the "Classpath" exception as provided in the 
+ * LICENSE file that accompanied this code.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License,
+ * along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+package com.redhat.ceylon.model.loader.mirror;
+
+import java.util.List;
+
+/**
+ *
+ * @author Stéphane Épardaud <stef@epardaud.fr>
+ */
+public class FunctionalInterface {
+
+    public final ClassMirror klass;
+    public final MethodMirror method;
+    public final TypeMirror returnType;
+    public final List<TypeMirror> parameterTypes;
+
+    public FunctionalInterface(ClassMirror klass, MethodMirror method, TypeMirror returnType, List<TypeMirror> parameterTypes) {
+        this.klass = klass;
+        this.method = method;
+        this.returnType = returnType;
+        this.parameterTypes = parameterTypes;
+    }
+}