740295abf7203bdacb7cba36a418672157afbe81
Stephane Epardaud <stef@epardaud.fr>
2016-07-04 15:13:22 +0200

    Beef up test

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/interop/LambdasJava.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/interop/LambdasJava.java
@@ -1,10 +1,22 @@
 package com.redhat.ceylon.compiler.java.test.interop;
 
-import java.util.function.Consumer;
+import java.util.function.*;
 
 public class LambdasJava {
 
-    public <T> void foo(Consumer<T> consumer, T t){
+    public <T> void consumer(Consumer<T> consumer, T t){
         consumer.accept(t);
+    }
+
+    public <T,R> R function(Function<T,R> function, T t){
+        return function.apply(t);
+    }
+
+    public void intConsumer(IntConsumer consumer){
+        consumer.accept(1);
+    }
+
+    public int intSupplier(IntSupplier supplier){
+        return supplier.getAsInt();
     }
 }
