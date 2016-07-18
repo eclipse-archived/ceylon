214f5f09dea3e73826b7ea45fd536c9e04e96003
Enrique Zamudio <alterbass@gmail.com>
2016-07-18 12:56:59 -0500

    check for type arguments as well #5956

--- language/runtime-js/jsint/Modulo/findServiceProviders.js
+++ language/runtime-js/jsint/Modulo/findServiceProviders.js
@@ -2,15 +2,23 @@ function findServiceProviders(serv,$targs,buscados) {
   var r=[];
   var key=serv.string+"/"+qname$($targs.Service$findServiceProviders);
   if (this.$servimpl$ && this.$servimpl$[key]) {
+    //Found impls in module-level service cache
     r.push.apply(r,this.$servimpl$[key]);
   } else {
-    var typelit=typeLiteral$meta({Type$typeLiteral:$targs.Service$findServiceProviders});
+    //Get the declaration for the service provider
+    var typelit=typeLiteral$meta({Type$typeLiteral:$targs.Service$findServiceProviders}).declaration;
+    //Iterate over this module's packages
     var pkg;for (var pkgIter=this.members.iterator();(pkg=pkgIter.next())!==finished();) {
+      //Iterate over each package's classes
       var tipo;for (var titer=pkg.members({Kind$members:{t:ClassDeclaration$meta$declaration}}).iterator();(tipo=titer.next())!==finished();) {
+        //Check each class' satisfied types, looking for the service provider
         var sat;for (var siter=tipo.satisfiedTypes.iterator();(sat=siter.next())!==finished();) {
-          if (sat.declaration.string.equals(typelit.declaration.string)) {
+          //If declarations match, compare OpenType vs Type (to easily match type arguments)
+          if (sat.declaration.equals(typelit) && sat.string.equals(serv.string)) {
+            //Service provider satisfied, check its service annotation
             var ann=tipo.annotations({Annotation$annotations:{t:ServiceAnnotation}});
-            if (ann.size==1 && ann.first.contract.equals(typelit.declaration)) {
+            if (ann.size==1 && ann.first.contract.equals(typelit)) {
+              //Instantiate the service and store it in module cache
               var impl=tipo.instantiate()
               if (!this.$servimpl$)this.$servimpl$={};
               if (this.$servimpl$[key]) {
@@ -25,7 +33,7 @@ function findServiceProviders(serv,$targs,buscados) {
       }
     }
   }
-  //Deps lookup
+  //Search dependencies
   if (buscados)buscados.push(this.string);else buscados=[this.string];
   var dep;for (var depsIter=this.dependencies.iterator();(dep=depsIter.next())!==finished();) {
     var mod=modules$meta().find(dep.name,dep.version);
