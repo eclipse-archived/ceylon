5cb48a78079fba9c581129f86acf48731909dc48
Stephane Epardaud <stef@epardaud.fr>
2016-08-01 13:57:35 +0200

    Test for #6404: reified type descriptor wrong for shortcut methods

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTests_6000_6499.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTests_6000_6499.java
@@ -210,4 +210,10 @@ public class IssuesTests_6000_6499 extends CompilerTests {
     public void testBug6391() {
         compareWithJavaSource("bug63xx/Bug6391");
     }
+
+    @Test
+    public void testBug6404() {
+        compareWithJavaSource("bug64xx/Bug6404");
+        run("com.redhat.ceylon.compiler.java.test.issues.bug64xx.bug6404");
+    }
 }
