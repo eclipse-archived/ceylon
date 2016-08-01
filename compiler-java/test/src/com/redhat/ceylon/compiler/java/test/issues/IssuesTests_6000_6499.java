9cc20be7a042e28b8d062573ef13b32142f24a36
Stephane Epardaud <stef@epardaud.fr>
2016-08-01 16:23:41 +0200

    Test for #6377: subtyping java inner static class

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTests_6000_6499.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTests_6000_6499.java
@@ -207,6 +207,12 @@ public class IssuesTests_6000_6499 extends CompilerTests {
     }
 
     @Test
+    public void testBug6377() {
+        compile("bug63xx/Bug6377Java.java");
+        compareWithJavaSource("bug63xx/Bug6377");
+    }
+
+    @Test
     public void testBug6391() {
         compareWithJavaSource("bug63xx/Bug6391");
     }
