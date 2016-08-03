1cac9780c24d3af038ce0d57b6b43fc45a109d09
Tako Schotanus <tako@ceylon-lang.org>
2016-08-03 17:47:17 +0200

    Added test for #6310

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTests_6000_6499.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/IssuesTests_6000_6499.java
@@ -207,6 +207,11 @@ public class IssuesTests_6000_6499 extends CompilerTests {
     }
 
     @Test
+    public void testBug6310() {
+        compile("bug63xx/bug6310/Bug6310.ceylon", "bug63xx/bug6310/module.ceylon", "bug63xx/bug6310/Bug6310Java.java");
+    }
+
+    @Test
     public void testBug6344() {
         compareWithJavaSource("bug63xx/Bug6344");
     }
