9143ebef7b9be9b66a81dfeba7471e80d54d954f
Enrique Zamudio <alterbass@gmail.com>
2016-09-01 12:28:58 -0500

    fix #6463 on JS

--- language/runtime-js/Array/findLast.js
+++ language/runtime-js/Array/findLast.js
@@ -1,6 +1,7 @@
 function(f){
   for (var i=this.arr$.length-1;i>=0;i--){
-    if (f(this.arr$[i]))return this.arr$[i];
+    var e=this.arr$[i];
+    if (e!==null && f(e))return e;
   }
   return null;
 }
