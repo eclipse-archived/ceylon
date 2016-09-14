a721fb2aa1b6b672aedc7645c799e3ccbc0f0ca6
Tako Schotanus <tako@ceylon-lang.org>
2016-09-14 12:22:59 +0200

    Added `AllToolProviderTests` test suite for the `tool-provider` to be in line with the way tests are handled in the other projects

---
+++ tool-provider/test/com/redhat/ceylon/compiler/java/runtime/AllToolProviderTests.java
@@ -0,0 +1,12 @@
+package com.redhat.ceylon.compiler.java.runtime;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class) 
+@SuiteClasses({
+    ToolsTest.class
+})
+public class AllToolProviderTests {
+}
