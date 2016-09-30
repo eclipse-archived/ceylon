188df1e45195b2ea54d9fbcc2ff0af0c215f8fd9
Bastien Jansen <bastien.jansen@gmx.com>
2016-09-30 11:18:27 +0200

    Add a description to Warning, to be used by the IDEs.

--- typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/Warning.java
+++ typechecker/src/com/redhat/ceylon/compiler/typechecker/analyzer/Warning.java
@@ -1,37 +1,47 @@
 package com.redhat.ceylon.compiler.typechecker.analyzer;
 
 public enum Warning {
-    filenameNonAscii,
-    filenameCaselessCollision,
-    deprecation,
-    compilerAnnotation,
-    doclink,
-    expressionTypeNothing,
-    unusedDeclaration,
-    unusedImport,
-    ceylonNamespace,
-    javaNamespace, 
-    suppressedAlready, 
-    suppressesNothing, 
-    unknownWarning, 
-    ambiguousAnnotation,
-    similarModule,
-    importsOtherJdk,
-    javaAnnotationElement,
-    syntaxDeprecation,
-    smallIgnored,
-    literalNotSmall, 
-    disjointEquals,
-    disjointContainment,
-    redundantNarrowing, 
-    redundantIteration,
-    missingImportPrefix,
-    uncheckedTypeArguments, 
-    expressionTypeCallable
+    filenameNonAscii("File names are not ASCII"),
+    filenameCaselessCollision("File names case collisions"),
+    deprecation("Deprecations"),
+    compilerAnnotation("Compiler annotations"),
+    doclink("Broken documentation links"),
+    expressionTypeNothing("Expressions of type 'Nothing'"),
+    unusedDeclaration("Unused declarations"),
+    unusedImport("Unused imports"),
+    ceylonNamespace("Discouraged 'ceylon' namespaces"),
+    javaNamespace("Discouraged 'java' namespaces"),
+    suppressedAlready("Already suppressed warnings"),
+    suppressesNothing("Warnings suppressing 'nothing'"),
+    unknownWarning("Unknown warnings"),
+    ambiguousAnnotation("Ambiguous annotations"),
+    similarModule("Similar modules"),
+    importsOtherJdk("Imports another JDK"),
+    javaAnnotationElement("Java annotation elements"),
+    syntaxDeprecation("Deprecated syntax"),
+    smallIgnored("'small' is ignored"),
+    literalNotSmall("Literal not 'small'"),
+    disjointEquals("Disjoint 'equals'"),
+    disjointContainment("Disjoint containments"),
+    redundantNarrowing("Redundant narrowings"),
+    redundantIteration("Redundant iterations"),
+    missingImportPrefix("Missing import prefixes"),
+    uncheckedTypeArguments("Unchecked type arguments"),
+    expressionTypeCallable("Expressions of type 'Callable'");
     /*
      * Don't forget to update the documentation
      * of the suppressWarnings annotation in
      * language/src/ceylon/language/annotations.ceylon
      * when adding new warnings.
      */
+
+    /** Used by the IDEs */
+    private String description;
+    Warning(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
