4a62273db9a88c8958ce6229c874c65e3ea447de
Tom Bentley <tom.bentley@cantab.net>
2016-10-06 11:53:33 +0100

    Issue #6524: Add annotations for nullability

---
+++ language/runtime/com/redhat/ceylon/compiler/java/metadata/Nullable.java
@@ -0,0 +1,16 @@
+package com.redhat.ceylon.compiler.java.metadata;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(CLASS)
+@Target({ FIELD, METHOD, PARAMETER, LOCAL_VARIABLE })
+public @interface Nullable {
+
+}
