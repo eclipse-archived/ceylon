eclipse-archived/ceylon

commit 040542df66a31d7c07e60f3099280c3f62161112

Transform Generic<Null> to Generic<j.l.Object> to support statics in metamodel

compiler-java/src/com/redhat/ceylon/compiler/java/codegen/AbstractTransformer.java
@@ -739,7 +739,7 @@ public abstract class AbstractTransformer implements Transformation {
     boolean isOptional(Type type) {
         // Note we don't use typeFact().isOptionalType(type) because
         // that implements a stricter test used in the type checker.
-        return typeFact().getNullValueDeclaration().getType().isSubtypeOf(type);
+        return typeFact().getNullValueDeclaration().getType().isSubtypeOf(type) && !type.isNull();
     }
     
     boolean isNull(Type type) {

commit ea97582b2402162e3a83fa073b3c019347ec6f18

Fix erasure bug caused by 040542df66a31d7c07e60f3099280c3f62161112

compiler-java/src/com/redhat/ceylon/compiler/java/codegen/AbstractTransformer.java
@@ -742,6 +742,10 @@ public abstract class AbstractTransformer implements Transformation {
         // that implements a stricter test used in the type checker.
         return typeFact().getNullValueDeclaration().getType().isSubtypeOf(type) && !type.isNull();
     }
+
+    boolean isExactlyNull(Type type) {
+        return type.isNull();
+    }
     
     boolean isNull(Type type) {
         return type.getSupertype(typeFact.getNullDeclaration()) != null;
@@ -1496,9 +1500,9 @@ public abstract class AbstractTransformer implements Transformation {
             java.util.List<Type> caseTypes = type.getCaseTypes();
             // special case for optional types
             if(caseTypes.size() == 2){
-                if(isOptional(caseTypes.get(0)))
+                if(isExactlyNull(caseTypes.get(0)))
                     return hasErasureResolved(caseTypes.get(1));
-                if(isOptional(caseTypes.get(1)))
+                if(isExactlyNull(caseTypes.get(1)))
                     return hasErasureResolved(caseTypes.get(0));
             }
             // must be erased