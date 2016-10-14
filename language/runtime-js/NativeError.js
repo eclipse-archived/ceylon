d0c931938e5ea8823d53e07511b7106f79e903c0
Lucas Werkmeister <mail@lucaswerkmeister.de>
2016-10-14 16:48:54 +0200

    DONTMERGE: Fix #6443
    Needs more work, but good enough for me right now. Why did I not do this
    months ago?

--- language/runtime-js/NativeError.js
+++ language/runtime-js/NativeError.js
@@ -9,8 +9,12 @@ function NatErr(e) {
         msg = "Native JavaScript Error";
     }
     Throwable(msg,null,that);
+    that.$naterr = e;
     return that;
 }
 initTypeProto(NatErr, 'ceylon.language::NativeError', $init$Throwable());
+NatErr.$$.prototype.printStackTrace = function() {
+    console.error(this.$naterr);
+}
 NatErr.$crtmm$=function(){return{nm:'NativeError',mt:'c',ps:[{$t:{t:Throwable},nm:'src',mt:'prm'}],pa:1,mod:$CCMM$,d:['$','Throwable']};}
 ex$.NatErr=NatErr;
