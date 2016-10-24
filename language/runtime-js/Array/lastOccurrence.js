f29c0f8d7baded59ba56682d7d23573821653b64
Enrique Zamudio <alterbass@gmail.com>
2016-10-24 09:51:02 -0500

    fix #6644

--- language/runtime-js/Array/lastOccurrence.js
+++ language/runtime-js/Array/lastOccurrence.js
@@ -3,7 +3,9 @@ function(e,from,len){
   else if (from>this.arr$.length)return null;
   if (len===undefined)len=this.arr$.length-from;
   var end=this.arr$.length-len-from;
+  if (end<0)end=0;
   var start=this.arr$.length-from-1;
+  if (start>=this.arr$.length)start=this.arr$.length-1;
   if (e===null) {
     for (var i=start;i>=end;i--) {
       if (this.arr$[i]===null)return i;
