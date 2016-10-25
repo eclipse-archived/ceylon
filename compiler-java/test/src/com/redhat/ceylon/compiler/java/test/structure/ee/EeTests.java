e7c83dbe7ce8aa5570707589db42bacd0e6532a7
Tom Bentley <tom.bentley@cantab.net>
2016-10-25 11:02:32 +0100

    Issue #6622: Fix EE tests

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/structure/ee/EeTests.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/structure/ee/EeTests.java
@@ -1,11 +1,20 @@
 package com.redhat.ceylon.compiler.java.test.structure.ee;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import com.redhat.ceylon.compiler.java.test.CompilerTests;
 
 public class EeTests extends CompilerTests {
 
+    @Override
+    protected void compareWithJavaSource(String java, String... ceylon) {
+        ArrayList<String> list = new ArrayList<String>(defaultOptions);
+        list.add("-ee");
+        compareWithJavaSource(list, java, ceylon);
+    }
+    
     @Test
     public void testNoFinalMethods() {
         compareWithJavaSource("NoFinalMethods");
