beed7ae4f1cf5912b5b5b95e837b5dd66cf229f7
Tako Schotanus <tako@ceylon-lang.org>
2016-11-17 21:04:28 +0100

    Added stupidly simple script to change versions

---
+++ dist/reversion.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [[ "$#" -ne 2 ]]; then
+    echo "Usage: reversion <old-version> <new-version>"
+    echo ""
+    echo "WARNING: this is just a stupid recursive replace in all files in the project!"
+    exit
+fi
+
+function unregex {
+   sed -e 's/[]\/()$*.^|[]/\\&/g' <<< "$1"
+}
+
+script=$(readlink -f "$0")
+dir="$(dirname $(dirname "$script"))"
+
+old=$(unregex "$1")
+new=$(unregex "$2")
+
+echo "Replacing $1 with $2 in $dir ..."
+find "$dir" -type f -exec sed -i "s/$old/$new/g" {} +
+
