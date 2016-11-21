60a784645384d4d5710f8b230f8a1defc04cab25
Tako Schotanus <tako@ceylon-lang.org>
2016-11-22 00:57:42 +0100

    Added a script for registering SDKMAN candidates

---
+++ dist/sdkman.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [[ "$#" -ne 4 ]]; then
+    echo "Usage: sdkman [candidate|announce] <ceylon-version> <key> <token>"
+    echo ""
+    echo "The action 'candidate' registers a new version while 'announce' broadcasts it to all the users"
+    exit
+fi
+
+VERSION=$2
+KEY=$3
+TOKEN=$4
+
+if [[ "$1" == "candidate" ]]; then
+
+	curl -X POST -H "consumer_key: $KEY" -H "consumer_token: $TOKEN" -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"candidate\":\"ceylon\",\"version\":\"$VERSION\",\"url\":\"https://downloads.ceylon-lang.org/cli/ceylon-$VERSION.zip\"}" https://vendors.sdkman.io/release
+
+	curl -X PUT -H "consumer_key: $KEY" -H "consumer_token: $TOKEN" -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"candidate\":\"ceylon\",\"default\":\"$VERSION\"}" https://vendors.sdkman.io/default
+
+elif [[ "$1" == "announce" ]]; then
+
+	curl -X POST -H "consumer_key: $KEY" -H "consumer_token: $TOKEN" -H "Content-Type: application/json" -H "Accept: application/json" -d "{\"candidate\": \"ceylon\", \"version\": \"$VERSION\", \"hashtag\": \"ceylonlang\"}" https://vendors.sdkman.io/announce/struct
+
+else
+
+	echo "Unknown action '$1', should be 'candidate' or 'announce'"
+
+fi
+
