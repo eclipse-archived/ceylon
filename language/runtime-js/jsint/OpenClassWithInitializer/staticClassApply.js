244eb6d4efddfa162bae74dae3f055988ec197a5
Enrique Zamudio <alterbass@gmail.com>
2017-02-08 00:59:48 -0600

    implement for #6629

--- language/runtime-js/jsint/OpenClassWithInitializer/staticClassApply.js
+++ language/runtime-js/jsint/OpenClassWithInitializer/staticClassApply.js
@@ -1,3 +1,18 @@
-function(){
-  return null;
+function(cont,targs,$mpt){
+  if (!this.$_static) {
+    throw TypeApplicationException$meta$model('Cannot staticClassApply a non-static declaration: use ' + (this.toplevel?'apply':'memberApply'));
+  }
+  if (this.$_static) {
+    //TODO copy type arguments from container into targs
+  }
+  var mm=this.tipo.$crtmm$;
+  if (mm.tp) {
+    var needed=Object.keys(mm.tp).length;
+    if (!targs || targs.size!=needed)throw TypeApplicationException$meta$model("Not enough type arguments provided: "+(targs?targs.size:0)+", but requires exactly "+needed);
+    //TODO generate targs
+  }
+  validate$params(mm.ps,$mpt.Arguments$staticClassApply,"Wrong number of Arguments for staticClassApply");
+  var eltipo={t:this.tipo};
+  //TODO add type arguments
+  return this.$_apply(targs,{Type$apply:eltipo});
 }
