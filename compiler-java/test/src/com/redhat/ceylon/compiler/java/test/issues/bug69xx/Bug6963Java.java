fe9f98f95d71977a7afcfa7ca1a97629bb7c184c
Stephane Epardaud <stef@epardaud.fr>
2017-03-13 11:24:38 +0100

    New test for #6963

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/bug69xx/Bug6963Java.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/bug69xx/Bug6963Java.java
@@ -1,6 +1,9 @@
 package com.redhat.ceylon.compiler.java.test.issues.bug69xx;
 
 public class Bug6963Java<T extends String> {
+    public interface F{
+        public void f(Inner i);
+    }
     public interface Inner {
         void fun(Bug6963Java<?> java);
     }
