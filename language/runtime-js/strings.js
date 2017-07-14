5795aa37317bbe78204241681b13ad5abcb2d5ca
Enrique Zamudio <alterbass@gmail.com>
2017-07-14 01:43:21 -0500

    StringSublist

--- language/runtime-js/strings.js
+++ language/runtime-js/strings.js
@@ -46,3 +46,69 @@ function codepointFromString(str, index) {
 }
 ex$.codepointFromString=codepointFromString;
 
+function StringSublist(str,offset,from,to) {
+  var that=new StringSublist.$$;
+  BaseCharacterList$impl(that);
+  that.str=str;
+  if (from<=0) {
+    that.start=offset;
+    that.to=to;
+  } else {
+    that.to=to-from;
+    if (offset<0||offset>str.size) {
+      that.start=str.size;
+    } else {
+      that.start=offset+from;
+    }
+  }
+  return that;
+}
+StringSublist.$crtmm$=function(){return{nm:'StringSublist',mt:'c',ps:[{nm:'str',$t:{t:$_String}},{nm:'from',$t:{t:Integer}},{nm:'to:',$t:{t:Integer}}],pa:1,mod:$CCMM$,d:['$','List']}};
+initTypeProto(StringSublist,'ceylon.language::StringSublist', $init$BaseCharacterList$impl());
+var ssubl$proto=StringSublist.$$.prototype;
+ssubl$proto.getFromFirst=function(i) {
+  if (i>this.to||i<0)return null;
+  return this.str.getFromFirst(this.start+i);
+}
+atr$(ssubl$proto, 'empty', function(){return this.to<0||this.start>this.str.size;},
+  undefined, function(){return{mod:$CCMM$,$t:{t:$_Boolean},pa:0,$cont:StringSublist,d:['$','List','$at','empty']};});
+atr$(ssubl$proto, 'lastIndex', function(){
+  var s=this.size;
+  return s>0 ? s-1 : null;
+  },undefined, function(){return{mod:$CCMM$,$t:{t:'u',l:[{t:Integer},{t:Null}]},pa:0,$cont:StringSublist,d:['$','List','$at','lastIndex']};});
+atr$(ssubl$proto, 'size', function(){return Math.min(this.str.size-this.start,this.to+1);},
+  undefined, function(){return{mod:$CCMM$,$t:{t:Integer},pa:0,$cont:StringSublist,d:['$','List','$at','size']};});
+ssubl$proto.iterator=function() {
+  $init$BaseIterator$impl();
+  var that=new BaseIterator$impl.$$;
+  BaseIterator$impl({Element$BaseIterator:{t:Character},Absent$BaseIterator:{t:Null}},that);
+  that.offset=this.start;
+  that.index=0;
+  that.outer=this;
+  that.next=function() {
+    if (this.offset<this.outer.str.size && this.index<=this.outer.to) {
+      var cp=this.outer.str.getFromFirst(this.offset);
+      this.offset++;
+      this.index++;
+      return cp;
+    }
+    return finished();
+  }
+  return that;
+}
+ssubl$proto.contains=function contains(o) {
+  if (is$(o,{t:$_String})) {
+    if (o.size>(this.start-this.to))return false;
+    var idx=this.str.indexOf(o,this.start);
+    return idx>=this.start && idx<=1+this.start+this.to-o.size;
+  } else if (is$(o,{t:Character})) {
+    return this.str.occurs(o,this.start,this.to) >= 0;
+  }
+  return false;
+}
+ssubl$proto.sublist=function(f,t) {
+  return StringSublist(this.str,this.start,f,Math.min(t,this.to));
+}
+ssubl$proto.sublistFrom=function(f) {
+  return this.sublist(f, this.str.size);
+}
