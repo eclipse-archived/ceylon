d916b3ffe45c571ad802391a3a66d104e678e86b
Stephane Epardaud <stef@epardaud.fr>
2017-07-27 12:38:49 +0200

    Test for #6930

---
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/issues/bug69xx/Bug6930Java.java
@@ -0,0 +1,7 @@
+package com.redhat.ceylon.compiler.java.test.issues.bug69xx;
+
+import java.awt.event.ActionListener;
+
+class Bug6930MweJava {
+    public Bug6930MweJava(String arg, ActionListener... listeners) { }
+}
