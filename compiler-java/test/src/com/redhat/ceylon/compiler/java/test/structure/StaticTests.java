20ef67b0fc89e4b5d026f394089fda03d226e669
Stephane Epardaud <stef@epardaud.fr>
2017-08-01 16:21:06 +0200

    Test for Invalid bytecode in language #7175

--- compiler-java/test/src/com/redhat/ceylon/compiler/java/test/structure/StaticTests.java
+++ compiler-java/test/src/com/redhat/ceylon/compiler/java/test/structure/StaticTests.java
@@ -3,9 +3,12 @@ package com.redhat.ceylon.compiler.java.test.structure;
 import java.lang.reflect.Constructor;
 import java.util.Arrays;
 
+import org.junit.Assume;
 import org.junit.Test;
 
+import com.redhat.ceylon.compiler.java.test.CompilerError;
 import com.redhat.ceylon.compiler.java.test.CompilerTests;
+import com.redhat.ceylon.model.cmr.JDKUtils;
 
 public class StaticTests extends CompilerTests {
     @Override
@@ -51,5 +54,18 @@ public class StaticTests extends CompilerTests {
     @Test
     public void testStaticLate() {
         compareWithJavaSource("attribute/StaticLate");
+    }
+
+    @Test
+    public void testStaticInterfaceMethods() {
+        assertErrors("klass/StaticInterfaceMethods",
+                Arrays.asList("-target", "7", "-source", "7"),
+                null,
+                    new CompilerError(23, "declaration of static interface member is not allowed unless you set the -target flag to 8: --javac=-target=8"),
+                    new CompilerError(25, "declaration of static interface member is not allowed unless you set the -target flag to 8: --javac=-target=8"),
+                    new CompilerError(29, "declaration of static interface member is not allowed unless you set the -target flag to 8: --javac=-target=8"),
+                    new CompilerError(31, "declaration of static interface member is not allowed unless you set the -target flag to 8: --javac=-target=8"),
+                    new CompilerError(34, "declaration of static interface member is not allowed unless you set the -target flag to 8: --javac=-target=8"));
+        compile(Arrays.asList("-target", "8", "-source", "8"), "klass/StaticInterfaceMethods.ceylon");
     }
 }
