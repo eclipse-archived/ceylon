b06373b4b5bdde9356992311eeceb4312ff38128
gavinking <gavin@ceylon-lang.org>
2017-09-07 18:01:29 +0200

    minor

--- compiler-java/src/com/redhat/ceylon/compiler/java/codegen/Naming.java
+++ compiler-java/src/com/redhat/ceylon/compiler/java/codegen/Naming.java
@@ -2100,7 +2100,8 @@ public class Naming extends NamingBase implements LocalId {
     }
     
     public static boolean isLowerCase(String name){
-        return !name.isEmpty() && Character.isLowerCase(name.codePointAt(0));
+        return !name.isEmpty() 
+            && Character.isLowerCase(name.codePointAt(0));
     }
 
     public static String getInitializationFieldName(String fieldName) {
@@ -2110,8 +2111,9 @@ public class Naming extends NamingBase implements LocalId {
     public JCExpression makeNamedConstructorName(Constructor constructor, boolean delegation) {
         DeclNameFlag[] flags = delegation ? new DeclNameFlag[]{DeclNameFlag.QUALIFIED, DeclNameFlag.DELEGATION}: new DeclNameFlag[]{DeclNameFlag.QUALIFIED};
         Class cls = (Class)constructor.getContainer();
-        if (cls.isToplevel() || 
-                (cls.isMember() && ((TypeDeclaration)cls.getContainer()).isToplevel())) {
+        if (cls.isToplevel() 
+                || cls.isMember() 
+                && ((TypeDeclaration)cls.getContainer()).isToplevel()) {
             return makeTypeDeclarationExpression(null, constructor, flags);
         } else {
             return maker.TypeCast(
@@ -2122,7 +2124,9 @@ public class Naming extends NamingBase implements LocalId {
     }
     
     public JCExpression makeNamedConstructorType(Constructor constructor, boolean delegation) {
-        DeclNameFlag[] flags = delegation ? new DeclNameFlag[]{DeclNameFlag.QUALIFIED, DeclNameFlag.DELEGATION}: new DeclNameFlag[]{DeclNameFlag.QUALIFIED};
+        DeclNameFlag[] flags = delegation 
+                ? new DeclNameFlag[]{DeclNameFlag.QUALIFIED, DeclNameFlag.DELEGATION}
+                : new DeclNameFlag[]{DeclNameFlag.QUALIFIED};
         return makeTypeDeclarationExpression(null, constructor, flags);
     }
 
@@ -2131,7 +2135,8 @@ public class Naming extends NamingBase implements LocalId {
      * so for those we have to remember the exact name
      */
     public static boolean isAmbiguousGetterName(TypedDeclaration attr) {
-        return !attr.isToplevel() && isAmbiguousGetterName(attr.getName());
+        return !attr.isToplevel() 
+            && isAmbiguousGetterName(attr.getName());
     }
 
     /**
