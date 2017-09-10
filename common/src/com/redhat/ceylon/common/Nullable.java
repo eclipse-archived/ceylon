7f892ba8cb703e0c71bc341937f971be8a87d7c1
Bastien Jansen <bastien.jansen@gmx.com>
2017-09-10 09:20:20 +0200

    Use RententionPolicy.RUNTIME on Nullable and NonNull, otherwise they can’t be accessed using reflection which makes them basically useless.

--- common/src/com/redhat/ceylon/common/Nullable.java
+++ common/src/com/redhat/ceylon/common/Nullable.java
@@ -4,12 +4,12 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.RetentionPolicy.CLASS;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Retention(CLASS)
+@Retention(RUNTIME)
 @Target({ FIELD, METHOD, PARAMETER, LOCAL_VARIABLE })
 // WARNING: that class is used by name in Resolve.java
 public @interface Nullable {
