ec3e9d7dc1349049ba1ae902edba96e9b101ad56
Stephane Epardaud <stef@epardaud.fr>
2017-11-14 14:43:32 +0100

    Removed various bogus copyright headers

--- cli/src/org/eclipse/ceylon/common/tools/help/DocBookMarkdownVisitor.java
+++ cli/src/org/eclipse/ceylon/common/tools/help/DocBookMarkdownVisitor.java
@@ -1,23 +1,3 @@
-/*
- * This class is heavily based on org.tautua.markdownpapers.HtmlEmitter
- * which comes with the following header:  
- */
-
-/*
- * Copyright 2011, TAUTUA
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package org.eclipse.ceylon.common.tools.help;
 
 import static org.tautua.markdownpapers.util.Utils.EOL;
