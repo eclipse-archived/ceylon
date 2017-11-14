87975a11fc7bbf6fc696003ed61720c604cf5392
Stephane Epardaud <stef@epardaud.fr>
2017-11-14 15:06:35 +0100

    Removed extraneous licence/copyright headers

--- common/src/org/eclipse/ceylon/common/ModuleDescriptorReader.java
+++ common/src/org/eclipse/ceylon/common/ModuleDescriptorReader.java
@@ -1,36 +1,3 @@
-/*
- * Copyright Red Hat Inc. and/or its affiliates and other contributors
- * as indicated by the authors tag. All rights reserved.
- *
- * This copyrighted material is made available to anyone wishing to use,
- * modify, copy, or redistribute it subject to the terms and conditions
- * of the GNU General Public License version 2.
- * 
- * This particular file is subject to the "Classpath" exception as provided in the 
- * LICENSE file that accompanied this code.
- * 
- * This program is distributed in the hope that it will be useful, but WITHOUT A
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License,
- * along with this distribution; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA  02110-1301, USA.
- */
-
-package org.eclipse.ceylon.common;
-
-import java.io.File;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.List;
-
-/*
- * Really crappy proxy class for org.eclipse.ceylon.compiler.ModuleDescriptorReader 
- * to prevent a problem with that class accessing all kinds of needed dependencies
- * before we have been able to configure our class loader with the required paths
- */
 public class ModuleDescriptorReader {
     private Object instance;
     private Method moduleVersion;
