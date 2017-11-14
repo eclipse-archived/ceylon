62550630d0e6b29733b5384a9804bf2b69985657
Stephane Epardaud <stef@epardaud.fr>
2017-11-14 15:20:56 +0100

    Added ASL copyright headers to please Eclipse

--- runtime/testsuite/src/test/java/ceylon/paris/f2f/ext/$module_.java
+++ runtime/testsuite/src/test/java/ceylon/paris/f2f/ext/$module_.java
@@ -1,5 +1,12 @@
-
-
+/********************************************************************************
+ * Copyright (c) {date} Red Hat Inc. and/or its affiliates and others
+ *
+ * This program and the accompanying materials are made available under the 
+ * terms of the Apache License, Version 2.0 which is available at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0 
+ ********************************************************************************/
 package ceylon.paris.f2f.ext;
 
 import org.eclipse.ceylon.compiler.java.metadata.Import;
