65572cda6fc52449e406051727e88d3dbae9864f
Stephane Epardaud <stef@epardaud.fr>
2018-10-02 12:34:28 +0200

    Make sure we write the POM files in the same order in every JDK #7263

--- compiler-java/src/org/eclipse/ceylon/compiler/java/tools/MavenPomUtil.java
+++ compiler-java/src/org/eclipse/ceylon/compiler/java/tools/MavenPomUtil.java
@@ -25,8 +25,13 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import javax.xml.stream.XMLOutputFactory;
 import javax.xml.stream.XMLStreamException;
@@ -45,6 +50,78 @@ import org.eclipse.ceylon.model.typechecker.model.ModuleImport;
  * @author Stéphane Épardaud <stef@epardaud.fr>
  */
 public class MavenPomUtil {
+
+    // There only to keep the key order for tests
+    @SuppressWarnings("serial")
+    static class LinkedProperties extends Properties {
+        private Set<Object> keys = new LinkedHashSet<>();
+        
+        @Override
+        public Enumeration<Object> keys() {
+            return Collections.<Object>enumeration(keys);
+        }
+
+        @Override
+        public Set<Object> keySet() {
+            return keys;
+        }
+
+        @Override
+        public Object put(Object key, Object value) {
+            keys.add(key);
+            return super.put(key, value);
+        }
+
+        @Override
+        public Object remove(Object o) {
+            keys.remove(o);
+            return super.remove(o);
+        }
+
+        @Override
+        public void clear() {
+            keys.clear();
+            super.clear();
+        }
+
+        @Override
+        public void putAll(Map<? extends Object, ? extends Object> map) {
+            keys.addAll(map.keySet());
+            super.putAll(map);
+        }
+
+        @Override
+        public Set<Map.Entry<Object, Object>> entrySet() {
+            Set<Map.Entry<Object, Object>> entrySet = new LinkedHashSet<Map.Entry<Object, Object>>(keys.size());
+            for (Object key : keys) {
+                entrySet.add(new Entry(key, get(key)));
+            }
+
+            return entrySet;
+        }
+
+        static class Entry implements Map.Entry<Object, Object> {
+            private final Object key;
+            private final Object value;
+
+            private Entry(Object key, Object value) {
+                this.key = key;
+                this.value = value;
+            }
+
+            public Object getKey() {
+                return key;
+            }
+
+            public Object getValue() {
+                return value;
+            }
+
+            public Object setValue(Object o) {
+                throw new IllegalStateException("not implemented");
+            }
+        }
+    }
 
     public static void writeMavenManifest2(File outputFolder, Module module,  
             JdkProvider jdkProvider) {
@@ -199,7 +276,7 @@ public class MavenPomUtil {
 
     private static void writePomProperties(File outputFolder, String groupId, String artifactId, String version) {
         try (OutputStream os = new FileOutputStream(new File(outputFolder,"pom.properties"))){
-            Properties properties = new Properties();
+            Properties properties = new LinkedProperties();
             properties.put("version", version);
             properties.put("groupId", groupId);
             properties.put("artifactId", artifactId);
